aafa1906978ee8c580a09fd0871dc685a864b6a8
Barry Carter <carter.barry@gmail.com>
2010-07-10 11:51:25 -0600

    Still cleaning up lib and debugging, changed 00readme.txt to README to make github happy

---
+++ README
@@ -0,0 +1,6 @@
+Instead of keeping all of my miniscule apps in a different github, I'll 
+put them all here for now.
+
+If any of them grows big enough, I'll create a new github for it.
+
+Barry Carter (carter.barry@gmail.com)
--- bclib.pl
+++ bclib.pl
@@ -1,7 +1,7 @@
 # Barry Carter's Perl library (carter.barry@gmail.com)
 
 # HACK: not sure this is right way to do this
-our(%opts);
+our(%globopts);
 
 # largest possible path
 $ENV{PATH} = "/sw/bin/:/bin/:/usr/bin/:/usr/local/bin/:/usr/X11R6/bin/:$ENV{HOME}/bin:$ENV{HOME}/PERL";
@@ -21,18 +21,21 @@ sub list2hash {
 
 =item defaults($x)
 
-given a string like "FOO=1&X=BLAH", sets $FOO and $X in opts hash,
+given a string like "FOO=1&X=BLAH", sets $FOO and $X in globopts hash,
 unless they're already set (allows a program to set default options
 where user hasn't already set options)
 
 =cut
 
 sub defaults {
+  debug("ALPHA");
   my(%hash) = str2hash($_[0]);
+  debug("GOT: $_[0]");
 
   for $i (sort keys %hash) {
-    if (defined($opts{$i})) {next;}
-    $opts{$i} = $hash{$i};
+    debug("ALPHA: $i");
+    if (defined($globopts{$i})) {next;}
+    $globopts{$i} = $hash{$i};
   }
 
   # calls parse_options again to do any special processing
@@ -47,5 +50,92 @@ Given $str like x=1&y=2&z=3, return the hash mapping x->1, y->2, z->3
 =cut
 
 sub str2hash {return %{split(/[\&\=]/,$_[0])};}
+
+=item parse_options()
+
+Parses things like --foo and --bar=1 on the command line and removes
+them from @ARGV
+
+=cut
+
+sub parse_options {
+
+  # find arguments that set options and remove them from ARGV
+  my(@x)=grep(/^\-\-[a-z]/i,@ARGV);
+  @ARGV=grep(!/^\-\-[a-z]/i,@ARGV);
+
+
+  for $i (@x){
+    # remove the --
+    $i=~s/^\-\-//;
+
+    if ($i=~/^(.*?)=(.*)$/) {
+      # for cases like --bar=1
+      $globopts{$1}=$2;
+    } else {
+      # if its just --foo, assume --foo=1
+      $globopts{$i}=1;
+    }
+  }
+}
+
+=item unfold($ref)
+
+Given a reference to any object, descends into it and returns XML-y
+string representation
+
+=cut
+
+sub unfold {
+  my(@aa)=@_;
+  my(@ac);
+  my(%ad);
+  
+  # if someone sends a list call unfold with a ref to that list
+  if ($#aa>0) {return(unfold(\@aa));}
+
+  # it's not a list so only the first element counts
+  my($aa)=$aa[0];
+  my($ab)=ref($aa);
+  if ($ab eq "") {return($aa);}
+
+  # recursively print object (or at least return string to print)
+  if ($ab eq "SCALAR") {
+    push(@ac,"<scalar $aa>",unfold($$aa),"</scalar $aa>");
+  } elsif ($ab eq "ARRAY" || UNIVERSAL::isa($aa,"ARRAY")) {
+    push(@ac,"<array $aa>",map(unfold($_),@$aa),"</array $aa>");
+  } elsif ($ab eq "HASH" || UNIVERSAL::isa($aa,"HASH")) {
+    %ad=%$aa;
+    push(@ac,"<hash $aa>");
+    for $i (keys %ad) {
+      if (!ref($i) && !ref($ad{$i})) {
+	push(@ac,"$i: $ad{$i}");
+      } else {
+	push(@ac,"<key>",unfold($i),"</key>");
+	push(@ac,"<val>",unfold($ad{$i}),"</val>");
+      }
+    }
+    push(@ac,"</hash $aa>");
+  } elsif ($ab eq "REF") {
+    push(@ac,"<ref $aa>",unfold($$aa),"</ref $aa>");
+  } else {
+    debug("NO CONDITION CAUGHT","AA IS",@aa,"AB: $ab");
+  }
+
+  return(join("\n",@ac)."\n");
+}
+
+=item debug(@list)
+
+Print list of messages to the standard error, separated by
+newlines if --debug given at command line
+
+=cut
+
+sub debug {
+  if($globopts{debug}) {
+    print STDERR join("\n",@_),"\n";
+  }
+}
 
 1;
