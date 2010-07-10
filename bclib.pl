166fd8f3b309109f45d564cdfc4225845ac9b39b
Barry Carter <carter.barry@gmail.com>
2010-07-10 11:30:54 -0600

    Checkpoint save for my Perl lib (which isn't working right now because it calls functions I haven't imported [from my personal version of the lib] yet.

--- bclib.pl
+++ bclib.pl
@@ -1,7 +1,9 @@
 # Barry Carter's Perl library (carter.barry@gmail.com)
 
-# largest possible path
+# HACK: not sure this is right way to do this
+our(%opts);
 
+# largest possible path
 $ENV{PATH} = "/sw/bin/:/bin/:/usr/bin/:/usr/local/bin/:/usr/X11R6/bin/:$ENV{HOME}/bin:$ENV{HOME}/PERL";
 
 =item list2hash(@list)
@@ -16,5 +18,34 @@ sub list2hash {
   for $i (@list) {$hash{$i} = 1;}
   return %hash;
 }
+
+=item defaults($x)
+
+given a string like "FOO=1&X=BLAH", sets $FOO and $X in opts hash,
+unless they're already set (allows a program to set default options
+where user hasn't already set options)
+
+=cut
+
+sub defaults {
+  my(%hash) = str2hash($_[0]);
+
+  for $i (sort keys %hash) {
+    if (defined($opts{$i})) {next;}
+    $opts{$i} = $hash{$i};
+  }
+
+  # calls parse_options again to do any special processing
+  # TODO: could this cause an infinite loop?
+  parse_options();
+}
+
+=item str2hash($str)
+
+Given $str like x=1&y=2&z=3, return the hash mapping x->1, y->2, z->3
+
+=cut
+
+sub str2hash {return %{split(/[\&\=]/,$_[0])};}
 
 1;
