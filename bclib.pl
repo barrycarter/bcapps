8de0da9612ebfe1c45e16d018be961f4b67b6305
Barry Carter <carter.barry@gmail.com>
2010-07-10 11:11:50 -0600

    Replaced hack to find Unix command w/ better path (dull, but better).

--- bclib.pl
+++ bclib.pl
@@ -1,1 +1,20 @@
-/home/user/PERL/bclib.pl
+# Barry Carter's Perl library (carter.barry@gmail.com)
+
+# largest possible path
+
+$ENV{PATH} = "/sw/bin/:/bin/:/usr/bin/:/usr/local/bin/:/usr/X11R6/bin/:$ENV{HOME}/bin:$ENV{HOME}/PERL";
+
+=item list2hash(@list)
+
+Converts @list to a hash where all elements of @list map to 1
+
+=cut
+
+sub list2hash {
+  my(@list) = @_;
+  my(%hash);
+  for $i (@list) {$hash{$i} = 1;}
+  return %hash;
+}
+
+1;
