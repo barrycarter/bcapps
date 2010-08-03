197b9d039c1252585b195d7e237127cb43a1392b
Barry Carter <carter.barry@gmail.com>
2010-08-02 21:20:32 -0600

    Revised Risk strategy -- old version was invaild

--- bc-calc-risk-odds.m
+++ bc-calc-risk-odds.m
@@ -50,7 +50,30 @@ p1[n_,m_] := 2890/7776*pf[p[n,m-2]] +
              2611/7776*pf[p[n-1,m-1]] +
              2275/7776*pf[p[n-2,m]] /; m>=2
 
-Table[{n,m,N[p1[n,m]]},{n,3,10},{m,1,10}]
+(* of course, we're now using strategy 0's odds for strategy 1. This
+leads us to ... strategy 2 *)
+
+(* now the probabilities for this new strategy p2 *)
+
+(* same base conditions *)
+p2[n_,m_] := 0 /; n<=3
+
+(* if defense has no troops left, you've won *)
+p2[n_,0] := 1
+
+(* if only one defender... *)
+
+p2[n_,1] := 855/1296 + 441/1296*pf[p1[n-1,1]]
+
+(* and the basic case *)
+p2[n_,m_] := 2890/7776*pf[p1[n,m-2]] +
+             2611/7776*pf[p1[n-1,m-1]] +
+             2275/7776*pf[p1[n-2,m]] /; m>=2
+
+(* of course, this leads us to strategy 3... ad infinitum *)
+
+
+
 
 
 
