fe5ab6c641d3b445a5578debb8f373753e22bd3b
Barry Carter <carter.barry@gmail.com>
2010-08-03 11:54:33 -0600

    Added generalized function, attempt to remember values (currently fails)

--- bc-calc-risk-odds.m
+++ bc-calc-risk-odds.m
@@ -72,8 +72,43 @@ p2[n_,m_] := 2890/7776*pf[p1[n,m-2]] +
 
 (* of course, this leads us to strategy 3... ad infinitum *)
 
+(* so we create q[x,n,m], the probability of winning with strategy x *)
 
+(* base conditions from above *)
 
+(* never attack with 3 troops or fewer *)
+q[x_,n_,m_] := 0 /; n<=3
 
+(* if defense has no troops left, you've won *)
+q[x_,n_,0] := 1
 
+(* if only one defender ... *)
+q[x_,n_,1] := 855/1296 + 441/1296*pf[q[x-1,n-1,1]]
 
+(* q0 is just the basic strategy *)
+q[0,n_,m_] := p[n,m]
+
+(* general case *)
+
+q[x_,n_,m_] := 2890/7776*pf[q[x-1,n,m-2]] +
+             2611/7776*pf[q[x-1,n-1,m-1]] +
+             2275/7776*pf[q[x-1,n-2,m]] /; m>=2
+
+(* for mathematica efficiency, use r *)
+
+(* TODO: this only remembers the values you explicitly give it, not
+  the values it gets while doing recursion -- is tweaking the
+  definitions above the only way to fix this? *)
+
+r[x_,n_,m_] := r[x,n,m] = q[x,n,m]
+
+(* My Mathematica has problems w/ graphics, so I must do this *)
+showit := Module[{},
+Export["/tmp/math.jpg",%, ImageSize->{800,600}]; Run["display /tmp/math.jpg&"]]
+
+(*** JFF GRAPHING (want to avoid loading this every time)
+
+ListPlot[Table[q[x,25,15],{x,0,20}], AxesOrigin->{0,0}, PlotJoined->True, 
+ PlotRange -> All]
+
+***)
