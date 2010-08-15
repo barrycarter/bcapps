36769919c2f2859477d4541e9e3b4d61fce286a0
Barry Carter <carter.barry@gmail.com>
2010-08-15 17:34:15 -0600

    This version actually dumps something useful, though not VERY useful.

--- bc-log-die-rolls.user.js
+++ bc-log-die-rolls.user.js
@@ -14,8 +14,44 @@
 // http://conquerclub.barrycarter.info/TEST/conquerclub.snapshot.diceroll.html
 
 var attack;
-attack = document.evaluate('//ul', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-GM_log("ALPHA");
-GM_log(attack.snapshotItem(1));
+attack = document.evaluate('//ul', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+GM_log("ALPHA "+attack.snapshotItem(1));
+GM_log(dump(attack.snapshotItem(1).textContent));
 GM_log("BETA");
 
+/**
+ * Function : dump()
+ * Arguments: The data - array,hash(associative array),object
+ *    The level - OPTIONAL
+ * Returns  : The textual representation of the array.
+ * This function was inspired by the print_r function of PHP.
+ * This will accept some data as the argument and return a
+ * text that will be a more readable version of the
+ * array/hash/object that is given.
+ * Docs: http://www.openjs.com/scripts/others/dump_function_php_print_r.php
+ */
+function dump(arr,level) {
+  var dumped_text = "";
+  if(!level) level = 0;
+  
+  //The padding given at the beginning of the line.
+  var level_padding = "";
+  for(var j=0;j<level+1;j++) level_padding += "    ";
+  
+  if(typeof(arr) == 'object') { //Array/Hashes/Objects 
+    for(var item in arr) {
+      var value = arr[item];
+      
+      if(typeof(value) == 'object') { //If it is an array,
+	dumped_text += level_padding + "'" + item + "' ...\n";
+	dumped_text += dump(value,level+1);
+      } else {
+	dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+      }
+    }
+  } else { //Stings/Chars/Numbers etc.
+    dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+  }
+  return dumped_text;
+}
