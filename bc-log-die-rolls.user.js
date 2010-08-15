2a6f882dc8743c403ec4272bd6e2f92576c1fb2e
Barry Carter <carter.barry@gmail.com>
2010-08-15 17:10:55 -0600

    Changes + created test page

--- bc-log-die-rolls.user.js
+++ bc-log-die-rolls.user.js
@@ -2,16 +2,19 @@
 // @name bc-log-die-rolls.users.js
 // @namespace http://barrycarter.info/
 // @description Logs all conquerclub.com die rolls to central server
-// @include http://*conquerclub.com*
+// intentionally not putting http below, I want to match files too
+// @include *conquerclub*
 // ==/UserScript==
+
 
 // TODO: production include is: http://www.conquerclub.com/game.php?game=*
 
 // currently in testing mode as I learn greasemonkey/JS
-// test page: http://conquerclub.barrycarter.info/
+// test page:
+// http://conquerclub.barrycarter.info/TEST/conquerclub.snapshot.diceroll.html
 
 var attack;
-attack = document.evaluate('//a[@href]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+attack = document.evaluate('//ul', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 GM_log("ALPHA");
 GM_log(attack.snapshotItem(1));
 GM_log("BETA");
