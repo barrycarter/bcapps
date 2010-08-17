321db6c286d5de50921ad0f98272763758f7aeac
Barry Carter <carter.barry@gmail.com>
2010-08-16 19:19:21 -0600

    Minor comment changes.

--- TEST/test.js
+++ TEST/test.js
@@ -1,6 +1,13 @@
 // the real version of this file is named test.user.js and it does at least
 // load into GM even if it doesn't work
 
+// When I run this script on the page below
+// @include http://test.barrycarter.info/select1.html
+// I want it to alert "foo" everytime I change selections.
+// Instead, I get "Error: Component is not available"
+
+// NOTE: works fine in firebug
+
 // generic test greasemonkey script
 
 sel = document.evaluate('//select',
