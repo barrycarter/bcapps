d0b50f111e5aa3c43a1e1708bb2a10065dfb8575
Barry Carter <carter.barry@gmail.com>
2010-11-11 10:47:32 -0700

    ckpt

--- box-option-value.m
+++ box-option-value.m
@@ -10,7 +10,7 @@
 (* Source: http://math.stackexchange.com/questions/9608/determining-distribution-of-maximum-of-dependent-normal-variables/9740 *)
 
 boxvalue[p0_, v_, p1_, p2_, t1_, t2_] := 
- Module[{pdflp, cdfmaxlp},
+ Module[{},
 
   (* the pdf of Log[price] at t1; chance that Log[price]=x *)
   pdflp[x_] = 
@@ -18,12 +18,12 @@ boxvalue[p0_, v_, p1_, p2_, t1_, t2_] :=
 
   (* the anti-cdf of the max of Log[price]-Log[price[t1]] between t1 and t2;
      chance this value is more than x, for x > 0 *)
-   cdfmaxlp[x_] = 1-Erf[x/(v*Sqrt[t2-t1]/Sqrt[365.2425/Sqrt[2]])];
+   cdfmaxlp[x_] = 1-Erf[x/(v*Sqrt[t2-t1]/Sqrt[24*365.2425/Sqrt[2]])];
 
   (* 3 cases: price[t1] < p1, between p1 and p2, or price[t1] > p2 *)
-  N[
-  Integrate[pdflp[x]*cdfmaxlp[Log[p1]-x],{x,-Infinity,Log[p1]}] +
-  Integrate[pdflp[x]*1,{x,Log[p1],Log[p2]}] +
-  Integrate[pdflp[x]*cdfmaxlp[x-Log[p2]],{x,Log[p2],Infinity}]
-  ]
+  upandin = NIntegrate[pdflp[x]*cdfmaxlp[Log[p1]-x],{x,-Infinity,Log[p1]}];
+  hitleftedge = NIntegrate[pdflp[x]*1,{x,Log[p1],Log[p2]}];
+  downandin = NIntegrate[pdflp[x]*cdfmaxlp[x-Log[p2]],{x,Log[p2],Infinity}];
+
+  upandin+hitleftedge+downandin
 ]
