66c1f8662d3b229405b2a8b3002912478e56e4cd
Barry Carter <Barry Carter>
2011-04-07 06:49:21 -0600

    .

--- bc-closest-gmap.pl
+++ bc-closest-gmap.pl
@@ -14,11 +14,35 @@ $pi = 4*atan(1);
 # latitude and longitude of points
 %points = (
  "Albuquerque" => "35.08 -106.66",
- "Paris" => "48.87 2.33",
- "Barrow" => "71.26826 -156.80627",
- "Wellington" => "-41.2833 174.783333"
+ "Paris" => "48.87 2.33"
+# "Barrow" => "71.26826 -156.80627",
+# "Wellington" => "-41.2833 174.783333"
 # "Rio de Janeiro" => "-22.88  -43.28"
 );
+
+# testing w/ nearby points
+# %points = (
+# "Auckland" => "-36.840417 174.7398694",
+# "Christchurch" => "-43.53; 172.62028",
+# "Sydney" => "-33.859972 151.21111",
+# "Wellington" => "-41.2833 174.783333"
+# );
+
+# with known good points
+# %points = (
+# "NPole" => "90 0",
+# "SPole" => "-90 0",
+# "Center" => "0 0",
+# "AntiCenter" => "0 180"
+# );
+
+# with diff known good points
+# %points = (
+# "A" => "0 0",
+# "B" => "0 30",
+# "C" => "0 60",
+# "D" => "0 90"
+# );
 
 # convert to 3D
 for $i (sort keys %points) {
@@ -27,9 +51,19 @@ for $i (sort keys %points) {
   $x = cos($lat/180*$pi)*cos($lon/180*$pi);
   $y = cos($lat/180*$pi)*sin($lon/180*$pi);
 
+  debug("$i -> $x $y $z");
+
   # we need to number points for qhull
   $qhull[$n] = "$x $y $z";
-  $city[$n++] = $i;
+  $city[$n] = $i;
+
+  # just in case we need these
+  $x[$n] = $x;
+  $y[$n] = $y;
+  $z[$n] = $z;
+
+  $n++;
+
 }
 
 open(A,">/tmp/qclose.txt");
@@ -46,7 +80,9 @@ close(A);
 # qhull does the real work
 # http://www.qhull.org/html/qh-faq.htm#vsphere
 # TODO: this doesn't tell what two cities we're separating
-@res = `qconvex n < /tmp/qclose.txt`;
+# TODO: remove QJ
+# @res = `qconvex QJ n < /tmp/qclose.txt`;
+@res = `qhull Qz v n < /tmp/qclose.txt`;
 debug(@res);
 
 # TODO: really should be using chdir(tmpdir()) universally
@@ -57,28 +93,29 @@ for $i (2..$#res) {
   # plane equation
   ($a,$b,$c,$d) = split(/\s+/, $res[$i]);
 
-=item comment
-
-  # normalize (qconvex doesn't do this? hmph!)
-  debug("XYZ: $x0 $y0 $z0");
-  debug($x0*$x0+$y0*$y0+$z0*$z0);
-  $len = sqrt(1.*$x0*$x0+$y0*$y0+$z0*$z0);
-  debug("LEN: $len");
-  $x0/=$len;
-  $y0/=$len;
-  $z0/=$len;
-
-=cut
+  # d is backwards?
+#  $d*=-1;
 
   # construct a line for each dividing line
   @line = ();
 
   # for each value of z, compute x and y consistent w/ plane and sphere
-  for ($z=-.5; $z<=+.5; $z+=0.01) {
+  for ($z=-1; $z<=+1; $z+=0.01) {
     # equations courtesy mathematica
+
+    # TODO: hideous use of error flag
+    $SQRT_ERROR = 0;
     $x = ($a**2*$d - $a**2*$c*$z - $b*Sqrt(-($a**2*(($d - $c*$z)**2 + $a**2*(-1 + $z**2) + $b**2*(-1 + $z**2)))))/($a*($a**2 + $b**2));
     $y = ($b*$d - $b*$c*$z + Sqrt(-($a**2*(($d - $c*$z)**2 + $a**2*(-1 + $z**2) + $b**2*(-1 + $z**2)))))/($a**2 + $b**2);
+    if ($SQRT_ERROR) {next;}
+
+    debug("ABCD: $a $b $c $d");
     debug("FOO: $x $y $z");
+    debug("SQ SUM:". ($x**2+$y**2+$z**2));
+
+    # compute distance from given points
+    debug("DIST FOO-ABQ: ". (dist($x,$y,$z,$x[0],$y[0],$z[0])));
+    debug("DIST FOO-CDG: ". (dist($x,$y,$z,$x[1],$y[1],$z[1])));
 
     $lon = atan2($y,$x)/$pi*180;
     $lat = asin($z)/$pi*180;
@@ -118,5 +155,13 @@ MARK
 sub Sqrt {
   my($x) = @_;
   if ($x>0) {return sqrt($x);}
+  $SQRT_ERROR = 1;
   return 0;
+}
+
+# 3D distance between two points (can be improved/generalized/etc)
+
+sub dist {
+  my($x1,$y1,$z1,$x2,$y2,$z2) = @_;
+  return sqrt(($x2-$x1)**2 + ($y2-$y1)**2 + ($z2-$z1)**2);
 }
