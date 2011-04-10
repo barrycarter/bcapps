9980ac9ed495264052b85ca1b7e2adb28a88e963
Barry Carter <Barry Carter>
2011-04-10 07:32:58 -0600

    About to add KML

--- bc-temperature-voronoi.pl
+++ bc-temperature-voronoi.pl
@@ -9,7 +9,7 @@ chdir(tmpdir());
 # pull data from live weather (also available at metar.db.94y.info)
 # (my own copy is local); cp avoids db lock
 system("cp /tmp/metar-live.db .");
-@res = sqlite3hashlist("SELECT -strftime('%s', replace(n.time, '-4-','-04-'))+strftime('%s', 'now') AS age, n.code, n.temperature, s.latitude, s.longitude FROM nowweather n JOIN stations s ON (n.code=s.metar) WHERE age>0 AND age<3600", "metar-live.db");
+@res = sqlite3hashlist("SELECT -strftime('%s', replace(n.time, '-4-','-04-'))+strftime('%s', 'now') AS age, n.code, n.temperature, s.latitude, s.longitude FROM nowweather n JOIN stations s ON (n.code=s.metar) WHERE age>0 AND age<7200", "metar-live.db");
 
 # create file for qvoronoi (header)
 open(A,">/tmp/qhull-file.txt");
@@ -21,7 +21,10 @@ for $i (@res) {
   %hash = %{$i};
   ($lat[$n], $lon[$n], $temp[$n], $stat[$n]) = 
     ($hash{latitude}, $hash{longitude}, $hash{temperature}*1.8+32, $hash{code});
-  debug("CODE: $stat[$n] -> $temp[$n]");
+  # skip NZSP Antarctica for now (boo!)
+  # TODO: fix
+  if ($lat[$n]<-85) {next;}
+
   # convert to Mercator for qhull
   $mlat = log(tan($PI/4+$lat[$n]/180*$PI/2));
   $mlon = $lon[$n]/180*$PI;
@@ -39,7 +42,6 @@ system("qvoronoi s o < /tmp/qhull-file.txt > /tmp/qhull-out.txt");
 # first two lines give info
 ($di) = shift(@regions);
 ($pts, $regions, $x) = split(/\s+/,shift(@regions));
-debug("PVR: $#res+1 vs $regions");
 
 # write in google maps "format"
 # <h>A "fomrat" is an extremely well organized rodent</h>
@@ -64,18 +66,43 @@ for $i ($pts+1..$#regions) {
 
   unless (@points) {next;}
 
+  debug("BEFORE: $lat[$index],$lon[$index] ->",@points);
+
   # for google maps
   print C "var myCoords = [\n";
+
+  # keep track of miny and maxy just in case we have 180 condition
+  ($miny, $maxy) = (180,-180);
 
   # do any of the points go outside world boundaries?
   # if so, adjust them down
   for $j (@points) {
     ($x,$y) = split(/\s+/,$j);
-    $y=max(min($y,180),-180);
-    $x=max(min($x,90),-90);
+
+    # TODO: really need to figure out how to handle this better
+#    if ($x<-1*$PI) {$x=-1*$PI;}
+#    if ($x>$PI) {$x=$PI;}
+    if ($y<-1*$PI) {$y=-1*$PI;}
+    if ($y>$PI) {$y=$PI;}
+
+    # TODO: restore or fix this
+#    $y=max(min($y,$PI),-1*$PI);
+#    $x=max(min($x,$PI/2),-1*$PI/2);
+
+    # out of range nums tend to create problems
+    # TODO: create a function or something for this; easier using "mod"?
+#    while($x < -1*$PI) {$x+=2*$PI;}
+#    while($x > $PI) {$x-=2*$PI;}
+#    while($y < -1*$PI) {$y+=2*$PI;}
+#    while($y > $PI) {$y-=2*$PI;}
 
     # convert back from mercator
     $y = $y/$PI*180;
+
+    # keep track of max/min
+    if ($y<$miny) {$miny=$y;}
+    if ($y>$maxy) {$maxy=$y;}
+
     $x = atan(sinh($x))/$PI*180;
     $j = "$x $y";
     debug("LL: $x,$y");
@@ -83,7 +110,7 @@ for $i ($pts+1..$#regions) {
     print C "new google.maps.LatLng($x, $y),\n";
   }
 
-  debug("BET: $lat[$index],$lon[$index] ->",@points);
+  debug("AFTER: $lat[$index],$lon[$index] ->",@points);
 
   # google maps wants the first point repeated (also, close off coords)
   ($x,$y) = split(/\s+/,$points[0]);
@@ -94,17 +121,27 @@ for $i ($pts+1..$#regions) {
   # NOTE: this is my own mapping; not NOAA approved!
   debug("INDEX: $index");
   $hue=5/6-($temp[$index]/100)*5/6;
+
+  # below: testing for limited number of colors
+#  $hue = floor($hue*4+.5)/4;
+
   $col=hsv2rgb($hue,1,1);
+
+
   debug("COL: $hue -> $col");
+
+  debug("Y RANGE: $miny $maxy");
+
+  if (abs($miny-$maxy) >=90) {debug("BIG Y RANGE");}
 
   # and now the poly itself (and push marker to array for MarkerCluster)
   print C << "MARK";
 
 myPoly = new google.maps.Polygon({
  paths: myCoords,
- strokeColor: "$col",
+ strokeColor: "#000000",
  strokeOpacity: 0.8,
- strokeWeight: 0,
+ strokeWeight: .01,
  fillColor: "$col",
  fillOpacity: 0.4
 });
