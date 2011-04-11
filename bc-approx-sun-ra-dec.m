85a43ae714b2e642868ee76944fa8af247fe6b19
Barry Carter <Barry Carter>
2011-04-11 14:07:36 -0600

    Finished off approx RA/DEC for sun... now to incorporate into Perl program

--- bc-approx-sun-ra-dec.m
+++ bc-approx-sun-ra-dec.m
@@ -1,5 +1,7 @@
 (* 
 
+DO NOT "<<" this file; it's more of a cut-and-paste sort of thing
+
 Mathematica script to approximate Suns RA/DEC for bc-sun-always-shines.pl
 <h>that video is creepy</h>
 
@@ -64,17 +66,13 @@ decfour[n_] := decfour[n] = NIntegrate[
 
 (* <h> Time passes...</h> *)
 
-(*
-
-RAW RESULTS (not sure why constant term is non-0):
+(* by storing results here, I avoid having to recalc them *)
 
 decfour[0] = 2.982794797849734*^8
 decfour[1] = -2.9677988934045434*^8 + 9.170689132027664*^9*I
 decfour[2] = 1.432586534498814*^8 + 4.548038984377269*^7*I
 decfour[3] = 7.830599335722238*^6 - 6.707145902500911*^7*I
 decfour[4] = -2.97798216111644*^6 - 1.2071250071517676*^6*I
-
-*)
 
 (* determine phase and amplitude *)
 
@@ -83,17 +81,21 @@ Table[{n, Abs[2*decfour[n]/(ved25-ved0)],
 {n,1,4}]
 
 (* and now lets turn it into an equation (could've done this in one
-step, but clearer this way; also add in constant term *)
+step, but clearer this way); also add in constant term *)
 
 decguess[x_] = Sum[Abs[2*decfour[n]/(ved25-ved0)]*Cos[2*Pi*n*x/decyear -
         Arg[2*decfour[n]/(ved25-ved0)]],
 {n,1,4}] + decfour[0]/(ved25-ved0)
 
-(* compare to real declination *)
+(* compare to real declination; commenting out in final form *)
+
+(*
 
 decdiff = Plot[decguess[x] - 
  AstronomicalData["Sun", {"Declination", ToDate[ved0+x]}], 
  {x, 0, ved25-ved0}]
+
+*)
 
 (* shows accurate to .007 degrees *)
 
@@ -109,37 +111,69 @@ vera25 = x /. FindRoot[AstronomicalData["Sun", {"RightAscension",
 
 rayear = (vera25-vera0)/25
 
-(* getting rid of that pesky linear term; when RA~0 this can yield
-   spurious 24s; hopefully, Fourier can ignore them *)
+(* getting rid of pesky linear term; yielding (almost) equation of time
+   <h>equations for love and tenderness appear elsewhere</h> *)
 
 racorrected[x_]:= AstronomicalData["Sun",{"RightAscension", ToDate[x+vera0]}] -
  24*Mod[x/rayear,1]
 
-(* the below looks odd but workable; should get back equation of time 
-   <h>equations for love and tenderness appear elsewhere</h> *)
+(* when RA~0 this can yield spurious 24s, which we fix here *)
 
-Plot[racorrected[x], {x,0,vera25-vera0}]
+racorrected2[x_] = Mod[racorrected[x]-12,24]-12
 
 (* and now, take it away, Mr Fourier *)
 
-rafour[n_] := rafour[n] = NIntegrate[
- AstronomicalData["Sun", {"RightAscension", ToDate[vera0+x]}]*
- Exp[2*Pi*I*x*n/rayear],
+rafour[n_] := rafour[n] = NIntegrate[racorrected2[x]*Exp[2*Pi*I*x*n/rayear],
  {x,0,vera25-vera0}]
 
-Table[rafour[n], {n,0,4}]
+(* storing these results here prevents recalculation *)
 
-(* Same sum as before *)
+rafour[0] = -9.753108862236838*^7
+rafour[1] = 4.643901170298403*^7 + 1.3624915962714499*^7*I
+rafour[2] = 4.454072138584064*^6 - 6.5070804901101105*^7*I
+rafour[3] = -1.9589302261188729*^6 - 719022.6267241215*I
+rafour[4] = -211981.0193558234 + 1.4253123615474603*^6*I
+
+(* and now lets turn it into an equation *)
+
+(* this actually guesses racorrected2[], not ra *)
 
 raguess[x_] = Sum[Abs[2*rafour[n]/(vera25-vera0)]*Cos[2*Pi*n*x/rayear -
         Arg[2*rafour[n]/(vera25-vera0)]],
 {n,1,4}] + rafour[0]/(vera25-vera0)
 
-radiff = Plot[racorrected[x] - raguess[x], {x, 0, vera25-vera0}]
+(* commenting out plotting, as we don't need it in final form *)
 
-(* result is hideous!; pushing to git and trying different approach *)
+(* radiff = Plot[racorrected2[x] - raguess[x],{x,0,(vera25-vera0)}] *)
 
-(* TODO: have Mathematica manipulate equations to make them Perl friendly?? *)
+(* accurate to .0015 hour which is .0225 degrees *)
+
+(* the raw formulas; ie, the ultimate result of this file *)
+
+ved0 = 3.509652093789344*10^9
+
+decguess[x_] = 0.37808401703940736 + 0.3810373468678206*
+     Cos[0.3074066821871051 - 3.9821243192021897*^-7*x] + 
+    23.260776335116*Cos[1.6031468236573432 - 1.9910621596010949*^-7*x] + 
+    0.17118769496986821*Cos[1.4545723908701285 + 5.973186478803284*^-7*x] + 
+    0.008146125242501636*Cos[2.756482750676952 + 7.964248638404379*^-7*x]
+
+(* <h>pretend you don't notice ved0 and vera0 differ by ~39s</h> *)
+
+vera0 = 3.5096520548642015*10^9
+
+raguess[x_] = -0.12362547330377642 + 0.003653051241507621*
+     Cos[1.7184400853442734 - 7.964247794495168*^-7*x] + 
+    0.1226898815038194*Cos[0.2853850527151302 - 1.991061948623792*^-7*x] + 
+    0.16534691830933743*Cos[1.502453306625312 + 3.982123897247584*^-7*x] + 
+    0.005290041878837918*Cos[2.7898112178483023 + 5.973185845871376*^-7*x]
+
+(* now, write in Perl format *)
+
+raperl = ToLowerCase[StringReplace[ToString[raguess[x], CForm], {"x" -> "$x"}]]
+decperl=ToLowerCase[StringReplace[ToString[decguess[x], CForm], {"x" -> "$x"}]]
+
+{raperl, decperl} >> /tmp/perldecra.txt
 
 (* TODO: at some point, document my failed attempts as well (why?) *)
 
