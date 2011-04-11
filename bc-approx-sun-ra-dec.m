b0e11d6d93bb54c5dafae2500fab6d6f40ef3c52
Barry Carter <Barry Carter>
2011-04-11 10:21:33 -0600

    Checkpoint save.

--- bc-approx-sun-ra-dec.m
+++ bc-approx-sun-ra-dec.m
@@ -1,11 +1,59 @@
-(* Mathematica script to approximate Suns RA/DEC for bc-sun-always-shines.pl
- <h>that video is creepy</h>
+(* 
+
+Mathematica script to approximate Suns RA/DEC for bc-sun-always-shines.pl
+<h>that video is creepy</h>
+
+TODO: ending formulas are simple enough for JavaScript, so someone
+could write a Javascript that lets you watch "sunset sweep
+majestically over Albuquerque" <h>(or some such bullshit anyway)</h>
+
+NOTE: Mathematica whines a lot when using AstronomicalData as a pure
+numerical function because it tries to simplify it symbolically
+first. Example:
+
+AstronomicalData::notprop: 
+   {"Declination", ToDate[x]} is not a known property for AstronomicalDat
+     a. Use AstronomicalData["Properties"] for a list of properties.
+
+ToDate::tdn: First argument #1 should be an integer or a real number.
+
+Despite this, it ultimately comes up w/ the right answers
+
 *)
 
-(* TODO: ending formulas are simple enough for JavaScript, so
-  someone could write a Javascript that lets you watch "sunset sweep
-  majestically over Albuquerque" <h>(or some such bullshit anyway)</h>
-*)
+(* the Unix epoch; we'll be feeding results to Perl ultimately *)
+
+epoch = AbsoluteTime[{1970,1,1}]
+
+(* hardcoding "today" so others can verify my results, if desire *)
+
+today = {2011,4,15}
+mathtoday = AbsoluteTime[today]
+
+(* the nearest vernal equinox + the one 25 years hence, declination-based *)
+
+(* below: 20 Mar 2011 at 23:21:33.7893; agrees w wikipedia *)
+
+ved0 = x /. FindRoot[
+ AstronomicalData["Sun", {"Declination", ToDate[x]}] == 0, {x,mathtoday}]
+
+ved25 = x /. FindRoot[AstronomicalData["Sun", {"Declination", ToDate[x]}] == 0,
+  {x,mathtoday+365.2425*86400*25}]
+
+(* average year length, according to declination *)
+
+decyear = (ved25-ved0)/25
+
+(* find Fourier coefficients numerically, and store *)
+
+(* for easier calc [less CPU], use ved0 as x=0 *)
+
+decfour[n_] := decfour[n] = NIntegrate[
+ AstronomicalData["Sun", {"Declination", ToDate[ved0+x]}]*
+ Exp[2*Pi*I*x*n/decyear],
+ {x,0,ved25-ved0}]
+
+
 
 
 
