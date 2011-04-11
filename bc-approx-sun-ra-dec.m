ec0fc6e934292f23cbad32f93f30ed4d4c563511
Barry Carter <Barry Carter>
2011-04-11 11:31:30 -0600

    About to nuke hideously wrong attempt to approximate RA

--- bc-approx-sun-ra-dec.m
+++ bc-approx-sun-ra-dec.m
@@ -19,7 +19,14 @@ ToDate::tdn: First argument #1 should be an integer or a real number.
 
 Despite this, it ultimately comes up w/ the right answers
 
+"setenv TZ GMT" before starting Mathematica to force GMT
+
 *)
+
+(* work around "new" graphics handling in Mathematica 7 *)
+
+showit := Module[{},
+Export["/tmp/math.jpg",%, ImageSize->{800,600}]; Run["display /tmp/math.jpg&"]]
 
 (* the Unix epoch; we'll be feeding results to Perl ultimately *)
 
@@ -53,7 +60,86 @@ decfour[n_] := decfour[n] = NIntegrate[
  Exp[2*Pi*I*x*n/decyear],
  {x,0,ved25-ved0}]
 
+(* <h> decten[data_] = bartender </h> *)
 
+(* <h> Time passes...</h> *)
 
+(*
 
+RAW RESULTS (not sure why constant term is non-0):
+
+decfour[0] = 2.982794797849734*^8
+decfour[1] = -2.9677988934045434*^8 + 9.170689132027664*^9*I
+decfour[2] = 1.432586534498814*^8 + 4.548038984377269*^7*I
+decfour[3] = 7.830599335722238*^6 - 6.707145902500911*^7*I
+decfour[4] = -2.97798216111644*^6 - 1.2071250071517676*^6*I
+
+*)
+
+(* determine phase and amplitude *)
+
+Table[{n, Abs[2*decfour[n]/(ved25-ved0)], 
+          Arg[2*decfour[n]/(ved25-ved0)]}, 
+{n,1,4}]
+
+(* and now lets turn it into an equation (could've done this in one
+step, but clearer this way; also add in constant term *)
+
+decguess[x_] = Sum[Abs[2*decfour[n]/(ved25-ved0)]*Cos[2*Pi*n*x/decyear -
+        Arg[2*decfour[n]/(ved25-ved0)]],
+{n,1,4}] + decfour[0]/(ved25-ved0)
+
+(* compare to real declination *)
+
+decdiff = Plot[decguess[x] - 
+ AstronomicalData["Sun", {"Declination", ToDate[ved0+x]}], 
+ {x, 0, ved25-ved0}]
+
+(* shows accurate to .007 degrees *)
+
+(* now, same thing for RA, but take care of linear/sawtooth component first;
+   Fourier will handle sawtooth, but only w/ infinite number of terms *)
+
+vera0 = x /. FindRoot[
+ AstronomicalData["Sun", {"RightAscension", ToDate[x]}] == 0, {x,mathtoday}]
+
+vera25 = x /. FindRoot[AstronomicalData["Sun", {"RightAscension", 
+ ToDate[x]}] == 0,
+  {x,mathtoday+365.2425*86400*25}]
+
+rayear = (vera25-vera0)/25
+
+(* getting rid of that pesky linear term; when RA~0 this can yield
+   spurious 24s; hopefully, Fourier can ignore them *)
+
+racorrected[x_]:= AstronomicalData["Sun",{"RightAscension", ToDate[x+vera0]}] -
+ 24*Mod[x/rayear,1]
+
+(* the below looks odd but workable; should get back equation of time 
+   <h>equations for love and tenderness appear elsewhere</h> *)
+
+Plot[racorrected[x], {x,0,vera25-vera0}]
+
+(* and now, take it away, Mr Fourier *)
+
+rafour[n_] := rafour[n] = NIntegrate[
+ AstronomicalData["Sun", {"RightAscension", ToDate[vera0+x]}]*
+ Exp[2*Pi*I*x*n/rayear],
+ {x,0,vera25-vera0}]
+
+Table[rafour[n], {n,0,4}]
+
+(* Same sum as before *)
+
+raguess[x_] = Sum[Abs[2*rafour[n]/(vera25-vera0)]*Cos[2*Pi*n*x/rayear -
+        Arg[2*rafour[n]/(vera25-vera0)]],
+{n,1,4}] + rafour[0]/(vera25-vera0)
+
+radiff = Plot[racorrected[x] - raguess[x], {x, 0, vera25-vera0}]
+
+(* result is hideous!; pushing to git and trying different approach *)
+
+(* TODO: have Mathematica manipulate equations to make them Perl friendly?? *)
+
+(* TODO: at some point, document my failed attempts as well (why?) *)
 
