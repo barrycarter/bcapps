0dcfd99d53b956386f571be5d8670490fb82522c
Barry Carter <Barry Carter>
2011-04-11 08:28:06 -0600

    creating Mathematica script to approximate Sun (and more?) RA/DEC

---
+++ bc-approx-sun-ra-dec.m
@@ -0,0 +1,11 @@
+(* Mathematica script to approximate Suns RA/DEC for bc-sun-always-shines.pl
+ <h>that video is creepy</h>
+*)
+
+(* TODO: ending formulas are simple enough for JavaScript, so
+  someone could write a Javascript that lets you watch "sunset sweep
+  majestically over Albuquerque" <h>(or some such bullshit anyway)</h>
+*)
+
+
+
