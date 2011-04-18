cc40e4cf6e71886955630649aa8f6bf6fc72fb30
Barry Carter <Barry Carter>
2011-04-18 10:04:22 -0600

    Some progress, now need to look at WP API side

--- bc-stack2wp.pl
+++ bc-stack2wp.pl
@@ -12,18 +12,62 @@ chdir(tmpdir());
 
 # TODO: cheating and hardcoding this, but could get it from any of my stack ids
 $assoc_id = "aa1073f7-7e3b-4d4d-ace5-f2fca853f998";
+$apikey = "jm3pC2swyEWCN_sm3BhjTQ";
 
-# ($out, $err, $stat) = cache_command("curl http://stackauth.com/1.1/sites?page=1&pagesize=2147483647&minimal=true","age=86400");
+# find all stack sites (only need this because /associated below does
+# NOT give URLs, grumble)
 
-# ($out, $err, $stat) = cache_command("curl 'http://stackauth.com/1.1/sites?page=1&pagesize=10&minimal=true'","age=86400");
+# below won't work when stack grows over 100 sites!
+$fname = cache_command("curl 'http://stackauth.com/1.1/sites?page=1&pagesize=100&key=$apikey'","age=86400&retfile=1");
+
+system("gunzip -fc $fname > json0");
+$sites = read_file("json0");
+
+# parse..
+$json = JSON::from_json($sites);
+%jhash = %{$json};
+@items = @{$jhash{items}};
+
+# get data I need
+for $i (@items) {
+  %hash = %{$i};
+  %hash2 = %{$hash{main_site}};
+  $site{$hash2{name}} = $hash2{api_endpoint};
+}
 
 # find all my ids
 
-$fname = cache_command("curl 'http://stackauth.com/1.1/users/$assoc_id/associated'","age=86400&retfile=1");
+$fname = cache_command("curl 'http://stackauth.com/1.1/users/$assoc_id/associated?key=$apikey'","age=86400&retfile=1");
 
 # unzip results
 system("gunzip -c $fname > json1");
+$json = JSON::from_json(read_file("json1"));
+%jhash = %{$json};
+@items = @{$jhash{items}};
 
-debug(read_file("json1"));
+# get data I need (my id on the site)
+for $i (@items) {
+  %hash = %{$i};
 
+  # TODO: weird case, maybe fix later
+  if ($hash{site_name} eq "Area 51") {next;}
+
+  # map URL to id, not name to id
+  $myid{$site{$hash{site_name}}} = $hash{user_id};
+}
+
+# and now, my questions on all sites
+for $i (sort keys %myid) {
+
+  $url = "$i/1.0/users/$myid{$i}/questions";
+  debug("URL: $url");
+
+  warn "TESTING"; next;
+
+  # my questions
+  $fname = cache_command("curl '$url'","age=86400&retfile=1");
+
+  debug(read_file($fname));
+
+}
 
