71c1fd429850848254bd837bf0a35f5bf0d50e57
Barry Carter <Barry Carter>
2011-04-18 11:23:21 -0600

    This version works! (now to prune)

--- playground.pl
+++ playground.pl
@@ -3,9 +3,51 @@
 # Script where I test code snippets; anything that works eventually
 # makes it into a library or real program
 
-# reading Mathematica interpolation files
+# chunks are normally separated with 'die "TESTING";'
 
 require "bclib.pl";
+
+# RPC-XML
+
+# using raw below so i can cache and stuff
+
+$req=<<"MARK";
+<?xml version="1.0"?><methodCall>
+<methodName>system.listMethods</methodName>
+</methodCall>
+MARK
+;
+
+write_file($req,"/tmp/rpc1.txt");
+
+system("curl -o /tmp/rpc2.txt --data-binary \@/tmp/rpc1.txt http://wordpress.barrycarter.info/xmlrpc.php");
+
+die "TESTING";
+
+use RPC::XML;
+use RPC::XML::Parser;
+require RPC::XML::Client;
+
+$cli = RPC::XML::Client->new('http://wordpress.barrycarter.info/xmlrpc.php');
+$resp = $cli->send_request('system.listMethods');
+
+debug(@{$resp->value});
+
+die "TESTING";
+
+
+$req = RPC::XML::request->new('fetch_prime_factors', 
+ RPC::XML::int->new(985120528));
+
+debug($req);
+
+$resp = RPC::XML::Parser->new()->parse(STREAM);
+
+debug($resp);
+
+die "TESTING";
+
+# reading Mathematica interpolation files
 
 $all = read_file("sample-data/manytables.txt");
 
