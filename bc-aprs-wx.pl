0e73ca9463133fa5c6f2f2e4102e29da577a8355
Barry Carter <Barry Carter>
2011-05-07 13:19:57 -0600

    Checkpoint save. About to create KML file for google maps

--- bc-aprs-wx.pl
+++ bc-aprs-wx.pl
@@ -5,41 +5,36 @@
 
 require "bclib.pl";
 use Fcntl;
+chdir(tmpdir());
 
 # <h>let's do the time zone again</h>
 $ENV{TZ} = "GMT";
 
-# could "use Socket" here but this is cooler?
-open(A,"echo 'user READONLY pass -1' | ncat rotate.aprs.net 23 |");
-
-# unblock socket just in case we get disconnected
-fcntl(A,F_SETFL,O_NONBLOCK|O_NDELAY);
-
-# confirm connection
-while (<A>) {
-  debug($_);
-  print STDERR "Diagnostic: $_\n)";
-  if (/verified/i) {
-    debug("CONFIRMED");
-    last;
-  }
-}
-
 for(;;) {
+
+  # if we've been waiting too long for a line, something's wrong so restart;
+  # this also handles startup case where $lastlinetime isn't defined yet
+  $since = time()-$lastlinetime;
+  if ($since > 1) {debug("SINCE: $since");}
+  if ($since > 10) {
+    debug("(RE)STARTING");
+    close(A);
+    # could "use Socket" here but this is cooler?
+    open(A,"echo 'user READONLY pass -1' | ncat rotate.aprs.net 23 |") || warn("FAIL: Error, $!");
+    # unblock socket just in case we get disconnected
+    fcntl(A,F_SETFL,O_NONBLOCK|O_NDELAY);
+    # TODO: does reset count as reading a line?
+    $lastlinetime = time();
+  }
+
   $line = <A>;
 
-  # if we've been waiting too long for a line, something's wrong
-  if ((time()-$lastlinetime) > 30) {
-    debug("WAITING!");
-    # TODO: restart process above
-  }
-
   # if we see a blank line, chill for a bit
-  unless ($line) {sleep 1;}
+  unless ($line) {sleep 1; next;}
 
   $lastlinetime = time();
 
-  debug($line);
+#  debug("LINE: $line");
 
   # TODO: this is an inaccurate and improper way to find weather data
   # 't' is case sensitive
@@ -50,13 +45,13 @@ for(;;) {
   # latitude/longitude
   unless (($lat, $lats, $lon, $lons) =
  ($line=~m%([\d\.]{3,})([N|S])/([\d\.]{3,})([E|W])%)) {
-    debug("BAD POS");
+#    debug("BAD POS: $line");
     next;
   }
 
   # time
   unless (($da, $ho, $mi)=($line=~m%(\d{2})(\d{2})(\d{2})z%i)) {
-    debug("BAD TIME");
+#    debug("BAD TIME: $line");
     next;
   }
 
@@ -70,7 +65,7 @@ for(;;) {
   # last day of previous month
 
   # "speaker"
-  $speaker = ($line=~m%^(.*?)>%);
+  ($speaker) = ($line=~m%^(.*?)>%);
 
   # decimalize latitude/longitude (TODO: functionalize this)
   $latd = floor($lat/100);
@@ -83,15 +78,30 @@ for(;;) {
   $lonfinal = $lond+$lonm/60;
   if ($lons eq "W") {$lonfinal*=-1;}
 
-  print "$speaker $utime $latfinal $lonfinal ${temp}F\n";
+  push(@queries, "REPLACE INTO aprswx (station, time, lat, lon, temp) VALUES
+ ('$speaker', '$utime', '$latfinal', '$lonfinal', '$temp')");
+
+#  print "$speaker $utime $latfinal $lonfinal ${temp}F\n";
 
   # how long has it been since last update (push to db?)
-  if ((time()-$lastupdate) > 60) {do_update();}
+  $dbsince = time()-$lastupdate;
+  debug("DBSINCE: $dbsince");
+  if ($dbsince > 10) {do_update();}
 
 }
 
 # TODO: everything
 sub do_update {
+  # run queries in transaction
+  unshift(@queries, "BEGIN");
+  push(@queries, "COMMIT;\n");
+  my($query) = join(";\n", @queries);
+
+  write_file($query, "queries");
+  system("sqlite3 /home/barrycarter/20110507/aprswx.db < queries");
+
+  # reset
+  @queries = ();
   $lastupdate = time();
 }
 
@@ -101,7 +111,7 @@ sub do_update {
 
 database to hold these (latest report from station obsoletes prior report):
 
-CREATE TABLE aprswx (station, lat, lon, temp);
-CREATE UNIQUE INDEX ON aprswx(station);
+CREATE TABLE aprswx (station, time, lat, lon, temp);
+CREATE UNIQUE INDEX i1 ON aprswx(station);
 
 =cut
