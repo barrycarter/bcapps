fae8e82da28b8d27de82f9167565270caa1a125d
Barry Carter <Barry Carter>
2011-05-07 18:53:09 -0600

    Working version: about to improve

--- bc-aprs-wx.pl
+++ bc-aprs-wx.pl
@@ -7,9 +7,6 @@ require "bclib.pl";
 use Fcntl;
 chdir(tmpdir());
 
-warn "TESTING";
-do_update(); # for testing, jump to this
-
 # <h>let's do the time zone again</h>
 $ENV{TZ} = "GMT";
 
@@ -18,156 +15,201 @@ for(;;) {
   # if we've been waiting too long for a line, something's wrong so restart;
   # this also handles startup case where $lastlinetime isn't defined yet
   $since = time()-$lastlinetime;
-  if ($since > 1) {debug("SINCE: $since");}
-  if ($since > 10) {
-    debug("(RE)STARTING");
-    close(A);
-    # could "use Socket" here but this is cooler?
-    open(A,"echo 'user READONLY pass -1' | ncat rotate.aprs.net 23 |") || warn("FAIL: Error, $!");
-    # unblock socket just in case we get disconnected
-    fcntl(A,F_SETFL,O_NONBLOCK|O_NDELAY);
-    # TODO: does reset count as reading a line?
-    $lastlinetime = time();
-  }
+  if ($since > 10) {do_connect();}
 
-  $line = <A>;
+  # read next line, but ignore + sleep if blank
+  unless ($line = <A>) {sleep 1; next;}
 
-  # if we see a blank line, chill for a bit
-  unless ($line) {sleep 1; next;}
-
+  # we have a valid line, so update lastlinetime
   $lastlinetime = time();
 
-#  debug("LINE: $line");
+  # process line
+  %hash = parse_line($line);
 
-  # TODO: this is an inaccurate and improper way to find weather data
-  # 't' is case sensitive
-  unless (($temp) = ($line=~/t(\d{3})/)) {next;}
-  # strip leading 0s
-  $temp=~s/^0+//isg;
+  unless (%hash) {next;}
 
-  # latitude/longitude
-  unless (($lat, $lats, $lon, $lons) =
- ($line=~m%([\d\.]{3,})([N|S])/([\d\.]{3,})([E|W])%)) {
-#    debug("BAD POS: $line");
-    next;
-  }
+  # query
+  $query = "REPLACE INTO aprswx (station, time, lat, lon, temp) VALUES
+ ('$hash{speaker}', '$hash{utime}', '$hash{lat}', '$hash{lon}', '$hash{temp}')";
 
-  # time
-  unless (($da, $ho, $mi)=($line=~m%(\d{2})(\d{2})(\d{2})z%i)) {
-#    debug("BAD TIME: $line");
-    next;
-  }
-
-  # convert to Unix
-  # find current month (rest of info is useless)
-  ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime(time());
-  # and now, mktime
-  $utime = mktime(0, $mi, $ho, $da, $mon, $year);
-
-  # TODO: ignoring corner case: today is 1st of month, report was on
-  # last day of previous month
-
-  # "speaker"
-  ($speaker) = ($line=~m%^(.*?)>%);
-
-  # decimalize latitude/longitude (TODO: functionalize this)
-  $latd = floor($lat/100);
-  $latm = $lat - $latd*100;
-  $latfinal = $latd+$latm/60;
-  if ($lats eq "S") {$latfinal*=-1;}
-
-  $lond = floor($lon/100);
-  $lonm = $lon - $lond*100;
-  $lonfinal = $lond+$lonm/60;
-  if ($lons eq "W") {$lonfinal*=-1;}
-
-  push(@queries, "REPLACE INTO aprswx (station, time, lat, lon, temp) VALUES
- ('$speaker', '$utime', '$latfinal', '$lonfinal', '$temp')");
-
-#  print "$speaker $utime $latfinal $lonfinal ${temp}F\n";
+  push(@queries, $query);
 
   # how long has it been since last update (push to db?)
   $dbsince = time()-$lastupdate;
-  debug("DBSINCE: $dbsince");
   if ($dbsince > 10) {do_update();}
-
 }
 
 sub do_update {
-  my($count);
+  my(@vpoints);
+  my(@temp);
+  my(@polys);
+
+  # calling this resets $lastupdate
+  $lastupdate = time();
+
+  # nuke entries older than an hour or in the future
+  my($now) = time();
+  my($cull) = $now - 3600;
+#  debug("CULL: $cull");
+  push(@queries, "DELETE FROM aprswx WHERE time+0 < $cull OR time+0 > $now");
 
   # run queries in transaction
   unshift(@queries, "BEGIN");
-
-  # nuke entries older than an hour
-  my($cull) = time() - 3600;
-  push(@queries, "DELETE FROM aprswx WHERE time < $cull");
   push(@queries, "COMMIT;\n");
   my($query) = join(";\n", @queries);
-
   write_file($query, "queries");
+  debug("QUERIES:",read_file("queries"));
   system("sqlite3 /home/barrycarter/20110507/aprswx.db < queries");
 
   # now, pull data are create KML file
   my(@res)=sqlite3hashlist("SELECT lat,lon,temp FROM aprswx", "/home/barrycarter/20110507/aprswx.db");
 
-  # voronoi points
-  my(@vpoints);
-
-  # keep track of temperature
-  my(%tempr) = ();
-
-  for $i (@res) {
-    my(%hash) = %{$i};
-    push(@vpoints, $hash{lon}, $hash{lat});
-    # TODO: indexing on real numbers is really really bad
-    $tempr{$hash{lon}}{$hash{lat}} = $hash{temp};
+  unless ($#res>=0) {
+    debug("NO RECORDS YET...");
+    return;
   }
+
+  for $i (0..$#res) {
+    my(%hash) = %{$res[$i]};
+    # the voronoi point list
+    push(@vpoints, $hash{lon}, $hash{lat});
+    # keep track of temperature
+    $temp[$i] = $hash{temp};
+  }
+
 
   # create diagram
   # TODO: for now, using equiangular mapping
   my(@poly) = voronoi(\@vpoints);
 
-  # this will hold the style + polygon portion of the KML file
-  my(@polys);
-
-  # and now KML file
-  for $i (@poly) {
-
-    # style for this polygon
-    $count++;
-    push(@poly, '<Style id="$count">');
-    push(@poly, '<PolyStyle><color>$kmlcol</color>');
-    push(@poly, '<fill>1</fill><outline>0</outline></PolyStyle></Style>');
-
-    push(@polys, "<Placemark><Polygon><outerBoundaryIs><LinearRing><coordinates>");
-    for $j (@{$i}) {
-      $j=~s/ /,/isg;
-      push(@polys, $j);
-    }
-    push(@polys, "</coordinates></LinearRing></outerBoundaryIs></Polygon></Placemark>");
+  # create KML for each polygon, first determining color
+  for $i (0..$#poly) {
+    my($hue) = 5/6-($temp[$i]/100)*5/6;
+    my($col) = hsv2rgb($hue,1,1,"kml=1&opacity=60");
+    push(@mypolys, poly_kml($poly[$i], $col));
   }
 
   # KML header
-  my($kml) = << "MARK";
+  my($kmlhead) = << "MARK";
 <?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 <Document>
 MARK
 ;
+  # KML footer
+  my($kmlfoot) = "</Document></kml>\n";
 
-  # and the polygons combined
-  my($poly) = join("\n",@polys)."\n";
-  write_file("$kml\n$poly\n</Document></kml>\n", 
-"/home/barrycarter/BCINFO/sites/TEST/aprswx.kml");
+  # polygons
+  my($polystring) = join("\n",@mypolys)."\n";
 
-#  debug(unfold(@poly));
-  debug(@polys);
-  die "TESTING";
+  # write to file
+  write_file("$kmlhead$polystring$kmlfoot", "/home/barrycarter/BCINFO/sites/TEST/aprswx.kml");
 
-  # reset
-  @queries = ();
-  $lastupdate = time();
+  debug("UPDATED!");
+
+  return;
+}
+
+# given a polygon and a color, return KML for it (specific to this script)
+sub poly_kml {
+  my($poly, $col) = @_;
+  my(@coords);
+  # pretend static var
+  $static{count}++;
+
+  # style
+  my($style) = << "MARK";
+<Style id="$static{count}">
+<PolyStyle><color>$col</color>
+<fill>1</fill><outline>0</outline></PolyStyle></Style>
+MARK
+;
+
+  # polygon header
+  my($polyhead) = << "MARK";
+<Placemark><gx:balloonVisibility>0</gx:balloonVisibility>
+<styleUrl>\#$static{count}</styleUrl>
+<title>TITLE</title>
+<description>BLANK</description>
+<Polygon><outerBoundaryIs><LinearRing><coordinates>
+MARK
+;
+
+  my(@poly) = @{$poly};
+  map(s/ /,/isg, @poly);
+
+  # the coordinates
+  my($polybody) = join("\n", @poly)."\n";
+
+  # footer
+  my($polyfoot) = << "MARK";
+</coordinates></LinearRing></outerBoundaryIs></Polygon></Placemark>
+MARK
+;
+
+  return "$style$polyhead$polybody$polyfoot";
+}
+
+# make connection to APRS server, perhaps unsuccessfuly
+sub do_connect {
+
+  # TODO: calling this resets lastlinetime, but should it?
+  $lastlinetime = time();
+
+  debug("(RE)CONNECTING");
+  close(A);
+  # could "use Socket" here but this is cooler?
+  open(A,"echo 'user READONLY pass -1' | ncat rotate.aprs.net 23 |") || warn("FAIL: Error, $!");
+  # unblock socket just in case we get disconnected
+  fcntl(A,F_SETFL,O_NONBLOCK|O_NDELAY);
+}
+
+# parse an APRS line, returning a hash of values, or blank if line is
+# not in WX format
+
+sub parse_line {
+  my($line) = @_;
+  my(%hash) = ();
+  my($lat, $lats, $lon, $lons, $da, $ho, $mi);
+
+  # grab temperature data, return if none
+  # TODO: this is an inaccurate and improper way to find weather data
+  # 't' is case sensitive
+  unless (($hash{temp}) = ($line=~/t(\d{3})/)) {return();}
+
+  # latitude/longitude
+  unless (($lat, $lats, $lon, $lons) =
+ ($line=~m%([\d\.]{3,})([N|S])/([\d\.]{3,})([E|W])%)) {return();}
+
+  # observation time
+  unless (($da, $ho, $mi)=($line=~m%(\d{2})(\d{2})(\d{2})z%i)) {return();}
+
+  # we now know we have a valid line, so process it
+
+  # convert time to unix time
+  # TODO: ignoring corner case: today is 1st of month, report was on
+  # last day of previous month
+  # find current month (rest of info is useless)
+  my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime(time());
+  # and now, mktime
+  $hash{utime} = mktime(0, $mi, $ho, $da, $mon, $year);
+
+  # "speaker" (probably shouldve been station?)
+  ($hash{speaker}) = ($line=~m%^(.*?)>%);
+
+  # decimalize latitude/longitude (TODO: functionalize this)
+  my($latd) = floor($lat/100);
+  my($latm) = $lat - $latd*100;
+  $hash{lat} = $latd+$latm/60;
+  if ($lats eq "S") {$hash{lat}*=-1;}
+
+  my($lond) = floor($lon/100);
+  my($lonm) = $lon - $lond*100;
+  $hash{lon} = $lond+$lonm/60;
+  if ($lons eq "W") {$hash{lon}*=-1;}
+
+  return %hash;
+
 }
 
 # Format: "@221533z2950.68N/09529.95W_308"
