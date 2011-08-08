c8d88cd4631133a9083a24cf65a605e7f7069298
Barry Carter <Barry Carter>
2011-08-08 08:25:17 -0600

    NADEX stuff.

---
+++ nadex.m
@@ -0,0 +1,31 @@
+(* Mathematica stuff for NADEX *)
+
+
+(* TODO: sublibrary to include in all .m files *)
+
+showit := Module[{}, 
+Export["/tmp/math.jpg",%, ImageSize->{800,600}]; Run["display /tmp/math.jpg&"]]
+
+(* value of spread from lo to hi at price p *)
+
+spread[p_, lo_, hi_] = Min[Max[p,lo],hi]
+
+(* profit at price q if you buy 1 unit of parity at price p; not
+directly NADEX, but useful for hedging *)
+
+profit[p_, q_] = (q/p-1)
+
+
+(* testing and plots *)
+
+Plot[spread[p, .99, 1.01], {p,.95,1.05}]
+
+Plot[10000*profit[.99, q], {q,.98,1}]
+
+Plot[10000*profit[1, q], {q,.5,2}]
+
+Plot[10000*profit[1, q], {q,.5,2}]
+
+
+
+
