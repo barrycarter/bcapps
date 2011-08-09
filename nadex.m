df4de8c6ce1bb796fc231eedd1c05eb5c964a1cf
Barry Carter <Barry Carter>
2011-08-09 07:24:46 -0600

    Checkpoint save. About to parametrize bc-forex-alert.pl

--- nadex.m
+++ nadex.m
@@ -1,6 +1,5 @@
 (* Mathematica stuff for NADEX *)
 
-
 (* TODO: sublibrary to include in all .m files *)
 
 showit := Module[{}, 
@@ -10,11 +9,15 @@ Export["/tmp/math.jpg",%, ImageSize->{800,600}]; Run["display /tmp/math.jpg&"]]
 
 spread[p_, lo_, hi_] = Min[Max[p,lo],hi]
 
+(* profit at price p if you sell a q option for $r [incl NADEX fees;
+if trading over 7 contracts, reduce fee as needed] *)
+
+optionprofit[p_, q_, r_] =  r + If[p>q,-101,-2]
+
 (* profit at price q if you buy 1 unit of parity at price p; not
 directly NADEX, but useful for hedging *)
 
 profit[p_, q_] = (q/p-1)
-
 
 (* testing and plots *)
 
@@ -26,6 +29,7 @@ Plot[10000*profit[1, q], {q,.5,2}]
 
 Plot[10000*profit[1, q], {q,.5,2}]
 
-
-
+Plot[210000*profit[.9905,q] - 
+ (spread[q, .99, 1] - .9911)*21*10000,
+{q,.98,1}]
 
