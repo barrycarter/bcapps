95e4cc52027cf8c2a7390b39678093aafd3ba3df
Barry Carter <Barry Carter>
2011-08-09 07:30:13 -0600

    Parametrized.

--- bc-forex-alert.pl
+++ bc-forex-alert.pl
@@ -14,10 +14,10 @@ if ($globopts{until} && $now < $globopts{until}) {exit(0);}
 $email = read_file("/home/barrycarter/bc-email.txt");
 chomp($email);
 
-@x=forex_quote("USD/CAD",time(),"list=true");
+# obtain from args (parity format: USD/CAD)
+($parity, $low, $high) = @ARGV;
 
-# TODO: move next line out of GIT since I keep changing it pointlessly
-($low, $high) = (0.9430, 0.9860);
+@x=forex_quote($parity,time(),"list=true");
 
 for $i (@x) {
   %hash = ();
@@ -36,18 +36,16 @@ for $i (@x) {
   # TODO: separate out phone, regular email, and xmessage messages
 
   if ($hash{bid} < $low) {
-#    $msg = "BC FOREX ALERT: USDCAD: $hash{bid} < $low ($hash{time}/$now)";
-    $msg = "BC FOREX ALERT: USDCAD < $low";
+    $msg = "BC FOREX ALERT: $parity < $low";
   } elsif ($hash{offer} > $high) {
-#    $msg = "BC FOREX ALERT: USDCAD: $hash{offer} > $high ($hash{time}/$now)";
-    $msg = "BC FOREX ALERT: USDCAD > $high";
+    $msg = "BC FOREX ALERT: $parity > $high";
   } else {
     $msg = "";
   }
 
   # if there is a msg, display it + kill old messages to avoid clutter
   if ($msg) {
-    system("pkill -f 'BC FOREX ALERT: USDCAD'");
+    system("pkill -f 'BC FOREX ALERT: $parity'");
     system("xmessage -geometry 1024 -nearmouse '$msg' &");
     sendmail("alert\@barrycarter.info", $email, "", $msg);
     # no need to check rest
