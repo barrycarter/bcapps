c5e74cb484a98e851f5482dc407a2eea82a364c8
Barry Carter <Barry Carter>
2011-09-10 12:20:24 -0600

    Checkpoint save.

--- bc-planet-pos.m
+++ bc-planet-pos.m
@@ -19,14 +19,16 @@ Export["/tmp/math.jpg",%, ImageSize->{800,600}]; Run["display /tmp/math.jpg&"]]
 
 (* every 6m *)
 
-xyz[planet_, year_] := Table[
- {t, AstronomicalData[planet, {"Position", DateList[t]}]},
- {t, AbsoluteTime[{year,1,1}], AbsoluteTime[{year+1,1,1}], 86400}]
+xyz[planet_, year_] := Module[{x,s},
+ x = Table[{t, AstronomicalData[planet, {"Position", DateList[t]}]},
+ {t, AbsoluteTime[{year,1,1}], AbsoluteTime[{year+1,1,1}], 86400}];
+ s = StringJoin[{"/home/barrycarter/BCGIT/planetdata/xyz-",
+ ToString[planet], "-", ToString[year],  ".txt"}];
+ Print[s];
+ Put[x,s];
+]
 
 t1 = xyz["Mars", 2011]
- 
-
-
 
 AstronomicalData["Mars", "Position"]
 
