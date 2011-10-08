394b5f9f5ad3b00b855a4b938ab029525f28388b
Barry Carter <Barry Carter>
2011-10-08 17:40:30 -0600

    Checkpoint save.

--- playground.rb
+++ playground.rb
@@ -1,10 +1,9 @@
 #!/usr/local/bin/ruby
 
-$LOAD_PATH << "/usr/lib/ruby/gems/1.8/gems/jnunemaker-crack-0.1.4/lib"
-require 'crack'
-xml = File.read("/tmp/test1.xml")
-foo = Crack::XML.parse(xml)
-print foo["data"]["reports"].inspect()
+require 'rexml/document'
+doc = REXML::Document.new(File.new("/tmp/test1.xml"))
+print doc.inspect()
+
 
 
 
