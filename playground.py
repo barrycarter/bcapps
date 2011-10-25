b23bba8335c0035788f33e3e224fd0a2315a6ec7
Barry Carter <Barry Carter>
2011-10-25 11:50:26 -0600

    Checkpoint save. Works with picloud.

--- playground.py
+++ playground.py
@@ -3,19 +3,10 @@
 # cloud testing (you must have a picloud.com account AND set
 # cloudconf.py correctly for any of this to work)
 
-import cloud
-import os
-
-#os.system("date")
-# exit("TESTING")
-
-def sys(x): os.system(x)
-jid = cloud.call(sys, "curl http://barrycarter.info/")
+import cloud; import os;
+jid = cloud.call(os.system, "curl http://pi.barrycarter.info/")
 print cloud.result(jid)
 
-# os.system("perl -v")
+# in my logs:
 
-# die
-# jid = cloud.call(lambda: 3*3)
-# jid = cloud.call(sys("perl -v"))
-
+# 184.73.142.136 pi.barrycarter.info - [25/Oct/2011:11:44:35 -0600] "GET / HTTP/1.1" 200 94 "-" "curl/7.21.0 (x86_64-pc-linux-gnu) libcurl/7.21.0 OpenSSL/0.9.8o zlib/1.2.3.4 libidn/1.18"
