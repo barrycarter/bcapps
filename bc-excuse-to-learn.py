ab060b347fefeed37a351b63161867452cc3b093
Barry Carter <Barry Carter>
2011-11-06 11:30:13 -0700

    working as subroutine

--- bc-excuse-to-learn.py
+++ bc-excuse-to-learn.py
@@ -6,8 +6,6 @@
 
 import cloud, os, csv, math, string
 
-# print string.join(["a","b","c"], "\n")
-
 # how much water can air at 'temperature' (in Celsius) hold
 def saturationVaporPressure(temperature):
     # from http://www.ehow.com/how_8279076_calculate-relative-humidity-dewpoint.html
@@ -16,67 +14,63 @@ def saturationVaporPressure(temperature):
     T = float(temperature)+273.15
     return 6.11*math.exp((L/Rv)*(1/273-1/T))
 
-# do all work in temporary (but fixed) directory
-tmpdir = "/tmp/bcetlp"
-if not(os.path.isdir(tmpdir)): os.mkdir(tmpdir)
-os.chdir(tmpdir)
+def doWeatherStuff():
 
-# TODO: below is just for now; in reality, new copy each time (which
-# is actually automatic in cloud...)
+    # do all work in temporary (but fixed) directory
+    tmpdir = "/tmp/bcetlp"
+    if not(os.path.isdir(tmpdir)): os.mkdir(tmpdir)
+    os.chdir(tmpdir)
 
-if (not(os.path.isfile(tmpdir+"/metar.txt") and os.path.isfile(tmpdir+"/buoy.txt"))):
-    parfile = open("parallel", "w")
-    parfile.write("curl http://weather.aero/dataserver_current/cache/metars.cache.csv.gz | gunzip | tail -n +6 > metar.txt\n")
-    parfile.write("curl -o buoy.txt http://www.ndbc.noaa.gov/data/latest_obs/latest_obs.txt\n")
-    parfile.close()
-    os.system("/usr/local/bin/parallel -j 0 < parallel 1> par.out 2> par.err")
+    # TODO: below is just for now; in reality, new copy each time (which
+    # is actually automatic in cloud...)
 
-reader = csv.DictReader(open("metar.txt"))
+    if (not(os.path.isfile(tmpdir+"/metar.txt") and os.path.isfile(tmpdir+"/buoy.txt"))):
+        parfile = open("parallel", "w")
+        parfile.write("curl http://weather.aero/dataserver_current/cache/metars.cache.csv.gz | gunzip | tail -n +6 > metar.txt\n")
+        parfile.write("curl -o buoy.txt http://www.ndbc.noaa.gov/data/latest_obs/latest_obs.txt\n")
+        parfile.close()
+        os.system("/usr/local/bin/parallel -j 0 < parallel 1> par.out 2> par.err")
 
-# fields I want, and a hash to store them in (+ computed value)
-fields = ['temp_c', 'sea_level_pressure_mb', 'wind_speed_kt']
-data = {}
+    reader = csv.DictReader(open("metar.txt"))
 
-for i in fields: data[i] = []
-# special case
-data['humidity'] = [] 
+    # fields I want, and a hash to store them in (+ computed value)
+    fields = ['temp_c', 'sea_level_pressure_mb', 'wind_speed_kt']
+    data = {}
 
-for row in reader:
-    # ignore empty lat/lon
-    if (row['latitude'] == "" or row['longitude'] == ""): continue
+    for i in fields: data[i] = []
+    # special case
+    data['humidity'] = [] 
 
-    for i in fields:
-        if (row[i] != ''):
-            data[i].append([row['latitude'], row['longitude'], row[i]])
+    for row in reader:
+        # ignore empty lat/lon
+        if (row['latitude'] == "" or row['longitude'] == ""): continue
 
-    # compute humidity
-    if (row['temp_c'] != '' and row['dewpoint_c'] != ''):
-        humidity = saturationVaporPressure(row['dewpoint_c'])/saturationVaporPressure(row['temp_c'])
-        data['humidity'].append([row['latitude'], row['longitude'], humidity])
+        for i in fields:
+            if (row[i] != ''):
+                data[i].append([row['latitude'], row['longitude'], row[i]])
 
-# parallel compute del and vor
-par2 = open("parallel2", "w")
+            # compute humidity
+            if (row['temp_c'] != '' and row['dewpoint_c'] != ''):
+                humidity = saturationVaporPressure(row['dewpoint_c'])/saturationVaporPressure(row['temp_c'])
+                data['humidity'].append([row['latitude'], row['longitude'], humidity])
 
-# write files for qhull
-for i in data.keys():
-    f = open(i,"w")
-    f.write("2\n")
-    f.write(str(len(data[i]))+"\n")
-    f.write(string.join(map(lambda x: x[0]+' '+x[1], data[i]),"\n")+"\n")
-    f.close()
+    # parallel compute del and vor
+    par2 = open("parallel2", "w")
 
-    par2.write("qvoronoi s o < "+i+" > vor"+i+"\n")
-    par2.write("qdelaunay i < "+i+" > del"+i+"\n")
+    # write files for qhull
+    for i in data.keys():
+        f = open(i,"w")
+        f.write("2\n")
+        f.write(str(len(data[i]))+"\n")
+        f.write(string.join(map(lambda x: x[0]+' '+x[1], data[i]),"\n")+"\n")
+        f.close()
 
-par2.close()
-os.system("parallel -j 0 < parallel2")
+        par2.write("qvoronoi s o < "+i+" > vor"+i+"\n")
+        par2.write("qdelaunay i < "+i+" > del"+i+"\n")
 
-
-
-
-
-    
+    par2.close()
+    os.system("parallel -j 0 < parallel2")
 
 
-
+doWeatherStuff()
 
