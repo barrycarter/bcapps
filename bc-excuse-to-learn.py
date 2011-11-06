7d1b83a8409a83561abd870b806b2667844e82c5
Barry Carter <Barry Carter>
2011-11-06 08:36:02 -0700

    Checkpoint. Python not really thrilling me so far

--- bc-excuse-to-learn.py
+++ bc-excuse-to-learn.py
@@ -4,9 +4,15 @@
 # maps for various atmospheric data, using "the cloud". This should be
 # more efficient than doing these maps separately
 
-import cloud
-import os
-import csv
+import cloud, os, csv, math
+
+# how much water can air at 'temperature' (in Celsius) hold
+def saturationVaporPressure(temperature):
+    # from http://www.ehow.com/how_8279076_calculate-relative-humidity-dewpoint.html
+    L = 2.453*10**6
+    Rv = 461
+    T = float(temperature)+273.15
+    return 6.11*math.exp((L/Rv)*(1/273-1/T))
 
 # do all work in temporary (but fixed) directory
 tmpdir = "/tmp/bcetlp"
@@ -24,21 +30,25 @@ if (not(os.path.isfile(tmpdir+"/metar.txt") and os.path.isfile(tmpdir+"/buoy.txt
     os.system("/usr/local/bin/parallel < parallel 1> par.out 2> par.err")
 
 reader = csv.DictReader(open("metar.txt"))
-temps = []
-press = []
+
+# fields I want, and a hash to store them in
+fields = ['temp_c', 'sea_level_pressure_mb', 'wind_speed_kt']
+data = {}
+for i in fields: data[i] = []
 
 for row in reader:
     # ignore empty lat/lon
     if (row['latitude'] == "" or row['longitude'] == ""): continue
 
-    # temperature when avail
-    if (row['temp_c'] != ''):
-        temps.append([row['latitude'], row['longitude'], row['temp_c']])
+    for i in fields:
+        if (row[i] != ''):
+            data[i].append([row['latitude'], row['longitude'], row[i]])
 
-    if (row['sea_level_pressure_mb'] != ''):
-        press.append([row['latitude'], row['longitude'], row['sea_level_pressure_mb']])
+    # compute humidity
+    if (row['temp_c'] != '' and row['dewpoint_c'] != ''):
+        humidity = saturationVaporPressure(row['dewpoint_c'])/saturationVaporPressure(row['temp_c'])
+        data['humidity'].append([row['latitude'], row['longitude'], humidity])
 
-print (press)
 
 exit(0)
 
@@ -52,9 +62,6 @@ print (headers)
 # headers = metar.pop()
 # print (headers)
 
-
-
-       
-
-       
+def humidity(temperature, dewpoint):
+    return
 
