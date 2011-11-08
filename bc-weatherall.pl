a312e76494d409cd23023e85e649ad3b42286c20
Barry Carter <Barry Carter>
2011-11-08 15:43:44 -0700

    About to port python script: dislike python

---
+++ bc-weatherall.pl
@@ -0,0 +1,8 @@
+#!/bin/perl
+
+# Gave up on learning python (whitespace is significant? blech!), and
+# porting bc-excuse-to-learn.py to Perl: this attempts to perform all
+# weather functions that other programs have done individually: create
+# Voronoi and Delauney maps for multiple data, download latest data,
+# maintain db, etc. Once complete, this program will replace many others
+
