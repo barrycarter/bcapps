d46e58c829f7f8644114d6291f3496e7a9a379fa
Barry Carter <Barry Carter>
2011-12-15 09:27:17 -0700

    And done!

--- bc-send-more-money.pl
+++ bc-send-more-money.pl
@@ -3,26 +3,35 @@
 # Brute force solves http://www.thelab.gr/xmas2011/auga.php and may
 # serve as a template for similar problems
 
+# This program is dedicated to Christina Sereti (sereti at gmail dot com)
+
 # The problem: HHBBBL + LEBBTL + LTLHEH = TH3L4B, with E=3
+
+
 
 require "bclib.pl";
 
-# TODO: nested loops are NOT the best way to do this, but they work
+# digits map to themselves
+for $i (0..9) {$temp{$i}=$i;}
+
 # letting E be a variable just for fun
-for $try{H} (0..9) {
-  for $try{B} (0..9) {
-    for $try{L} (0..9) {
-      for $try{T} (0..9) {
-	for $try{E} (0..9) {
-	  $sum = compute("HHBBBL") + compute("LEBBTL") + compute("LTLHEH");
-	  $tot = compute("TH3L4B");
-	  debug("SUM: $sum, TOT: $tot");
-	}
-      }
-    }
+
+for $i ("00000".."99999") {
+  @digits = split(//, $i);
+  # assign digits to letters in this order
+  for $j (split(//,"HBLTE")) {
+    $temp{$j} = shift(@digits);
   }
+
+  @sums = (compute("HHBBBL"), compute("LEBBTL"), compute("LTLHEH"));
+  # NOTE: yes, there's a better way to do below
+  $sum = $sums[0] + $sums[1] + $sums[2];
+  $tot = compute("TH3L4B");
+  debug("SUM: $sum, TOT: $tot");
+  if ($sum == $tot) {last;}
 }
 
+debug(@sums);
 
 # given a string, compute its value using the 'try' hash
 # TODO: no need to hardcode hash
@@ -32,7 +41,7 @@ sub compute {
   my($sum);
   # most significant digit first
   for $i (split(//, $str)) {
-    $sum = $sum*10 + $i;
+    $sum = $sum*10 + $temp{$i};
   }
 
   return $sum;
