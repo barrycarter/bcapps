12f72cd63a408dd02b7be4ca5188e42530086ee9
Barry Carter <Barry Carter>
2011-12-15 09:46:28 -0700

    Uniqueness of solution; ways to improve script

--- bc-send-more-money.pl
+++ bc-send-more-money.pl
@@ -7,7 +7,16 @@
 
 # The problem: HHBBBL + LEBBTL + LTLHEH = TH3L4B, with E=3
 
+# solution: H=1, B=5, L=2, E=3, T=6 (appears to be unique)
 
+# TODO: generalize (but problem not always trivial; search space is
+# 10^(number of unique letters), assuming you allow for repeats)
+
+# TODO: when search range is large, add --random option to search in
+# random order?
+
+# TODO: BIG: divine information about some letters from assigned
+# values to others (eg, 2*L + H = B (mod 10) from last digits)
 
 require "bclib.pl";
 
@@ -28,12 +37,12 @@ for $i ("00000".."99999") {
   $sum = $sums[0] + $sums[1] + $sums[2];
   $tot = compute("TH3L4B");
   debug("SUM: $sum, TOT: $tot");
-  if ($sum == $tot) {last;}
+  if ($sum == $tot && ++$count>1) {last;}
 }
 
 debug(@sums);
 
-# given a string, compute its value using the 'try' hash
+# given a string, compute its value using the 'temp' hash
 # TODO: no need to hardcode hash
 
 sub compute {
