539aa361741e3b136dc175f3ce938f39f0e079a2
Barry Carter <Barry Carter>
2012-02-05 11:51:19 -0700

    Checkpoint save. About to be even more clever

--- bc-montastic.pl
+++ bc-montastic.pl
@@ -7,9 +7,19 @@
 require "bclib.pl";
 
 dodie('chdir("/var/tmp/montastic")');
+system("rm commands.txt");
 
 # format of this file, each line is "username:password", # starts comments
+for $i (split(/\n/,read_file("$ENV{HOME}/montastic.txt"))) {
+  if ($i=~/^\#/ || $i=~/^\s*$/) {next;}
+  ($user,$pass) = split(":",$i);
+  append_file("curl -H 'Accept: application/xml' -o $user -u $user:$pass https://www.montastic.com/checkpoints/index\n", "commands.txt");
+  push(@users, $user);
+}
 
-# this is ugly (it started out as a oneliner)
-# system(qq%egrep -v '^#' ~/montastic.txt | perl -nle '\$x=\$_; s/:.*//; print "curl -o \$_ -H \47Accept: application/xml\47 -u \$x https://www.montastic.com/checkpoints/index"' | parallel%);
+system("parallel -j 20 < commands.txt");
+
+# look through results
+for $i (@users) {
+  
 
