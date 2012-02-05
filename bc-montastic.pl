4a92399d237cf7eacbd2077b6269710e6450a394
Barry Carter <Barry Carter>
2012-02-05 12:46:31 -0700

    Working.

--- bc-montastic.pl
+++ bc-montastic.pl
@@ -4,22 +4,41 @@
 # accounts) and "report" these to ~/ERR which ultimately prints to my
 # background image
 
+# -nocurl: dont actually query montastic API (useful for testing)
+
 require "bclib.pl";
 
 dodie('chdir("/var/tmp/montastic")');
-system("rm commands.txt");
+
+unless ($globopts{nocurl}) {system("rm commands.txt results.txt err.txt");}
 
 # format of this file, each line is "username:password", # starts comments
 for $i (split(/\n/,read_file("$ENV{HOME}/montastic.txt"))) {
   if ($i=~/^\#/ || $i=~/^\s*$/) {next;}
   ($user,$pass) = split(":",$i);
-  append_file("curl -H 'Accept: application/xml' -o $user -u $user:$pass https://www.montastic.com/checkpoints/index\n", "commands.txt");
+  append_file("curl -H 'Accept: application/xml' -u $user:$pass https://www.montastic.com/checkpoints/index\n", "commands.txt");
   push(@users, $user);
 }
 
-system("parallel -j 20 < commands.txt");
+unless ($globopts{nocurl}) {
+  system("parallel -j 20 < commands.txt > results.txt");
+}
 
-# look through results
-for $i (@users) {
-  
+# look at results
+$all = read_file("results.txt");
+
+while ($all=~s%<checkpoint>(.*?)</checkpoint>%%is) {
+  $res = $1;
+
+  # ignore good results
+  if ($res=~m%<status type="integer">1</status>%) {next;}
+
+  # offending URL
+  $res=~m%<url>(.*?)</url>%isg;
+
+  # write it to file
+  append_file("$1\n","err.txt");
+}
+
+
 
