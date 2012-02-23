363ec6883b5ba0c1f16c45d431a4b7b9836124bd
Barry Carter <Barry Carter>
2012-02-23 08:21:33 -0700

    Even better triangle.

--- bc-triangle-man.m
+++ bc-triangle-man.m
@@ -1,9 +1,14 @@
-(* An attempt to find all quantities for a given unscaled triangle in free space *)
+(* An attempt to find all quantities for a given unscaled triangle in
+free space *)
 
 (* Given any unscaled triangle in free space, you can resize it,
 reflect it and place it on the complex plane such that two vertices
-are 0 and 1, and the other point is x+yi, where x>=0 y>=0. We
-therefore let x+yi 'define' the triangle in question * )
+are 0 and 1, and the other point is z (where Re(z) and Im(z) are both
+nonnegative. We therefore let z 'define' the triangle in question * )
+
+(* Reference triangle: triangle-in-complex-plane.png *)
+
+(* Sample triangle: 1/6 + 3/5*i, sort of like reference triangle *)
 
 
 
