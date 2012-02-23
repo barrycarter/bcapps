40016db4ee48d70b69da40f45e5af912d92b3dbc
Barry Carter <Barry Carter>
2012-02-23 08:01:51 -0700

    Triangle man ho!

---
+++ bc-triangle-man.m
@@ -0,0 +1,10 @@
+(* An attempt to find all quantities for a given unscaled triangle in free space *)
+
+(* Given any unscaled triangle in free space, you can resize it,
+reflect it and place it on the complex plane such that two vertices
+are 0 and 1, and the other point is x+yi, where x>=0 y>=0. We
+therefore let x+yi 'define' the triangle in question * )
+
+
+
+
