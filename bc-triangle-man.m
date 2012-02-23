cd33bf41731a47cf7f36340000d8df2d8f0f6868
Barry Carter <Barry Carter>
2012-02-23 10:46:21 -0700

    Checkpoint save

--- bc-triangle-man.m
+++ bc-triangle-man.m
@@ -7,7 +7,7 @@ free space *)
 (* Given any unscaled triangle in free space, you can resize it,
 reflect it and place it on the complex plane such that two vertices
 are 0 and 1, and the other point is z (where Re(z) and Im(z) are both
-nonnegative. We therefore let z 'define' the triangle in question * )
+nonnegative. We therefore let z 'define' the triangle in question *)
 
 (* Reference triangle: triangle-in-complex-plane.png *)
 
@@ -31,64 +31,24 @@ lena[z_] = Abs[1-z]
 
 line[z1_,z2_] = Function[z1 + #1*(z2-z1)]
 
-(* intersection of two lines *)
-
+(* intersection of two lines, by 4 points *)
 (* Re and Im here is ugly! *)
 
-Solve[{
- Re[line[z1,z2][t]] == Re[line[z3,z4][u]], 
- Im[line[z1,z2][t]] == Im[line[z3,z4][u]]
-}, {u,t}, Reals]
+intersectionbypts[z1_,z2_,z3_,z4_] = z1 + (z2-z1)*
+(t /. Flatten[Solve[{
+ Re[z1] + t*(Re[z2] - Re[z1]) ==  Re[z3] + u*(Re[z4] - Re[z3]),
+ Im[z1] + t*(Im[z2] - Im[z1]) ==  Im[z3] + u*(Im[z4] - Im[z3])},
+{t,u}]])
 
-(* generic *)
+(* intersection of lines by formula *)
 
-Solve[z1 + t*z2 == z3 + u*z4, {t,u}]
+intersect[line1_, line2_] =
+intersectionbypts[line1[0],line1[1],line2[0],line2[1]]
 
-Solve[
+test1[line1_, line2_] = {line1[0],line1[1],line2[0],line2[1]}
 
-Solve[{
-Re[z1 + t*z2] == Re[z3 + u*z4],
-Im[z1 + t*z2] == Im[z3 + u*z4]},
-{t,u}, Reals]
-
-Solve[{
-Re[z1] + t*Re[z2] == Re[z3] + u*Re[z4],
-Im[z1] + t*Im[z2] == Im[z3] + u*Im[z4]},
-{t,u}]
-
-Solve[{
-z1r + t*z2r == z3r + u*z4r,
-z1i + t*z2i == z3i + u*z4i},
-{t,u}, Reals]
-
-Solve[{line[z1,z2][t] == line[z3,z4][u], Im[t]==0, Im[u]==0},
- {t,u}]
-
-Solve[line[z1,z2][t] == line[z3,z4][u], t]
-
-Eliminate[line[z1,z2][t] == line[z3,z4][u], t]
-
-
-
-
-
-
-
-
-I + 3t and -t-tI
-
-I + 3*(t /. Flatten[Solve[I + 3t == -u-u*I,{t,u},Reals]])
-
-Solve[I + 3t == -u-u*I,{t},Reals]
-
-intersect[line1_,line2_] = Solve[line1[z][t] == line2[z][u]]
-
-line[0,(z+1)/2]
-line[1,z/2]
-
-Solve[line[0,(z+1)/2][t] == line[1,z/2][u]]
-
-Solve[sidea[test][t] == sideb[test][u]]
+intersectionbypts[sidea[test][0],sidea[test][1],sideb[test][0],sideb[test][1]]
+intersect[sidea[test],sideb[test]]
 
 (* the side formulas *)
 
