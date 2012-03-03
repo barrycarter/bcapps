788dcbb0de4099aadf8e7bf4badc29cd949edfab
Barry Carter <Barry Carter>
2012-03-03 10:47:06 -0700

    Checkpoint save

--- sqlite32rss.pl
+++ sqlite32rss.pl
@@ -4,13 +4,21 @@
 # doesn't work if query has tables inside it
 require "bclib.pl";
 
+print << "MARK";
+<?xml version="1.0" encoding="ISO-8859-1" ?><rss version="0.91">
+<channel><title>title</title><description>channel desc</description>
+
+MARK
+;
+
+
 while (<STDIN>) {
   # start of new item, which is also title
   # note that table inside description must be escaped
   s%^<tr><td>(.*?)</td>$%<item><title>$1</title><description>&lt;table border&gt;&lt;tr&gt;%i;
 
   # end of item
-  s%^</tr>$%&lt;/&lt;/tr&gt;table&gt;</description><item>%i;
+  s%^</tr>$%&lt;/&lt;/tr&gt;table&gt;</description></item>%i;
 
   # all else is part of the description (as a table cell w/ escaped HTML)
   if (/^<td>/i) {
@@ -20,3 +28,8 @@ while (<STDIN>) {
 
   print $_;
 }
+
+print << "MARK";
+</channel></rss>
+MARK
+;
