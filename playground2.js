24a14bceaa6e9d3a4f59aa89bdeaecd619b86773
Barry Carter <Barry Carter>
2012-03-04 10:27:49 -0700

    Moving things w/ JavaScript

--- playground2.js
+++ playground2.js
@@ -1,6 +1,3 @@
-<meta http-equiv="refresh" content="60">
-Last updated: Sun Mar 4 16:30:03 GMT 2012
-<!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
@@ -25,10 +22,10 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById("map_canvas"),
       myOptions);
 
-// BEGIN INSERTED CODE
-
 pt = new google.maps.LatLng(-6.16453214907868,297.19423018679);
 ap = new google.maps.LatLng(6.16453214907868,477.19423018679);
+
+test = new google.maps.LatLng(35,-106);
 
 new google.maps.Marker({
  position: pt,
@@ -44,291 +41,32 @@ new google.maps.Marker({
  title:"Nemesis"
 });
 
+var light = new Array();
+var dark = new Array();
 
-new google.maps.Circle({
+for (r=1; r<=15; r++) {
+
+   light.push(new google.maps.Circle({
  center: pt,
- radius: 10018760,
- map: map,
- strokeWeight: 2,
- fillOpacity: 0.2,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 10018760,
- map: map,
- strokeWeight: 1,
- fillOpacity: 0.2,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 9350842.66666667,
+ radius: 667917*r,
  map: map,
  strokeWeight: 0.1,
  fillOpacity: 0.01,
  fillColor: "#ffffff"
-});
+		  }));
 
-new google.maps.Circle({
+ dark.push(new google.maps.Circle({
  center: ap,
- radius: 9350842.66666667,
+ radius: 667917*r,
  map: map,
  strokeWeight: 1,
  fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 8682925.33333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
  fillColor: "#ffffff"
-});
+}));
 
-new google.maps.Circle({
- center: ap,
- radius: 8682925.33333333,
- map: map,
- strokeWeight: 1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
+}
 
-
-new google.maps.Circle({
- center: pt,
- radius: 8015008,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 8015008,
- map: map,
- strokeWeight: 1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 7347090.66666667,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 7347090.66666667,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 6679173.33333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 6679173.33333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 6011256,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 6011256,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 5343338.66666667,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 5343338.66666667,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 4675421.33333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 4675421.33333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 4007504,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 4007504,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 3339586.66666667,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 3339586.66666667,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 2671669.33333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 2671669.33333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 2003752,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 2003752,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 1335834.66666667,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 1335834.66666667,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
-
-new google.maps.Circle({
- center: pt,
- radius: 667917.333333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0.01,
- fillColor: "#ffffff"
-});
-
-new google.maps.Circle({
- center: ap,
- radius: 667917.333333333,
- map: map,
- strokeWeight: 0.1,
- fillOpacity: 0,
- fillColor: "#000000"
-});
-
+dark[7].setCenter(test);
 
 mpt = new google.maps.LatLng(17.3185462836248,68.9703629502967);
 
