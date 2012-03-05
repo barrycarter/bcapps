246395ae911ab60951880b9411c46461087fdcba
Barry Carter <Barry Carter>
2012-03-05 10:18:31 -0700

    checkpoint <h>(still not broken!)</h>

--- playground2.js
+++ playground2.js
@@ -1,5 +1,6 @@
 <html>
 <head>
+
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 <style type="text/css">
   html { height: 100% }
@@ -11,7 +12,13 @@
 </script>
 <script type="text/javascript">
 
+// <h>TODO: use solar and lunar as much as possible to sound more
+// scientifical... but not terran, that's just snobby</h>
+
 var startime = new Date();
+
+// terran circumference, in meters
+earthcirc = 40075040;
 
 function initialize() {
   var myLatLng = new google.maps.LatLng(0,0);
@@ -27,8 +34,12 @@ function initialize() {
 // TODO: get these numbers from true source
 sunlon = 297.19423018679;
 sunlat = -6.16453214907868;
+moonlat = 17.3185462836248;
+moonlon = 68.9703629502967;
+
 pt = new google.maps.LatLng(sunlat,sunlon);
 ap = new google.maps.LatLng(-1*sunlat,sunlon+180.);
+mpt = new google.maps.LatLng(moonlat,moonlon);
 
 // the longitude change of the sun/moon per second assuming their
 // ra/dec don't change (ie, 360 degrees in 24 hours)
@@ -48,15 +59,24 @@ nem = new google.maps.Marker({
  title:"Nemesis"
 });
 
+
+moon = new google.maps.Marker({
+ position: mpt,
+ map: map,
+ Icon: "http://test.barrycarter.info/moon.png",
+ title:"Moon"
+});
+
+
 var light = new Array();
 var dark = new Array();
 
-for (r=1; r<=15; r++) {
+for (r=0; r<=14; r++) {
  var nowtime = new Date();
    light.push(new google.maps.Circle({
  center: pt,
  // TODO: make this value more accurate!
- radius: 667917*r,
+ radius: earthcirc/60*(r+1),
  map: map,
  strokeWeight: 0.1,
  fillOpacity: 0.01,
@@ -65,7 +85,7 @@ for (r=1; r<=15; r++) {
 
  dark.push(new google.maps.Circle({
  center: ap,
- radius: 667917*r,
+ radius: earthcirc/60*r,
  map: map,
  strokeWeight: 1,
  fillOpacity: 0,
@@ -74,30 +94,9 @@ for (r=1; r<=15; r++) {
 
 }
 
-function moveme() {
- // how much time has elapsed since program started (seconds)
- nowtime = (new Date()-startime)/1000;
+// the moon has just one circle: rise/set
 
- // new solar longitude (latitude doesn't change) and antisun point
- newsun = new google.maps.LatLng(sunlat,sunlon-nowtime*move);
- newnem = new google.maps.LatLng(-1*sunlat,sunlon-nowtime*move+180);
-
- // move sun and antisun
- sun.setPosition(newsun);
- nem.setPosition(newnem);
-
- // and then the surrounding "ripples"
- for (r=1; r<=15; r++) {
-  light[r].setCenter(newsun);
-  dark[r].setCenter(newnem);
-}
-}
-
-window.setInterval(moveme, 1000);
-
-mpt = new google.maps.LatLng(17.3185462836248,68.9703629502967);
-
-new google.maps.Circle({
+mooncirc = new google.maps.Circle({
  center: mpt,
  radius: 10018760,
  map: map,
@@ -107,14 +106,31 @@ new google.maps.Circle({
  fillColor: "#ffffff"
 });
 
-new google.maps.Marker({
- position: mpt,
- map: map,
- Icon: "http://test.barrycarter.info/moon.png",
- title:"Moon"
-});
+function moveme() {
+ // how much time has elapsed since program started (seconds)
+ nowtime = (new Date()-startime)/1000;
 
+ // new solar/lunar/antisun longitude (latitude doesn't change)
+ newsun = new google.maps.LatLng(sunlat, sunlon-nowtime*move);
+ newnem = new google.maps.LatLng(-1*sunlat, sunlon-nowtime*move+180);
+ newmoon = new google.maps.LatLng(moonlat, moonlon-nowtime*move);
 
+ // move sun and antisun
+ sun.setPosition(newsun);
+ nem.setPosition(newnem);
+ moon.setPosition(newmoon);
+
+ // move moon circle
+ mooncirc.setCenter(newmoon);
+
+ // and then the surrounding "ripples"
+ for (r=0; r<=14; r++) {
+  light[r].setCenter(newsun);
+  dark[r].setCenter(newnem);
+}
+}
+
+window.setInterval(moveme, 1000);
 }
 
 </script>
