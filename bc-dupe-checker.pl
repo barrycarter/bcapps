9f9f724f1a70c16a4df23c82dbb4319f1220d1ba
Barry Carter <Barry Carter>
2012-03-07 11:22:40 -0700

    Quick and dirty script to get rid of dupes using sha1sum

---
+++ bc-dupe-checker.pl
@@ -0,0 +1,34 @@
+#!/bin/perl
+
+# given a file with the (unsorted) output of sha1sum, list duplicate files
+# fairly trivial to write
+
+require "bclib.pl";
+
+chdir(tmpdir());
+
+system("sort $ARGV[0] > sha1sorted.txt");
+$sha1s = read_file("sha1sorted.txt");
+
+for $i (split(/\n/, $sha1s)) {
+  # confirm the line is really sha1 or md5 followed by filename
+  unless ($i=~/^([0-9a-f]{32})\s+(.*)$/ || $i=~/^([0-9a-f]{40})\s+(.*)$/) {
+    warnlocal("BAD LINE: $i");
+    next;
+  }
+
+  # otherwise, record sha1 and filename
+  ($sha1, $file) = ($1, $2);
+
+  # are we seeing the same sha1 again?
+  if ($sha1 eq $cur) {
+    print "'$file'\n";
+    next;
+  }
+
+  # not a match, so this is the new current
+  $cur = $sha1;
+
+#  debug("I: $i");
+}
+
