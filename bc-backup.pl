4a0f85a23064da99138fbf5566092eb56813c231
Barry Carter <Barry Carter>
2012-03-09 13:49:01 -0700

    More specs, no code <h>(the way god intended it!)</h>

--- bc-backup.pl
+++ bc-backup.pl
@@ -1,5 +1,7 @@
 #!/bin/perl
 
+# --hostid: use this hostid, not the one from `hostid`
+
 # This is a shell <h>(as in snails, not tcsh)</h> for a backup program
 # with these properties:
 #
@@ -7,8 +9,25 @@
 #   - saves files based on sha1 (duplicate files stored just once)
 #   - can backup a single directory, but keeps track of what's been backed up globally
 #   - versioning
+#   - creates backup "chunks" of a given size (eg, fit to DVD), and remembers what its backed up
 
 # Tried this in sqlite3 once, but sense postgresl/mysql or flat files
 # might be better
 
 # bc-filename-encrypt.pl will become a part of this, most likely
+
+# TODO: sort/uniq can cleanup dupes when needed
+
+require "bclib.pl";
+
+=item schema
+
+Data we store (whether in an SQL table or flat file):
+
+filedata: hostid path mtime sha1sum
+
+backupdata: sha1sum where-stored
+
+symlinks: hostid path mtime target (merge w/ filedata?)
+
+=cut
