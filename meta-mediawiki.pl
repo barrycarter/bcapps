4e83def7ece72e43861d215aae1dd28e3d837aad
Barry Carter <Barry Carter>
2012-03-13 16:29:42 -0600

    Checkpoint save

--- meta-mediawiki.pl
+++ meta-mediawiki.pl
@@ -38,9 +38,9 @@ write_file($pagename, "/tmp/meta-".time());
 # mediawikifs not that great, using api
 ($all, $err, $res) = cache_command("curl 'http://$wiki/api.php?action=query&prop=revisions&rvprop=content&format=xml&titles=$pagename'");
 
-# remove XML
-# TODO: can content itself have XML? (if so, need to fix this!)
-$all=~s/<.*?>//isg;
+# remove XML (hopefully no embedded <rev> tags)
+$all=~m%<rev[> ].*?>(.*?)</rev>%is;
+$all = $1;
 
 TEST:
 
@@ -52,12 +52,13 @@ $all = "[[$pagename!!$all]]";
 # needs to be protected
 
 # TODO: this matches [[foo}} (which it shouldn't)
+# this builds %add which tells us what to add to which pages
 while 
   ($all=~s/(\[\[?|\{\{?)([^\[\]\{\}]*?)(\]\]?|\}\}?)/parse_text($1,$2,$3)/iseg) {
   $round++;
 }
 
-# do the same for all additions to all pages
+# "add" things to pages as needed (actually, replace existing section)
 for $i (sort keys %add) {
   @add = @{$add{$i}};
 
@@ -67,6 +68,10 @@ for $i (sort keys %add) {
     $j=~s/<<p(\d+)>>/convert_text($text[$1])/iseg;
   }
 
+  # content generated from $pagename for page $i
+  # TODO: improve this
+  $content = join("<br>",@add);
+
   # and reset @{$add{$i}}
   # TODO: modify list directly, no middle step
   @{$add{$i}} = @add;
@@ -75,11 +80,13 @@ for $i (sort keys %add) {
   # TODO: escape title if needed
   $iurl = urlencode($i);
   ($page, $err, $res) = cache_command("curl 'http://$wiki/api.php?action=query&prop=revisions&rvprop=content&format=xml&titles=$iurl'");
-  $page=~s/<.*?>//isg;
+  $page=~m%<rev[> ].*?>(.*?)</rev>%is;
 
-  # TODO: this will eventually REPLACE existing psuedosection, not add
-  # to page directly
-  $page = "{$pagename}". join("\n",@add) . "{/$pagename}\n";
+  # Case 1: this page had previously created a section and will replace it
+  unless ($page=~s%(<$pagename>)(.*?)(</$pagename>)%$1$content$2%) {
+    # Case 2: didn't already have it, so add it
+    $page = "$page\n<$pagename>$content</$pagename>\n";
+  }
 
   $res = write_wiki_page_anon($wiki, $i, $page, "AUTO");
 
@@ -89,67 +96,6 @@ for $i (sort keys %add) {
   debug("ADD($i)", @add);
 
 # die "TESTING";
-}
-
-die "TESTING";
-
-# db queries to update table
-# delete anything this page created previously
-push(@query, "DELETE FROM metatab WHERE creator='$pagename'");
-
-# TODO: could combine w/ above?
-for $i (sort keys %add) {
-  for $j (@{$add{$i}}) {
-    # convert ' to '' for sqlite3
-    $j=~s/\'/''/isg;
-    push(@query, "INSERT INTO metatab (creator, page, info) VALUES
-    ('$pagename', '$i', '$j')");
-  }
-}
-
-# TODO: make this db more permanent
-unshift(@query, "BEGIN");
-push(@query,"COMMIT;");
-$querys = join(";\n", @query);
-write_file($querys, "/tmp/bcmm.txt");
-system("sqlite3 /tmp/wikithing.db < /tmp/bcmm.txt");
-
-# query all pages that this page affected
-for $i (sort keys %add) {
-  push(@affected, "'$i'");
-}
-
-# TODO: := not working right, probably minor fix
-
-$affected = join(", ", @affected);
-
-# rebuild all affected pages
-$query="SELECT * FROM metatab WHERE page IN ($affected) ORDER BY page,creator";
-@res = sqlite3hashlist($query, "/tmp/wikithing.db");
-
-debug("ABOUT TO WRITE");
-
-for $i (@res) {
-  %hash = %{$i};
-  # add section to current page from source page
-  push (@{$page{$hash{page}}}, "From $hash{creator}: $hash{info}");
-}
-
-for $i (sort keys %page) {
-  # TODO: figure out why below happens and fix it
-  if ($i=~/^\s*$/) {next;}
-  $curpage = join("\n\n", @{$page{$i}});
-
-  debug("I: $i");
-  ($out, $err, $res) = 
-#    write_wiki_page("http://wiki.barrycarter.info/api.php", $i, $curpage, "",
-#		  $bcwiki{user}, $bcwiki{pass});
-
-    write_wiki_page("http://carterpeanuts.wikia.com/api.php", $i, $curpage, "",
-		  $wikia{user}, $wikia{pass});
-
-  debug("$out/$err/$res");
-
 }
 
 sub parse_text {
@@ -191,6 +137,8 @@ sub parse_text {
     # p$n indicates "parsed once already"
     $originfo=~s/<<s(\d+)>>/<<p$1>>/isg;
     $info=~s/<<s(\d+)>>/$text[$1]/isg;
+
+    # %add is a global hash (of lists)
     push(@{$add{$page}}, $info);
     return $originfo;
   }
@@ -216,7 +164,9 @@ sub convert_text {
   return $text;
 }
 
-# write wiki page when no login is required (faster)
+# Retursn the command to write wiki page when no login is required
+# (faster). Results can be run using GNU parallel
+
 # TODO: limit anon writes to 127.0.0.1
 
 sub write_wiki_page_anon {
@@ -232,7 +182,7 @@ sub write_wiki_page_anon {
   write_file("action=edit&title=$page&text=$newcontent&summary=$comment&token=%2B\\", $tmpfile);
 
   # can't cache this command, but using cache_command to get vals
-  return cache_command("curl '$wiki/api.php' -d \@$tmpfile");
+  return "curl '$wiki/api.php' -d \@$tmpfile";
 }
 
 
