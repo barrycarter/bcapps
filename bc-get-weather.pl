3fe1414b5a6ed1f96cc6f70d29c79db69b7754e1
Barry Carter <Barry Carter>
2012-03-17 11:59:29 -0600

    Now the moon, the hard way

--- bc-get-weather.pl
+++ bc-get-weather.pl
@@ -6,28 +6,86 @@ require "bclib.pl";
 # below is demo key, not my personal key
 $key = "1bf62599411c7c17";
 
-# obtain + JSON parse data
-($out, $err, $res) = cache_command("curl http://api.wunderground.com/api/$key/conditions/forecast/astronomy/q/KABQ.json", "age=60");
+# obtain + JSON parse data (caching solely for testing)
+if ($globopts{test}) {$age=300;} else {$age=-1;}
+($out, $err, $res) = cache_command("curl http://api.wunderground.com/api/$key/conditions/forecast/astronomy/q/KABQ.json", "age=$age");
 $json = JSON::from_json($out);
 
 # <h>intermediate variables are for sissies!</h>
 for $i (@{$json->{forecast}->{simpleforecast}->{forecastday}}) {
 
-  debug("PER: $i->{period}");
-
   # for now, only want today/tomorrow forecost
   if ($i->{period} > 2) {next;}
 
   # I want date:conditions/hi/lo/%pop (probability of precipitation)
-  push(@str, join("", $i->{date}->{day},":",$i->{conditions},"/",
+  push(@forecast, join("", $i->{date}->{day},":",$i->{conditions},"/",
 	      $i->{high}->{fahrenheit}, "/", $i->{low}{fahrenheit}, "/",
 	      $i->{pop},"%"));
 }
 
-debug("STR",@str);
+# now, current: conditions/temp/wc/humid (dewpt)
 
-# next two days
-$str = join(",",@str);
+  $current = join("",
+		$json->{current_observation}->{weather}, "/",
+		$json->{current_observation}->{temp_f}, "/",
+		$json->{current_observation}->{windchill_f}, "F/",
+		$json->{current_observation}->{relative_humidity}, " (",
+		$json->{current_observation}->{dewpoint_f}, "F)"
+	       );
+
+# time of obs (compacted w/ time first)
+$time = $json->{current_observation}->{observation_time};
+# get rid of useless string and tz
+$time=~s/^last updated on //isg;
+# this IS case sensitive
+$time=~s/\s*[A-Z]+$//;
+$time=~s/^(.*?),\s*(.*?)$/$2, $1/;
+
+# <h>wunderground keeps sunrise/set under moon_phase, interesting</h>
+
+debug(sort keys %{$json->{moon_phase}});
+
+# sun and moon rise/set
+debug(unfold($json->{moon_phase}));
+
+# sun line ("0" below is major chat, since sun always rises between 4-8)
+$sun = join("", 
+		"S:0", $json->{moon_phase}->{sunrise}->{hour},
+		$json->{moon_phase}->{sunrise}->{minute}, "-",
+		$json->{moon_phase}->{sunset}->{hour},
+		$json->{moon_phase}->{sunset}->{minute}
+);
+
+# wind and pressure (dir spdGgust (press))
+# treat no gust special
+unless ($json->{current_observation}->{wind_gust_mph}) {
+  $json->{current_observation}->{wind_gust_mph} = "";
+}
+
+$wind = join("", 
+	     $json->{current_observation}->{wind_dir}, " ",
+	     $json->{current_observation}->{wind_mph}, "G",
+	     $json->{current_observation}->{wind_gust_mph}, " (",
+	     $json->{current_observation}->{pressure_in}, "in)"
+);
+
+# TODO: moonrise/set (wunderground does NOT give these)
+
+# print in order I want (even if I change mind later)
+$str = << "MARK";
+$time
+$sun
+M: NA
+$wind
+$current
+$forecast[0]
+$forecast[1]
+MARK
+;
+
+print $str;
 
 # and write to file
 write_file($str, "/home/barrycarter/ERR/forecast.err");
+
+# TODO: add tz stuff here (since I do it nowhere else)
