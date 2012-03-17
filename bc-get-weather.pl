c61916c5dfb82c658ad2b9c5d3031c26349c9ee0
Barry Carter <Barry Carter>
2012-03-17 10:56:17 -0600

    Improving

--- bc-get-weather.pl
+++ bc-get-weather.pl
@@ -6,7 +6,28 @@ require "bclib.pl";
 # below is demo key, not my personal key
 $key = "1bf62599411c7c17";
 
+# obtain + JSON parse data
 ($out, $err, $res) = cache_command("curl http://api.wunderground.com/api/$key/conditions/forecast/astronomy/q/KABQ.json", "age=60");
+$json = JSON::from_json($out);
+
+# debug(unfold($json));
+
+# debug(keys %{$json->{forecast}});
+
+# <h>intermediate variables are for sissies!</h>
+for $i (@{$json->{forecast}->{simpleforecast}->{forecastday}}) {
+  debug("I: $i");
+}
+
+# debug(%{$json}{forecast});
+
+for $i (keys %{$json{forecast}}) {
+  debug("KEY: $i");
+}
+
+# debug($json{simpleforecast});
+
+die "TESTING";
 
 # main goal here it to minimize what I print and how
 
