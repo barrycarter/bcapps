a54ca4055052af07383a66b64c80d2d31f06cd57
Barry Carter <Barry Carter>
2012-03-21 18:35:58 -0600

    Getting closer...

--- bc-alert-sherpa.pl
+++ bc-alert-sherpa.pl
@@ -10,19 +10,32 @@ require "bclib.pl";
 %name2surl = get_strips();
 %name2gurl = get_gocomics();
 
-debug(sort keys %name2gurl);
-debug("SPLIT");
-debug(sort keys %name2surl);
+# debug(sort keys %name2gurl);
+# debug("SPLIT");
+# debug(sort keys %name2surl);
 
 # determine the gurl of each surl using transitivity
 
 for $i (sort keys %name2surl) {
+#  debug("$i, $name2gurl{$i}");
   $surl2gurl{$name2gurl{$i}} = $name2surl{$i};
-  debug("$i s-> $name2surl{$i}");
-  debug("$i g-> $name2gurl{$i}");
+#  debug("$i s-> $name2surl{$i}");
+#  debug("$i g-> $name2gurl{$i}");
 }
 
-debug("ALPHA",%surl2gurl);
+# debug("ALPHA",%surl2gurl);
+
+# now, get the db.94y.info URL that yields the recent comments for a
+# given strip
+
+for $i (sort keys %surl2gurl) {
+  debug("I: $i");
+  # got this by using sniffit
+  $post = "query=SELECT+body%2C%22http%3A%2F%2Fgocomics.com%2F%22%7C%7Cstrip%7C%7C%22%2F%22%7C%7Cyear%7C%7C%22%2F%22%7C%7CSUBSTR%28%220%22%7C%7Cmonth%2C-2%2C2%29%7C%7C%22%2F%22%7C%7CSUBSTR%28%220%22%7C%7Cdate%2C-2%2C2%29+AS+url+FROM+comments+WHERE+strip%3D%27$i%27+ORDER+BY+timestamp+DESC+LIMIT+50+";
+  # do NOT redirect here, we want the Location: URL
+  $cmd = "curl -D - -d '$post' http://gocomics.db.barrycarter.info/";
+  print "$cmd\n";
+}
 
 # experimenting with extreme subroutining
 
@@ -51,7 +64,7 @@ sub get_gocomics {
   my($page) = cache_command("curl -A 'ikinhasagent\@barrycarter.info' http://www.gocomics.com/explore/sherpa_list", "age=86400");
 
   # list of comics (title and URL)
-  while ($page=~s%/(.*?)\" class=\"alpha_list\">(.*?)</a>%%) {
+  while ($page=~s%/(.*?)\" class=\".*?\">(.*?)</a>%%) {
     $res{$2} = $1;
   }
 
