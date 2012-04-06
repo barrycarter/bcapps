9cfd4168a523c7773b9892f6c7c92385040e5091
Barry Carter <Barry Carter>
2012-04-05 21:25:17 -0600

    Print first, randomize later

--- GEOLOCATION/bc-random-c.pl
+++ GEOLOCATION/bc-random-c.pl
@@ -1,11 +1,19 @@
 #!/bin/perl
 
-# Generates random addresses in EVERY "class C" network, with the
+# Generates '''random''' addresses in EVERY "class C" network, with the
 # intent of hitting every major ISP; of course, we now use CIDR, so
 # "class C" is meaningless
 
 push(@INC,"/usr/local/lib");
 require "bclib.pl";
 
-@l = (0..256**3);
-@l = randomize(\@l);
+for $i (0..255) {
+  for $j (0..255) {
+    for $k (0..255) {
+      $l = int(rand()*256);
+      # sorting by this random number will randomize the entries later
+      $rand = rand();
+      print "$rand $i.$j.$k.$l\n";
+    }
+  }
+}
