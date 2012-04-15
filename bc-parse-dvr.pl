1cf5f58729caafb0ec1bac2fd953800370d1c0bb
Barry Carter <Barry Carter>
2012-04-15 07:34:58 -0600

    Checkpoint save

---
+++ bc-parse-dvr.pl
@@ -0,0 +1,11 @@
+#!/bin/perl
+
+# I use i.tech's mdvr to record TV shows; however, the timer feature
+# works poorly, so I just record 8-hour "batches" of shows, and this
+# program parses them
+
+# This is another program that's almost entirely for me, but I'm
+# putting it into github JFF
+
+require "bclib.pl";
+
