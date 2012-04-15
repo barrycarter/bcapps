beef60c50ebe16135ca5d93718803aece1f8feea
Barry Carter <Barry Carter>
2012-04-15 09:20:29 -0600

    Minor fixups

--- bc-parse-dvr.pl
+++ bc-parse-dvr.pl
@@ -16,18 +16,11 @@ use Date::Manip;
 
 # debug(DateCalc(ParseDate("14 Apr 2012"), "next Saturday"));
 # debug(Date_GetNext("14 Apr 2012", "Sat", 1, 19));
-
-debug(Date_GetNext("epoch 1234", "Sat", 1, 19));
-
-
-
-
-die "TESTING";
+# debug(Date_GetNext("epoch 1234.56", "Sat", 1, 19));
+# debug(Date_GetNext("epoch 1334449133", "Sat", 1, 19, 30));
 
 # buffer, in seconds, around each "chunk"
 $buffer = 180;
-
-debug(strftime("%H:%M:%S", gmtime(100)));
 
 # file (fixed for now)
 $file = "/var/tmp/mDVR001.3GP";
@@ -40,8 +33,36 @@ if ($out=~/ID_LENGTH=(.*?)\n/is) {$len=$1;} else {die "No length?";}
 # the timestamp is the end time
 ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$sizefile, $atime,$mtime,$ctime,$blksize,$blocks) = stat($file);
 
-# so start time is...
-$stime = $mtime-$len;
+# so start time is... (using round to make Date::Manip happy)
+$stime = round($mtime-$len);
+
+# for each line in bc-parse-dvr.txt, determine if this chunk contains
+# that timeframe
+@lines = split(/\n/, read_file("/home/barrycarter/BCGIT/bc-parse-dvr.txt"));
+
+for $i (@lines) {
+  # ignore comments/blanks
+  if ($i=~/^\#/ || $i=~/^\s*$/) {next;}
+
+  ($show, $day, $time, $len) = split(/\s+/, $i);
+  $len||=30;
+
+  # split time into hhmm for Date::Manip
+  $time=~/^(\d{2})(\d{2})$/||die "BAD TIME: $time";
+  ($hour, $min) = ($1, $2);
+  debug("HOUR: $hour");
+
+  # Date::Manip oddness?
+  $day = ucfirst($day);
+
+  # next time this show starts
+  debug("SENDING: $stime, $day, $hour, $min");
+  $next = Date_GetNext("epoch $stime", $day, 1, $hour, $min);
+
+#  debug("$show, $day, $time, $len, $next");
+}
+
+die "TESTING";
 
 # and length is (presumably read from file)
 $length = 30*60 + 2*$buffer;
