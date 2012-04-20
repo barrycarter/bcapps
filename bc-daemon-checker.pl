3913424351c2dad960e97fff94791c34b8da6413
Barry Carter <Barry Carter>
2012-04-20 09:38:44 -0600

    Almost working 100%

--- bc-daemon-checker.pl
+++ bc-daemon-checker.pl
@@ -4,6 +4,8 @@
 # running too long (eg, a long 'curl' run can hang a program waiting
 # for it to end; timed-run would be another solution)
 
+# Runs from cron (so doesn't need to be in @must below)
+
 push(@INC,"/usr/local/lib");
 require "bclib.pl";
 
@@ -15,6 +17,45 @@ shift(@procs); # ignore header line
 
 # TODO: turn this into an external file list or something
 
+# processes that MUST always be running (I should be able to trim
+# down this list?)
+
+# NOTE: full path varies because of the way these procs start
+
+@must = (
+	 "init", "syslogd", "klogd", "/usr/sbin/sshd", "ntpd",
+	 "/usr/libexec/mysqld", "/usr/libexec/postfix/master", "qmgr",
+	 "crond", "/usr/sbin/atd", "/sbin/mingetty",
+	 "/usr/local/bin/bc-voronoi-temperature.pl",
+	 "/usr/local/bin/bc-delaunay-temperature.pl",
+	 "/usr/local/bin/bc-metar-db.pl", 
+	 "/usr/local/bin/bc-gocomics-comments.pl",
+	 "/usr/sbin/lighttpd", "/usr/local/bin/php-cgi",
+	 "teenydns", "pickup"
+	);
+
+# processes that MAY run forever but aren't required to do so (choices
+# like /sbin/udevd are weird, but I don't really care if hotplug is
+# working?)
+
+# "sshd:" represents a specific login; the main daemon must always
+# run, but the client daemon doesn't have to always run (but can if it
+# wants)
+
+@may = (
+	"SCREEN", "screen", "-csh", "sh", "/bin/sh", "/sbin/udevd",
+	"/usr/libexec/gam_server", "sshd:", "-bin/tcsh"
+	);
+
+# Processes on this list must be killed if they run over 5m
+@kill = (
+	 "curl"
+	 );
+
+# easier as hashes
+%must = list2hash(@must);
+%may = list2hash(@may);
+%kill = list2hash(@kill);
 
 for $i (@procs) {
   # cleanup proc line and split into fields
@@ -36,13 +77,50 @@ for $i (@procs) {
   # can't do much w/ defunct procs
   if ($i=~/<defunct>/) {next;}
 
-  debug("PROC: $proc");
+  # if this program is permitted to run forever, but not required, stop here
+  # TODO: add check if process is on two lists?
+  if ($may{$proc}) {next;}
 
+  # if this process must run, record it and continue
+  if ($must{$proc}) {
+    $isproc{$proc}=1;
+    next;
+  }
 
+  # how long has program been running?
+  if ($time=~/^(\d+)\-(\d{2}):(\d{2}):(\d{2})$/) {
+    $sec = $1*86400+$2*3600+$3*60+$4;
+  } elsif ($time=~/^(\d{2}):(\d{2}):(\d{2})$/) {
+    $sec = $1*3600+$2*60+$3;
+  } elsif ($time=~/^(\d{2}):(\d{2})$/) {
+    $sec = $1*60+$2;
+  } else {
+    warnlocal("Can't convert $time into seconds");
+    next;
+  }
 
+  # any process permitted to run up to 5m
+  # TODO: specific limits for procs where 5m is wrong
+  if ($sec<=300) {next;}
 
+  # if I'm allowed to kill this process, do so now
+  if ($kill{$proc}) {
+    system("kill $pid");
+    next;
+  }
 
+  # process is running long, and is neither permitted nor required to
+  # run forever, but I'm now allowed to kill it.... so whine
 
+  push(@err, "$proc ($pid): running > 300s, but no perm to kill");
 }
 
+# confirm all "must" processes are in fact running
+
+for $i (sort keys %MUST) {
+  if ($isproc{$i}) {next;}
+  push(@err, "$i: not running, but is required");
+}
+
+debug(@err);
 
