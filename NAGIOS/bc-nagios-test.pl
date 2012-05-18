b470a14ae762c7cc7ad6a8d3ed0e11157cf88913
Barry Carter <Barry Carter>
2012-05-18 10:03:29 -0600

    Checkpoint save

--- NAGIOS/bc-nagios-test.pl
+++ NAGIOS/bc-nagios-test.pl
@@ -16,8 +16,38 @@ my($bin,$arg) = ($1,$2);
 
 # TODO: allow for non-plugin runs (at which point splitting bin and
 # arg will make sense
-$res = system("$bin $arg");
-
 # >>8 converts Perl exit value to program exit value (kind of)
-exit($res>>8);
+$res = system("$bin $arg")>>8;
+
+# run function on result before returning?
+if ($globopts{func}) {$res = func($globopts{func}, $res);}
+
+exit($res);
+
+=item func($func, $val)
+
+Applies $func to $val, where func is a very simple function.
+
+Used primarily to turn return values of 1 to 0 when needed
+
+=cut
+
+sub func {
+  my($func,$val) = @_;
+
+  # when I want grep to fail, 1 is good, other values are bad
+  # (values like 2 indicate other problems that I do need to be aware of
+  if ($func eq "1is0") {
+    if ($val==1) {return 0;}
+    if ($val==0) {return 2;}
+    return $val;
+  }
+
+  # some scripts return '1' to mean bad, but I want to return 2
+  if ($func eq "1is2") {
+    if ($val==1) {return 2;}
+    return $val;
+  }
+
+}
 
