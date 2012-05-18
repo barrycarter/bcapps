cb97e5dbeb028bad9661802d9e9c69852e02c340
Barry Carter <Barry Carter>
2012-05-17 19:47:25 -0600

    Checkpoint save

--- NAGIOS/bc-nagios-test.pl
+++ NAGIOS/bc-nagios-test.pl
@@ -6,9 +6,20 @@
 push(@INC,"/usr/local/lib");
 require "bclib.pl";
 
-open(A,">/tmp/bc-nagios-test.txt");
-for $i (sort keys %ENV) {
-  print A "$i -> $ENV{$i}\n";
-}
-close(A);
+# what are we being asked to run?
+my($cmd) = $ENV{NAGIOS_ARG1};
+
+# split into command and arguments (removing quotes first)
+$cmd=~s/\"//isg;
+$cmd=~/^\s*(.*?)\s+(.*)$/;
+my($bin,$arg) = ($1,$2);
+
+# TODO: allow for non-plugin runs (at which point splitting bin and
+# arg will make sense
+$run = "/usr/lib/nagios/plugins/$bin $arg";
+write_file("RUN: $run","/tmp/bntd.txt");
+$res = system($run);
+
+# >>8 converts Perl exit value to program exit value (kind of)
+exit($res>>8);
 
