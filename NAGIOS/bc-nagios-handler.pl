e0d5a0601446539b5b8bb596aaf0b6a0d6bfb771
Barry Carter <Barry Carter>
2012-05-18 07:21:37 -0600

    Checkpoint save; about to remove fake tests

--- NAGIOS/bc-nagios-handler.pl
+++ NAGIOS/bc-nagios-handler.pl
@@ -3,10 +3,17 @@
 # This script will handle all nagios notifications (currently empty)
 
 # Important environment variables
+
+# hosts
 # NAGIOS_HOSTADDRESS -> 192.168.0.7
 # NAGIOS_HOSTALIAS -> bcpc
 # NAGIOS_HOSTSTATE -> UP
 # NAGIOS_HOSTSTATEID -> 0
+
+# services
+# NAGIOS_SERVICEDESC -> check_disk
+# NAGIOS_SERVICESTATE -> CRITICAL
+# NAGIOS_SERVICESTATEID -> 2
 
 push(@INC,"/usr/local/lib");
 require "bclib.pl";
@@ -15,6 +22,13 @@ require "bclib.pl";
 if ($ENV{NAGIOS_HOSTSTATEID}) {
   $fbase = "/home/barrycarter/ERR/nagios.$ENV{NAGIOS_HOSTALIAS}";
   write_file("nagios.$ENV{NAGIOS_HOSTALIAS}.down", "$fbase.new");
+  system("mv $fbase.err $fbase.old; mv $fbase.new $fbase.err");
+}
+
+# if service is down...
+if ($ENV{NAGIOS_SERVICESTATEID}) {
+  $fbase = "/home/barrycarter/ERR/nagios.$ENV{NAGIOS_HOSTALIAS}.$ENV{NAGIOS_SERVICEDESC}";
+  write_file("nagios.$ENV{NAGIOS_HOSTALIAS}.$ENV{NAGIOS_SERVICEDESC}.down", "$fbase.new");
   system("mv $fbase.err $fbase.old; mv $fbase.new $fbase.err");
 }
 
