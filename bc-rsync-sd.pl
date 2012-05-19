e50f6737bc3434cc50a1eea15e5326b52e290487
Barry Carter <Barry Carter>
2012-05-19 10:15:05 -0600

    Checkpoint save

--- bc-rsync-sd.pl
+++ bc-rsync-sd.pl
@@ -15,43 +15,51 @@ my($source, $target) = @ARGV;
 # device and mount point
 my($dev, $dir) = ("/dev/sdb1", "/mnt/usbext/");
 
-# is device mounted? (this is a very kludgey way to check, but 'mount'
-# sometimes gives me bad answers, not sure why
-($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$sizefile, $atime,$mtime,$ctime,$blksize,$blocks) = stat("/");
-$rootdev = $dev;
-($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$sizefile, $atime,$mtime,$ctime,$blksize,$blocks) = stat($dir);
-$dirdev = $dev;
-
-debug("$dirdev vs $rootdev");
-
-die "TESTING";
-
-
-
-# start out by unmounting
-$res = system("sudo umount $dev")>>8;
-debug("RES: $res");
-die "TESTING";
-$res = system("sudo mount $dev $dir")>>8;
-die "TESTING";
+# mount device
+mount($dev,$dir);
 
 # what would <h>jesus^H^H^H^H^H</h> rsync do?
 # remote systems rounds times, thus --modify-window=1
 # ignore directories via egrep
-($out,$err,$res) = cache_command("rsync --modify-window=1 -Prtn $source $target | egrep -v '/$'");
+($out,$err,$res) = cache_command("rsync --modify-window=1 -Prtn $source $target | egrep -v '/\$'");
 
 debug("OUT: $out");
 
 =item mount($dev, $dir, $umount)
 
 Mount $dev on $dir (or unmount if $umount set), using stat to confirm
-the device number matches/doesn't match the root device. Not perfect,
+the device number matches/doesnt match the root device. Not perfect,
 but safer than straight mount/umount
 
 =cut
 
 sub mount {
   my($dev, $dir, $umount) = @_;
+  my($res);
+
+  # current state
+  my($rootdev) = stat("/");
+  my($dirdev) = stat($dir);
+
+  # if requesting mount and devs differ, already ok
+  if ($rootdev != $dirdev && !$umount) {return;}
+
+  # if requesting umount and devs same, already ok
+  if ($rootdev == $dirdev && $umount) {return;}
+
+  # mount or umount as requested
+  if ($umount) {
+    $res = system("umount $dir");
+  } else {
+    $res = system("mount $dev $dir");
+  }
+
+  # since we checked whether already mounted, command above should not fail
+  if ($res) {die "COMMAND RETURNED: $res";}
+
+  # to be even safer, we call this subroutine again, expecting it to
+  # return instantly
+  mount($dev,$dir,$umount);
 }
 
 
