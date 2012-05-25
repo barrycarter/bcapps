c9e500c200842b10f4a10245624011df43a9f159
Barry Carter <Barry Carter>
2012-05-25 10:44:07 -0600

    Checkpoint save

--- bc-shrink-gsn.pl
+++ bc-shrink-gsn.pl
@@ -13,4 +13,49 @@ require "/usr/local/lib/bclib.pl";
 
 $all = read_file("/home/barrycarter/BCGIT/db/program_schedule_print.html");
 
-debug($all);
+# rows
+while ($all=~s%<tr>(.*?)</tr>%%is) {
+  push(@rows, $1);
+}
+
+for $i (@rows) {
+  # break into cells
+  @cells = ();
+  while ($i=~s%<td[^>]*?>(.*?)</td>%%is) {
+    push(@cells, $1);
+  }
+
+  # first cell is timing info
+  $time = shift(@cells);
+
+  for $j (@cells) {
+    # remove HTML
+    $j=~s/<.*?>//isg;
+
+    # remove newlines
+    $j=~s/\s+/ /isg;
+
+    # preserve parentheses for these (multiple hosts)
+    $j=~s/family feud/FF/isg;
+    $j=~s/^.*newlywed game/NG/isg;
+    $j=~s/match game/MG/isg;
+    $j=~s/^.*pyramid/PMD/isg;
+
+    # do not preserve parens (single host or paren info useless)
+    $j=~s/^.*deal or no deal.*$/DOND/isg;
+    $j=~s/^.*paid programming.*$/--/isg;
+    $j=~s/^.*password.*$/PW/isg;
+    $j=~s/^.*press your luck.*$/W!/isg;
+    $j=~s/^.*jeopardy.*$/J!/isg;
+    $j=~s/^.*lingo.*$/LNG/isg;
+    $j=~s/^.*press your luck.*$/W!/isg;
+    $j=~s/^.*catch 21.*$/C21/isg;
+    $j=~s/^.*millionaire.*$/M\$/isg;
+    $j=~s/^.*1 vs 100.*$/1V100/isg;
+
+    $j=trim($j);
+
+  }
+
+  debug("CELLS",@cells);
+}
