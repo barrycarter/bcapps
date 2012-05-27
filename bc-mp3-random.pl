d306411b5346c47c708ae0c7d871775f39b3faca
Barry Carter <Barry Carter>
2012-05-27 11:57:04 -0600

    Checkpoint

--- bc-mp3-random.pl
+++ bc-mp3-random.pl
@@ -9,9 +9,24 @@ require "/usr/local/lib/bclib.pl";
 # I keep these on a different machine using sshfs (list changes infrequently)
 my($out, $err, $res) = cache_command("ls /mnt/sshfs/MP3/*.mp3 | sort -R", "age=86400");
 @mp3s = split(/\n/, $out);
+$pos = 0; # starting position in file
 
-debug(@mp3s);
+# loop forever
+for (;;) {
+  # play current song
+  system("mplayer -really-quiet -af scaletempo,volnorm -speed 1.5 file $mp3s[$pos] < /dev/null &");
+
+  # wait for song to end
+  # TODO: this is ugly and catches other mplayer processes
+  $res=system("pgrep mplayer");
+  debug("RES: $res");
+  die "TESTING";
+  debug("WAITING...");
+  sleep(1);
+}
+
+# debug(@mp3s);
 
 # NOTES: mplayer -af scaletempo -speed 1.5 file
-# mplayer -really-quiet -af scaletempo -speed 1.5 file /mnt/sshfs/MP3/file.mp3
+# mplayer -really-quiet -af scaletempo -speed 1.5 file /mnt/sshfs/MP3/file.mp3 < /dev/null &
 
