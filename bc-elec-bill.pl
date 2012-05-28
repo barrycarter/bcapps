1240b8f73ec20529ca72ba55a72cdbb3216b6d68
Barry Carter <Barry Carter>
2012-05-28 16:50:17 -0600

    checkpoint

--- bc-elec-bill.pl
+++ bc-elec-bill.pl
@@ -7,12 +7,12 @@
 # -norecord: don't record reading in ~/elecbill.txt
 
 require "/usr/local/lib/bclib.pl";
-
-debug(str2time("2012-05-22 08:00:00 MST7MDT"));
-
-die "TESTING";
-
 defaults("norecord=1"); warn "TESTING";
+
+# tiered usage cost (first 450 at .0906, next 450 at .1185, rest at .1284)
+# http://www.nmprc.state.nm.us/consumer-relations/company-directory/electric/pnm/forms/form90.pdf is accurate, at least for May 2012
+# TODO: this doesn't need to be a constant
+@tiers = ([450, 0.0906237], [450, 0.1185101], [+Infinity, 0.1283520]);
 
 # yyyy-mm-dd when meter last read, and amount
 ($time,$read) = ("2012-05-22", "50492");
@@ -29,7 +29,36 @@ unless ($globopts{norecord}) {
 # <h>this is the only really clever bit to this program, assuming there is one</h>
 
 # max and min number of seconds since meter read
-$maxtime = $now-str2time("$time 08:00:00 US/Mountain");
-$mintime = $now-str2time("$time 17:00:00 US/Mountain");
+$maxtime = $now-str2time("$time 08:00:00 MST7MDT");
+$mintime = $now-str2time("$time 17:00:00 MST7MDT");
 
-debug("$mintime/$maxtime");
+# average kilowatt usage (reading is in kilowatthours)
+$max = ($cur-$read)/($now-$mintime)*3600;
+$min = ($cur-$read)/($now-$maxtime)*3600;
+
+# per month (365.2425 days in a year, Gregorian calendar)
+($monthmin, $monthmax) = ($min*365.2425/12*24, $max*365.2425/12*24);
+($costmin, $costmax) = (tiered_cost($monthmin), tiered_cost($monthmax));
+
+printf("Last reading: %s\n", $time);
+printf("Usage to date: %d (\$%.2f)\n", $cur-$read, tiered_cost($cur-$read));
+printf("Average usage: %d - %d watts\n",$max*1000,$min*1000);
+printf("Monthly usage: %d - %d kwh\n",$monthmax,$monthmin);
+printf("Cost: \$%.2f - \$%.2f\n",$costmax,$costmin);
+
+# work out cost of $n kilowatthours of electricity, using tiers
+sub tiered_cost {
+  my($n) = @_;
+
+  my($total) = 0;
+  for $i (@tiers) {
+    my($tier,$price) = @$i;
+
+    # if not used up entire tier, return
+    if ($n < $tier) {return $total+$n*$price;}
+
+    # used up entire tier, so keep going
+    $total += $tier*$price;
+    $n -= $tier;
+  }
+}
