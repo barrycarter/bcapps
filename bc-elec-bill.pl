e9eb4a6658ea683b78b03342043f132179357957
Barry Carter <Barry Carter>
2012-06-02 08:58:17 -0600

    Checkpoint; may rewrite bc-elec-bill.pl to use ranges better

--- bc-elec-bill.pl
+++ bc-elec-bill.pl
@@ -4,20 +4,25 @@
 # set of assumptions/conditions [assumes I can read my meter's current value]
 
 # Options:
-# -norecord: don't record reading in ~/elecbill.txt
+# --norecord: don't record reading in ~/elecbill.txt
+# --last: use last reading in ~/elecbill.txt, not new measurement
 
 # TODO:
 # add --last option that just shows results from previous reading
 # show "what if" scenarios
 # measure usage since last reading(s)
-# add +-.1 inaccuracy in reading itself
 # add +-1 minute inaccuracy in reading time (though 8h window sort of covers this)
-# sort elecbill.txt as I might start allowing out-of-order entries
 # allow --time= entries for previous times
+# report out of order readings?
+# add "median" estimates too?
 
 require "/usr/local/lib/bclib.pl";
-defaults("norecord=1");
-warn "--norecord set by default; use --norecord=0 to record";
+
+# useful constants
+$secspermonth = 365.2425*86400/12; # gregorian
+
+# sort (just in case of out-of-order entries in the future)
+system("sort -n $ENV{HOME}/elecbill.txt -o $ENV{HOME}/elecbill.txt");
 
 # tiered usage cost (first 450 at .0906, next 450 at .1185, rest at .1284)
 # http://www.nmprc.state.nm.us/consumer-relations/company-directory/electric/pnm/forms/form90.pdf is accurate, at least for May 2012
@@ -27,30 +32,59 @@ warn "--norecord set by default; use --norecord=0 to record";
 # yyyy-mm-dd when meter last read, and amount
 # TODO: this obviously shouldn't be hardcoded
 ($time,$read) = ("2012-05-22", "50492");
-# current time
-$now = time();
-# current reading (given on cmd line)
-(($cur)=@ARGV)||die("Usage: $0 <current_reading>");
 
-unless ($globopts{norecord}) {
-  append_file("$now $cur\n", "$ENV{HOME}/elecbill.txt");
+# min and max time read, assuming meter was read 8a-5p
+$minreadtime = str2time("$time 08:00:00 MST7MDT");
+$maxreadtime = str2time("$time 17:00:00 MST7MDT");
+
+# if reading from file...
+if ($globopts{last}) {
+  my($out,$err,$res) = cache_command("tail -1 $ENV{HOME}/elecbill.txt");
+  ($now, $cur) = split(/\s+/, $out);
+} else {
+  # current time
+  $now = time();
+  # current reading (given on cmd line)
+  (($cur)=@ARGV)||die("Usage: $0 <current_reading>");
+
+  unless ($globopts{norecord}) {
+    append_file("$now $cur\n", "$ENV{HOME}/elecbill.txt");
+  }
 }
 
 # I don't know WHEN on $time meter was read, so calculate for both 8am and 5pm
 # <h>this is the only really clever bit to this program, assuming there is one</h>
 
 # max and min number of seconds since meter read
-$maxtime = $now-str2time("$time 08:00:00 MST7MDT");
-$mintime = $now-str2time("$time 17:00:00 MST7MDT");
+$maxtime = $now-$minreadtime;
+$mintime = $now-$maxreadtime;
+
+# look at last few entries and determine usage
+open(A,"tac $ENV{HOME}/elecbill.txt|"); 
+
+while (<A>) {
+  my($rtime, $reading) = split(/\s+/, $_);
+
+  # ignore readings older than last known reading
+  if ($rtime < $maxreadtime) {last;}
+
+  # compare to most recent reading
+  $elapsed = $now - $rtime;
+  $used = $cur - $reading;
+  elec_stats($used, $elapsed);
+}
+
+close(A);
 
 # average kilowatt usage (reading is in kilowatthours)
-$max = ($cur-$read)/$mintime*3600;
-$min = ($cur-$read)/$maxtime*3600;
+# allow .1 fudge in reading
+$max = ($cur+.1-$read)/$mintime*3600;
+$min = ($cur-.1-$read)/$maxtime*3600;
 
 debug("MAX/MIN: $max/$min");
 
 # per month (365.2425 days in a year, Gregorian calendar)
-($monthmin, $monthmax) = ($min*365.2425/12*24, $max*365.2425/12*24);
+($monthmin, $monthmax) = ($min*$secspermonth/3600, $max*$secspermonth/3600);
 ($costmin, $costmax) = (tiered_cost($monthmin), tiered_cost($monthmax));
 
 printf("Last reading: %s\n", $time);
@@ -75,3 +109,54 @@ sub tiered_cost {
     $n -= $tier;
   }
 }
+
+# given kwh usage and number of seconds, print out (TODO: blech!)
+# information about usage, allowing for +-.1 error in reading and
+# +-60s error in time [per reading]
+
+sub elec_stats {
+  my($usage, $sec) = @_;
+  debug("elec_stats($usage,$sec)");
+
+  # max and min seconds (+-60s each way, so total 120s)
+  my($minsec, $maxsec) = ($sec-120, $sec+120);
+  # and usage (+-.1 each way, so .2 total)
+  my($minuse, $maxuse) = ($usage-.2, $usage+.2);
+
+  # maximum and minimum usage (in watts)
+  my($maxwatts) = $maxuse/$minsec*3600*1000;
+  my($minwatts) = $minuse/$maxsec*3600*1000;
+
+  debug("RANGE: $minwatts-$maxwatts");
+
+  # number of seconds left this month
+  my($secsleftmax) = $secspermonth - $mintime;
+  my($secsleftmin) = $secspermonth - $maxtime;
+
+  # we could do +-.1 on current reading, but it won't really matter
+  my($usagetodate) = ($cur-$read);
+
+  # max and min estimated usage for month
+  my($maxusage) = $usagetodate + ($secsleftmax*$maxwatts)/3600000;
+  my($minusage) = $usagetodate + ($secsleftmin*$minwatts)/3600000;
+
+  debug("MINMAX: $minusage-$maxusage");
+
+  # and price
+  my($maxprice) = tiered_cost($maxusage);
+  my($minprice) = tiered_cost($minusage);
+
+  debug("MINMAX: $minprice-$maxprice");
+
+  # TODO: subroutines printing is bad!
+print << "MARK";
+
+Usage (since $time): $usagetodate
+Average (last $minsec-$maxsec seconds): $minwatts-$maxwatts
+Total usage for month: $minusage-$maxusage
+Total cost for month: $minprice-$maxprice
+MARK
+;
+
+}
+
