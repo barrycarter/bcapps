a4e659c80e0b05922389eb1dbddbff9083f12347
Barry Carter <Barry Carter>
2012-06-02 14:49:51 -0600

    Checkpoint

--- bc-overdrive.pl
+++ bc-overdrive.pl
@@ -3,7 +3,8 @@
 # quick n dirty script that creates an iMacro to search Kindle books
 # in all categories in Albuquerque Digital Library
 
-# --limit: limit to this many pages (currently in alpha order)
+# --only=m,n: only provide iim for subjects m through n, 0 based (eg,
+# --only=0,4 would choose the first five subjects the user wants)
 
 #<h>Part of the useless-except-to-me series of programs!</h>
 
@@ -31,10 +32,37 @@ debug(sort values %subs);
 	  "engineering", "erotic", "fantasy", "finance", "folklore", "games",
 	  "humor", "math", "science", "TV");
 
+# figure out which subjects user has chosen
+for $i (sort keys %subs) {
+
+  # does this meet a regex I want
+  $wanted = 0;
+  for $j (@regex) {
+    if ($subs{$i}=~/$j/i) {
+      $wanted=1;
+      last;
+    }
+  }
+
+    if ($wanted) {push(@wanted,$i);}
+}
+
+# parse --only (or choose everything if no such option)
+if ($globopts{only}=~/^(\d+)\-(\d+)$/) {
+  ($start, $end) = ($1, $2);
+} else {
+  ($start, $end) = (0, $#wanted);
+}
+
+debug("START/END: $start/$end");
+
 # TODO: allow user to select subs?
 
+# write to iim directory (probably a bad idea?)
+open(B,">$ENV{HOME}/iMacros/Macros/bc-overdrive.iim");
+
 # the guts of the iim file
-for $i (sort keys %subs) {
+for $i (@wanted[$start..$end]) {
 
   # does this meet a regex I want
   $wanted = 0;
@@ -50,11 +78,13 @@ for $i (sort keys %subs) {
 
   if ($globopts{limit} && $n > $globopts{limit}) {last;}
 
+  $count++; # yes, I should use $n for this, sigh
+
   # the fixed values for format and page are Kindle Book, and 25 hits/page
   # and most recent books (since I tend to run this often?)
 
   # the #$subs{$i} below is solely for end user to know what was searched
-  print << "MARK";
+  print B << "MARK";
 TAB OPEN
 TAB T=$n
 URL GOTO=http://cabq.lib.overdrive.com/en/AdvancedSearch.htm
@@ -66,4 +96,8 @@ TAG POS=1 TYPE=A ATTR=TXT:date<SP>added<SP>to<SP>site
 MARK
 ;
 }
+
+close(B);
+
+print "$count subjects (of $#wanted+1) chosen; use --only to limit\n";
 
