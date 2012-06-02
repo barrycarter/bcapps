ef34d5140d293a6cc67ce64d0edc0f1480cc6ba6
Barry Carter <Barry Carter>
2012-06-02 14:09:12 -0600

    Broken

--- bc-overdrive.pl
+++ bc-overdrive.pl
@@ -3,6 +3,8 @@
 # quick n dirty script that creates an iMacro to search Kindle books
 # in all categories in Albuquerque Digital Library
 
+# --limit: limit to this many pages (currently in alpha order)
+
 #<h>Part of the useless-except-to-me series of programs!</h>
 
 require "/usr/local/lib/bclib.pl";
@@ -46,15 +48,21 @@ for $i (sort keys %subs) {
 
   $n++;
 
+  if ($globopts{limit} && $n > $globopts{limit}) {last;}
+
   # the fixed values for format and page are Kindle Book, and 25 hits/page
+  # and most recent books (since I tend to run this often?)
+
+  # the #$subs{$i} below is solely for end user to know what was searched
   print << "MARK";
 TAB OPEN
 TAB T=$n
-URL GOTO=http://cabq.lib.overdrive.com/en/AdvancedSearch.htm
+URL GOTO=http://cabq.lib.overdrive.com/en/AdvancedSearch.htm#$subs{$i}
 TAG POS=1 TYPE=SELECT FORM=ACTION:BANGSearch.dll ATTR=ID:format CONTENT=%420
 TAG POS=1 TYPE=SELECT FORM=ACTION:BANGSearch.dll ATTR=ID:page CONTENT=%25
 TAG POS=1 TYPE=SELECT FORM=ACTION:BANGSearch.dll ATTR=ID:sub CONTENT=%$i
 TAG POS=1 TYPE=INPUT:SUBMIT FORM=ACTION:BANGSearch.dll ATTR=VALUE:Search
+TAG POS=1 TYPE=A ATTR=TXT:date<SP>added<SP>to<SP>site
 MARK
 ;
 }
