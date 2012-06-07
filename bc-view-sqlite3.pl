99c3397419ee9bc1ae74a7af1e43f412c86d5517
Barry Carter <Barry Carter>
2012-06-07 15:41:01 -0600

    checkpoint save

--- bc-view-sqlite3.pl
+++ bc-view-sqlite3.pl
@@ -2,25 +2,52 @@
 
 # View an SQLite3 db using a web interface
 
+# TODO: allow multi-edit
+
 # NOTE: I'm usually opposed to 'web programs', but I originally wrote
 # this a long time ago, which makes it ok (well, not really)
+
+# NOTE: this used to be a PHP script, as is perhaps obvious in some places
 
 require "/usr/local/lib/bclib.pl";
 
 print "Content-type: text/html\n\n";
 
+# obtain and dehtmlify the STDIN
+$stdin = <STDIN>;
+$stdin=~s/%(..)/chr(hex($1))/iseg;
+
+print "STDIN IS: $stdin\n";
+
+# split and parse
+@cmds = split(/\&/, $stdin);
+
+print "CMDS",@cmds;
+
 # db is fixed for now (TODO: allow choice)
 $db = "/home/barrycarter/ofx.db";
 
-# open(A,">/tmp/test.html");
-print  "<table border>\n";
+print "<form method='POST'>\n";
+print "<table border><tr><th>Show Columns</th><th>Sort Columns</th></tr>\n";
+
+print  "<tr><td><table border>\n";
 print  join("\n",show_table("ofxstatements"));
-print  "</table>\n";
+print  "</table></td>\n";
+
+print  "<td><table border>\n";
+print  join("\n",show_table("ofxstatements",1));
+print  "</table></td></tr>\n";
+
+print "<tr><th colspan=2><input type='submit'></th></tr>";
+print "</table>\n";
+
+
+
 # close(A);
 
 debug(show_table("ofxstatements"));
 
-# shows the table, with options to select columns/sorting; if sort
+# shows the table, with options to select columns/sorting; if $sort
 # set, return sort table, otherwise return coltable
 
 sub show_table {
@@ -28,6 +55,7 @@ sub show_table {
   my($check);
   my(%cols) = sqlite3cols($tabname, $db);
   my(@cols) = keys %cols;
+  my(@ret);
 
   # below lets user choose fewer than $#cols
   push(@cols, "-");
@@ -57,7 +85,9 @@ sub show_table {
       # the radio button is on a per-column basis; only one column can be
       # in the 5th position, for example
       # sortcol for sorting, just col for showing
-      push(@ret, qq%<td><input type="radio" name="$cols[$j]" value="$j" $check></td>%);
+      if ($sort) {$sort="sort";}
+      debug("SORT: $sort");
+      push(@ret, qq%<td><input type="radio" name="${sort}col[$j]" value="$cols[$i]" $check></td>%);
     }
     # end row
     push(@ret,"</tr>");
