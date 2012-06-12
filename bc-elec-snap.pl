45628ddf1ebca823fb3ce564a7637219361eab61
Barry Carter <Barry Carter>
2012-06-12 07:56:35 -0600

    And... working!

--- bc-elec-snap.pl
+++ bc-elec-snap.pl
@@ -22,9 +22,16 @@ if ($devroot eq $sshroot) {die "ssfs not mounted";}
 $file = `/bin/date +%Y%m%d.%H%M%S`;
 chomp($file);
 
-# and snapshot
-my($out, $err, $res) = cache_command("xawtv-remote snap jpeg win /mnt/sshfs/ELEC2012/$file.jpg");
+# and snapshot (I am running xawtv-remote as root but have sshfs
+# mounted as myself, requiring the odd cp-rm hack below)
+my($out, $err, $res) = cache_command("xawtv-remote snap jpeg win /var/tmp/$file.jpg");
 
-debug("RES: $out/$err/$res");
+# wait for file to exist (slight possibility it has 0 size, but ignoring that for now)
 
-sleep(10);
+for (;;) {
+  if (-f "/var/tmp/$file.jpg") {last;}
+  sleep(1);
+  # TODO: maybe give up after n seconds
+}
+
+system("cp /var/tmp/$file.jpg /mnt/sshfs/ELEC2012/; sudo rm /var/tmp/$file.jpg");
