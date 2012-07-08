9e90c13831ed38ad5e745590e55fd565abaf7afa
Barry Carter <Barry Carter>
2012-07-08 16:24:58 -0600

    Working, and with two sample functions to boot

--- bc-draw-grid.pl
+++ bc-draw-grid.pl
@@ -5,6 +5,8 @@
 # otherwise 'zoomed' maps); future idea is to use slippy tiles (OSM)
 # and not try to recreate everything myself
 
+# TODO: adaptive zooming (zoom more where needed)
+
 require "/usr/local/lib/bclib.pl";
 
 # spacing in degrees
@@ -13,17 +15,25 @@ $lonspace = 20;
 
 # the function
 $f = \&img_idtest;
+# $f = \&shrinkpole;
 
-warn "TESTING w single value of x y";
+# TODO: better temp file naming
+open(A,">/tmp/bdg.fly");
+print A << "MARK";
+new
+size 800,600
+MARK
+;
 
 # loop through all level 4 slippy tiles
 for $x (0..15) {
   # TODO: not sure why y can't be 0 or 15?
-  for $y (1..14) {
-#    unless ($x==3 && $y==6) {next;}
+  for $y (2..14) {
+    debug("XY: $x $y");
 
-    print "copy 0,0,0,0,800,600,/tmp/bcdg-$x-$y.gif\n";
+    print A "copy 0,0,0,0,800,600,/tmp/bcdg-$x-$y.gif\n";
 
+    # TODO: better temp file naming (but do cache stuff like this)
     if (-f "/tmp/bcdg-$x-$y.gif") {next;}
     # reset border and distortion parameters
     %border = ();
@@ -36,9 +46,6 @@ for $x (0..15) {
 	($lat,$lon) = slippy2latlon($x,$y,4,$px,$py);
 	# how we would map this border
 	($myx, $myy) = &$f($lat,$lon);
-	# store this info
-#	$translate{$px}{$py}{x} = $myx;
-#	$translate{$px}{$py}{y} = $myy;
 	# the distortion parameter for convert (imagemagick)
 	push(@params, "$px,$py,$myx,$myy");
 
@@ -54,9 +61,11 @@ for $x (0..15) {
   }
 }
 
-die "TESTING";
+close(A);
 
-open(A,">/tmp/bcdg.fly");
+system("fly -i /tmp/bdg.fly -o /tmp/bdg1.gif");
+
+open(A,">/tmp/bdg2.fly");
 
 print A << "MARK";
 new
@@ -89,7 +98,7 @@ for ($lat=90; $lat>=-90; $lat-=$latspace) {
 
 close(A);
 
-system("fly -i /tmp/bcdg.fly -o /tmp/bcdg.gif && xv /tmp/bcdg.gif&");
+system("fly -i /tmp/bdg2.fly -o /tmp/bdg2.gif");
 
 # equiangular
 sub img_idtest {
@@ -98,6 +107,17 @@ sub img_idtest {
   # using 800x600 hardcoded here = bad?
   my($y) = (90-$lat)*600/180;
   my($x) = ($lon+180)*800/360;
+
+  return round($x),round($y);
+}
+
+# I don't know the name for this, but it's often used
+
+sub shrinkpole {
+  my($lat,$lon) = @_;
+
+  my($x) = 400 + ($lon/180)*400*cos($lat*$DEGRAD);
+  my($y) = (90-$lat)*600/180;
 
   return round($x),round($y);
 }
