ef470e7898b89de9ad55b3ddf0fefcd5d2fc4286
Barry Carter <Barry Carter>
2012-07-09 09:12:08 -0600

    Playing around w proj4

--- OSM/proj4-playground.pl
+++ OSM/proj4-playground.pl
@@ -20,18 +20,33 @@ MARK
 # in theory, could use multiple calls to cs2cs, but this seems more efficient
 for ($lat=90; $lat>=-90; $lat-=$latspace) {
   for ($lon=180; $lon>=-180; $lon-=$lonspace) {
-    # this is inefficient
     ($x, $y) = projection($lat,$lon);
+
+    debug("XY: $x,$y");
+
+    # skip special cases
+    # TODO: improve this?
+    if ($x == -1) {next;}
+
+    debug("DOING STUFF");
 
     # position string a little "SE" of dot
     my($sx,$sy) = ($x+5, $y+5);
     print A "string 0,0,0,$sx,$sy,tiny,$lat,$lon\n";
 
+    # line to next east longitude
+    my($xe,$ye) = projection($lat, $lon+20);
+    if ($xe == -1) {next;}
+    print A "line $x,$y,$xe,$ye,255,0,0\n";
 
-#    debug($x,$y);
+    # line to next south latitude
+    my($xs,$ys) = projection($lat-15, $lon);
+    if ($xs == -1) {next;}
+    print A "line $x,$y,$xs,$ys,0,0,255\n";
 
-#    print A "$lat $lon\n";
-#    push(@coords, "$lat,$lon");
+    # fcircle must come last to avoid being overwritten by lines
+    print A "circle $x,$y,5,0,0,0\n";
+
   }
 }
 
@@ -51,11 +66,25 @@ for $i (0..$#coords) {
 
 sub projection {
   my($lat,$lon) = @_;
-  my($x, $y) = split(/\s+/, `echo $lat $lon | cs2cs +proj=latlong +to +proj=ortho`);
+  #  my($x, $y) = split(/\s+/, `echo $lat $lon | cs2cs +proj=latlong +to +proj=ortho`);
+  # div for above is 6378137
+
+#  my($x, $y) = split(/\s+/, `echo $lat $lon | cs2cs +unit=km +proj=latlong +to +proj=merc`);
+#  my($x, $y) = split(/\s+/, `echo $lat $lon | cs2cs +proj=latlong +unit=km +to +proj=natearth`);
+
+  my($x, $y) = split(/\s+/, `echo $lat $lon | cs2cs +proj=latlong +to +proj=robin`);
+
+#  my($div) = 6378137*4;
+#  my($div) = 20*10**6;
+  my($div) = 10*10**6;
+
+  # special case
+  if ($x eq "*") {return -1,-1;}
+
   # below is for orthographic projection, but it varies from
   # projection to projection (grumble)
-  $x = 400 + $x/6378137*400;
-  $y = 300 + $y/6378137*300;
+  $x = 400 + $x/$div*400;
+  $y = 300 + $y/$div*300;
 
   return round($x), round($y);
 }
