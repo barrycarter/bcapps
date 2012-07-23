21eeb734e830251bf507e53fe63ec635ac088f47
Barry Carter <Barry Carter>
2012-07-23 16:38:32 -0600

    Checkpoint; about to make less bad

--- bc-stream-twitter.pl
+++ bc-stream-twitter.pl
@@ -9,15 +9,17 @@ require "/home/barrycarter/bc-private.pl";
 # note: using big locations for testing, will trim
 $data = "track=math+tutor,math+help,homework+help,\@barrycarter";
 
+debug("BAD LIST:", @badtwitterregex);
+
 # write to logfile (not root, so can't write to /var/log?)
 open(B,">>/var/tmp/log/twitstream.txt");
 
 open(A,"curl -s -u $twitter{user}:$twitter{pass} 'https://stream.twitter.com/1/statuses/filter.json?$data'|");
 
 while (<A>) {
-  debug("RAW: $_");
+#  debug("RAW: $_");
   if (/^\s*$/s) {
-    debug("BLANK MESSAGE");
+#    debug("BLANK MESSAGE");
     next;
   }
 
@@ -36,35 +38,18 @@ while (<A>) {
 
   # the list comes from bc-private.pl and includes ^rt to avoid retweets
   for $i (@badtwitterregex) {
-    if ($tweet=~/$i/) {
+    debug("Comparing $tweet to $i");
+    if ($tweet=~/$i/i) {
       debug("MATCH: $i to $tweet, ignoring");
       next;
     }
   }
 
-  # ignore RTs (we catch them the first time)
-  if ($tweet=~/^rt/i) {next;}
-
-  # filter (TODO: seriously genericize this)
-  $FLAG = 0;
-  # I don't know what this means, but it doesn't sound good
-  if ($json->{text}=~/fuck bitches/i) {$FLAG=1;}
-  # to another person and not mentioning me?
-  if ($json->{text}=~/^\@/ && $json->{text}!~/barry/i) {$FLAG=1;}
-  # sick to death of hearing about "911 kid"
-  if ($json->{text}=~/calls 911/i) {$FLAG=1;}
-  # and SONA2012
-  if ($json->{text}=~/sona2012/i) {$FLAG=1;}
-  # and hungry dolphins (seriously need to generalize this!)
-  if ($json->{text}=~/hungry dolphin/i) {$FLAG=1;}
-  # this guy's doing what I am
-  if ($json->{user}{screen_name}=~/mathtutor01/i) {$FLAG=1;}
-
-  # TODO: handle RTs (for both filtering and username-specification)
-  if ($FLAG) {next;}
+  # to another person and not mentioning me? (special case, not simple regex)
+  if ($tweet=~/^\@/ && $tweet!~/barry/i) {next;}
 
   # create URL specific to user
-  my($out,$err,$res) = cache_command("echo http://tutor.u.94y.info/\?$json->{user}{screen_name} | surl");
+  my($out,$err,$res) = cache_command("echo http://tutor.u.94y.info/\?$json->{user}{screen_name} | surl -s is.gd");
   chomp($out);
 
   $str = "[$json->{created_at}] ($json->{id} : $json->{in_reply_to_status_id}) <$json->{user}{screen_name}> $json->{text}";
