3cd37cd07ae76b24febf125231ad452f7fdb7bca
Barry Carter <Barry Carter>
2012-08-03 08:25:47 -0600

    Checkpoint save

--- bc-zoomtimetile.pl
+++ bc-zoomtimetile.pl
@@ -7,23 +7,34 @@ require "/usr/local/lib/bclib.pl";
 # get google query
 %query = str2hash($ENV{QUERY_STRING});
 
-# determine LOG of x coords (in absolute 2^29 coords) [ignoring y]
-# this formula after a lot of work, which I probably should've shown
+# for now, only draw on strip below equator
+unless ($query{y}==2**($query{zoom}-1)) {exit;}
 
-if ($query{x}==0) {
-  $logx=0;
-} else {
-  $logx = 32.5141 - 1.54829*$query{zoom} + 2.23371*log($query{x});
+# convert x to -1 to +1 scale (sx = scaled x)
+$sx = $query{x}/2**($query{zoom}-1)-1;
+$sx2 = ($query{x}+1)/2**($query{zoom}-1)-1;
+
+# converts google location (on x=[-1,1] scale) to seconds pre/post 1 BCE
+sub g2s {
+  my($x) = @_;
+
+  # the max log we represent is exp(45) seconds (1.1 trillion years)
+  my($logx) = abs($x*45);
+
+  if ($x>0) {return exp($logx);}
+  return -exp($logx);
 }
 
-$lognx = 32.5141 - 1.54829*$query{zoom} + 2.23371*log($query{x}+1);
-
-# determine other useful information
+# converting seconds to years
+$lx = display_sec(g2s($sx));
+$rx = display_sec(g2s($sx2));
 
 $printstr = << "MARK";
 x=$query{x},y=$query{y},zoom=$query{zoom}
-LX: $logx
-LNX: $lognx
+LX: $lx
+RX: $rx
+SX: $sx
+SX2: $sx2
 MARK
 ;
 
@@ -49,3 +60,21 @@ for $i (split(/\n/, $printstr)) {
 }
 
 close(A);
+
+# kludge function to print a large number of seconds nicely?
+# TODO: do this much better!
+
+sub display_sec {
+  my($s) = @_;
+
+  if ($s<60) {return "$s sec";}
+  $s/=60.;
+  if ($s<60) {return "$s min";}
+  $s/=60.;
+  if ($s<24) {return "$s hrs";}
+  $s/=24.;
+  if ($s<365.2425) {return "$s days";}
+  $s/=365.2425;
+  return "$s years";
+}
+
