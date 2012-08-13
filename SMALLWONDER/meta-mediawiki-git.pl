6a87ced87b7e6c7c66a9df6a4777dff190b7ed03
Barry Carter <Barry Carter>
2012-08-12 19:03:17 -0600

    Small Wonder project starts

---
+++ SMALLWONDER/meta-mediawiki-git.pl
@@ -0,0 +1,9 @@
+#!/bin/perl
+
+# This is a different version of meta-mediawiki that uses .mmw pages
+# ('meta-media-wiki') to create .mw pages suitable for use with
+# wikipediafs; however, I plan to permit direct API access as well
+
+require "/usr/local/lib/bclib.pl";
+
+
