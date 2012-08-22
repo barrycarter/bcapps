3834a08c73239521669e68666c97cda0751da623
Barry Carter <Barry Carter>
2012-08-22 11:04:46 -0600

    Wow, no streaming and limited to single query?!

--- bc-seek-help-face.pl
+++ bc-seek-help-face.pl
@@ -3,14 +3,26 @@
 # Does what bc-seek-help.pl does, but for facebook
 
 require "/usr/local/lib/bclib.pl";
+require "/home/barrycarter/bc-private.pl";
 
-# using the watermelon example
-my($cmd) = "curl -N -s 'https://graph.facebook.com/search?q=watermelon&type=post'";
-
-open(A,"$cmd|");
-
-while (<A>) {
-  debug("THUNK: $_");
+# fix twitter tags to facebook format
+for $i (@twitter_tags) {
+  $i=~s/\#//isg;
+  push(@tags, $i);
 }
+
+$phrase=join(",",@tags);
+
+debug($phrase);
+
+# no streaming API (or its hiding?)
+
+# TODO: reduce age
+# ($out, $err, $res) = cache_command("curl 'https://graph.facebook.com/search?q=math,perl,graphviz&locale=FB'", "age=3600");
+# ($out, $err, $res) = cache_command("curl 'https://graph.facebook.com/search?q=math&q=perl&q=graphviz&locale=FB'", "age=3600");
+($out, $err, $res) = cache_command("curl 'https://graph.facebook.com/search?q=math|perl|graphviz&locale=FB'", "age=3600");
+
+print $out;
+
 
 
