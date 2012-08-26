f1a6f501a5d8ab449cf3486f5a0a7b8fe0b95df1
Barry Carter <Barry Carter>
2012-08-25 18:17:55 -0600

    Checkpoint

--- HTML5/bc-testdates.pl
+++ HTML5/bc-testdates.pl
@@ -12,20 +12,42 @@ MARK
 ;
 
 
+# some historical events
+@res = sqlite3hashlist("SELECT * FROM events ORDER BY RANDOM() LIMIT 100", "/home/barrycarter/BCINFO/sites/DB/history.db");
+
+for $i (@res) {
+  %row= %{$i};
+  # TODO: this is inaccurate and for testing only
+  $year = $row{stardate}/10000.;
+  $year = 2013-$year;
+  # in seconds
+  $pos = log($year*365.2425*86400)*100;
+
+  print qq%<text x="$pos" y="300" fill="black" style="font-size:1" transform="rotate(-90,$pos,0)">$row{shortname} ($row{stardate})</text>\n%;
+
+}
+
+=item comment
+
 for($i=-2000;$i<=2000;$i+=100) {
   debug("I: $i");
   if ($i>0) {
-    $pos = log($i*365.2425*86400);
+    $pos = log($i*365.2425*86400)*100;
   } elsif ($i<0) {
-    $pos = -log(-$i*365.2425*86400);
+    $pos = -log(-$i*365.2425*86400)*100;
   } else {
     $pos = 0;
   }
 
   debug("POS: $pos");
 
-  print qq%<text x="$pos" y="0" fill="black" style="font-size:0.15">$i</text>\n%;
+  print qq%<text x="$pos" y="0" fill="black" style="font-size:15" transform="rotate(-90,$pos,0)">$i</text>\n%;
 
 }
 
+=cut
+
 print "</g></svg>\n";
+
+print read_file("svgtry.html");
+
