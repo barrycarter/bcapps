9f339b99caae64805e079811d4cabe717771d72c
Barry Carter <Barry Carter>
2012-09-06 10:12:06 -0600

    checkpoint save

--- ELECREAD/bc-elec-read.pl
+++ ELECREAD/bc-elec-read.pl
@@ -15,3 +15,33 @@ require "/usr/local/lib/bclib.pl";
 # general idea is to read circles radiating out from center points and
 # find "darkest point" for each dial
 
+# This is for testing only
+system("convert 20120617.173402.jpg /tmp/bcer1.png");
+debug(unfold(pnm("/tmp/bcer1.png")));
+
+=item pnm($file)
+
+Given a PNM format graphic $file, return array of pixels
+
+=cut
+
+sub pnm {
+  my($file) = @_;
+  my($all) = read_file($file);
+
+  # find the x and y dims (first three lines are info lines, 2nd is dims)
+  $all=~s/^(.*?)\n(.*?)\n(.*?)\n//s;
+  my($x,$y) = split(/\s+/,$2);
+
+  my(@ret);
+
+  for $i (0..$y) {
+    # translate pixels into numbers, row by row
+    my($row) = substr($all,$x*$i,$x);
+    my(@pix) = map($_=ord($_),split(//,$row));
+    push(@ret,\@pix);
+  }
+
+  return @ret;
+}
+
