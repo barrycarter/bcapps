c6e4c6ef57a47e1da672d96d0b102a3f7e7959ee
Barry Carter <Barry Carter>
2012-09-19 21:05:06 -0600

    .

--- FOODTRACK/bc-weight-loss.pl
+++ FOODTRACK/bc-weight-loss.pl
@@ -11,16 +11,16 @@ $stime = 1347412858;
 $startime = stardate($stime-6*3600);
 $sweight = 191.8;
 
-# target weights
-$t1 = 180;
-$t2 = 150;
+# target weights (lowest is border between regular/underweight)
+@t=(180,150,120);
+
+print "\n";
 
 # I store my current weight in /home/barrycarter/TODAY/yyyymmdd.txt
 # files as 'x#%%' where x is my weight in pounds [there used to be
 # numbers before the % signs but not any more]
 
 # go backwards through days until finding a weight
-
 for (;;) {
   $stardate = strftime("%Y%m%d",localtime(time()-86400*$i++));
   # last result is the one I want
@@ -46,11 +46,12 @@ print "Starting weight: $sweight at $startime\nCurrent weight: $wt at $stardate.
 printf("Loss of %0.2f lbs in %0.2f days\nLoss/day: %0.2f lbs\nLoss/week: %0.2f lbs\n\n", $tloss, $days, $tloss/$days, $tloss/$days*7);
 
 # time to targets (linear)
-$time1 = ($wt-$t1)/($tloss/$days)*86400+$secs;
-$time2 = ($wt-$t2)/($tloss/$days)*86400+$secs;
+for $i (0..$#t) {
+  $time[$i] = ($wt-$t[$i])/($tloss/$days)*86400+$secs;
+  print strftime("Achieve $t[$i] lbs (linear): %c\n",localtime($time[$i]));
+}
 
-print strftime("Achieve $t1 lbs (linear): %c\n",localtime($time1));
-print strftime("Achieve $t2 lbs (linear): %c\n\n",localtime($time2));
+print "\n";
 
 # weight loss (log)
 $pctloss = $wt/$sweight;
@@ -58,11 +59,9 @@ $pctloss = $wt/$sweight;
 printf("Loss of %0.2f%% in %0.2f days\nLoss/day: %0.2f%\nLoss/week: %0.2f%\n\n", 100*(1-$pctloss), $days, 100*(1-($pctloss**(1/$days))), 100*(1-($pctloss**(7/$days))));
 
 # time to targets (log)
-$ltime1 = (log($wt)-log($t1))/(log($sweight)-log($wt))*$days*86400+$secs;
-$ltime2 = (log($wt)-log($t2))/(log($sweight)-log($wt))*$days*86400+$secs;
+for $i (0..$#t) {
+  $ltime[$i] = (log($wt)-log($t[$i]))/(log($sweight)-log($wt))*$days*86400+$secs;
+  print strftime("Achieve $t[$i] lbs (log): %c\n",localtime($ltime[$i]));
+}
 
-print strftime("Achieve $t1 lbs (log): %c\n",localtime($ltime1));
-print strftime("Achieve $t2 lbs (log): %c\n\n",localtime($ltime2));
-
-
-
+print "\n";
