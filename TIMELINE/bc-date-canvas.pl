2ffd6c7494740789f4aae8cd1fd932600b6303f6
Barry Carter <Barry Carter>
2012-09-22 11:21:47 -0600

    Getting there, slowly

--- TIMELINE/bc-date-canvas.pl
+++ TIMELINE/bc-date-canvas.pl
@@ -10,9 +10,16 @@ require "/usr/local/lib/bclib.pl";
 $start = "19500000";
 $end = "20130000";
 $width = 1024;
+$height=768;
+
+# the header
+print read_file("canvas-header.html");
+
+# TODO: in real select, we can limit to events longer than a certain
+# min (ie, longer than the min draw threshold)
 
 # birth/death of same person, both occurring 1950-2013
-$query = "SELECT e1.shortname, e1.longname, e1.stardate AS birth, e2.stardate AS death FROM events e1 JOIN events e2 ON (e1.shortname=e2.shortname) AND e1.type='BIRTHS' AND e2.type='DEATHS' AND e1.stardate>$start AND e2.stardate<$end ORDER BY e2.stardate DESC LIMIT 50";
+$query = "SELECT e1.shortname, e1.longname, e1.stardate AS birth, e2.stardate AS death FROM events e1 JOIN events e2 ON (e1.shortname=e2.shortname) AND e1.type='BIRTHS' AND e2.type='DEATHS' AND e1.stardate>$start AND e2.stardate<$end ORDER BY e2.stardate DESC LIMIT 5";
 
 @res = sqlite3hashlist($query,"/home/barrycarter/BCINFO/sites/DB/history.db");
 
@@ -25,11 +32,17 @@ $query = "SELECT e1.shortname, e1.longname, e1.stardate AS birth, e2.stardate AS
 
 # go through events
 for $i (@res) {
-  $lifelen = $i->{death}-$i->{birth};
-  # how many pixels will this take up?
-  $frac = $width*$lifelen/($end-$start);
-  debug("LIFELEN: $lifelen, $frac");
+  # start and end pixels (if we choose to draw this)
+  $spixel = ($i->{birth}-$start)/($end-$start)*$width;
+  $epixel = ($i->{death}-$start)/($end-$start)*$width;
+  # and print it
+  print "ctx.fillRect ($spixel, $height/2, 5, 10);\n";
+  print "ctx.fillText('$i->{shortname}', $spixel, $height/2);\n";
+
+  debug("PIX: $spixel-$epixel");
 }
+
+print "</script>\n";
 
 
 
