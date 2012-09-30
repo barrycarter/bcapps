619389b3edb3f4d80b3a7b13e1498256ea2f3b1b
Barry Carter <Barry Carter>
2012-09-29 19:07:44 -0600

    .

--- ELECREAD/bc-elec-read.pl
+++ ELECREAD/bc-elec-read.pl
@@ -5,24 +5,29 @@
 
 require "/usr/local/lib/bclib.pl";
 unless ($ARGV[0]) {die "Usage: $0 file";}
-debug("FILENAME: $ARGV[0]");
+$filename = $ARGV[0];
+$shortfile = $filename;
+$shortfile=~s/^.*\///isg;
+# use filename as part of tempfiles
+$tmpbase = $shortfile;
 
-# straighten image and then slice into 5 dials
-# if pulling from my original (uncropped) collection, also crop
+# if pulling from my original (uncropped) collection, crop
+# TODO: figure out whether orig or clipped by looking at dimensions
 if ($globopts{orig}) {
-  system("convert -crop 448x140+191+89 -rotate -5.6579 $ARGV[0] /tmp/bcer0.pnm");
+  system("convert -crop 448x140+191+89 -colorspace gray -rotate -5.6579 $ARGV[0] /tmp/bcer0.pnm");
 } else {
-  system("convert -rotate -5.6579 $ARGV[0] /tmp/bcer0.pnm");
+  system("convert -colorspace gray -rotate -5.6579 $ARGV[0] /tmp/bcer0.pnm");
 }
 
-system("convert -colorspace gray /tmp/bcer0.pnm /tmp/bcer1.pnm");
-system("convert -colorspace gray /tmp/bcer0.pnm /tmp/bcer0.gif");
+# for fly (TODO: newer versions of fly read PNMs?)
+system("convert /tmp/bcer0.pnm /tmp/bcer0.gif");
 
 # potential x values for centers (y value = mid of image)
+# this where we start looking for brightest points
 
 @centers=(75,159,241,321,402);
 
-@pnm = pnm("/tmp/bcer1.pnm");
+@pnm = pnm("/tmp/bcer0.pnm");
 
 open(A,">/tmp/bcer.fly");
 print A "existing /tmp/bcer0.gif\n";
@@ -35,21 +40,18 @@ for $dial (0..4) {
   # mark where we start the search
   $xcenter = $centers[$dial];
   $ycenter = 92;
-#  print A "setpixel $centers[$dial],92,255,0,0\n";
 
   for $i (-5..5) {
     for $j (-5..5) {
       # the x/y coords we're searching around
       $x = $centers[$dial]+$j;
       $y = 92+$i;
-      # note y,x format
+      # note @pix is in y,x (row, column) format
       $pix{"$x,$y"} = $pnm[$y][$x];
-#      $pix{"$i,$j"} = $pnm[92+$i][$centers[$dial]+$j];
     }
   }
   # sort
   @pix = sort {$pix{$a} <=> $pix{$b}} keys %pix;
-  debug("DIAL: $dial");
 
   # find/show lightest spot
   ($lx,$ly) = split(/\,/,$pix[-1]);
@@ -57,11 +59,6 @@ for $dial (0..4) {
   # TODO: seriously cleanup this code
   push(@yvals, $ly);
   push(@xvals, $lx);
-#  debug("LXLY: $lx, $ly");
-
-  for $k (@pix) {
-#    debug("$k -> $pix{$k}");
-  }
 }
 
 # want to find deviation, so sort
@@ -69,20 +66,34 @@ for $dial (0..4) {
 $ydev = $yvals[-1]-$yvals[0];
 
 # ydev=2 seems OK, ydev=4 excessive
-debug("YDEV: $ydev");
+# debug("YDEV: $ydev");
 
 if ($ydev>=4) {exit(0);}
 
 for $dial (0..4) {
   for $radius (10..30) {
+#    debug("SENDING: $xvals[$dial],$yvals[$dial]");
     $ret = reading($radius,$xvals[$dial],$yvals[$dial]);
-    debug("DIAL: $dial, RADIUS: $radius, ANGLE: $ret");
+
+    # compute which of 10 numbers this mean (ignoring reversals for now)
+    # always read toward lower number, not closest number
+    # 0 -> 7.5, so -18-+18 -> 7, +18-+54 -> 8, and so on
+    $read = fmodp(($ret-18)/36+8,10);
+    debug("READING: $read FROM ANGLE: $ret");
+
+    # angles for this dial (returned clockwise starting at "7.5")
+    $angs[$dial][$radius] = $ret;
   }
+}
+
+for $dial (0..4) {
+#  debug("DIAL: $dial, ANGLES:", @{$angs[$dial]});
 }
 
 close(A);
 system("fly -q -i /tmp/bcer.fly -o /tmp/bcer2.gif");
-system("display /tmp/bcer2.gif&");
+system("cp /tmp/bcer2.gif $tmpbase.gif");
+system("display $tmpbase.gif &");
 
 die "TESTING";
 
@@ -183,10 +194,6 @@ sub reading {
   # find the darkest point(s)
   my(@keys) = sort {$hash{$a} <=> $hash{$b}} keys %hash;
 
-#  for $i (@keys) {
-#    debug("DIAL $dial, RADIUS: $r, KEY: $i -> $hash{$i}");
-#  }
-
   # find angle of darkest point from center (and lightest)
   my($dx,$dy) = split(/\,/, $keys[0]);
   my($lx,$ly) = split(/\,/, $keys[-1]);
@@ -195,11 +202,13 @@ sub reading {
   print A "setpixel $dx,$dy,255,0,0\n";
 #  print A "setpixel $lx,$ly,0,0,255\n";
 
-  debug("DARK($dial,$r): $dx-$cx,$dy-$cy");
+#  debug("DARK($dial,$r): $dx-$cx,$dy-$cy");
   # this is a 90 degree right rotate so 0 meter = 0 degrees
 
   # return just the angle
-  my($an) = fmodp((atan2($cy-$dy,$dx-$cx)-$PI/2)*$RADDEG,360);
+#  debug("ATAN: $cy-$dy, $cx-$dx");
+  my($an) = atan2($cy-$dy,$cx-$dx)*$RADDEG;
+#  debug("GOT: $an");
   return $an;
 }
 
