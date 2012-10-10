a61d18aa6e8268c565b9da21c82d48db3cadb856
Barry Carter <Barry Carter>
2012-10-10 11:12:34 -0600

    Better sourcing for foods.gnumeric, minor corrections

--- USDA/bc-usda.pl
+++ USDA/bc-usda.pl
@@ -5,29 +5,40 @@
 require "/usr/local/lib/bclib.pl";
 
 # input (both the schema AND the data in one swell foop)
-warn("TESTING: remove head -n below");
-open(A,"cat /home/barrycarter/BCGIT/USDA/schema.txt; bzcat /home/barrycarter/BCGIT/USDA/data.txt.bz2| head -100|");
+# warn("TESTING: remove head -n below");
+open(A,"cat /home/barrycarter/BCGIT/USDA/schema.txt; bzcat /home/barrycarter/BCGIT/USDA/data.txt.bz2|");
 # output
 open(B,">/tmp/usdaq.sql");
 # transaction
 print B "BEGIN;\n";
 
 while (<A>) {
-
+  chomp;
   # DROP TABLE to DROP TABLE IF EXISTS (to avoid non-fatal warning)
   s/DROP TABLE /DROP TABLE IF EXISTS /isg;
+  # kill (xg) for measurements (including leading space in one case)
+  # {0,2} below to handle mu and g by itself
+  s/\(.{0,2}g\)//isg;
+  # below for error in two cases
+  s/mg\)//isg;
+  # sqlite3 disallows + and * in column names
+  s/[\+\*]/_/isg;
+  # add semicolons to insert
+  if (/^INSERT INTO/) {
+    $_="$_;";
+    # every so often COMMIT and BEGIN just to catch errors early
+    if (rand()<.01) {print B "COMMIT;\nBEGIN;\n";}
+  }
 
-  # removing all parens (excessive?)
-#  s/[\(\)]//isg;
-
-  debug("THUNK: $_");
-  print B $_;
+  print B "$_\n";
 }
 
 print B "COMMIT;\n";
 
 close(A);
 close(B);
+
+debug("Finished printing, now running sqlite3 command");
 
 system("rm /tmp/usda.db; sqlite3 /tmp/usda.db < /tmp/usdaq.sql");
 
