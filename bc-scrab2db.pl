bffe8b02a104baed8ce8fc2ec2d6eb632e2ed931
Barry Carter <Barry Carter>
2012-11-18 12:01:23 -0700

    Checkpoint

--- bc-scrab2db.pl
+++ bc-scrab2db.pl
@@ -5,16 +5,48 @@
 
 require "/usr/local/lib/bclib.pl";
 
-for $i (split(/\n/, read_file("scrabble-words.txt"))) {
+for $i (split(/\n/, read_file("/home/barrycarter/BCGIT/scrabble-words.txt"))) {
   # split word and definition (words always in caps!)
   $i=~/^([A-Z]+)\s+(.*?)$/;
   ($word, $def) = ($1, $2);
 
+  # list of words for this definition
+  @words = ($word);
+
   # sometimes definition itself contains other words (as "-- FOO/BAR")
-  if ($def=~/\-\-\s+(.*?)$/) {
-    debug("OTHER WORDS: $1");
+  if ($def=~s/\-\-\s+([A-Z\/]+)//) {
+    push(@words, split(/\//, $1));
   }
 
-  debug("WORD/DEF: $word/$def");
+  # I find "-- foo" annoying
+  $def=~s/\-\-(.*?)$/($1)/;
+
+  # sqlite3 escape
+  $def=~s/\'/''/isg;
+
+  # for each word, we include two "signatures"
+  # sig1: letters in word in alphabetical order
+  # sig2: sig1 with duplicates removed
+
+  for $j (@words) {
+    $sig1 = join("",sort(split(//,$j)));
+    $sig2 = $sig1;
+    $sig2=~s/(.)\1+/$1/sg;
+    push(@querys, "INSERT INTO words (word, definition, sig1, sig2) VALUES
+('$j', '$def', '$sig1', '$sig2');");
+  }
 }
+
+print << "MARK";
+DROP TABLE IF EXISTS words;
+CREATE TABLE words (word, definition, sig1, sig2);
+BEGIN;
+MARK
+;
+
+print join("\n",@querys),"\n";
+
+print "COMMIT;\n";
+
+
 
