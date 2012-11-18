2e9d0ee02322770d6e242541ed932418d98455cf
Barry Carter <Barry Carter>
2012-11-18 12:21:40 -0700

    Checkpoint; about to subroutinify sql query

--- bc-word-transform.pl
+++ bc-word-transform.pl
@@ -8,9 +8,8 @@
 
 require "/usr/local/lib/bclib.pl";
 
-word_drop_letter("source");
-
-# drop letter - drops letter from word
+# debug(word_drop_letter("adulate"));
+debug(word_add_letter("source"));
 
 =item word_drop_letter($word)
 
@@ -21,14 +20,55 @@ Returns valid words/definitions formed by dropping letter for $word
 sub word_drop_letter {
   my($word) = @_;
   my(@words);
+  my(%rethash);
 
   # potential words, quoted
   for $i (1..length($word)) {
-    push(@words, "'".substr($word,0,$i-1).substr($word,$i)."'");
+    push(@words, "'".uc(substr($word,0,$i-1).substr($word,$i))."'");
   }
 
   # build SQL query
   my($words) = join(",",@words);
-  my($query) = "SELECT word,definition FROM words WHERE word IN ($words);";
+  my(@res) = sqlite3hashlist("SELECT word,definition FROM words WHERE word IN ($words)", "/home/barrycarter/BCINFO/sites/DB/scrab.db");
 
+  # return just word and definition
+  # TODO: I really need to write an sqlite3hash function
+  for $i (@res) {
+    $rethash{$i->{word}} = $i->{definition};
+  }
+
+  return %rethash;
 }
+
+=item word_add_letter($word)
+
+Returns valid words/definitions formed by dropping letter to $word
+
+=cut
+
+sub word_add_letter {
+  my($word) = @_;
+  my(@words);
+  my(%rethash);
+
+  # potential words, quoted
+  for $i (1..length($word)) {
+    for $j ("a".."z") {
+      push(@words, "'".uc(substr($word,0,$i).$j.substr($word,$i))."'");
+    }
+  }
+
+  # build SQL query
+  my($words) = join(",",@words);
+  my(@res) = sqlite3hashlist("SELECT word,definition FROM words WHERE word IN ($words)", "/home/barrycarter/BCINFO/sites/DB/scrab.db");
+
+  # return just word and definition
+  # TODO: I really need to write an sqlite3hash function
+  for $i (@res) {
+    $rethash{$i->{word}} = $i->{definition};
+  }
+
+  return %rethash;
+}
+
+
