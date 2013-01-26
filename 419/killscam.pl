1ae84df4a05f35df491d3faa40a546bcf437bca5
Barry Carter <Barry Carter>
2013-01-26 10:05:25 -0700

    Starting plan to stop scammers

---
+++ 419/killscam.pl
@@ -0,0 +1,16 @@
+#!/bin/perl
+
+=item random_address()
+
+Create a random name/address in the USA that looks valid.
+
+NOTE: I am probably putting WAY too much effort into this AND people
+have probably done this before.
+
+=cut
+
+sub random_address {
+  
+
+}
+
