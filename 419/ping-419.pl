129f477303ac71c3c59a2a6bf1c36da01f1e1d87
Barry Carter <Barry Carter>
2013-01-26 20:19:42 -0700

    Checkpoint

---
+++ 419/ping-419.pl
@@ -0,0 +1,34 @@
+#!/bin/perl
+
+# Ping a list of potential 419 scammers with a generic message;
+# however, make the To: address their actual address instead of
+# bcc'ing to be more convincing
+
+# TODO: let from be "Name <email>", currently must be pure email addr
+
+# TODO: tag emails somehow to recognize when they come back
+
+require "/usr/local/lib/bclib.pl";
+
+# check valid call
+$usagestring="$0 --from=from --to=filename --subject=subject message_file";
+for $i ("from","to","subject") {unless ($globopts{$i}) {die $usagestring;}}
+my($msg,$fname) = cmdfile();
+my($tolist) = read_file($globopts{to});
+unless ($tolist) {die "$tolist appears to be empty";}
+
+# TODO: keep track of who i sent to in order to avoid duplicates
+# TODO: report addresses that bounce
+for $i (split(/\n/,$tolist)) {
+  debug("I: $i");
+  if ($i=~/^\#/) {next;}
+  # HACK: during testing, the emails are sent to me FROM the scammer
+  debug(sendmail($globopts{from}, $i, $globopts{subject}, $msg));
+}
+
+
+
+
+
+
+
