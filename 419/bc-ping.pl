40d1aa569e71d9370ccaef6962672048a0399806
Barry Carter <Barry Carter>
2013-01-28 16:59:39 -0700

    And away we go...

--- 419/bc-ping.pl
+++ 419/bc-ping.pl
@@ -4,21 +4,59 @@
 
 require "/usr/local/lib/bclib.pl";
 $dir = "/home/barrycarter/BCGIT/419";
+dodie('chdir($dir)');
 # gmail catcher addr
 $catch = "leonard.zeptowitz";
+$now = time();
 
-# read entries from toping.txt
-@emails = `egrep -v '^#|^\$' $dir/toping.txt | sort | uniq`;
+# read entries from toping.txt (too much Unix below?)
+@emails = `egrep -v '^#|^\$' toping.txt | sort | uniq | sed 's/ //g'`;
 
-warn "TODO: remove people in pinged.txt";
+# read entries from already pinged
+@pinged = `cut -d' ' -f 2 pinged.txt`;
 
-for $i (@emails) {
+# subtract pinged from emails
+@res = minus(\@emails, \@pinged);
+
+unless (@res) {
+  die "There are no new addresses in toping.txt, edit pinged.txt or toping.txt";
+}
+
+# will add these to pinged.txt
+open(A,">>pinged.txt");
+# shell file to actually send emails
+open(B,">/var/tmp/bcping.sh");
+
+for $i (@res) {
+  chomp($i);
   # use TOD for sending addr
   # TODO: bad?
-  $fromaddr = strftime("$catch+%Y%m%d%H%M%S\@gmail.com", gmtime(time()));
-  # not that we can really send email more than 1/s
-  sleep(1);
+  # <h>y2.1k!</h>
+  $fromaddr = strftime("$catch+%y%m%d%H%M%S\@gmail.com", gmtime($now++));
 
-  debug("I: $i, $fromaddr");
+  # note that subject/body are fixed (for now)
+my($str) = << "MARK";
+From: Leonard Zeptowitz <$fromaddr>
+To: $i
+Subject: About the email you sent me?
+
+Could you please give me details about the email you sent me? Thank you!
+
+MARK
+;
+
+  # write string to /var/tmp/bcping-from.txt
+  write_file($str,"/var/tmp/bcping-$fromaddr.txt");
+  # write to shell file
+  print B "sendmail -v -f$fromaddr -t < /var/tmp/bcping-$fromaddr.txt 1> /var/tmp/bcping-$fromaddr.out 2> /var/tmp/bcping-$fromaddr.err\n";
+
+  # write to pinged.txt
+  print A "$fromaddr $i\n";
 }
+
+close(A);
+
+print "To actually send mail:\nsh /var/tmp/bcping.sh\n";
+
+
 
