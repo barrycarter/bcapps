4a2bcd0710f72899cf0a48b29a8406d5320ee4e0
Barry Carter <Barry Carter>
2013-01-28 08:40:02 -0700

    checkpoint

--- 419/bc-hit-scammer.pl
+++ 419/bc-hit-scammer.pl
@@ -10,7 +10,25 @@ require "/usr/local/lib/bclib.pl";
 
 for $i (@addr) {
   $rand = int(rand()*(1e+10-1e+9))+1e+9;
+
+$msg = << "MARK";
+
+I have now sent the transfer fees by Western Union.
+
+SENDER: Carl Carlson
+MCTN: $rand
+
+Please confirm you have received these, thank you.
+
+MARK
+;
+
+
 #  sendmail("adam.adamson\@dudmail.com", $i, "I have sent the money", "The MTCN they gave me is $rand. Please reply back that you get this money, thank you!");
   # recorded twilio number forwarding to free sex line
-  sendmail("bob.bobson\@dudmail.com", $i, "Please call me now", "Sequel to your email, please call me at 802-275-4787 in the USA as soon as you can!");
+#  sendmail("bob.bobson\@dudmail.com", $i, "Please call me now", "Sequel to your email, please call me at 802-275-4787 in the USA as soon as you can!");
+
+#  sendmail("carl.carlson\@dudmail.com", $i, "Western Union Money Transfer", $msg);
+  sendmail("donald.donaldson\@dudmail.com", $i, "The FBI has called me", "The FBI wants to know more about our emails, what should I tell them?");
+
 }
