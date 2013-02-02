7fd37935fb17a2c08eb05a2a89ed063a2b9ff285
Barry Carter <Barry Carter>
2013-02-02 11:53:02 -0700

    Checkpoint

--- playground5.py
+++ playground5.py
@@ -11,5 +11,17 @@ def nsrise(date, lat, long, horizon, f, object, seek=1):
         return nsrise(date+timedelta(hours=12*seek), lat, long, horizon, f, object, seek)
 
 obs = ephem.Observer()
-print nsrise(datetime(2011,5,16,12), '89.5', '0', '-0:34', obs.previous_setting, ephem.Sun(), 1)
+lat, lon, date = '89.5', '0', datetime(2011,5,16,12)
+print "SR",nsrise(date,lat,lon,'-0:34',obs.previous_rising, ephem.Sun(),-1)
+print "SS",nsrise(date,lat,lon,'-0:34',obs.next_setting, ephem.Sun(),+1)
+
+print "CTS",nsrise(date,lat,lon,'-6',obs.previous_rising, ephem.Sun(),-1)
+print "CTE",nsrise(date,lat,lon,'-6',obs.previous_setting, ephem.Sun(),-1)
+print "CTS",nsrise(date,lat,lon,'-6',obs.next_rising, ephem.Sun(),+1)
+print "CTE",nsrise(date,lat,lon,'-6',obs.next_setting, ephem.Sun(),+1)
+
+print "MR",nsrise(date,lat,lon,'-0:34',obs.previous_rising, ephem.Moon(),-1)
+print "MS",nsrise(date,lat,lon,'-0:34',obs.previous_setting, ephem.Moon(),-1)
+print "MR",nsrise(date,lat,lon,'-0:34',obs.next_rising, ephem.Moon(),+1)
+print "MS",nsrise(date,lat,lon,'-0:34',obs.next_setting, ephem.Moon(),+1)
 
