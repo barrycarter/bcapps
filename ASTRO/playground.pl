558856a5312f3e3d1b0d54b4ebbf555e688f84ab
Barry Carter <Barry Carter>
2013-02-04 15:52:46 -0700

    Working, at least a little

--- ASTRO/playground.pl
+++ ASTRO/playground.pl
@@ -7,14 +7,36 @@ use Astro::Nova;
 use Data::Dumper 'Dumper';
 $Data::Dumper::Indent = 0;
 
-$now = Astro::Nova::get_julian_from_sys();
+# my location
+my $observer = Astro::Nova::LnLatPosn->new();
+$observer->set_lat(35);
+$observer->set_lng(-106);
 
-$test = new Astro::Nova::LnLatPosn();
-$test::lat = 35;
+# julian day
+$jd = Astro::Nova::get_julian_from_sys();
 
+# solar geocentric(?) coords
+$c1 = Astro::Nova::get_solar_geom_coords($jd);
+
+# ra/dec
+$radec = Astro::Nova::get_solar_equ_coords($jd);
+
+debug("RADEC: ",$radec->get_ra(), $radec->get_dec());
+
+die "TESTING";
+
+$dms = Astro::Nova::DMS->new(0,25,03,22);
+debug($dms->get_degrees());
+debug($dms->get_minutes());
+debug(dump_var($dms));
+
+die "TESTING";
+
+$observer->set_lat(Astro::Nova::DMS->from_string("49 degrees")->to_degrees);
+$observer->set_lng(Astro::Nova::DMS->from_string("8 E"));
 
 # debug("NOW",dump_var($now));
-debug(dump_var($test));
+debug(dump_var($observer));
 
 =item docs
 
