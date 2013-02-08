9a86ac72ec11b663dd062c85d1a59bb84205b095
Barry Carter <Barry Carter>
2013-02-07 20:00:28 -0700

    Cleanup?

---
+++ 419/bc-fiesta.pl
@@ -0,0 +1,17 @@
+#!/bin/perl
+
+# uses imacro to add to list of scammers without hitting fiesta.cc
+# "large list limit"
+
+require "/usr/local/lib/bclib.pl";
+
+@addr = split(/\n/,`egrep -v '^#|^\$' /home/barrycarter/BCGIT/419/confirmed.txt`);
+
+for $i (@addr) {
+print << "MARK";
+TAG POS=1 TYPE=INPUT:TEXT FORM=ACTION:/add ATTR=ID:add-field CONTENT=$i
+TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:add ATTR=VALUE:Add<SP>to<SP>list
+MARK
+;
+}
+
