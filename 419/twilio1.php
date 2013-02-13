9f6d0bdcca03f8fa1b26fc10cc875d53ceab49ae
Barry Carter <Barry Carter>
2013-02-12 17:44:31 -0700

    Checkpoint

--- 419/twilio1.php
+++ 419/twilio1.php
@@ -7,7 +7,25 @@
   // comments without screwing up XML, may come in useful later, and
   // means I can use a non-XML extension
 
-  // initial test: tellme (1-800-555-TELL)
+  // initial test: tellme (1-800-555-TELL) + time temp
+
+  // I can get twilio numbers easily, first test is to +1 940-468-3927
+
+  // verified scammers and the recordings that verify them:
+
+  // 17042492811 james newell (not yet verified)
+  // 15856266050 doris brock (not yet verified)
+
+  // Verification procedure:
+  //
+  // find email with phone number
+  // tell sender i am sending code words by phone
+  // reset number below to scammer phone number
+  // mirror file to live site
+  // determine code words (randomly)
+  // note down code words for number to see if i get them back
+  // prep mac speech code to read form letter
+  // call with code words [repeat twice if possible]
 
 header("Content-type: application/xml"); 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
@@ -16,6 +34,6 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <Response>
 <Dial record="true">
-<Number>+18005558355</Number>
+<Number>+15856266050</Number>
 </Dial>
 </Response>
