9eef1c808dfd59d4d0f3f59ae59ca3acdf76e536
Barry Carter <Barry Carter>
2013-02-15 13:13:42 -0700

    Checkpoint

--- FOODTRACK/bc-nutrisystem2db.pl
+++ FOODTRACK/bc-nutrisystem2db.pl
@@ -8,6 +8,18 @@ require "/usr/local/lib/bclib.pl";
 
 gnumeric2sqlite3("/home/barrycarter/BCGIT/FOODTRACK/nutrisystem.gnumeric", "foods", "/tmp/myfoods.db");
 
+# you would think sqlite3hashlist and hashlist2sqlite do the opposite
+# of each other, but not really, because, together they can fix broken
+# column order
+@res = sqlite3hashlist("SELECT * FROM foods WHERE UPC", "/tmp/myfoods.db");
+@querys = hashlist2sqlite(\@res, "foods");
+
+for $i (@querys) {print "$i;\n";}
+
+exit();
+
+
+
 my(@coln) = sqlite3cols("foods", "/tmp/myfoods.db");
 my(@colo) = sqlite3cols("foods", "/home/barrycarter/BCINFO/sites/DB/myfoods.db");
 
