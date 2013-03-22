29348afd3b1d1f0e2978133d1468d56883f94220
Barry Carter <Barry Carter>
2013-03-22 11:22:08 -0600

    Checkpoint

--- bc-make-skype.pl
+++ bc-make-skype.pl
@@ -3,7 +3,8 @@
 # trivial script to convert skype logs to text (thin wrapper around
 # Makefile.skypelog) for all my skype accounts)
 
-# one of my few scripts that does NOT use bclib.pl!
+# one of my few scripts that didnt use bclib.pl (but now does)
+require "/usr/local/lib/bclib.pl";
 
 # does not work with sh for some reason
 $ENV{SHELL} = "tcsh";
@@ -16,5 +17,6 @@ for $i (glob ("/home/barrycarter/.Skype/*/")) {
     system("cp /home/barrycarter/BCGIT/Makefile.skypelog $i");
   }
 
-  system("make");
+  my($out,$err,$res) = cache_command("make");
+  debug("OUT: $out", "ERR: $err", "RES: $res");
 }
