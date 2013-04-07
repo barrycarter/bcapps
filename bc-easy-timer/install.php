43b3d6811f940b20791b6a8c46b87cac45b31b0d
Barry Carter <Barry Carter>
2013-04-07 15:36:23 -0600

    No longer piggybacking on this existing plugin

--- bc-easy-timer/install.php
+++
@@ -1,9 +0,0 @@
-<?php $easy_timer_default_options = array(
-'cookies_lifetime' => 15,
-'default_timer_prefix' => 'dhms',
-'javascript_enabled' => 'yes');
-
-$easy_timer_options = get_option('easy_timer');
-foreach ($easy_timer_default_options as $key => $value) {
-if ($easy_timer_options[$key] == '') { $easy_timer_options[$key] = $easy_timer_default_options[$key]; } }
-update_option('easy_timer', $easy_timer_options);
