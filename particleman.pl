4b3f807ee8a84dd30d29e21295631ac10bafb807
Barry Carter <Barry Carter>
2013-04-11 16:57:08 -0600

    Checkpoint

--- particleman.pl
+++ particleman.pl
@@ -2,5 +2,38 @@
 
 # attempt to convert particleman.c to Perl
 require "/usr/local/lib/bclib.pl";
-use OpenGL;
+use OpenGL qw(:all);
 
+# originally main:
+glutInit();
+
+# use double buffering + depth buffering
+glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+
+# optional: window size
+glutInitWindowSize(800,600);
+
+# create the window
+glutCreateWindow("Particle Man");
+
+# glutDisplayFunc(): what to do when display is damaged
+glutDisplayFunc(display);
+
+# glutIdleFunc(): what to do when application is idle
+# often same as glutDisplayFunc()
+glutIdleFunc(display);
+
+glutMainLoop();
+
+<>;
+
+sub display {
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+glPushMatrix();
+glBegin(GL_POINTS);
+glColor3f(128,128,128);
+glVertex2f(10,10);
+glEnd();
+glPopMatrix();
+glutSwapBuffers();
+}
