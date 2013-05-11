4bab2051a597ea520b76ed610f087b20f15c548f
Barry Carter <Barry Carter>
2013-05-10 19:07:47 -0600

    More garbage

---
+++ bc-null.pl
@@ -0,0 +1,5 @@
+#!/bin/perl
+
+# sometimes I need a symlink to be a script that does nothing at all;
+# this is it (I have no idea why I made it a Perl script...)
+
--- greatcircle.m
+++ greatcircle.m
@@ -3,11 +3,25 @@
 (* Let p1 and p2 be the (unit) vectors representing lat1,lon1 and
 lat2,lon2 on the unit sphere: *)
 
+(* p3 is our desired solution *)
+
 p1={Cos[lon1]*Cos[lat1], Sin[lon1]*Cos[lat1], Sin[lat1]}
 p2={Cos[lon2]*Cos[lat2], Sin[lon2]*Cos[lat2], Sin[lat2]}
+p3={Cos[lon3]*Cos[lat3], Sin[lon3]*Cos[lat3], Sin[lat3]}
 
 p1={x1,y1,z1}
 p2={x2,y2,z2}
+p3={x3,y3,z3}
+
+(* the cross product p1 x p2 is the vector perpendicular to the p1/p2/origin plane *)
+
+cp = Cross[p1,p2]
+
+(* find vector whose dot product with p1 is given quantity AND vector
+must also be in plane *)
+
+Solve[{p3.p1==r,p3.Cross[p1,p2]==0,Norm[p3]==1},{lon3,lat3}]
+
 
 norm[v_] := Simplify[Sum[v[[i]]^2,{i,1,Length[v]}]]
 
@@ -45,4 +59,27 @@ t[ang_] = Simplify[t /. Solve[ang[t] == ang, t][[1]]]
 (* point on sphere corresponding to angle *)
 
 xyz[ang_] = Simplify[v2[t[ang]]]
+
+(* below is just numerical playing *)
+
+(* journey 35N -120W to 35N +120E *)
+
+lat1 = 35. Degree;
+lat2 = 35. Degree;
+lon1 = -120. Degree;
+lon2 = +120. Degree;
+p1={Cos[lon1]*Cos[lat1], Sin[lon1]*Cos[lat1], Sin[lat1]}
+p2={Cos[lon2]*Cos[lat2], Sin[lon2]*Cos[lat2], Sin[lat2]}
+cp = Cross[p1,p2]
+
+p3={Cos[lon3]*Cos[lat3], Sin[lon3]*Cos[lat3], Sin[lat3]}
+
+p3 /. 
+Solve[{p3.p1==Cos[10 Degree],p3.Cross[p1,p2]==0,Norm[p3]==1},{lon3,lat3}][[1]]
+
+ListPointPlot3D[{{p1,p2,%}},PlotRange->{x,-1,1}]
+
+
+
+
 
