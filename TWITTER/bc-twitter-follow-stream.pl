b3678f0e61032d9ddab32a0c4fe6f26466ea4ed1
Barry Carter <Barry Carter>
2013-06-03 14:14:18 -0600

    Checkpoint

---
+++ TWITTER/bc-twitter-follow-stream.pl
@@ -0,0 +1,13 @@
+#!/bin/perl
+
+require "/usr/local/lib/bclib.pl";
+require "/home/barrycarter/bc-private.pl";
+
+# allows multiple people (not just me) to follow others based on
+# individualized interests from the live twitter stream
+
+# NOTE: I use *my* (not clients) *twitter* (not supertweet) id to
+# connect to the stream. In other words, I could run this program for
+# other people using my twitter username/password, even if I didn't
+# want to follow anyone myself
+
