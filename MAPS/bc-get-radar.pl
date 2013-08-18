24f3dad39d63ca56b415b301187c4aac1fa833fb
Barry Carter <Barry Carter>
2013-08-17 18:57:51 -0600

    Checkpoint

--- MAPS/bc-get-radar.pl
+++ MAPS/bc-get-radar.pl
@@ -4,16 +4,28 @@
 
 require "/usr/local/lib/bclib.pl";
 
+$site = "ABX";
+
 for $i ("N0R","N0S","N0V","N0Z","N1P","NCR","NET","NTP","NVL") {
-  # TODO: decide on "best" URL here
-#  my($url) = "http://radar.weather.gov/ridge/RadarImg/$i/ABX_${i}_0.gif";
-  my($url) = "http://radar.weather.gov/ridge/lite/$i/ABX_0.png";
+  # this one is current even when timestamp looks odd
+  my($url) = "http://radar.weather.gov/ridge/RadarImg/$i/${site}_${i}_0.gif";
+
   # radar is updated every 3m, so 90s is a good cache time
   # cache_command2 doesn't support retfile yet
   my($out) = cache_command("curl $url","age=90&retfile=1");
-  # convert to half-transparent png
-  system("convert $out -transparent white -channel Alpha -evaluate Divide 2 /var/tmp/ABX_$i.png");
 
+  # convert to half-transparent png
+  system("convert $out -transparent white -channel Alpha -evaluate Divide 2 /var/tmp/${site}_$i.png");
+
+  # these files almost never change...
+  ($out,$err,$res) = cache_command2("curl http://radar.weather.gov/ridge/RadarImg/$i/${site}_${i}_0.gfw", "age=86400");
+
+  # from http://forecast.weather.gov/jetstream/doppler/gis.htm
+  my($xp, $r1, $r2, $yp, $xc, $yc) = split(/\r\n/, $out);
+  # assuming 600x550
+  my($sc) = $yc+$yp*550;
+  my($ec) = $xc+$xp*600;
+  debug("$xc/$ec");
   # create KML file (the box is hardcoded for Albuquerque; in theory,
   # could use GFX files to determine bounding box)
   $str = << "MARK";
@@ -22,18 +34,18 @@ for $i ("N0R","N0S","N0V","N0Z","N1P","NCR","NET","NTP","NVL") {
 <Document>
 <GroundOverlay>
 <Icon>
-<href>http://data.bcinfo3.barrycarter.info/ABX_$i.png</href>
+<href>http://data.bcinfo3.barrycarter.info/${site}_$i.png</href>
 </Icon>
 <LatLonBox>
-<north>37.5650361494585</north>
-<south>32.726168961958</south>
-<east>-104.17921697443</east>
-<west>-109.457981178977</west>
+<north>$yc</north>
+<south>$sc</south>
+<east>$ec</east>
+<west>$xc</west>
 </LatLonBox>
 </GroundOverlay>
 </Document>
 </kml>
 MARK
 ;
-  write_file($str,"/var/tmp/ABX_$i.kml");
+  write_file($str,"/var/tmp/${site}_$i.kml");
 }
