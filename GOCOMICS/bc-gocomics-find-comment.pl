6a20bafe3112d8178bfc126e3be96c4db72905fc
Barry Carter <Barry Carter>
2013-08-21 12:47:17 -0600

    Checkpoint

--- GOCOMICS/bc-gocomics-find-comment.pl
+++ GOCOMICS/bc-gocomics-find-comment.pl
@@ -8,15 +8,29 @@
 
 require "/usr/local/lib/bclib.pl";
 
-# find initial slope
+# find initial slope (TODO: put this in loop?)
 @p1=get_page_comments(1);
 @p2=get_page_comments(2);
-$m1 = median(\@p1);
-$m2 = median(\@p2);
-$slope = $m1-$m2;
+# debug("P1",@p1,"P2",@p2);
+$median{1} = median(\@p1);
+$median{2} = median(\@p2);
+debug("$median{1} vs $median{2}");
 
 for $i (@ARGV) {
+  # initial slope and page (for each comment that we seek)
+  $slope = $median{1}-$median{2};
+  $page = 1;
   debug("SEARCHING FOR: $i");
+
+  # enter loop to find page
+  for (;;) {
+    debug("SLOPE/PAGE: $slope/$page/$i");
+    my($guess) = int($page + ($median{$page}-$i)/$slope);
+    my(@guessids) = get_page_comments($guess);
+    debug("GUESS: $guess, IDS:",@guessids);
+    die "TESTING"
+  }
+
 }
 
 die "TESTING";
@@ -38,7 +52,7 @@ for (;;) {
 sub get_page_comments {
   my($page) = @_;
   my(@ids);
-  my($out,$err,$res) = cache_command("curl -H 'Accept: text/html' -A 'Fauxzilla' http://www.gocomics.com/comments/page/$page","age=3600");
+  my($out,$err,$res) = cache_command2("curl -H 'Accept: text/html' -A 'Fauxzilla' http://www.gocomics.com/comments/page/$page","age=3600");
   while ($out=~s/comment_(\d+)//) {push(@ids,$1);}
   return @ids;
 }
