bd85d81d3e041e802cfb3121bc3c38264126db28
Barry Carter <Barry Carter>
2013-08-21 19:52:19 -0600

    Moved cs2cs back to bc-draw-map.pl for cleanup

--- bclib.pl
+++ bclib.pl
@@ -3494,52 +3494,6 @@ sub my_tmpfile2() {
   return "/var/tmp/cache/$d1/$d2/$x";
 }
 
-=item cs2cs(\@lonlat, $proj, $options)
-
-Given a list of longitude/latitudes (simple list of even length),
-return the mapping of these longitude/latitudes under cs2cs projection
-$proj as a hash such that:
-
-$rethash{$lon}{$lat}{x} = the x coordinate of the transform
-$rethash{$lon}{$lat}{y} = the y coordinate of the transform
-$rethash{$lon}{$lat}{z} = the z coordinate of the transform
-
-A simple wrapper around cs2cs.
-
-$options currently unused
-
-=cut
-
-sub cs2cs {
-  my($listref, $proj, $options) = @_;
-  my(@lonlat) = @{$listref};
-  my($str);
-  my(%rethash);
-
-  # write data to file
-  while (@lonlat) {
-    # TODO: this may break if right side eval isn't in order
-    my($lon,$lat) = (shift(@lonlat),shift(@lonlat));
-    $str .= "$lon $lat\n";
-  }
-
-  my($tmpfile) = my_tmpfile2();
-  write_file($str, $tmpfile);
-  debug("TMPFILE: $tmpfile");
-
-  # -r since we're doing lonlat, not latlon; oddly, "lonlat" for proj4
-  # ALSO requires lat/lon order, bizarre
-  my($out,$err,$res) = cache_command("cs2cs -r -E -e 'ERR ERR' +proj=latlon +to +proj=$proj < $tmpfile","age=86400");
-  for $i (split(/\n/,$out)) {
-    my(@fields) = split(/\s+/,$i);
-    $rethash{$fields[0]}{$fields[1]}{x} = $fields[2];
-    $rethash{$fields[0]}{$fields[1]}{y} = $fields[3];
-    $rethash{$fields[0]}{$fields[1]}{z} = $fields[4];
-  }
-
-  return %rethash;
-}
-
 # cleanup files created by my_tmpfile (unless --keeptemp set)
 sub END {
   debug("END: CLEANING UP TMP FILES");
