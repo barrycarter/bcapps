faa0fc1eab680ac3222cd3f7765090357a4fb68c
Barry Carter <Barry Carter>
2013-09-08 21:29:44 -0600

    Checkpoint

--- WEATHER/bc-voronoi-new.pl
+++ WEATHER/bc-voronoi-new.pl
@@ -8,18 +8,14 @@ require "/usr/local/lib/bclib.pl";
 # temporary db for testing (is local)
 $db = "/home/barrycarter/20130908/metarnew.db";
 
-$query = << "MARK";
+$metar_query = "SELECT station_id, latitude, longitude, temp_c*1.8+32 AS temp_f FROM metar_now";
+$buoy_query = "SELECT STN AS station_id, LAT AS latitude, LON AS longitude,
+ATMP*1.8+32 AS temp_f FROM buoy_now WHERE ATMP!='MM'";
+$ship_query = "SELECT station_id, latitude, longitude, temp_c*1.8+32 AS temp_f FROM ship_now WHERE temp_c!=''";
 
-SELECT station_id, latitude, longitude, temp_c*1.8+32 AS temp_f FROM
-metar_now UNION
-SELECT STN AS station_id, LAT AS latitude, LON AS longitude,
-ATMP*1.8+32 AS temp_f FROM buoy_now WHERE ATMP!='MM' UNION
-SELECT station_id, latitude, longitude, temp_c*1.8+32 AS temp_f FROM
-ship_now WHERE temp_c!=''
+$query = "$metar_query UNION $buoy_query UNION $ship_query";
 
-MARK
-;
-
+# warn "TESTING";
 @res = sqlite3hashlist($query,$db);
 
 # mathematica format (testing)
@@ -27,6 +23,9 @@ my(@print);
 for $i (@res) {
   # this is an error that I've fixed, but need to ignore for now
   if ($i->{longitude}=~/\-$/) {next;}
+  # not sure what causes this..
+  if (abs($i->{latitude})>90) {next;}
+
   push(@print,"{$i->{longitude}, $i->{latitude}, $i->{temp_f}}");
 }
 
