4137af229fce4d76fa35fd0faff925c38e7e7517
Barry Carter <Barry Carter>
2013-09-20 10:55:22 -0600

    Checkpoint

--- MAPS/bc-get-radar.pl
+++ MAPS/bc-get-radar.pl
@@ -9,6 +9,10 @@ require "/usr/local/lib/bclib.pl";
 
 dodie('chdir("/var/tmp/radar")');
 
+debug(get_current_radar("ABX","N0R"));
+
+die "TESTING";
+
 my($out,$err,$res);
 my(@overlays);
 
@@ -138,3 +142,40 @@ write_file($str,"/sites/data/radar_$i.kml");
 if ($globopts{nodaemon}) {exit(0);}
 sleep(60);
 exec($0);
+
+=item get_current_radar($station,$type)
+
+Obtain the current $type radar for $station and put it in
+/var/tmp/radar. Return the filename (which also contains the
+timestamp)
+
+If latest radar is in /var/tmp/radar, just return filename
+
+=cut
+
+sub get_current_radar {
+  my($station,$type) = @_;
+
+  # obtain site index (upto 1m old)
+  # TODO: putting this in file (for debugging) is ugly
+  # TODO: allow parallelizing this, one at a time may be too slow
+  my($out,$err,$res) = cache_command2("curl -o /var/tmp/radar/$station-$type.out http://radar.weather.gov/ridge/RadarImg/$type/$station/","age=60");
+  $out = read_file("/var/tmp/radar/$station-$type.out");
+
+  # look for hrefs sort by time reversed
+  my(@files) = reverse(sort($out=~m/<a href="($station.*?)"/sg));
+
+  # attempt to get most current
+  for $i (@files) {
+    # if we already have this one and its over the dreaded 331 bytes, return it
+    if (-f "/var/tmp/radar/$i" && -s "/var/tmp/radar/$i" > 331) {return $i;}
+    # we don't have it, so try to get it
+    ($out,$err,$res) = cache_command2("curl -o /var/tmp/radar/$i http://radar.weather.gov/ridge/RadarImg/$type/$station/$i");
+    # if we have it now, return it
+    if (-f "/var/tmp/radar/$i" && -s "/var/tmp/radar/$i" > 331) {return $i;}
+  }
+
+  # this means we didnt get it at all!
+  return;
+}
+
