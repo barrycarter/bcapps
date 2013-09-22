ff3ad372f499ef1013b19550bc8a78f4124ae4ab
Barry Carter <Barry Carter>
2013-09-22 15:48:22 -0600

    Checkpoint

--- WEATHER/playground.pl
+++ WEATHER/playground.pl
@@ -20,17 +20,33 @@ sub get_raws_obs {
   # use parallel
   local(*A);
   open(A, "| parallel -j 10");
+  my(@stns);
 
   # find hrefs and push to "well known" files
   while ($out=~s%"(http://raws.wrh.noaa.gov/.*?)"%%s) {
     my($url) = $1;
     unless ($url=~/stn\=(.*)/s) {next;}
     my($stn) = $1;
+    push(@stns,$stn);
     # if we have a file less than an hour old, do nothing
     if (-f $stn && -M $stn < 1/24.) {next;}
     print A "curl -o $stn '$url'\n";
   }
-
   close(A);
+
+  # now look thru data
+  for $i (@stns) {
+    my($data) = read_file($i);
+    # get lat/lon/name/id
+    $data=~s/\={20,}\s*(.*?)(\r|\n)+.*\={20,}\s+//s;
+    my($meta) = $1;
+    # first 22 chars are name
+    $meta=~s/^(.{22})//;
+    my($name) = $1;
+    # rest don't have spaces, so...
+    my($day, $time, $temp, $dew, $wind) = 
+
+    debug("NAME: $name");
+  }
 }
 
