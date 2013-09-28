4ea84a76a6d336b4a849a9e12e55bf7432e96a6a
Barry Carter <Barry Carter>
2013-09-28 14:08:52 -0600

    Checkpoint

--- GEONAMES/bc-geonames2sqlite.pl
+++ GEONAMES/bc-geonames2sqlite.pl
@@ -11,11 +11,6 @@
 # out of date anyway); please obtain it yourself:
 # http://download.geonames.org/export/dump/allCountries.zip
 
-# These files are assumed to be in the current directory:
-# allCountries.zip
-# admin1CodesASCII.txt
-# countryInfo.txt
-
 use utf8;
 use Text::Unidecode;
 use Math::Round;
@@ -235,46 +230,3 @@ sub cleanup {
 
   return $name;
 }
-
-=item sql
-
-To actually create the sqlite3 db, run these commands
-
-CREATE TABLE geonames (
- geonameid INTEGER PRIMARY KEY,
- asciiname TEXT,
- latitude INT,
- longitude INT,
- feature_code INT,
- parent INT,
- population INT,
- timezone INT,
- elevation INT
-);
-
-CREATE INDEX i_feature_code ON geonames(feature_code);
-CREATE INDEX i_parent ON geonames(parent);
-.separator "\t"
-.import /var/tmp/geonames.out geonames
-
-CREATE TABLE altnames (
- geonameid INT,
- name TEXT
-);
-
-CREATE INDEX i_name ON altnames(name);
-.import /var/tmp/altnames2.out altnames
-DELETE FROM altnames WHERE name = '';
-INSERT INTO altnames VALUES (0,'');
-INSERT INTO geonames (geonameid) VALUES (0);
-VACUUM;
-
-CREATE TABLE tzones (
- timezoneid INTEGER PRIMARY KEY,
- name TEXT
-);
-.separator "\t"
-.import /var/tmp/tzones.out tzones
-
-=cut
-
