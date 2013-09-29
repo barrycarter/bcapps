6ba8704c37c16532c0ba68d571182414e5147646
Barry Carter <Barry Carter>
2013-09-29 15:05:12 -0600

    Checkpoint... think I've almost got this

--- GEONAMES/geonames2.sql
+++ GEONAMES/geonames2.sql
@@ -1,24 +1,50 @@
 -- yet another attempt to create geonames "quickly" wo a perl script
 
+: in shell
+
+CREATE TABLE geonames (
+ geonameid INTEGER PRIMARY KEY,
+ asciiname TEXT,
+ latitude DOUBLE,
+ longitude DOUBLE,
+ feature_code INT,
+ country_code INT,
+ admin4_code INT,
+ admin3_code INT,
+ admin2_code INT,
+ admin1_code INT,
+ population INT, 
+ elevation INT,
+ timezone TEXT
+);
+
+: below in shell
+
+# all-scramble is a "sort -R"'d version of allCountries.txt, better for testing
+perl -F"\t" -anle 'print join("\t",@F[0,2,4,5,7,8,10..15,17])' all-scramble.txt | head -1000 > test1.txt
+
+.separator "\t"
+.import test1.txt geonames
+
 CREATE TEMPORARY TABLE geonames (
-geonameid,
-name,
-asciiname,
-alternatenames,
-latitude,
-longitude,
-featureclass,
-featurecode,
-countrycode,
-cc2,
-admin1code,
-admin2code,
-admin3code,
-admin4code,
-population,
-elevation,
+0 geonameid,
+1 name,
+2 asciiname,
+3 alternatenames,
+4 latitude,
+5 longitude,
+6 featureclass,
+7 featurecode,
+8 countrycode,
+9 cc2,
+10 admin1code,
+11 admin2code,
+12 admin3code,
+13 admin4code,
+14 population,
+15 elevation,
 dem,
-timezone,
+17 timezone,
 modificationdate DEFAULT ''
 );
 
