b6de831539f9e13794d38a3ca163dbc5a0a82ce6
Barry Carter <Barry Carter>
2013-10-20 16:27:13 -0600

    Weather stations parsed correctly at last!

--- WEATHER/bc-parse-stations.pl
+++ WEATHER/bc-parse-stations.pl
@@ -5,146 +5,235 @@
 # http://www.rap.ucar.edu/weather/surface/stations.txt
 # but only after parsing MLI list which trumps these lists
 
+# all subroutines must: check/set seen hash and close(A)
+
 # -noprint: don't print anything (useful for debugging)
 
 require "/usr/local/lib/bclib.pl";
 
-# which METAR are already in stations.db?
-@res = sqlite3hashlist("SELECT metar FROM stations","/sites/DB/stations.db");
-for $i (@res) {$seen{$i->{metar}}=1;}
+# in order of most useful info (I think)
+nsd();
+mli();
+ucar();
+meso();
 
-open(A, "fgrep stn= /home/barrycarter/BCGIT/WEATHER/meso_station.cgi.html|");
+# fake subroutine so I can control order in which files are read
+sub meso {
+  local(*A);
+  open(A, "fgrep stn= /home/barrycarter/BCGIT/WEATHER/meso_station.cgi.html|");
 
-# cols where data is
-@cols = (74, 83, 117, 122, 134, 147, 157);
+  # cols where data is
+  @cols = (74, 83, 117, 122, 134, 147, 157);
 
-while (<A>) {
-  debug("THUNK: $_");
-  my(@data) = column_data($_, [@cols]);
-  # clean data
-  for $i (@data) {
-    $i=~s/<.*?>//isg;
-    $i=~s/ft//sg;
-    $i=~s/\"//isg;
-    $i = trim($i);
+  while (<A>) {
+    debug("THUNK: $_");
+    my(@data) = column_data($_, [@cols]);
+    # clean data
+    for $i (@data) {
+      $i=~s/<.*?>//isg;
+      $i=~s/ft//sg;
+      $i=~s/\"//isg;
+      $i = trim($i);
+    }
+
+    # global SEEN hash
+    if ($seen{$data[0]}) {next;}
+    $seen{$data[0]} = 1;
+
+    # print for stations.db
+    print join("\t", $data[0], "NULL", $data[1], $data[2], "US", $data[3],
+	       $data[4], $data[5],
+	       "http://mesowest.utah.edu/cgi-bin/droman/meso_station.cgi"),
+		 "\n";
   }
-
-  debug("DATA",@data);
-  # print for stations.db
-  print join("\t", $data[0], "NULL", $data[1], $data[2], "US", $data[3],
-	    $data[4], $data[5], "http://mesowest.utah.edu/cgi-bin/droman/meso_station.cgi")."\n";
+  close(A);
 }
 
-die "TESTING";
+# fake subroutine
+sub nsd {
+  local(*A);
+  open(A,"/home/barrycarter/BCGIT/WEATHER/nsd_cccc_annotated.txt");
 
-open(A,"/home/barrycarter/BCGIT/WEATHER/nsd_cccc_annotated.txt");
-
-while (<A>) {
+  while (<A>) {
   # x1, x2 = useless to me
-  my($indi,$wmob,$wmos,$place,$state,$country,$wmoregion,$lat,$lon,$x1,$x2,$elev) = split(/\;/, $_);
-  # seen by MLI? if so, ignore
-  if ($seen{$indi}) {next;}
-  # record that we've seen this station
-  $seen{$indi}=1;
+    my($indi,$wmob,$wmos,$place,$state,$country,$wmoregion,$lat,$lon,$x1,$x2,$elev) = split(/\;/, $_);
 
-  # convert lat to decimal (probably much better ways to do this!)
-  if ($lat=~/^(\d{2})\-(\d{2})(N|S)/) {
-    ($lad,$lam,$las,$lax)=($1,$2,0,$3);
-  } elsif ($lat=~/^(\d{2})\-(\d{2})\-(\d{2})(N|S)/) {
-    ($lad,$lam,$las,$lax)=($1,$2,$3,$4);
-  } else {
-    die("BAD LAT: $lat");
+    # seen by anyone else?
+    if ($seen{$indi}) {
+      debug("NSD SEEN: $indi");
+      next;
+    }
+
+    debug("NSD NEW: $indi");
+    $seen{$indi}=1;
+
+    # convert lat to decimal (probably much better ways to do this!)
+    if ($lat=~/^(\d{2})\-(\d{2})(N|S)/) {
+      ($lad,$lam,$las,$lax)=($1,$2,0,$3);
+    } elsif ($lat=~/^(\d{2})\-(\d{2})\-(\d{2})(N|S)/) {
+      ($lad,$lam,$las,$lax)=($1,$2,$3,$4);
+    } else {
+      warn("BAD LAT: $lat");
+    }
+    
+    $flat=$lad+$lam/60+$las/3600;
+    if ($lax eq "S") {$flat=-$flat;}
+    
+    if ($lon=~/^(\d{2,3})\-(\d{2})(E|W)/) {
+      ($lod,$lom,$los,$lox)=($1,$2,0,$3);
+    } elsif ($lon=~/^(\d{2,3})\-(\d{2})\-(\d{2})(E|W)/) {
+      ($lod,$lom,$los,$lox)=($1,$2,$3,$4);
+    } else {
+      warn("BAD LON: $lon");
+    }
+
+    $flon=$lod+$lom/60+$los/3600;
+    if ($lox eq "W") {$flon=-$flon;}
+
+    # correct elevation, combo field for WMO
+    $elev = round2(convert($elev,"m","ft"));
+    $wmobs = $wmob*1000+$wmos;
+
+    debug("NSD ABOUT TO PRINT");
+    # print in importable format (for sqlite3)
+    print join("\t", ($indi, $wmobs, $place, $state, $country, $flat, $flon, $elev, "http://weather.noaa.gov/data/nsd_cccc.txt")),"\n";
   }
-
-  $flat=$lad+$lam/60+$las/3600;
-  if ($lax eq "S") {$flat=-$flat;}
-
-  if ($lon=~/^(\d{2,3})\-(\d{2})(E|W)/) {
-    ($lod,$lom,$los,$lox)=($1,$2,0,$3);
-  } elsif ($lon=~/^(\d{2,3})\-(\d{2})\-(\d{2})(E|W)/) {
-    ($lod,$lom,$los,$lox)=($1,$2,$3,$4);
-  } else {
-    die("BAD LON: $lon");
-  }
-
-  $flon=$lod+$lom/60+$los/3600;
-  if ($lox eq "W") {$flon=-$flon;}
-
-  # correct elevation, combo field for WMO
-  $elev = round2(convert($elev,"m","ft"));
-  $wmobs = $wmob*1000+$wmos;
-
-  # print in importable format (for sqlite3)
-  print join("\t", ($indi, $wmobs, $place, $state, $country, $flat, $flon, $elev, "http://weather.noaa.gov/data/nsd_cccc.txt")),"\n";
+  close(A);
 }
 
-close(A);
+sub ucar {
+  # columns where data starts (not actually all data, just start/end cols I need)
+  @cols = (0, 3, 20, 26, 32, 39, 47, 55, 61, 80, 99);
+  local(*A);
+  open(A,"/home/barrycarter/BCGIT/WEATHER/stations.txt");
 
-# now, parse stations.txt (the UCAR list)
-# columns where data starts (not actually all data, just start/end cols I need)
-@cols = (0, 3, 20, 26, 32, 39, 47, 55, 61, 80, 99);
-open(A,"/home/barrycarter/BCGIT/WEATHER/stations.txt");
+  while (<A>) {
+    # ignore comments (start with "!") and blank lines
+    if (/^\!/ || /^\s*$/) {next;}
 
-while (<A>) {
-  # ignore comments (start with "!") and blank lines
-  if (/^\!/ || /^\s*$/) {next;}
+    # if third column is non-blank, this is a header line, not a data line
+    unless (substr($_,2,1)=~/\s/) {next;}
 
-  # if third column is non-blank, this is a header line, not a data line
-  unless (substr($_,2,1)=~/\s/) {next;}
+    # get data from columns
+    @data=();
+    for $j (0..$#cols) {
+      $item = substr($_, $cols[$j], $cols[$j+1]-$cols[$j]);
+      $item=trim($item);
+      push(@data,$item);
+    }
 
-  # get data from columns
-  @data=();
-  for $j (0..$#cols) {
-    $item = substr($_, $cols[$j], $cols[$j+1]-$cols[$j]);
-    $item=trim($item);
-    push(@data,$item);
+    # assign data to vars
+    ($state, $name, $code, $iata, $synop, $lat, $lon, $elev, $junk, $country) = @data;
+
+    # ignore blank codes and seen codes and header "ICAO" code
+    if ($code=~/^\s*$/ || $seen{$code} || $code=~/^ICAO$/) {next;}
+
+    # third col test above insufficient; if $code starts with number, ignore
+    if ($code=~/^\d/) {next;}
+
+    # have we seen this code?
+    if ($seen{$code}) {next;}
+    $seen{$code} = 1;
+
+    # icky code copying from above, except "-" becomes " "
+    # convert lat to decimal (probably much better ways to do this!)
+    if ($lat=~/^(\d{2})\s(\d{2})(N|S)/) {
+      ($lad,$lam,$las,$lax)=($1,$2,0,$3);
+    } elsif ($lat=~/^(\d{2})\s(\d{2})\s(\d{2})(N|S)/) {
+      ($lad,$lam,$las,$lax)=($1,$2,$3,$4);
+    } else {
+      warn("BAD LAT: $lat");
+    }
+
+    $flat=$lad+$lam/60+$las/3600;
+    if ($lax eq "S") {$flat=-$flat;}
+
+    if ($lon=~/^(\d{2,3})\s(\d{2})(E|W)/) {
+      ($lod,$lom,$los,$lox)=($1,$2,0,$3);
+    } elsif ($lon=~/^(\d{2,3})\s(\d{2})\s(\d{2})(E|W)/) {
+      ($lod,$lom,$los,$lox)=($1,$2,$3,$4);
+    } else {
+      warn("BAD LON: $lon");
+    }
+
+    $flon=$lod+$lom/60+$los/3600;
+    if ($lox eq "W") {$flon=-$flon;}
+
+    # correct elevation
+    $elev = round2(convert($elev,"m","ft"));
+
+    # print in importable format (for sqlite3)
+    print join("\t", ($code, $wmobs, $name, $state, $country, $flat, $flon, $elev, "http://www.rap.ucar.edu/weather/surface/stations.txt")),"\n";
   }
-
-  # assign data to vars
-  ($state, $name, $code, $iata, $synop, $lat, $lon, $elev, $junk, $country) = @data;
-
-  debug("JUNK: $junk, COUNTRY: $country");
-
-  # ignore blank codes and seen codes and header "ICAO" code
-  if ($code=~/^\s*$/ || $seen{$code} || $code=~/^ICAO$/) {next;}
-
-  # third col test above insufficient; if $code starts with number, ignore
-  if ($code=~/^\d/) {next;}
-
-#  debug("LAT: $lat, LONG: $lon");
-
-  # icky code copying from above, except "-" becomes " "
-  # convert lat to decimal (probably much better ways to do this!)
-  if ($lat=~/^(\d{2})\s(\d{2})(N|S)/) {
-    ($lad,$lam,$las,$lax)=($1,$2,0,$3);
-  } elsif ($lat=~/^(\d{2})\s(\d{2})\s(\d{2})(N|S)/) {
-    ($lad,$lam,$las,$lax)=($1,$2,$3,$4);
-  } else {
-    warn("BAD LAT: $lat");
-  }
-
-  $flat=$lad+$lam/60+$las/3600;
-  if ($lax eq "S") {$flat=-$flat;}
-
-  if ($lon=~/^(\d{2,3})\s(\d{2})(E|W)/) {
-    ($lod,$lom,$los,$lox)=($1,$2,0,$3);
-  } elsif ($lon=~/^(\d{2,3})\s(\d{2})\s(\d{2})(E|W)/) {
-    ($lod,$lom,$los,$lox)=($1,$2,$3,$4);
-  } else {
-    warn("BAD LON: $lon");
-  }
-
-  $flon=$lod+$lom/60+$los/3600;
-  if ($lox eq "W") {$flon=-$flon;}
-
-  # correct elevation
-  $elev = round2(convert($elev,"m","ft"));
-
-  # print in importable format (for sqlite3)
-  print join("\t", ($code, $wmobs, $name, $state, $country, $flat, $flon, $elev, "http://www.rap.ucar.edu/weather/surface/stations.txt")),"\n";
+  close(A);
 }
 
-close(A);
+sub mli {
+
+  @ls=split(/\n/,read_file("master-location-identifier-database-20130801.csv"));
+  # get rid of useless lines
+  for (1..4) {shift(@ls);}
+  # array-ify
+  map(push(@res, [csv($_)]), @ls);
+  ($hlref) = arraywheaders2hashlist(\@res);
+
+  for $i (@{$hlref}) {
+
+  # if ICAO blank, try to find it elsewhere
+    unless ($i->{icao}=~/\S/) {
+      if ($i->{icao_xref}=~/\S/) {
+	debug("CASE XREF");
+	$i->{icao} = $i->{icao_xref};
+      } elsif ($i->{stn_key}=~s/^USaa//) {
+	debug("CASE STNKEY");
+	$i->{icao} = $i->{stn_key};
+      } else {
+	debug("LINE HAS NO ICAO");
+	next;
+    }
+  }
+
+
+    # TODO: should I really ignore non-metar?
+
+    # ignore things without lat/lon too
+    $err = 0;
+    for $j ("icao", "lat_prp", "lon_prp") {
+      # just spaces and apostrophes
+      if ($i->{$j}=~m/^[\'\s]*$/) {$err=1;}
+    }
+
+    # impossible lat/lon
+    if (abs($i->{lat_prp})>90 || abs($i->{lon_prp})>180) {$err=1;}
+    
+    if ($err) {
+      debug("SKIPPING: ($i->{icao}) ($i->{lat_prp}) ($i->{lon_prp})");
+      next;
+    }
+
+    # no duplicates
+    if ($seen{$i->{icao}}) {next;}
+    $seen{$i->{icao}} = 1;
+
+    # cheat to make city look nicer
+    $i->{city}=~s/\|(.*)$/ ($1)/;
+
+    # most fields can be used as is (region -> state)
+    @l = ();
+    for $j ("icao", "wmo", "city", "region", "country", "lat_prp", "lon_prp") {
+      # if it has nothing but dashes/spaces, it's null
+      if ($i->{$j}=~/^[\s\-]*$/) {$i->{$j} = "NULL";}
+      push(@l, $i->{$j});
+    }
+
+    # fix elevation
+    push(@l, round2(convert($i->{elev_baro},"m","ft")));
+    # and source
+    push(@l, "http://www.weathergraphics.com/identifiers/master-location-identifier-database-20130801.csv");
+    print join("\t",@l),"\n";
+  }
+}
+
 
 =item schema
 
