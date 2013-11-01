598e0be9c71bae8466183b022626aa4099b83072
Barry Carter <Barry Carter>
2013-11-01 15:44:56 -0600

    Checkpoint

--- WEATHER/bc-meso-api.pl
+++ WEATHER/bc-meso-api.pl
@@ -12,9 +12,22 @@ my($out,$err,$res) = cache_command2($cmd,"age=3600");
 unless ($out=~/TOKEN\":\s*\"(.*?)\"/) {die "NO TOKEN";}
 my($token) = $1;
 
-$url = "http://api.mesowest.net/stations/?bbox=-110,35,-100,40&token=$token";
+# $url = "http://api.mesowest.net/stations?bbox=-110,35,-100,40&token=$token";
 # $url = "http://api.mesowest.net/stations?&state=dc,de&jsonformat=1&token=$token";
+# $url = "http://api.mesowest.net/stations?&st&token=$token";
+# $url = "http://api.mesowest.net/stations?bbox=-110,35,-100,40&status=active&complete=1&token=$token";
+# $url = "http://api.mesowest.net/stations?bbox=-107,35,-106,36&status=active&complete=1&token=$token";
+
+$url = "http://api.mesowest.net/stations?bbox=-107,35,-106,36&status=active&complete=1&latestobs=1&token=$token";
+
 ($out,$err,$res) = cache_command2("curl '$url'", "age=3600");
 
-debug("OUT: $out");
+# de-JSON-ify
+$json = JSON::from_json($out);
+
+debug(var_dump("json",$json));
+
+# debug("JSON: $json");
+
+# debug("OUT: $out");
 
