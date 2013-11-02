44cc3127ee96697a816efe1babf0e150734a810d
Barry Carter <Barry Carter>
2013-11-02 07:36:57 -0600

    Checkpoint

--- ASTRO/bc-get-astro.pl
+++ ASTRO/bc-get-astro.pl
@@ -18,6 +18,53 @@ my($lng,$lat) = (-106.55, 35.1);
 # determine next minute
 my($nm) = 60*floor(time()/60)+60;
 
-debug("NM: $nm");
+%sm = sunmooninfo($lng,$lat,$nm);
+
+# if the sun is above -6 degrees, we want start/end of "current"
+# twilight; otherwise, start/end of next twilight; same for naut/astro
+
+# TODO: this is inefficient, because I build the observer object
+# multiple times
+
+# altitudes for twilights
+%alts = ("astro" => -18, "naut" => -12, "civ" => -6, "sun" => -5/6);
+
+for $i ("astro", "naut", "civ", "sun") {
+  if ($sm{sun}{alt} >= $alts{$i}) {
+    # if we are in this/higher state, give previous "rise", next "set"
+    push(@times, np_rise_set($lng, $lat, $nm, $i, "rise", -1));
+    push(@times, np_rise_set($lng, $lat, $nm, $i, "set", +1));
+    # $cur will be highest state we've reached, empty for night
+    $cur = $i;
+  } else {
+    # not in this state? give next rise/set
+    push(@times, np_rise_set($lng, $lat, $nm, $i, "rise", -1));
+    # TODO: un-redundant line below
+    push(@times, np_rise_set($lng, $lat, $nm, $i, "set", +1));
+  }
+}
+
+# and moon
+if ($sm{moon}{alt} >= 0.125) {
+  # moon is up, give previous rise (always give next set)
+  push(@moon, np_rise_set($lng, $lat, $nm, "moon", "rise", -1));
+  $moonup = 1;
+} else {
+  # give next rise
+  push(@moon, np_rise_set($lng, $lat, $nm, "moon", "rise", +1));
+}
+
+# always give next set
+push(@moon, np_rise_set($lng, $lat, $nm, "moon", "set", +1));
+
+debug("$moonup/",@moon);
+debug("CUR: $cur");
+
+for $i (@times) {
+  # +30 for rounding
+  debug(strftime("%H:%M", localtime($i+30)));
+}
+
+debug(unfold(%sm));
 
 
