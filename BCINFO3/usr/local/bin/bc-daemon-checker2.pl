159df946a3ddaf1f5c24de5ee856a0698ecb93ab
Barry Carter <Barry Carter>
2013-11-07 11:34:01 -0700

    Checkpoint

--- BCINFO3/usr/local/bin/bc-daemon-checker2.pl
+++ BCINFO3/usr/local/bin/bc-daemon-checker2.pl
@@ -1,6 +1,8 @@
 #!/bin/perl
 
 # This is bc-daemon-checker.pl for bcinfo3 (writes to file, doesn't send mail)
+# --stderr: write to stderr not ERR file
+# TODO: make this restart failed daemons
 
 require "/usr/local/lib/bclib.pl";
 
@@ -17,12 +19,15 @@ $all = read_file("/home/barrycarter/BCGIT/BCINFO3/root/bcinfo3-procs.txt");
 
 # TODO: generalize this concept
 # TODO: allow comments in must/may/kill sections
+# the \n+ below eliminates empty lines
 $all=~s%<must>(.*?)</must>%%is;
-%must = list2hash(split(/\n/, $1));
+debug("1: $1");
+%must = list2hash(split(/\n+/, $1));
+debug("MUST",%must);
 $all=~s%<may>(.*?)</may>%%is;
-%may = list2hash(split(/\n/, $1));
+%may = list2hash(split(/\n+/, $1));
 $all=~s%<kill>(.*?)</kill>%%is;
-%kill = list2hash(split(/\n/, $1));
+%kill = list2hash(split(/\n+/, $1));
 
 for $i (@procs) {
   # cleanup proc line and split into fields
@@ -30,14 +35,11 @@ for $i (@procs) {
   $i=~s/\s+/ /isg;
   ($pid, $time, $rss, $vsz, $proc, $proc2, $proc3) = split(/\s+/,$i);
 
-  # bizarre error, 13:42 = newly created process
-  if ($time eq "13:42") {next;}
-
   # ignore [bracketed] processes (TODO: why?)
   if ($proc=~/^\[.*\]$/) {next;}
 
-  # for perl/xargs/python/ruby, the next non-option arg tells what the process really is
-  if ($proc=~m%/perl$%||$proc eq "xargs"||$proc=~m%/python$%||$proc=~m%(^|/)ruby$%) {
+  # for perl/xargs/python/ruby/sh, the next non-option arg tells what the process really is
+  if ($proc=~m%/perl$%||$proc eq "xargs"||$proc=~m%/python$%||$proc=~m%(^|/)ruby$%||$proc=~m%^/bin/sh%) {
 
     # TODO: this is imperfect
     if ($proc2=~/^\-/) {
@@ -77,6 +79,10 @@ for $i (@procs) {
     next;
   }
 
+  # processes are born 13:42 old (no idea why), this compensates
+  # TODO: HACK: figure this out
+  $sec-=822;
+
   # any process permitted to run up to 5m
   # TODO: specific limits for procs where 5m is wrong
   if ($sec<=300) {next;}
@@ -97,10 +103,16 @@ for $i (@procs) {
 
 for $i (sort keys %must) {
   if ($isproc{$i}) {next;}
+  debug("KEY $i");
   push(@err, "$i: not running, but is required");
 }
 
 # write errors to file EVEN IF empty (since I plan to rsync error
 # file, and rsync won't remove deleted files except with special
 # option)
-write_file_new(join("\n",@err),"/home/barrycarter/ERR/bcinfo3.err");
+if ($globopts{stderr}) {
+  print STDERR join("\n",@err),"\n";
+} else {
+  write_file_new(join("\n",@err),"/home/barrycarter/ERR/bcinfo3.err");
+}
+
