eadf861eb571651584314c77ebf86c0f50cf9691
Barry Carter <Barry Carter>
2013-11-25 16:53:00 -0700

    Checkpoint

--- ASTRO/astro.m
+++ ASTRO/astro.m
@@ -27,7 +27,7 @@ elevation at latitude
 *)
 
 ha[t_] = (366.2425/365.2425*360-360/27.321582)*Degree*t
-dec[t_] = 28.58*Sin[2*Pi*(t/27.321582)]
+dec[t_] = 28.58*Degree*Sin[2*Pi*(t/27.321582)]
 elev[t_,lat_] = 
 ArcSin[Sin[lat]*Sin[dec[t]]+Cos[lat]*Cos[dec[t]]*Cos[ha[t]]]
 
@@ -35,19 +35,5 @@ ArcSin[Sin[lat]*Sin[dec[t]]+Cos[lat]*Cos[dec[t]]*Cos[ha[t]]]
 
 elevdelta[t_, lat_] = D[elev[t,lat],t]
 
-(* this confirms nonunimodality *)
-
-Plot[elev[t, 60 Degree]/Degree, {t,5,6}]
-Plot[elev[t, 10 Degree]/Degree, {t,5,6}]
-Plot[elev[t, 30 Degree]/Degree, {t,5,6}]
-Plot[elev[t, 80 Degree]/Degree, {t,5,6}]
-
-(*
-
-Finding a real life example:
-
-
-
-
-
-*)
+(* since denominator can't be 0... *)
+Numerator[elevdelta[t,lat]]
