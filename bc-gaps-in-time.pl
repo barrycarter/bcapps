fd85be9aee41ecd706f51f84efb3aa71acdfdb15
Barry Carter <Barry Carter>
2013-11-26 10:06:03 -0700

    Checkpoint

---
+++ bc-gaps-in-time.pl
@@ -0,0 +1,29 @@
+#!/bin/perl
+
+# This script, useful for me only, looks at a file whose leftmost
+# entries are "hhmmss" and finds the largest gaps
+
+require "/usr/local/lib/bclib.pl";
+my($all) = cmdfile();
+
+for $i (split(/\n/, $all)) {
+  $i=~s/\s+.*$//;
+  if ($i=~/^\s*$/) {next;}
+
+  unless ($i=~/^(\d{2})(\d{2})(\d{2})$/) {warn "ERROR: $i"; next;}
+  $j = $1+$2/60+$3/3600;
+  my(%hash) = ();
+  $hash{gap} = $j-$last;
+  $hash{label} = "$lasti - $i";
+  push(@gaps, {%hash});
+  $last = $j;
+  $lasti = $i;
+}
+
+for $i (@gaps) {
+  debug("$i->{gap}, $i->{label}");
+}
+
+
+
+
