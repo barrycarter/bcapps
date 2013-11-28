0daec8fd9568e66862d535fdc6c541798b72b265
Barry Carter <Barry Carter>
2013-11-28 10:46:11 -0700

    Working no exception checking yet

--- ASTRO/ephem-loc-day.py
+++ ASTRO/ephem-loc-day.py
@@ -14,29 +14,8 @@ obs.lat,obs.long,date,obs.pressure=argv[1],argv[2],argv[3],0
 obs.date = date;
 print obs
 
-for i in [ephem.Sun(), ephem.Moon()]:
-    for j in ["previous_rising"]:
+for i in ["Sun", "Moon", "Mercury", "Venus", "Mars", "Jupiter", "Saturn"]:
+    for j in ["previous_rising", "previous_setting", "next_rising", "next_setting"]:
+        obj = getattr(ephem,i)()
         m = getattr(obs, j)
-        print m(i)
-        
-
-
-sys.exit();
-
-obs.lat,obs.long,obs,date,obs.pressure=argv[1],argv[2],argv[3],argv[4],0;
-print obs;
-
-print "SR",nsrise(date,'-0:34',obs.previous_rising, ephem.Sun(),-1)
-print "SS",nsrise(date,'-0:34',obs.previous_setting, ephem.Sun(),-1)
-print "SR",nsrise(date,'-0:34',obs.next_rising, ephem.Sun(),+1)
-print "SS",nsrise(date,'-0:34',obs.next_setting, ephem.Sun(),+1)
-
-print "CTS",nsrise(date,'-6',obs.previous_rising, ephem.Sun(),-1)
-print "CTE",nsrise(date,'-6',obs.previous_setting, ephem.Sun(),-1)
-print "CTS",nsrise(date,'-6',obs.next_rising, ephem.Sun(),+1)
-print "CTE",nsrise(date,'-6',obs.next_setting, ephem.Sun(),+1)
-
-print "MR",nsrise(date,'-0:34',obs.previous_rising, ephem.Moon(),-1)
-print "MS",nsrise(date,'-0:34',obs.previous_setting, ephem.Moon(),-1)
-print "MR",nsrise(date,'-0:34',obs.next_rising, ephem.Moon(),+1)
-print "MS",nsrise(date,'-0:34',obs.next_setting, ephem.Moon(),+1)
+        print i,j,m(obj)
