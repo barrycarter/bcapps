325e64fa8a4b5006573ed46d4f973b0bd4c62e35
Barry Carter <Barry Carter>
2013-11-28 19:46:57 -0700

    Checkpoint

--- ASTRO/ephem-loc-day.py
+++ ASTRO/ephem-loc-day.py
@@ -3,19 +3,45 @@
 # given the location and time, print various rise/set times (to be
 # called by a Perl program later)
 
-# Call as: $0 latitude longitude date(yyyy/mm/dd)
+# Call as: $0 longitude latitude date(yyyy/mm/dd)
 # TODO: add elevation
 
 import ephem;
 from sys import argv;
 
 obs = ephem.Observer();
-obs.lat,obs.long,date,obs.pressure=argv[1],argv[2],argv[3],0
+obs.long,obs.lat,date,obs.pressure=argv[1],argv[2],argv[3],0
 obs.date = date;
-print obs
+obs.horizon='-0:34'
 
-for i in ["Sun", "Moon", "Mercury", "Venus", "Mars", "Jupiter", "Saturn"]:
+# everything but the sun <h>(that's a song title, no?)</h>
+for i in ["Moon", "Mercury", "Venus", "Mars", "Jupiter", "Saturn"]:
     for j in ["previous_rising", "previous_setting", "next_rising", "next_setting"]:
-        obj = getattr(ephem,i)()
+        for k in ["00","12"]:
+            obs.date = date+" "+k+":00:00"
+            obj = getattr(ephem,i)()
+            m = getattr(obs, j)
+            # thing to print
+            try:
+                s = m(obj)
+            except(ephem.AlwaysUpError):
+                s = "ALWAYSUP"
+            except(ephem.NeverUpError):
+                s = "NEVERUP"
+        
+            print i,j,s
+
+# Sun at various heights, I don't use nautical or astro (yet)
+for h in ['-0:34', '-6:00', '-12:00', '-18:00']:
+    obs.horizon = h
+    for j in ["previous_rising", "previous_setting", "next_rising", "next_setting"]:
         m = getattr(obs, j)
-        print i,j,m(obj)
+        try:
+            s = m(ephem.Sun())
+        except(ephem.AlwaysUpError):
+            s = "ALWAYSUP"
+        except(ephem.NeverUpError):
+            s = "NEVERUP"
+        print "Sun"+h,j,s
+
+    
