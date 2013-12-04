20d1cb8f6f69004ff969382f4ccea10c953c7574
Barry Carter <carter.barry@gmail.com>
2013-12-04 09:30:51 -0700

    Checkpoint

--- bc-get-discogs.pl
+++ bc-get-discogs.pl
@@ -22,11 +22,29 @@ debug("FILETEST", -f "user-$user-p1");
 # cache information as much as possible
 # TODO: caching here is a bad idea if user adds releases (but OK for testing)
 unless (-f "user-$user-p1" && !$globopts{nocache}) {
-  debug("running curl");
   ($out,$err,$res) = cache_command2("curl -o user-$user-p1 'http://api.discogs.com/users/$user/collection/folders/0/releases?page=1&per_page=100'");
 }
 
+# TODO: slightly inefficient to read (though not load) p1 twice
 my($userinfo) = JSON::from_json(read_file("user-$user-p1"));
+
+# get all pages
+for $i (2..$userinfo->{pagination}{pages}) {
+  unless (-f "user-$user-p$i" && !$globopts{nocache}) {
+    ($out,$err,$res) = cache_command2("curl -o user-$user-p$i 'http://api.discogs.com/users/$user/collection/folders/0/releases?page=$i&per_page=100'");
+    # avoid hitting API too hard
+    sleep(1);
+  }
+}
+
+# now, go through the pages (including page 1)
+for $i (1..$userinfo->{pagination}{pages}) {
+  my($json) = JSON::from_json(read_file("user-$user-p$i"));
+  debug(var_dump("json",$json));
+}
+
+
+die "TESTING";
 
 # debug("USER",%user)
 debug(var_dump("user", $userinfo));
