c67496e509cb48782f54a3767c0765dd179ef18d
Barry Carter <carter.barry@gmail.com>
2013-12-25 08:43:48 -0700

    Checkpoint

--- bc-elec-bill.pl
+++ bc-elec-bill.pl
@@ -49,7 +49,7 @@ $tax = 1.07;
 # probably between 1315 and 1355 based on my pictorial record,
 # assuming rounding to nearest number
 
-($time,$read) = ("2013-11-21", "71220");
+($time,$read) = ("2013-12-20", "72184");
 
 # same month last year (as target)
 # $lastyearcost = "103.15";
