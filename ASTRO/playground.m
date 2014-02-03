e27ff5a9ce7a0065425828224caa132e20db3ca6
Barry Carter <carter.barry@gmail.com>
2014-02-03 09:14:21 -0700

    About to make changes

--- ASTRO/playground.m
+++ ASTRO/playground.m
@@ -13,3 +13,11 @@ tab2 = Table[Function[t,Evaluate[tab[[i]]]],{i,1,Length[tab]}]
 
 Plot[{tab2[[1]][t*2-1],tab2[[2]][t*2+1]},{t,-1,1}]
 showit
+
+f[t_] = Piecewise[{{tab2[[1]][t], t <= 1}, {tab2[[2]][t-2], t > 1}}]
+Plot[f[x],{x,-1,3}]
+showit
+
+g[t_] = Piecewise[Table[{tab2[[i]][t+2-2*i], t < -1 + 2*i},{i,1,Length[tab2]}]]
+
+Plot[g[t],{t,-1,95}]
