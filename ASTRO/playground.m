e06b61dc351db39f7ae680d47481161b5979a825
Barry Carter <carter.barry@gmail.com>
2014-02-23 09:41:36 -0700

    Still broken...

--- ASTRO/playground.m
+++ ASTRO/playground.m
@@ -1,13 +1,63 @@
+(* TODO: assuming a and b are global below, fix *)
+
 (* parametric ellipse *)
 
 a = 2; b = 1;
 
-(* TODO: assuming a and b are global below, fix *)
-
-(* x and y pos at time t, increases linearlly with angle *)
+(* below parametrizes an ellipse but NOT by angle, as we shall see *)
 
 x[t_] = a*Cos[t]
 y[t_] = b*Sin[t]
+
+(* let's look at theta compared to t for t = Pi/3 [theta wont be] *)
+
+(* the ellipse, top right part *)
+g1 = ParametricPlot[{x[t],y[t]},{t,0,Pi/2}]
+
+(* "randomly" chosen value of t to show it doesn't match theta *)
+(* t is NOT measured in degrees; degrees below is for convenience only *)
+samp = 55*Degree
+
+(* the lines from ellipse center and x/y axes to point, and angle arc *)
+g2 = {
+ Line[{{0,0},{x[samp],y[samp]}}],
+ Circle[{0,0}, 2/10, {0, ArcTan[x[samp],y[samp]]}],
+ Dashing[0.01],
+ Line[{{0,0},{x[samp],y[samp]}}], 
+ Dashing[0.01], 
+ Line[{{x[samp],0},{x[samp],y[samp]}}], 
+ Line[{{0,y[samp]},{x[samp],y[samp]}}],
+ Text[Style["b*Sin[t]", FontSize->25], {x[samp], y[samp]/2}, {-1.1,0}],
+ Text[Style["a*Cos[t]", FontSize->25], {x[samp]/2, y[samp]}, {0,-1.1}],
+ Text[Style["\[Theta]", FontSize->25], {0.2,0.05}, {-1,-1}]
+} 
+ 
+Show[g1,Graphics[g2]]
+showit
+
+(* we see than tan(theta) = (b*Sin[t])/(a*Cos[t]), solving for theta *)
+
+(* Mathematica solves below poorly, so doing my own formula *)
+
+(* Solve[Tan[theta] == (b*Sin[t])/(a*Cos[t]), theta] *)
+
+theta[t_] = ArcTan[b/a*Tan[t]]
+
+(* We can now reparametrize *)
+x[theta_] = a*Cos[theta[t]]
+y[theta_] = b*Sin[theta[t]]
+
+(* the radius squared at theta *)
+r2[theta_] = x[theta]^2 + y[theta]^2
+
+a = 2; b = 1;
+ParametricPlot[{x[t],y[t]},{t,0,Pi/2}]
+
+
+
+Solve[Tan[theta] == (b*Sin[t])/(a*Cos[t]), theta]
+Solve[Tan[theta] == (b*Sin[t])/(a*Cos[t]), theta, Reals]
+
 
 (* +-Sqrt[3] are really focii? yup! *)
 
@@ -16,7 +66,6 @@ Sqrt[(x[t]-Sqrt[3])^2 + y[t]^2] + Sqrt[(x[t]+Sqrt[3])^2 + y[t]^2]
 
 (* try drawing the problem out a bit *)
 
-g1 = ParametricPlot[{x[t],y[t]},{t,0,2*Pi}]
 
 g2 = Labeled[Point[{{0,0}, {Sqrt[3],0}, {-Sqrt[3],0}}], "foo"]
 
