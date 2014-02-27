c94701eb5a98a411092dd25d356270a809ec2b5c
Barry Carter <carter.barry@gmail.com>
2014-02-27 05:47:52 -0700

    Checkpoint

--- ASTRO/bc-ellipse-focus.m
+++ ASTRO/bc-ellipse-focus.m
@@ -20,6 +20,7 @@ g1 = ParametricPlot[{x[t],y[t]},{t,0,Pi/2}]
 
 (* "randomly" chosen value of t to show it doesn't match focal angle*)
 (* t is NOT measured in degrees; degrees below is for convenience only *)
+
 samp = 55*Degree
 
 (* the lines from ellipse center and x/y axes to point, and angle arc *)
@@ -50,17 +51,31 @@ dia = Show[g1,Graphics[g2]]
 (* unsetting a and b to resume general case *)
 Unset[a]; Unset[b];
 
-(* theta is accurate w/ respect to coordinates, theta2 is easier to invert *)
-(* theta[t_] = ArcTan[x[t]-focus[a,b], y[t]] *)
+(* conditions under which the below work *)
+conds = {a>b,b>0,x>0,x<Pi/2,Element[a,Reals],Element[b,Reals],Element[x,Reals],
+theta>0, Element[theta,Reals], theta < Pi/2}
+
+(* below only works for theta <= Pi/2; case theta > Pi/2 is NOT symmetric *)
 theta[t_] = ArcTan[y[t]/(x[t]-focus[a,b])]
+t[x_] = InverseFunction[theta][x]
+t[x_] = FullSimplify[t[x], conds]
 
-(* testing again *)
-a = 1.1; b = 1;
-Plot[{theta[t],theta2[t]},{t,0,Pi/2}]
-Unset[a]; Unset[b];
+(* reparametrizing using theta *)
 
-t[theta_] = InverseFunction[theta2][theta]
+x[theta_] = FullSimplify[a*Cos[t[theta]],conds]
+y[theta_] = FullSimplify[b*Sin[t[theta]],conds]
 
+(* the radius squared at theta *)
+r2[theta_] = FullSimplify[x[theta]^2 + y[theta]^2, conds]
+
+
+(* and the area *)
+parea[theta_] = FullSimplify[Integrate[r2[x]/2,{x,0,theta}], conds]
+
+
+
+
+Abort[]
 
 
 (* below aren't true in general, but are here and useful for simplication *)
@@ -77,14 +92,6 @@ assums = {a>b, Member[x,Reals], Member[theta,Reals]}
 
 t[x_] = InverseFunction[theta][x] /.
  {Sqrt[b_^4 + b_^4*Tan[x_]^2] -> b^2*Sec[x], (a_-b_)*(a_+b_) -> a^2-b^2}
-
-(* reparametrizing using theta *)
-
-x[theta_] = a*Cos[t[theta]]
-y[theta_] = b*Sin[t[theta]]
-
-(* the radius squared at theta *)
-r2[theta_] = FullSimplify[x[theta]^2 + y[theta]^2]
 
 parea[theta_] = Integrate[r2[x]/2,{x,0,theta}]
 
