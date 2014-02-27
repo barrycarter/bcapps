7eca6736e9db0bb8041a23145b9eba7b50cf8131
Barry Carter <carter.barry@gmail.com>
2014-02-26 18:01:57 -0700

    Checkpoint (broken)

--- ASTRO/bc-ellipse-focus.m
+++ ASTRO/bc-ellipse-focus.m
@@ -1,6 +1,6 @@
-(******** THIS STARTS OUT AS AN EXACT COPY OF bc-ellipse-center.m AND
+(* THIS STARTS OUT AS AN EXACT COPY OF bc-ellipse-center.m AND
 DOES NOT CURRENTLY CALCULATE AREA FROM FOCUS; I WILL REMOVE THIS NOTE
-WHEN IT DOES ********)
+WHEN IT DOES *)
 
 (* Derives the formula for area of ellipse matching focal angle *)
 
@@ -34,14 +34,14 @@ g2 = {
  Line[{{focus[a,b],0.05}, {x[samp],0.05}}],
  Line[{{x[samp],0}, {x[samp],0.1}}],
  Line[{{focus[a,b],0}, {focus[a,b],0.1}}],
- Text[Style["d", FontSize->25], {(x[samp]+focus[a,b])/2,0.05}, {0,-1}],
+ Text[Style[d, FontSize->25], {(x[samp]+focus[a,b])/2,0.05}, {0,-1}],
  RGBColor[0,0,0],
  Dashing[0.01],
  Line[{{x[samp],y[samp]}, {x[samp],0}}],
  Line[{{x[samp],y[samp]}, {0,y[samp]}}],
- Text[Style["b*Sin[t]", FontSize->25], {x[samp],y[samp]/2}, {-1.1,0}],
- Text[Style["a*Cos[t]", FontSize->25], {x[samp]/2,y[samp]}, {0,-1.1}],
- Text[Style["Sqrt[a^2-b^2]", FontSize->25], {focus[a,b]/2,0.05}, {0,-1}]
+ Text[Style[HoldForm[b*Sin[t]], FontSize->25], {x[samp],y[samp]/2}, {-1.1,0}],
+ Text[Style[HoldForm[a*Cos[t]], FontSize->25], {x[samp]/2,y[samp]}, {0,-1.1}],
+ Text[Style[HoldForm[Sqrt[a^2-b^2]], FontSize->25], {focus[a,b]/2,0.05}, {0,-1}]
 } 
 
 (* results in bc-ellipse-from-center.png in this directory *)
@@ -50,11 +50,33 @@ dia = Show[g1,Graphics[g2]]
 (* unsetting a and b to resume general case *)
 Unset[a]; Unset[b];
 
-(* from the diagram, we see Tan[theta] = (b*Sin[t])/(a*Cos[t])
-   Mathematica solves this for t poorly (extraneous junk), so
-   I just give the cleaned up result below *)
+(* theta is accurate w/ respect to coordinates, theta2 is easier to invert *)
+(* theta[t_] = ArcTan[x[t]-focus[a,b], y[t]] *)
+theta[t_] = ArcTan[y[t]/(x[t]-focus[a,b])]
 
-t[theta_] = ArcTan[a*Tan[theta]/b]
+(* testing again *)
+a = 1.1; b = 1;
+Plot[{theta[t],theta2[t]},{t,0,Pi/2}]
+Unset[a]; Unset[b];
+
+t[theta_] = InverseFunction[theta2][theta]
+
+
+
+(* below aren't true in general, but are here and useful for simplication *)
+
+(* some of these are very specific to this file/problem *)
+
+rewrite = {Sqrt[a_^2] -> a, Sqrt[Sec[a_]^2] -> Sec[a], Sqrt[a_^4] -> a^2}
+
+(* assumptions valid for what we are doing *)
+
+assums = {a>b, Member[x,Reals], Member[theta,Reals]}
+
+(* the assumptions below are hand-coded and not general case *)
+
+t[x_] = InverseFunction[theta][x] /.
+ {Sqrt[b_^4 + b_^4*Tan[x_]^2] -> b^2*Sec[x], (a_-b_)*(a_+b_) -> a^2-b^2}
 
 (* reparametrizing using theta *)
 
@@ -62,7 +84,41 @@ x[theta_] = a*Cos[t[theta]]
 y[theta_] = b*Sin[t[theta]]
 
 (* the radius squared at theta *)
-r2[theta_] = x[theta]^2 + y[theta]^2
+r2[theta_] = FullSimplify[x[theta]^2 + y[theta]^2]
+
+parea[theta_] = Integrate[r2[x]/2,{x,0,theta}]
+
+parea[theta_] = Simplify[parea[theta], {Member[theta,Reals]}]
+
+
+
+
+
+(* contradictory answers? 
+
+ArcTan[(b*Sin[t])/(-Sqrt[a^2 - b^2] + a*Cos[t])]
+
+
+*)
+
+
+
+(* Solve[theta[t]==x && a > 0 && b > 0 && x > 0,t,Reals] *)
+
+
+(******** IGNORING BELOW FOR NOW
+
+
+(* from the diagram, we see Tan[theta] = (b*Sin[t])/(a*Cos[t])
+   Mathematica solves this for t poorly (extraneous junk), so
+   I just give the cleaned up result below *)
+
+t[theta_] = ArcTan[a*Tan[theta]/b]
+
+
+x[theta_] = a*Cos[t[theta]]
+y[theta_] = b*Sin[t[theta]]
+
 
 (* and integrating r^2/2 from 0 to theta *)
 
@@ -75,3 +131,6 @@ parea[theta_] = a*b*ArcTan[a*Tan[theta]/b]/2
 
 Graphics[Text[Style["Sqrt[a^2-b^2]", FontSize->25], {0,0}]]
 
+
+
+**********)
--- oneliners.sh
+++ oneliners.sh
@@ -24,7 +24,7 @@ perl -le 'printf("%0.4f,%0.4f,%d\n",rand(360)-180,rand(180)-90,rand(10000))';
 
 exit;
 
-# (suggests) renaming files that make doesn't handle well
+# (suggests) renaming files that make doesn't handle well (removes spaces)
 \ls | perl -nle '$x=$_; if (s/[^a-z0-9_\.\-\%\,]/_/isg) {print "mv \"$x\" $_"}'
 
 exit;
