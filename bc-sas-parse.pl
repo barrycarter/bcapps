cac349d49138641313e71d1d8568fe1901a688e0
Barry Carter <carter.barry@gmail.com>
2014-03-03 05:04:40 -0700

    Checkpoint

--- bc-sas-parse.pl
+++ bc-sas-parse.pl
@@ -7,11 +7,17 @@ require "/usr/local/lib/bclib.pl";
 
 $all = read_file("/home/barrycarter/Download/sas-20140301.html");
 
-# since I reload all data each time, delete existing (sadly, creating
-# a unique index does not work, since separate rows can sometimes have
-# all identical values )
+# since I reload all data each time, re-create table (which I concede is ugly)
 
-print "DELETE FROM sas;\n";
+print << "MARK";
+DROP TABLE IF EXISTS sas;
+CREATE TABLE sas (date DATE, type TEXT, payee TEXT, description TEXT,
+method TEXT, amount DOUBLE, comments TEXT);
+-- my standard way of adding an 'oid' column, probably not best way to do it
+ALTER TABLE sas
+ADD oid INT UNSIGNED NOT NULL AUTO_INCREMENT, ADD INDEX (oid);
+MARK
+;
 
 for $i (split(/\n/,$all)) {
   chomp($i);
@@ -38,14 +44,7 @@ for $i (split(/\n/,$all)) {
   ($date=~s/^(\d{2})\/(\d{2})\/(\d{4})$/$3-$1-$2/s)||die("BAD DATE: $date");
 
   # query...
-  print "INSERT INTO sas (date, type, payee, description, amount) VALUES
-('$date', '$type', '$payee', '$description', '$amount');\n";
-
+  print "INSERT INTO sas (date, type, payee, description, method, amount)
+ VALUES ('$date', '$type', '$payee', '$description', '$method', '$amount');\n";
 }
 
-=item schema
-
-CREATE TABLE sas (date DATE, type TEXT, payee TEXT, description TEXT,
-amount DOUBLE, comments TEXT);
-
-=cut
