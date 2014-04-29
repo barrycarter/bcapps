c108c1669908e12ae12046d04cc02b463259e9d4
Barry Carter <carter.barry@gmail.com>
2014-04-29 17:39:00 -0600

    Checkpoint

--- bc-scan-pic.pl
+++ bc-scan-pic.pl
@@ -3,13 +3,18 @@
 # quick and dirty wrapper to scanimage: takes a 300 dpi full page scan
 # and outputs it to a unique file in the current directory
 
-# $scanner = "hp5590:libusb:002:006";
-# $scanner = "hp5590:libusb:001:003";
-# $scanner = "hpaio:libusb:001:004";
-$scanner = "hp5590:libusb:001:004";
+require "/usr/local/lib/bclib.pl";
+
+# find USB address of my ADF scanner
+%scanners = %{find_attached_scanners()};
+# must quote these strings, otherwise interpreted as numbers
+$usb = $scanners{"0x03f0"}{"0x1205"};
+unless ($usb) {die "Scanner not attached";}
+$scanner = "hp5590:$usb";
+
 my($date) = `date +%Y%m%d.%H%M%S.%N`;
 chomp($date);
-# system("sudo scanimage --mode Color -d $scanner --resolution 600 > $date.ppm");
-system("sudo scanimage -d $scanner --resolution 300 > $date.ppm");
+system("sudo scanimage --mode Color -d $scanner --resolution 600 > $date.ppm");
+# system("sudo scanimage -d $scanner --resolution 300 > $date.ppm");
 print "SCAN COMPLETE, CONVERTING\n";
 system("convert $date.ppm $date.jpg");
