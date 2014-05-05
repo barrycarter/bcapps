0a22c263e31fb815922b142dbfb6b4f01ea8dab4
Barry Carter <carter.barry@gmail.com>
2014-05-05 12:54:19 -0600

    Checkpoint

--- METAWIKI/bc-meta-pbs.pl
+++ METAWIKI/bc-meta-pbs.pl
@@ -24,23 +24,74 @@ for $i (split(/\n/, $data)) {
   if ($i=~/^\#/ || $i=~/^\s*$/) {next;}
 
   # split line into source page and then body
-  $i=~/^(.*?)\s+(.*)$/;
+  $i=~/^(.*?)\s*($dlb.*)$/;
   my($source, $body) = ($1,$2);
-  debug("LINE: $i");
   parse_text($source,$body);
+}
+
+# now, adding stuff to pages from the established triples
+for $i (keys %triple) {
+  for $j (keys %{$triple{$i}}) {
+    for $k (keys %{$triple{$i}{$j}}) {
+      debug("$i, $j, $k, $triple{$i}{$j}{$k}");
+    }
+  }
 }
 
 sub parse_text {
   my($source,$body) = @_;
   # return triplets
   my(@trip) = ();
+  # source may contain multiple pages
+  my(@source) = parse_source($source);
 
   # keep things like [[Pig]] as is, but tokenize so they won't bother us
+  # TODO: undo this before final wiki printing
   $body=~s/$dlb($cc+)$drb/\001$1\002/sg;
-  debug("BODY: $body");
 
-  while ($body=~s/$dlb($cc*?)::($cc*?)$drb//) {
-    debug("CHOMP: $1,$2");
+  # semantic triple
+  for $source (@source) {
+    while ($body=~s/$dlb($cc*?)::($cc*?)$drb//) {
+      # relation and value
+      my($relation,$value) = ($1,$2);
+      # either of these can be multiple
+      for $i (split(/\+/, $relation)) {
+	for $j (split(/\+/, $value)) {
+	  # TODO: allow non-1 values to set order
+	  $triple{$source}{$i}{$j} = 1;
+	}
+      }
+    }
   }
 }
+
+# convert things like
+# 2013-04-17-2013-04-19,2013-04-22,2013-04-23,2013-04-30,2013-05-01,2013-05-06-2013-05-08,2013-05-13-2013-05-15,2013-05-20-2013-05-22,2013-05-24,2013-05-29
+# to a list of source pages
+sub parse_source {
+  my($source) = @_;
+  my(@ret);
+
+  for $i (split(/\,/,$source)) {
+    # if source is date range (2002-06-03-2002-06-07), parse further
+    if ($i=~/^(\d{4}-\d{2}-\d{2})\-(\d{4}-\d{2}-\d{2})$/) {
+      push(@ret, parse_date_range($1,$2));
+    } else {
+      push(@ret, $i);
+    }
+  }
+  return @ret;
+}
+
+# convert 2002-06-03-2002-06-07 to list of dates
+sub parse_date_range {
+  my($st,$en) = @_;
+  my(@ret);
+  # integer division below
+  for $i (str2time($st)/86400..str2time($en)/86400) {
+    push(@ret, strftime("%Y-%m-%d", gmtime($i*86400)));
+  }
+  return @ret;
+}
+
 
