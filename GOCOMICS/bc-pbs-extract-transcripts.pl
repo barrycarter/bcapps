247df1be6a4dd068c38506e969b8ee193ee58447
Barry Carter <carter.barry@gmail.com>
2014-05-18 10:43:32 -0600

    About to fix

--- GOCOMICS/bc-pbs-extract-transcripts.pl
+++ GOCOMICS/bc-pbs-extract-transcripts.pl
@@ -1,20 +1,30 @@
-#!/bin/perl
+#!/bin/perl -w
 
 # extracts PBS transcripts from pages like http://www.icoolen.com/cartoon_single/pearlpig_2002_01
 
 require "/usr/local/lib/bclib.pl";
 
-for $i (glob "/home/barrycarter/20140518/pearlpig_*") {
+# for $i (glob "/home/barrycarter/20140518/pearlpig_*") {
+warn "TESTING";
+for $i (glob "/home/barrycarter/20140518/pearlpig_2002_06") {
   # determine start date from file name
   unless ($i=~/pearlpig_(\d{4})_(\d{2})/) {die "BAD FILE: $i"}
   $date = "$1-$2-01";
 
   $all = read_file($i);
 
+  @text = ();
+  # multiple phrase catching?
   while ($all=~s/<div style="line-height: 150%">(.*?)<\/div>//) {
-    $text = $1;
+    push(@text, $1);
+  }
+
+  while ($all=~s/<span style="color: #00afe1">(.*?)<\/span>//) {
+    push(@text, $1);
+  }
 
 
+  for $text (@text) {
     debug("TEXT: $text");
 
     # site above uses gray for Chinese characters
@@ -28,22 +38,30 @@ for $i (glob "/home/barrycarter/20140518/pearlpig_*") {
     # we don't distinguish between left and right quotes, though we could
     $text=~s/&([lr]dquo|quot);/\"/g;
     $text=~s/&[lr]squo;/\'/g;
-
+    
     # remove HTML tags and unprintables
     $text=~s/<.*?>//g;
     $text=~s/[^ -~]//g;
-
+    
     # convert escaped HTML
     $text=~s/&lt;/</;
     $text=~s/&gt;/>/;
-
+    
     # if nothing left, ignore
     if ($text=~/^\s*$/) {next;}
 
-    # if this is a date, note it
-    if ($text=~/^\d{4}-\d{2}-\d{2}$/) {$date = $text; next;}
+    debug("AFTER: $text");
 
-    print "$date: $text\n";
+    # if this is a date, note it
+    if ($text=~/^\s*\d{4}-\d{2}-\d{2}\s*$/) {
+      $date = $text;
+      debug("DATE: $date");
+#      next;
+    }
+
+#    debug("DATE: $date");
+
+    debug("ALPHA: $date: $text");
   }
 }
 
