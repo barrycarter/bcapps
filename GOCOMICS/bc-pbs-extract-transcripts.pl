ee7bb9789c791198770429cd210d43ac896c08b1
Barry Carter <carter.barry@gmail.com>
2014-05-18 09:16:42 -0600

    Checkpoint

--- GOCOMICS/bc-pbs-extract-transcripts.pl
+++ GOCOMICS/bc-pbs-extract-transcripts.pl
@@ -4,29 +4,50 @@
 
 require "/usr/local/lib/bclib.pl";
 
-my($all,$fname) = cmdfile();
+for $i (glob "/home/barrycarter/20140518/pearlpig_*") {
+  # determine start date from file name
+  unless ($i=~/pearlpig_(\d{4})_(\d{2})/) {die "BAD FILE: $i"}
+  $date = "$1-$2-01";
 
-while ($all=~s/<div style="line-height: 150%">(.*?)<\/div>//) {
-  $text = $1;
-  # site above uses gray for Chinese characters
-  if ($text=~/<span style=\"color: gray/) {next;}
+  $all = read_file($i);
 
-  # if not chinese, cleanup...
-  $text=~s/&nbsp;/ /g;
-  $text=~s/&rsquo;/'/g;
-  $text=~s/&hellip;/.../g;
-  # we don't distinguish between left and right quotes, though we could
-  $text=~s/&([lr]dquo|quot);/\"/g;
-  $text=~s/&[lr]squo;/\'/g;
+  while ($all=~s/<div style="line-height: 150%">(.*?)<\/div>//) {
+    $text = $1;
 
-  # remove HTML tags and unprintables
-  $text=~s/<.*?>//g;
-  $text=~s/[^ -~]//g;
-  # if nothing left, ignore
-  if ($text=~/^\s*$/) {next;}
 
-  debug("text: $text");
+    debug("TEXT: $text");
+
+    # site above uses gray for Chinese characters
+    if ($text=~/color: gray/) {next;}
+
+    # if not chinese, cleanup...
+    $text=~s/&nbsp;/ /g;
+    $text=~s/&rsquo;/'/g;
+    $text=~s/&hellip;/.../g;
+    $text=~s/&mdash;/--/g;
+    # we don't distinguish between left and right quotes, though we could
+    $text=~s/&([lr]dquo|quot);/\"/g;
+    $text=~s/&[lr]squo;/\'/g;
+
+    # remove HTML tags and unprintables
+    $text=~s/<.*?>//g;
+    $text=~s/[^ -~]//g;
+
+    # convert escaped HTML
+    $text=~s/&lt;/</;
+    $text=~s/&gt;/>/;
+
+    # if nothing left, ignore
+    if ($text=~/^\s*$/) {next;}
+
+    # if this is a date, note it
+    if ($text=~/^\d{4}-\d{2}-\d{2}$/) {$date = $text; next;}
+
+    print "$date: $text\n";
+  }
 }
 
 # debug($all);
 
+# TODO: catch format below for dates (only appears to be for dates)
+# <span style="color: #00afe1">2002-06-03</span>
