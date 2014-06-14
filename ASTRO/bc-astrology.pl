a63c66c1a14f800ddf8c152ff7bc2daff03c102e
Barry Carter <carter.barry@gmail.com>
2014-06-13 18:23:18 -0600

    Checkpoint

--- ASTRO/bc-astrology.pl
+++ ASTRO/bc-astrology.pl
@@ -6,21 +6,25 @@
 require "/usr/local/lib/bclib.pl";
 use Astro::Nova;
 
+my(@consts) = split(/\//, "Aries/Taurus/Gemini/Cancer/Leo/Virgo/Libra/Scorpio/Saggitarius/Capricornus/Aquarius/Pisces");
+
 # http://www.astro.com/astrology/in_signs_e.htm shows ecliptic longitude
 
 # derivative of ecliptic longitude also needed (signum only)
 
-$jd = get_julian_from_timet(time());
+$jd = get_julian_from_timet(str2time("2014-01-01 00:00:00 UTC"));
+$jd = 2456658.500000;
+debug("JD: $jd");
 
 for $i ((split(/\|/, "solar|lunar|mercury|venus|mars|jupiter|saturn|uranus|neptune|pluto"))) {
-  my($f) = "Astro::Nova::get_${i}_equ_coords($jd)";
-  my($res) = eval($f);
-  debug("RES: $res");
-  my($obj) = get_ecl_from_equ($res, $jd);
-#  debug("OBJ: $obj");
-  debug("RES: ",$obj->get_lng());
-#  debug("F: $f");
-#  debug("I: $i");
+
+  # get ecliptic longitude
+  my($equ) = eval("Astro::Nova::get_${i}_equ_coords($jd)");
+  my($ra,$dec) = ($equ->get_ra(), $equ->get_dec());
+  my($res) = get_ecl_from_equ($equ, $jd)->get_lng();
+  my($sign, $d, $m, $s) = dec2deg($res);
+  my($const, $into) = (floor($res/30), fmod($res,30));
+  debug("$i: $consts[$const] $into, $ra, $dec");
 }
 
 die "TESTING";
--- ASTRO/playground.py
+++ ASTRO/playground.py
@@ -2,5 +2,31 @@
 
 import ephem;
 
+
+
+
 e = ephem.Observer();
-print e.next_rising(ephem.Saturn());
+e.date = '2013/01/01 00:00:00';
+moon = ephem.Moon();
+moon.compute(e);
+print moon.ra, moon.dec
+
+e.date -= ephem.delta_t(e.date) * ephem.second
+print e
+moon.compute(e);
+print moon.ra, moon.dec
+
+die
+
+moon, e = ephem.Moon(), ephem.Observer()
+e.date = '2013/01/01 00:00:00'
+moon.compute(e)
+print moon.a_ra / ephem.degree, moon.a_dec / ephem.degree
+
+
+# print ephem.delta_t()*ephem.second
+
+# quit
+
+# e = ephem.Observer();
+# print e.next_rising(ephem.Saturn());
