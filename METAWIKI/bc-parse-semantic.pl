256e2cd3c011addfb13da3a6b8c44e0705bf7e8e
Barry Carter <carter.barry@gmail.com>
2014-06-29 11:14:26 -0600

    Checkpoint

--- METAWIKI/bc-parse-semantic.pl
+++ METAWIKI/bc-parse-semantic.pl
@@ -22,12 +22,6 @@ require "/usr/local/lib/bclib.pl";
 
 my($metadir) = "/home/barrycarter/BCGIT/METAWIKI";
 
-# debug(unfold(parse_semantic("2003-12-14", "[[character::Whale 20031214 (whale)+[[deaths::Skippy (seal)]]]] [[Whale 20031214 (whale)+Whale::notes::Assuming the whales from [[2003-12-14]] and [[2005-12-13]] are different]]")));
-
-# debug(unfold(parse_semantic("2003-12-14-2004-01-14", "[[character::Bob+[[Bob::cousin::Lou]]]]")));
-
-# die "TESTING";
-
 # load meta data (TODO: this is cheating, only one section so far)
 for $i (`egrep -v '^<|^#|^\$' $metadir/pbs-meta.txt`) {
   chomp($i);
@@ -43,12 +37,13 @@ my(%queries);
 for $i (split(/\n/, read_file("largeimagelinks.txt"))) {
   $i=~s/^(.*?)\s+.*?\/([0-9a-f]+)\?.*$//;
   my($strip, $hash) = ($1, $2);
-  $triples{$strip}{hash}{$hash}{largeimagelinks} = 1;
-  $triples{$strip}{class}{strip}{largeimagelinks} = 1;
+  $triples{$strip}{hash}{$hash} = "largeimagelinks";
+  $triples{$strip}{class}{strip} = "largeimagelinks";
 }
 
 for $i (`cat $metadir/pbs.txt $metadir/pbs-cl.txt | egrep -v '^#|^\$'`) {
   # TODO: multirefs!
+  # TODO: *_deaths
   # below allows for multiple dates
   unless ($i=~/^([\d\-,]+)\s+(.*)$/) {next;}
 
@@ -57,42 +52,41 @@ for $i (`cat $metadir/pbs.txt $metadir/pbs-cl.txt | egrep -v '^#|^\$'`) {
     map(s/\'/&\#39;/g, @$j);
     my($source, $k, $target, $datasource) = @$j;
     # the hash of triples
-    debug("RET: $source, $k, $target, $datasource");
-    $triples{$source}{$k}{$target}{$datasource} = 1;
+#    debug("RET: $source, $k, $target, $datasource");
+    $triples{$source}{$k}{$target} = $datasource;
 
     # determine relation type
     unless ($meta{$k}) {warn "NO MAPPING FOR: $k"; next;}
     my($for, $rev, $stype, $ttype) = @{$meta{$k}};
 
     # classes for source and target...
-    $triples{$source}{class}{$stype}{RELATION} = 1;
-    $triples{$target}{class}{$ttype}{RELATION} = 1;
+    $triples{$source}{class}{$stype} = "$datasource (source of $k)";
+    $triples{$target}{class}{$ttype} = "$datasource (target of $k)";
 
     # for character to character relations...
     if ($stype eq "character" && $ttype eq "character") {
 
       # delete the original relation (we'll never use it)
-      delete $triples{$source}{$k}{$target}{$datasource};
+#      delete $triples{$source}{$k}{$target};
 
       # create this as a 2 directional relation for both characters
-      $triples{$source}{relative}{"$target ($for)"}{$datasource} = 1;
-      $triples{$target}{relative}{"$source ($rev)"}{$datasource} = 1;
+      $triples{$source}{relative}{"$target ($for)"}="$datasource ($for/$rev)";
+      $triples{$target}{relative}{"$source ($rev)"}="$datasource ($for/$rev)";
 
       # both characters are considered to "appear in" this strip
-      $triples{$datasource}{character}{$source}{$datasource} = 1;
-      $triples{$datasource}{character}{$target}{$datasource} = 1;
+      $triples{$datasource}{character}{$source} = "$datasource ($k imply)";
+      $triples{$datasource}{character}{$target} = "$datasource ($k imply)";
 
       next;
     }
 
     # if either one is a character... (do this and keep going)
     if ($stype eq "character") {
-      $triples{$datasource}{character}{$source}{$datasource} = 1;
+      $triples{$datasource}{character}{$source} = "$datasource ($k imply)";
     }
     if ($ttype eq "character") {
-      $triples{$datasource}{character}{$target}{$datasource} = 1;
+      $triples{$datasource}{character}{$target} = "$datasource ($k imply)";
     }
-
 
     # for aliases, canonize
     if ($k eq "aka") {
@@ -120,7 +114,7 @@ MARK
 # delete $triples{string};
 # delete $triples{"*"};
 
-debug("TRIPLES:", keys %{$triples{'2002-01-07'}{character}{Pig}});
+# debug("TRIPLES:", keys %{$triples{'2002-01-07'}{character}{Pig}});
 
 # once again excessively terse
   open(A,"|tee /tmp/triples2.txt|sqlite3 /usr/local/etc/metawiki/pbs/pbs-triples.db 1>/tmp/pbs2-myout.txt 2>/tmp/pbs2-myerr.txt");
@@ -132,8 +126,8 @@ CREATE INDEX i1 ON triples(source);
 CREATE INDEX i2 ON triples(k);
 CREATE INDEX i3 ON triples(target);
 CREATE INDEX i4 ON triples(datasource);
--- prevent duplicates, which lets me be sloppy w cleanup queries
-CREATE UNIQUE INDEX i5 ON triples(source,k,target,datasource);
+-- data is single sourced only
+CREATE UNIQUE INDEX i5 ON triples(source,k,target);
 BEGIN;
 MARK
 ;
@@ -141,18 +135,13 @@ MARK
 for $i (sort keys %triples) {
   for $j (sort keys %{$triples{$i}}) {
     for $k (sort keys %{$triples{$i}{$j}}) {
-      for $l (sort keys %{$triples{$i}{$j}{$k}}) {
-	print A "INSERT OR IGNORE INTO triples VALUES('$i','$j','$k','$l');\n";
-      }
+      print A "INSERT OR IGNORE INTO triples VALUES('$i','$j','$k','$triples{$i}{$j}{$k}');\n";
     }
   }
 }
 
-close(A); die "TESTING";
-
 # commit queries above and start new batch
 print A "COMMIT;\nBEGIN;";
-
 
 # the queries from the parsing
 for $i (keys %queries) {
@@ -160,21 +149,24 @@ for $i (keys %queries) {
 }
 
 print A "COMMIT;\n";
+
+# TODO: put this in misc.sql
+print A "DELETE FROM triples WHERE k='class' AND target IN ('*', 'string');\n";
+
 close(A);
 
 # write some other ANNOS
 my($query) = << "MARK";
-SELECT ds, GROUP_CONCAT("[["||ds||"::"||k||"::"||target||"]]") FROM
-(SELECT source, k, target, 
- CASE WHEN datasource='SELF' THEN source ELSE datasource END AS ds
-FROM triples)
-WHERE ds NOT IN ('largeimagelinks', 'RELATION')
-GROUP BY ds;
+SELECT SUBSTR(datasource,1,10) AS ds,
+ GROUP_CONCAT("[["||source||"::"||k||"::"||target||"]]", "NEWLINE") AS data
+FROM triples GROUP BY ds ORDER BY ds;
 MARK
 ;
 
 for $i (sqlite3hashlist($query, "/usr/local/etc/metawiki/pbs/pbs-triples.db")) {
-  debug("I: $i->{ds}");
+  debug("SOURCE: $i->{ds}");
+  $i->{data}=~s/NEWLINE/\n/g;
+  write_file($i->{data}, "/mnt/extdrive/GOCOMICS/pearlsbeforeswine/page-$i->{ds}.gif.txt");
 }
 
 =item parse_semantic($dates, $string)
