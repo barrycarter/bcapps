e41ad573008e00dda66c2487d57e8d3aa0355dbd
Barry Carter <carter.barry@gmail.com>
2014-07-13 21:12:56 -0600

    Checkpoint

--- bc-calendar.pl
+++ bc-calendar.pl
@@ -36,19 +36,25 @@ my($gridcolor) = "0,0,255";
 my($eventspacing) = 15;
 my($eventsize) = "small";
 my($eventcolor) = "255,128,128";
-my($eventystart) = 20;
+my($eventystart) = 25;
 
 # last sunday (in days)
 my($time) = int(`date +%s -d 'last Sunday'`/86400);
+# and today (as stardate)
+my($now) = strftime("%Y%m%d", localtime(time()));
 
 open(A,"|fly -o /tmp/cal0.gif");
 print A "new\nsize $xsize,$ysize\nsetpixel 0,0,0,0,0\n";
 
 for $week (0..$weeks-1) {
   for $weekday (0..6) {
+
     # current day
-    $date = ($week*7+$weekday+$time+.5)*86400;
-    $day = strftime($dateformat, localtime($date));
+    my($date) = ($week*7+$weekday+$time+.5)*86400;
+    my($day) = strftime($dateformat, localtime($date));
+    # in "stardate" format (which is how I store entries)
+    my($stardate) = strftime("%Y%m%d", localtime($date));
+
     # x and y for top left
     my($x1, $y1) = ($xsize*$weekday/7, $ysize*$week/$weeks);
     # and bottom right
@@ -57,7 +63,7 @@ for $week (0..$weeks-1) {
     my($dx, $dy) = ($x1+$xpos*$xwid, $y1+$ypos*$ywid);
 
     # events for this day
-    my(@events) = @{$events{$week*7+$weekday+$time}};
+    my(@events) = @{$events{$stardate}};
     for $i (0..$#events) {
       my($eventy) = $y1+$eventystart+$eventspacing*$i;
       my($eventx) = $x1+5;
@@ -65,8 +71,13 @@ for $week (0..$weeks-1) {
     }
 
     print A "rect $x1,$y1,$x2,$y2,$gridcolor\n";
-    print A "string $datecolor,$dx,$dy,$datesize,$day\n";
-    debug("WEEK: $i, DAY: $j");
+
+    if ($stardate == $now) {
+      print A "frect,",$dx-3,",",$y1+2,",",$x2-3,",",$dy+20,",255,0,0\n";
+      print A "string 255,255,255,$dx,$dy,$datesize,$day\n";
+    } else {
+      print A "string $datecolor,$dx,$dy,$datesize,$day\n";
+    }
   }
 }
 
