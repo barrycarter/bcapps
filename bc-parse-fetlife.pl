17ac9c4f73115d736ae1fa899221e95f892c2c05
Barry Carter <carter.barry@gmail.com>
2014-08-10 08:31:39 -0600

    Checkpoint + daily update

--- bc-parse-fetlife.pl
+++ bc-parse-fetlife.pl
@@ -9,18 +9,6 @@
 
 require "/usr/local/lib/bclib.pl";
 
-system("rm -f /tmp/bcpfl.db");
-open(A,"|sqlite3 /tmp/bcpfl.db");
-
-# print the schema
-print A << "MARK";
-CREATE TABLE kinksters (userid INT, name, age INT, gender, role, city,
- orientation, active, latest, npics INT, nfriends INT);
-CREATE TABLE fetishes (user INT, type, fetish, role);
-BEGIN;
-MARK
-;
-
 # this is for a newer pull on 20140808
 # @files = glob("/home/barrycarter/20140808/[0-9]*");
 @files = split(/\n/, read_file("/home/barrycarter/20140808/filesbysize.txt"));
@@ -28,11 +16,24 @@ MARK
 for $i (@files) {
   if (++$count>20) {warn "TESTING"; last}
 
+  # user number in filename
+  $user = $i;
+  $user=~s/.*\///g;
+  # below is sort of pointless
+  $data{$user}{id} = $user;
+
   # read the file
   $all = read_file($i);
 
   # get rid of footer
   $all=~s/<em>going up\?<\/em>.*$//s;
+
+  # title (= username)
+  $all=~s%<title>(.*?) - Kinksters - FetLife</title>%%s||warn("BAD TITLE: $all");
+  $data{$user}{name} = $1;
+
+  # after getting title, get rid of header
+  $all=~s%^.*</head>%%s;
 
   # latest activity (could get all activity on front page, but no)
   $all=~s%<span class="quiet small">(.*? ago)</span>%%;
@@ -40,17 +41,13 @@ for $i (@files) {
   $data{$user}{latestactivity} = $1;
 
   # after getting latest activity, nuke the activity feed, it interferes
-  $all=~s%<ul id="mini_feed">(.*?)</ul>%%;
+  $all=~s%<ul id="mini_feed">(.*?)</ul>%%s;
 
-  # user number in filename
-  $user = $i;
-  $user=~s/.*\///g;
-  # below is sort of pointless
-  $data{$user}{id} = $user;
-
-  # title (= username)
-  $all=~s%<title>(.*?) - Kinksters - FetLife</title>%%s||warn("BAD TITLE: $all");
-  $data{$user}{name} = $1;
+  # now grab events (but not those in activity feed)
+  while ($all=~s%<a href="/events/(\d+)">(.*?)<%%s) {
+    $data{$user}{event}{$2} = 1;
+    $meta{event}{$2}{number} = $1;
+  }
 
   # number of pics (may have commas)
   if ($all=~s/view pics.*?\(([\,\d]+)\)//) {
@@ -65,7 +62,7 @@ for $i (@files) {
   }
 
   # age, and orientation/gender
-  $all=~s%<h2 class="bottom">$data{$user}{name}\s*<span class="small quiet">(\d+)(.*)\s+(.*?)</span></h2>%%||warn("NO EXTRA DATA: $all");
+  $all=~s%<h2 class="bottom">$data{$user}{name}\s*<span class="small quiet">(\d+)(.*)\s+(.*?)</span></h2>%%||warn("NO EXTRA DATA($i): $all");
   ($data{$user}{age}, $data{$user}{gender}, $data{$user}{role}) = ($1, $2, $3);
 
   # city if first /cities link in page
@@ -87,13 +84,13 @@ for $i (@files) {
     my($type, $fetishes) = ($1, $2);
 
     # look for ones with role attached first
-    while ($fetishes=~s%<a href="/fetishes/(\d+)">(.*?)</a> <span class="quiet smaller">\((.*?)\)</span>%%) {
+    while ($fetishes=~s%<a href="/fetishes/(\d+)">([^<>]*?)</a>\s*<span class="quiet smaller">\((.*?)\)</span>%%) {
       $data{$user}{fetish}{$type}{$2} = $3;
       $meta{fetish}{$2}{number} = $1;
     }
 
     # ones without a role
-    while ($fetishes=~s%<a href="/fetishes/(\d+)">(.*?)</a>%%) {
+    while ($fetishes=~s%<a href="/fetishes/(\d+)">([^<>]*?)</a>%%) {
       $data{$user}{fetish}{$type}{$2} = 1;
       $meta{fetish}{$2}{number} = $1;
     }
@@ -142,6 +139,18 @@ for $i (@files) {
 
 }
 
+system("rm -f /tmp/bcpfl.db");
+open(A,"|sqlite3 /tmp/bcpfl.db");
+
+# print the schema
+print A << "MARK";
+CREATE TABLE kinksters (id INT, name, age INT, gender, role, city,
+ orientation, active, latestactivity, npics INT, nfriends INT);
+CREATE TABLE fetishes (user INT, type, fetish, role);
+BEGIN;
+MARK
+;
+
 for $i (sort keys %data) {
   debug("USER: $i");
 #  debug("KEYS", sort keys %{$data{$i}});
@@ -152,8 +161,17 @@ for $i (sort keys %data) {
     delete $data{$i}{$j};
   }
 
-  for $j ("groups", "dsrelationshipstatus", "relationshipstatus", "fetish",
-	 "islookingfor") {
+  # cases with 4 levels of hashing
+  for $j ("dsrelationshipstatus", "relationshipstatus", "fetish") {
+    for $k (sort keys %{$data{$i}{$j}}) {
+      for $l (sort keys %{$data{$i}{$j}{$k}}) {
+	debug("BETA: $i $j $k $l");
+      }
+    }
+    delete $data{$i}{$j};
+  }
+
+  for $j ("groups", "islookingfor", "event") {
     for $k (sort keys %{$data{$i}{$j}}) {
       debug("BETA: $i $j $k $data{$i}{$j}{$k}");
     }
