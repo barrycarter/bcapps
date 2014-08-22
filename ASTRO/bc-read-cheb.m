d356a1878ad9f076839c0ff626d1310c8fbd5d06
Barry Carter <carter.barry@gmail.com>
2014-08-22 14:38:09 -0600

    Broken, coordinate issues (ie, xyz treated as separate days)

--- ASTRO/bc-read-cheb.m
+++ ASTRO/bc-read-cheb.m
@@ -2,6 +2,8 @@
 determine daily Taylor series to within 22m precision of Chebyshev
 polynomials *)
 
+(* day 0 = 1949-Dec-14 00:00:00.0000 UTC = JD 2433264.500000000 *)
+
 (* split coeffs into groups of 11 coefficients for Mars *)
 coeffs = Partition[coeffs,11]
 
@@ -11,9 +13,14 @@ cheb = Table[ChebyshevT[i,t],{i,0,10}]
 dayconv[n_,t_] = {Floor[n/32]+1, Mod[n,32]/16-1+t/32}
 
 
-(* the Chebyshev polynomial for day n *)
-tocheb[n_] := Expand[Total[coeffs[[Floor[n/32]+1]]*cheb] /. 
- t -> Mod[n,32]/16-1+t/16]
+(* the Chebyshev polynomial for day n, converted to Taylor *)
+
+tocheb[n_] := Round[1000*CoefficientList[Total[coeffs[[Floor[n/32]+1]]*cheb] /.
+t -> Mod[n,32]/16-1+t/16, t]]
+
+taylor = Table[tocheb[n], {n,0,Length[coeffs]*32/3-1}]
+
+Sum[tocheb[0][[i]]*t^(i-1),{i,1,Length[tocheb[0]]}]
 
 
 
