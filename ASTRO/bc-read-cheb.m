c7c23af0879b357997b2f4996285be3e7e890ce1
Barry Carter <carter.barry@gmail.com>
2014-08-22 13:09:23 -0600

    Still failing

--- ASTRO/bc-read-cheb.m
+++ ASTRO/bc-read-cheb.m
@@ -1,15 +1,24 @@
 (* given the output of bc-read-cheb.pl, a list of coefficients,
-determine daily Taylor series to within 10m precision of Chebyshev
+determine daily Taylor series to within 22m precision of Chebyshev
 polynomials *)
+
+(* given time t (fraction of a day) on the nth day, convert to list
+and point on Chebyshev polynomial *)
+
+tolist[n_, t_] = (t+n)/32+1
+
+(* convert to meters and round, will round again later *)
+coeffs = Round[coeffs*1000]
 
 (* split coeffs into groups of 11 coefficients for Mars *)
 coeffs = Partition[coeffs,11]
 
-(* convert a list to a Chebyshev polynomial *)
-tocheb[l_] := Function[x,
- Expand[Sum[l[[i]]*ChebyshevT[i-1,x],{i,1,Length[l]}]]]
+(* convert a list to a Chebyshev polynomial, get Taylor coeffs,
+multiply by 1000, round *)
 
-(* convert to meters and ignore terms smaller than a meter *)
-(* TODO: rounding before Taylor conversion = bad? *)
-coeffs = Round[coeffs*1000]
+tocheb[l_] :=
+Round[CoefficientList[Sum[l[[i]]*ChebyshevT[i-1,x],{i,1,Length[l]}],x]]
+
+coeffs2 = Table[tocheb[i], {i,coeffs}]
+
 
