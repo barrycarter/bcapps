6e021a1bbd64dd6cdbdfb6f99e3bb6a61f522ae5
Barry Carter <carter.barry@gmail.com>
2014-08-22 13:39:47 -0600

    Checkpoint, sort of kind of working

--- ASTRO/bc-read-cheb.m
+++ ASTRO/bc-read-cheb.m
@@ -2,16 +2,25 @@
 determine daily Taylor series to within 22m precision of Chebyshev
 polynomials *)
 
-(* given time t (fraction of a day) on the nth day, convert to list
-and point on Chebyshev polynomial *)
+(* split coeffs into groups of 11 coefficients for Mars *)
+coeffs = Partition[coeffs,11]
 
-tolist[n_, t_] = (t+n)/32+1
+(* a "blank" Chebyshev polynomial list, from 0 to 10 *)
+cheb = Table[ChebyshevT[i,t],{i,0,10}]
+
+dayconv[n_,t_] = {Floor[n/32]+1, Mod[n,32]/16-1+t/32}
+
+
+(* the Chebyshev polynomial for day n *)
+tocheb[n_] := Expand[Total[coeffs[[Floor[n/32]+1]]*cheb] /. 
+ t -> Mod[n,32]/16-1+t/16]
+
+
+
+
 
 (* convert to meters and round, will round again later *)
 coeffs = Round[coeffs*1000]
-
-(* split coeffs into groups of 11 coefficients for Mars *)
-coeffs = Partition[coeffs,11]
 
 (* convert a list to a Chebyshev polynomial, get Taylor coeffs,
 multiply by 1000, round *)
