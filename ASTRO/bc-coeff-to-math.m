26c3671bce08841dc95fcc032447f827ebf0d3e7
Barry Carter <carter.barry@gmail.com>
2014-09-10 21:52:14 -0600

    Checkpoint

--- ASTRO/bc-coeff-to-math.m
+++ ASTRO/bc-coeff-to-math.m
@@ -6,79 +6,81 @@
 
 (* Unix epoch is 2440587.500000 1970-01-01 or day 7323 in file *)
 
-<</home/barrycarter/20140823/raw-mercury.m
+<</home/barrycarter/20140823/raw-jupiter.m
 
 (* N to speed things up *)
-test0 = Partition[Partition[N[coeffs],14],3];
+test0 = Partition[Partition[N[coeffs],ncoeffs],3];
 
-test0 = Partition[Partition[coeffs,14],3];
+test0 = Partition[Partition[coeffs,ncoeff],3];
 
-Table[xpoly[n, t_] = chebyshev[test0[[n,1]],t], {n,1,Length[test0]}];
+Table[x0[t_ /; Evaluate[t>=(n-1)*ndays && t<n*ndays]] =
+ chebyshev[test0[[n,1]], 2*Mod[t,ndays]/ndays-1], {n,1,Length[test0]}];
 
-x0[t_] = Piecewise[Table[
- {chebyshev[test0[[n,1]], 2*Mod[t,ndays]/ndays-1],
- t>=(n-1)*ndays && t<n*ndays}, 
-{n,1,Length[test0]}]];
+Plot[x0[t],{t,0,365*12}]
 
-cond[n_] = t>=(n-1)*ndays && t<n*ndays
+(* for jupiter below *)
 
-f[x_] = x^2 /; x>0
-
-f[x_] = If[x>0,x^2]
-f[x_] = If[x<0,-x]
-
-f[x_ /; x>0] = x^2
-
-
-
-Table[
- x0[t_ /; t>=(n-1)*ndays && t<n*ndays] = 
- chebyshev[test0[[n,1]], 2*Mod[t,ndays]/ndays-1]
-, {n,1,10}]
-
-
-Table[
- x0[t_ /; Evaluate[cond[n]]] =
- chebyshev[test0[[n,1]], 2*Mod[t,ndays]/ndays-1]
-, {n,1,10}]
+max0 = t /. NMaximize[x0[t],{t,400,500}][[2]]
+min0 = t /. NMinimize[x0[t],{t,1500,3500}][[2]]
+mean0 = Mean[{x0[max0],x0[min0]}]
+mid0 = Mean[{max0,min0}]
+Plot[(x0[t]-mean0)/(x0[max0]-mean0),{t,max0,min0}]
+test1[t_] = ArcCos[(x0[t]-mean0)/(x0[max0]-mean0)]
+Plot[test1[t],{t,max0,min0},PlotRange->All,AxesOrigin->{max0,0}]
+ax[t_] = RationalInterpolation[test1[t], {t,6,4}, {t,max0,min0}]
+Plot[{Cos[ax[t]]*(x0[max0]-mean0)+mean0-x0[t]},{t,max0,min0}]
+showit
 
 
 
-Table[x0[t_] = chebyshev[test0[[n,1]], 2*Mod[t,ndays]/ndays-1] /;
- t>=(n-1)*ndays && t<n*ndays, {n,1,10}]
+ax[t_]=RationalInterpolation[x0[t], {t,4,4}, {t,0,88}]
+ax[t_]=RationalInterpolation[x0[t], {t,16,0}, {t,0,88}]
+
+Plot[Sin[t/88*2*Pi],{t,0,88}]
+
+Plot[{x0[t+76.7279]/Sin[t/87.9841*2*Pi]}, {t,0,88}]
+
+Plot[{x0[t+76.7279]/Sin[t/87.9841*2*Pi]}, {t,1,43},AxesOrigin->{0,0}]
+
+Plot[ArcSin[x0[t]/5.39982/10^7],{t,0,88}]
+
+(* finding 0s and maxs to do arcsin stuff with *)
+
+max0 = 12.1448
+min0 = 50.5263
+first0 = 76.7279
+firstmax = 100.114
+next0 = 118.144
+
+mean0 = Mean[{x0[max0],x0[min0]}]
+
+Plot[(x0[t]-mean0)/(x0[max0]-mean0),{t,max0,min0}]
+
+test1[t_] = ArcCos[(x0[t]-mean0)/(x0[max0]-mean0)]
+
+test1[t_] = ArcCos[x0[t]/Max[x0[max0],-x0[min0]]]
+
+Plot[test1[t],{t,max0,min0},AxesOrigin->{max0,0},PlotRange->All]
+
+ax[t_] = RationalInterpolation[test1[t], {t,4,3}, {t,76.7279,100.114}]
+
+ax[t_] = RationalInterpolation[test1[t], {t,8,0}, {t,76.7279,100.114}]
+
+ax[t_] = RationalInterpolation[test1[t], {t,4,3}, {t,max0,min0}]
+
+Plot[{ax[t]-test1[t]},{t,max0,min0}]
+
+Plot[{Cos[ax[t]]*(x0[max0]-mean0)+mean0-x0[t]},{t,max0,min0}]
+
+{Sin[ax[t]]*x0[100.114]-x0[t]},{t,76.7279,100.114},PlotRange->All]
 
 
-
-
-x0[t_] = xpoly[Floor[t/ndays]+1, 2*Mod[t,ndays]/ndays-1]
-
-(* possibly faster method below *)
-
-test1[t_] = {Floor[t/ndays]+1, 2*Mod[t,ndays]/ndays-1}
-
-Table[xfunc[t] = test0[[t,1]], {t,1,10}]
-
-test2[t_] = xfunc[Floor[t/ndays]+1]
-
-test2[t_] = chebyshev[xfunc[Floor[t/ndays]+1], t]
-
-
-
-x0[t_] = chebyshev[xfunc[Floor[t/ndays]+1], 2*Mod[t,ndays]/ndays-1]
-
-x0[t_] := chebyshev[test0[[Floor[t/ndays]+1,1]], 2*Mod[t,ndays]/ndays-1]
-y0[t_] := chebyshev[test0[[Floor[t/ndays]+1,2]], 2*Mod[t,ndays]/ndays-1]
-z0[t_] := chebyshev[test0[[Floor[t/ndays]+1,3]], 2*Mod[t,ndays]/ndays-1]
-
-(* above does not operate as function, so... *)
-
-x0[t_] = chebyshev[test0
+Plot[{ax[t]-x0[t]}, {t,0,88}]
 
 MiniMaxApproximation[x0[t],{t,{0,88},3,3}]
 
-test1 = Integrate[ax[t],{t,0,88}]/88.
+test1 = NIntegrate[x0[t],{t,0,88}]/88.
 
-ax[t_]=RationalInterpolation[x0[t]-test1, {t,3,3}, {t,0,88}]
 
 Plot[{ax[t]+test1,x0[t]}, {t,0,88}]
 
