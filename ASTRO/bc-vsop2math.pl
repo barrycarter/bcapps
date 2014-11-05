3e2571c19a83f5e988b63d5a583f5a6a236e4c2b
Barry Carter <carter.barry@gmail.com>
2014-11-05 10:28:32 -0700

    Checkpoint

--- ASTRO/bc-vsop2math.pl
+++ ASTRO/bc-vsop2math.pl
@@ -11,10 +11,14 @@ require "/usr/local/lib/bclib.pl";
 # "E" for barycentric
 $all = read_file("/home/barrycarter/SPICE/KERNELS/VSOP87E.ven");
 my(@coeffs);
+my($coeff, $varno);
 
 for $i (split(/\n/,$all)) {
   # does this line have a coefficient (if so, record it and move on)
-  if ($i=~/\*T\*\*(\d+)/) {$coeff = $1; next;}
+  if ($i=~/VARIABLE\s+(\d+)\s+.*\*T\*\*(\d+)/) {
+    ($varno, $coeff) = ($1,$2);
+    next;
+  }
 
   # only the last three fields matter
   # TODO: figure out what the other fields mean
@@ -23,16 +27,24 @@ for $i (split(/\n/,$all)) {
   my($a, $b, $c) = @fields[-3..-1];
   # TODO: rationalize
   # push to appropriate array
-  push(@{$coeffs[$coeff]}, "$a*Cos[$b + $c*t]");
+  push(@{$coeffs[$varno][$coeff]}, "$a*Cos[$b + $c*t]");
 }
 
-my(@sum);
+# debug("COEFFS",unfold(@{$coeffs[1]}));
 
-for $i (0..$#coeffs) {
-  my($terms) = join("+\n", @{$coeffs[$i]});
-  push(@sum, "($terms)*t^$i");
+# there is no var0
+for $varno (1..$#coeffs) {
+  my(@sum);
+  for $coeff (0..$#{$coeffs[$varno]}) {
+    debug("$varno/$coeff");
+    my($terms) = join("+\n", @{$coeffs[$varno][$coeff]});
+    debug("TERMS: $terms");
+    push(@sum, "($terms)*t^$coeff");
+  }
+
+  my($sum) = join("+\n", @sum);
+  print "var${varno}[t_] = $sum;\n";
+  # 1 AU = 149597870.7 km by definition
+  # fvar is in Unix days, and uses km, not AU
+  print "fvar${varno}[d_] = var${varno}[(d-10957.5000000000)/365250]*149597870.7;\n"
 }
-
-my($final) = "f[t_] = ".join("+\n", @sum).";";
-
-write_file($final, "/tmp/math1105.m");
