d1ecdac1faeaad08aa8db962b5fb15f7ea2269a6
Barry Carter <carter.barry@gmail.com>
2015-01-02 07:22:55 -0700

    Checkpoint

--- bc-reduce-redundancy.pl
+++ bc-reduce-redundancy.pl
@@ -8,19 +8,38 @@
 # we use 128-255 only to tokenize for (more efficient if we used 0-31
 # and maybe 127 too, but not now)
 
+# we will use 4 7-byte numbers to store, so up to 3 invalid are ok
+
 require "/usr/local/lib/bclib.pl";
 
 # Step 1 (outside this script for now):
 # cat files | sort | uniq -c | sort -nr > files.uniq
 
-$all = read_file("files.uniq");
+my($all) = read_file("files.uniq");
 
-if ($all=~/[\x7F-\xFF]/) {
-  die ("Files contain character in invalid range");
-}
+# copy I can modify
+my($all2) = $all;
 
-die "TESTING";
+# the hash to hold the translations
+my(%hash);
 
-for $i (split(/\n/, read_file("files.uniq"))) {
-  debug("I: $i");
+# we will use special characters to encode; if file already has
+# special characters, note them
+
+# below takes too long, doing it line by line but that adds complications
+# while ($all2=~s/([\x7F-\xFF]+)//s) {$hash{$1} = $1;}
+
+# the number for the hash
+my($count);
+
+for $i (split(/\n/, $all)) {
+  # lines CAN start with spaces, so only one space between count and line
+  $i=~/\s*(\d+) (.*)$/;
+  my($num,$line) = ($1,$2);
+
+  while ($line=~s/([\x7F-\xFF]+)//) {
+    # TODO: must handle this case!
+    warn("BAD CHAR: $1");
+  }
+  debug("$num/$line");
 }
