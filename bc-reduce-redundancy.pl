635fd30fc8e13f64bc9ceae14d813961cfcbc262
Barry Carter <carter.barry@gmail.com>
2015-01-02 06:23:03 -0700

    Checkpoint

---
+++ bc-reduce-redundancy.pl
@@ -0,0 +1,10 @@
+#!/bin/perl
+
+# Given a list of files, find redundant phrases in these files and
+# replace them with tokenized shorter phrases
+
+require "/usr/local/lib/bclib.pl";
+
+# Step 1 (outside this script for now): cat/sort/uniq all files in
+# question to "files.uniq"
+
