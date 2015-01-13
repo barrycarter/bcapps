5cb68ab5c6a7dde8ff3041a4ef378504eb933ce5
Barry Carter <carter.barry@gmail.com>
2015-01-13 09:23:26 -0700

    Checkpoint, not working

--- PEANUTS/bc-parse-peanuts.pl
+++ PEANUTS/bc-parse-peanuts.pl
@@ -4,28 +4,61 @@
 
 require "/usr/local/lib/bclib.pl";
 chdir("$bclib{githome}/PEANUTS");
+my(%data);
 
 for $i (glob "peanuts-???-????.txt") {
+
+  debug("READING: $i");
 
   # read file
   $all = read_file($i);
 
-  my(@colons) = split(/^([A-Z][a-z]{1,10}:)/m, $all);
+  # each pair is header/value (dashes permitted)
+  my(@colons) = split(/^([a-z-]{1,15}:)/im, $all);
 
-  for $j (@colons) {
-    debug("J: $j");
-  }
+  my($date);
 
-  next;
+  # TODO: not sure why I have an extra line here
+  while (scalar(@colons)>1) {
+    my($h,$v) = splice(@colons,1,2);
 
-  # split by date
-  for $j (split(/date:/i, $all)) {
+    # cleanup here
+    $h=~s/://;
+    $h = trim($h);
+    $v = trim($v);
+    $h =~s/\s+/ /g;
+    $v =~s/\s+/ /g;
 
-    # first line is the date itself
-    $j=~s/^(.*?)\n//;
-    my($date) = $1;
-    debug("J: $j");
+    # if header is date, set new date
+    if ($h=~/date/i) {$date = $v; next;}
+
+    # value separator for wiki is plus not comma (except for dates)
+    $v=~s/,\s*/+/g;
+
+    # otherwise, associate data with date
+#    debug("$h -> $v");
+    $data{$date}{$h} = $v;
   }
 }
 
+# now, put into format for wiki
+
+for $i (keys %data) {
+  debug("I: $i");
+
+  # TODO: this is really ugly: change the year to 1970 so str2time works
+  $i2 = $i;
+  $i2 =~s/(\d{4})/1970/;
+  my($d) = strftime("$1-%m-%d", localtime(str2time($i2)+43200));
+
+  my(@arr);
+
+  # chunks for each key/val pair
+  for $j (keys %{$data{$i}}) {
+    push(@arr, "[[".lc($j)."::$data{$i}{$j}]]");
+  }
+
+  debug("$d",@arr);
+
+}
 
