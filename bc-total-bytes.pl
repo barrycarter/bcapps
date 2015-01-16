58dbc01490de55883d696d2f8a3a2690f2ff07c6
Barry Carter <carter.barry@gmail.com>
2015-01-16 06:55:06 -0700

    About to reduce memory usage

--- bc-total-bytes.pl
+++ bc-total-bytes.pl
@@ -4,6 +4,10 @@ require "/usr/local/lib/bclib.pl";
 
 # --justfiles: just print out a list of files (ie, for tar --bzip --from-files)
 
+# Altered to use the output of: stat -c "%s %Y %Z %d %i %n"
+
+# TODO: generalize this
+
 # Given the (possibly filtered) output of "find [...] -ls", find the
 # total space used for each file (already given) and each directory,
 # including parent directories
@@ -12,16 +16,21 @@ require "/usr/local/lib/bclib.pl";
 
 my(%size);
 
+# list of fields, except filename which must be last due to spaces
+my(@format) = ("size", "mtime", "ctime", "devno", "inode");
+
 while (<>) {
 
   # cleanup
-  s/^\s*(.*?)\s*$/$1/;
+#  s/^\s*(.*?)\s*$/$1/;
 
+  my(%hash) = ();
   my(@data) = split(/\s+/, $_);
-  my($size) = $data[6];
+
+  for $i (@format) {$hash{$i} = shift(@data);}
 
   # this assumes filenames are split by single spaces = bad?
-  my($file) = join(" ",@data[10..$#data]);
+  my($file) = join(" ",@data);
 
   if ($globopts{justfiles}) {
     # if just printing, remove backslashes
@@ -30,8 +39,10 @@ while (<>) {
     next;
   }
 
+  debug("$file $hash{size}");
+
   # find all ancestor directories
-  do {$size{$file} += $size;} while ($file=~s/\/([^\/]*?)$//);
+  do {$size{$file} += $hash{size};} while ($file=~s/\/([^\/]*?)$//);
 }
 
 for $i (sort {$size{$b} <=> $size{$a}} keys %size) {print "$size{$i} $i\n";}
