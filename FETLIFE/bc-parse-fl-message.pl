a8360ee2f869dac775346c4a610237e8fe98b480
Barry Carter <carter.barry@gmail.com>
2015-01-27 05:44:42 -0700

    Checkpoint

--- FETLIFE/bc-parse-fl-message.pl
+++ FETLIFE/bc-parse-fl-message.pl
@@ -7,6 +7,57 @@ require "/usr/local/lib/bclib.pl";
 
 my($all,$fname) = cmdfile();
 
+# need title to find group name (+ also original URL)
+$all=~s%<title>(.*?)</title>%%s;
+my($htmltitle) = $1;
+$htmltitle=~s/.* \- (.*?) \- FetLife//s;
+my($group) = $1;
+
+$all=~s%The material appears at the following website addresses: (.*?)</p>%%;
+my($url) = $1;
+
+# group number
+$url=~m%/groups/(\d+)/%;
+my($gnum) = $1;
+
+# original message
+$all=~s/<div class="group_post clearfix">(.*?)<div id="group_post_comments_container" class="discussion_container">//s;
+
+my($post) = $1;
+
+# extract title/time/etc
+$post=~s%<h3>(.*?)</h3>%%s;
+my($title) = $1;
+
+# find group name from HTML title
+
+
+# this wipes out everything above the post too (so we have just the
+# message left?) and the (useless) date info
+
+$post=~s%^.*?by <a href="https://fetlife.com/users/(\d+)" class="quiet">(.*?)</a>.*?</p>%%s;
+
+my($uno, $una) = ($1,$2);
+
+$post = text_cleanup($post);
+$post=~s/^\s*//g;
+$post=~s/\s*$//g;
+
+print << "MARK";
+Original URL: $url
+
+## [$group][http://fetlife.com/groups/$gnum]
+
+### $title
+#### by [http://fetlife.com/users/$uno][$una]
+---
+$post
+
+#### Responses
+---
+MARK
+;
+
 while ($all=~s%<article[^>]*?>(.*?)</article>%%is) {
   my($comment) = $1;
 
@@ -23,7 +74,28 @@ while ($all=~s%<article[^>]*?>(.*?)</article>%%is) {
   my($text) = $1;
 
   # cleanups
-  $time=~s/\s*\+0000/UTC/;
+  $time=~s/\s*\+0000/ UTC/;
+  $text = text_cleanup($text);
+
+  # text cleanups
+  print << "MARK";
+**[http://fetlife.com/users/$uno][$una]** (*$time*)
+
+$text
+
+MARK
+;
+}
+
+# subroutine specific to this program
+sub text_cleanup {
+  my($text) = @_;
+
+  debug("ALPHA: $text");
+  $text=~s/\xe2\x80\xa6/.../g;
+  # TODO: this doesn't work well if both $1 and $2 are links
+  $text=~s%<a href="(.*?)"[^>]*>([^<]*?)</a>%[$1][$2]%g;
+  debug("BETA: $text");
   $text=~s/\n/ /sg;
   $text=~s/^\s*//;
   $text=~s/\s*$//;
@@ -32,12 +104,12 @@ while ($all=~s%<article[^>]*?>(.*?)</article>%%is) {
   $text=~s%<br>%\n\n%g;
   $text=~s%<blockquote>(.*?)</blockquote>%> $1%sg;
   $text=~s/<.*?>//sg;
+  # this must happen last
+  $text=~s/\&lt\;/</g;
+  $text=~s/\&gt\;/>/g;
+  $text=~s/\&amp\;/&/g;
+  $text=~s/\xe2\x80\x93/-/g;
+  $text=~s/\xe2\x80\x99/'/g;
 
-  print << "MARK";
-**[http://fetlife.com/users/$uno][$una]** (*$time*)
-
-$text
-
-MARK
-;
+  return $text;
 }
