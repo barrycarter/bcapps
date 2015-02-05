6d898b291cadef263d6eea1de1a19283d74a618f
Barry Carter <carter.barry@gmail.com>
2015-02-05 13:57:35 -0700

    Checkpoint

--- BACKUP/bcunix-dump.sh
+++ BACKUP/bcunix-dump.sh
@@ -4,9 +4,7 @@
 # dumps files and creates a reverse lookup table
 cd /
 date > bcunix-files.txt.new
-# the /bin below is required, otherwise uses window's find
-# trying this sans -xdev, may be a bad idea
-find / -print0|xargs -0 stat -c "%s %Y %i %f %g %u %n">>bcunix-files.txt.new
+find / -xdev -print0|xargs -0 stat -c "%s %Y %i %f %g %u %n">>bcunix-files.txt.new
 date >> bcunix-files.txt.new
 # bzip the previous instance and save it, keep current instance uncompressed
 bzip2 -v bcunix-files.txt
@@ -14,3 +12,13 @@ mv bcunix-files.txt.bz2 bcunix-files.txt.old.bz2
 mv bcunix-files.txt.new bcunix-files.txt
 # TODO: this leaves bcunix-files-rev.txt briefly incomplete
 perl -nle 's%^.*?\/%/%; print $_' bcunix-files.txt|rev|sort>bcunix-files-rev.txt
+# and now the extdrive
+cd /mnt/extdrive/
+date > extdrive-files.txt.new
+find /mnt/extdrive/ -xdev -print0|xargs -0 stat -c "%s %Y %i %f %g %u %n">>extdrive-files.txt.new
+date >> extdrive-files.txt.new
+bzip2 -v extdrive-files.txt
+mv extdrive-files.txt.bz2 extdrive-files.txt.old.bz2
+mv extdrive-files.txt.new extdrive-files.txt
+# TODO: this leaves extdrive-files-rev.txt briefly incomplete
+perl -nle 's%^.*?\/%/%; print $_' extdrive-files.txt|rev|sort>extdrive-files-rev.txt
