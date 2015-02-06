6ad1f59e88284e95564f18426779e7bc2f0fcece
Barry Carter <carter.barry@gmail.com>
2015-02-06 11:12:28 -0700

    Checkpoint

--- BACKUP/bc-chunk-backup.pl
+++ BACKUP/bc-chunk-backup.pl
@@ -11,7 +11,7 @@ defaults("stop=1");
 
 # setting $tot to $limit so first run inside loop increments chunk
 # TODO: $limit should be an option
-my($limit) = 5e+9;
+my($limit) = 50e+9;
 my($tot) = $limit;
 
 while (<>) {
