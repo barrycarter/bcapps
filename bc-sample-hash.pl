4a170e82937ea87cff478fea3c49e82cd070380e
Barry Carter <carter.barry@gmail.com>
2015-02-13 10:33:29 -0700

    Works

---
+++ bc-sample-hash.pl
@@ -0,0 +1,40 @@
+#!/bin/perl
+
+# Hashing very large files to compare them for equality is difficult;
+# this program cheats by "randomly" (but consistently) sampling 10K of
+# each 1M of the file, and hashing the result
+
+require "/usr/local/lib/bclib.pl";
+
+for $i (@ARGV) {
+  # this is important; must re-seed each time
+  srand(20150213666);
+
+  # TODO: test for non-openability
+  open(A,$i);
+
+  my($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)=stat($i);
+  # -1 because last chunk is incomplete
+  my($chunks) = floor($size/1000000)-1;
+  if ($chunks==0) {warn "SKIPPING $i, < 1M"; next;}
+
+  # where I store the sha1s for each chunk, appended together
+  my($sha);
+
+  # TODO: it would be more efficient to just have a bunch of random
+  # numbers pre-generated
+  for $j (0..$chunks) {
+    my($rand) = int(rand(1000000));
+    my($buf);
+#    debug("RAND: $rand");
+    # seek and read the 10K bytes
+    seek(A, $rand, SEEK_SET);
+    read(A, $buf, 10000);
+    $sha .= sha1_hex($buf);
+  }
+
+  # sha the appended sha strings
+  my($shafin) = sha1_hex($sha);
+  # and print
+  print "$shafin $i\n";
+}
