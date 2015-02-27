9f0a8782277cc8c63ab6898d4c005d0c214e54aa
Barry Carter <carter.barry@gmail.com>
2015-02-27 12:41:23 -0700

    Checkpoint

--- playground10.pl
+++ playground10.pl
@@ -2,18 +2,49 @@
 
 # use images instead of text in fly text
 
+require "/usr/local/lib/bclib.pl";
+
+@ret = flystring(100,500,"hello","tiny","255,0,0");
+
+print "new\nsize 800,600\nsetpixel 0,0,255,255,255\n";
+print join("\n",@ret),"\n";
+
 # from http://tecfa.unige.ch/guides/utils/fly-use.html
 # tiny (5x8), small (6x12), medium (7x13), large (8x16), giant (9x15)
+
+die "TESTING";
 
 print << "MARK";
 new
 size 800,600
-setpixel 0,0,0,0,0
+setpixel 0,0,255,255,255
 string 255,0,0,100,500,tiny,hello
-# copy 50,60,0,0,50,50,/home/barrycarter/20140716/m228.gif.temp 
-copy 50,60,0,0,50,50,/home/barrycarter/BCGIT/ASTRO/Earth_symbol.svg.gif
-# copyresized -1,-1,-1,-1,500,500,550,550,/home/barrycarter/BCGIT/ASTRO/Earth_symbol.svg.png
-# copyresized 550,550,600,600,0,0,50,50,/home/barrycarter/BCGIT/ASTRO/Earth_symbol.svg
-# copyresized -1,-1,-1,-1,500,500,600,600,/home/barrycarter/BCGIT/ASTRO/Earth_symbol.svg
+copyresized -1,-1,-1,-1,110,510,120,520,/home/barrycarter/BCGIT/ASTRO/Earth_symbol.svg.gif
+colourchange 0,0,0,255,0,0,0
 MARK
 ;
+
+=item flystring($x,$y,$str,$size,$color)
+
+Generate the fly commands to place $str of size $size at $x,$y but
+allow for images in strings, eg "hel[image:path-to-image]0"
+
+=cut
+
+sub flystring {
+  my($x,$y,$str,$size,$color) = @_;
+  my(@ret);
+
+  # assuming giant for now
+  # handle image case first, and then text
+
+  while ($str) {
+    $str=~s/^(.)//;
+    push(@ret, "string $color,$x,$y,$size,$1");
+    $x+=5;
+  }
+
+  return @ret;
+}
+
+
