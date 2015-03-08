590f436ed8c58c5faa8b3c9f3b9c2f0f7445894c
Barry Carter <carter.barry@gmail.com>
2015-03-08 17:01:31 -0600

    Checkpoint

--- playground11.pl
+++ playground11.pl
@@ -2,7 +2,48 @@
 
 # Fun w/ FUSE
 
+require "/usr/local/lib/bclib.pl";
 use Fuse::Simple qw(accessor main);
+
+# list of files in (currently hardcoded) zpaq file
+my($out,$err,$res) = cache_command("/root/build/ZPAQ71/zpaq list /home/barrycarter/20150308/testfile.zpaq", "age=86400");
+
+# TODO: error checking
+
+my(%fs);
+
+for $i (split(/\n/, $out)) {
+  my($symb, $date, $time, $size, $mode, $file) = split(/\s+/,$i,6);
+  # ignore directories and non-file entries
+  unless ($symb eq "-") {next;}
+  if ($mode=~/^d/) {next;}
+
+  # TODO: there must be a better way to do this (tm)
+#  $file=~s%/%\}\{%g;
+#  $file="\$fs->\{$file\} = sub {test(\"$file\")};";
+
+  $fs->{$file} = sub {test($file);};
+}
+
+debug("OUT: $out");
+
+$var = "x";
+$fs->{foo}{bar}{$var} = sub {test($var)};
+$fs->{foo}{bar}{y} = sub {test("y")};
+
+sub test {return $_[0];}
+
+die "TESTING";
+
+
+main("mountpoint" => "/home/barrycarter/20150308/fusetest", "/" => $fs);
+
+
+
+
+
+
+
   my $var = "this is a variable you can modify. write to me!\n";
   my $filesystem = {
     foo => "this is the contents of a file called foo\n",
@@ -17,6 +58,7 @@ use Fuse::Simple qw(accessor main);
     "var.b" => accessor(\ my $tmp),   # and an anonymous var
     "date" => sub {return time();}
   };
+
   main(
     "mountpoint" => "/home/barrycarter/20150308/fusetest",      # actually optional
     "debug"      => 0,           # for debugging Fuse::Simple. optional
