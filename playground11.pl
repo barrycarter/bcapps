08079f6e872fdb7f9fb6d912e81bff76767c21bd
Barry Carter <carter.barry@gmail.com>
2015-03-08 18:58:17 -0600

    Checkpoint

--- playground11.pl
+++ playground11.pl
@@ -10,59 +10,38 @@ my($out,$err,$res) = cache_command("/root/build/ZPAQ71/zpaq list /home/barrycart
 
 # TODO: error checking
 
-my(%fs);
+my($fs);
 
 for $i (split(/\n/, $out)) {
+
   my($symb, $date, $time, $size, $mode, $file) = split(/\s+/,$i,6);
   # ignore directories and non-file entries
   unless ($symb eq "-") {next;}
   if ($mode=~/^d/) {next;}
 
+#  $file="/$file";
+  my(@dirs) = split(/\//, $file);
+  map($_="{'$_'}", @dirs);
+
+  my($eval) = "\$fs->".join("",@dirs)."=sub{test('$file')}";
+  debug("EVAL: $eval");
+  eval($eval);
+
+  if ($count++>100) {last;}
+
+
   # TODO: there must be a better way to do this (tm)
 #  $file=~s%/%\}\{%g;
 #  $file="\$fs->\{$file\} = sub {test(\"$file\")};";
 
-  $fs->{$file} = sub {test($file);};
+#  debug("F: $file");
+
+#  $fs->{$file} = sub {test($file);};
 }
 
-debug("OUT: $out");
-
-$var = "x";
-$fs->{foo}{bar}{$var} = sub {test($var)};
-$fs->{foo}{bar}{y} = sub {test("y")};
+debug("FS",unfold($fs));
 
 sub test {return $_[0];}
 
-die "TESTING";
-
-
 main("mountpoint" => "/home/barrycarter/20150308/fusetest", "/" => $fs);
 
-
-
-
-
-
-
-  my $var = "this is a variable you can modify. write to me!\n";
-  my $filesystem = {
-    foo => "this is the contents of a file called foo\n",
-    subdir => {
-      "foo"  => "this foo is in a subdir called subdir\n",
-      "blah" => "this blah is in a subdir called subdir\n",
-    },
-    "blah" => \ "subdir/blah",        # scalar refs are symlinks
-    "magic" => sub { return "42\n" }, # will be called to get value
-    "var"  => accessor(\$var),        # read and write this variable
-    "var2" => accessor(\$var),        # and the same variable
-    "var.b" => accessor(\ my $tmp),   # and an anonymous var
-    "date" => sub {return time();}
-  };
-
-  main(
-    "mountpoint" => "/home/barrycarter/20150308/fusetest",      # actually optional
-    "debug"      => 0,           # for debugging Fuse::Simple. optional
-    "fuse_debug" => 0,           # for debugging FUSE itself. optional
-    "threaded"   => 0,           # optional
-    "/"          => $filesystem, # required :-)
-  );
