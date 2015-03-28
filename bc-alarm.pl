9405fe80466cce6259bc08e3be46e5f0a89b4f03
Barry Carter <carter.barry@gmail.com>
2015-03-28 17:39:47 -0600

    Checkpoint

--- bc-alarm.pl
+++ bc-alarm.pl
@@ -5,6 +5,7 @@
 # time must be in format understood by "at"
 
 require "/usr/local/lib/bclib.pl";
+require "/home/barrycarter/bc-private.pl";
 
 my($time) = shift(@ARGV);
 # TODO: catenating remaining args is probably a bad idea
@@ -12,6 +13,11 @@ my($msg) = join(" ",@ARGV);
 
 open(A,"|at -v $time")||die("Can't open at command, $!");
 # TODO: is TERM setting below necessary?
-print A "DISPLAY=:0.0; export DISPLAY; TERM=vt100; export TERM; xmessage -geometry 1024 $msg &";
+print A "DISPLAY=:0.0; export DISPLAY; TERM=vt100; export TERM; xmessage -geometry 1024 $msg & bc-call-func.pl sendmail $private{email}{from} $private{email}{sms} $msg &";
 close(A);
 
+# this uses ifttt.com to send SMS
+# TODO: FAIL!!! this can't occur right away
+# sendmail($private{email}{from}, $private{email}{sms}, $msg, "");
+
+# TODO: also add alarm text to bg image?
