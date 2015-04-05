90fa6d4ae716643901a7ed5825ac423d423e2578
Barry Carter <carter.barry@gmail.com>
2015-04-05 14:51:00 -0600

    Checkpoint

--- BACKUP/bc-zpaq2exclude.pl
+++ BACKUP/bc-zpaq2exclude.pl
@@ -1,11 +1,16 @@
 #!/bin/perl
 
 # Given the output of "zpaq list", outputs data that can be used to
-# exclude these files from next backup (via mtime, size and filename)
+# exclude these files from next backup (via mtime and filename)
+
+# The SQLite3 lines below are experimental
 
 require "/usr/local/lib/bclib.pl";
+open(A,">/tmp/bczpaq2exclude.sql");
+print A "BEGIN;\n";
 
 while (<>) {
+  chomp;
   my($symb, $date, $time, $size, $mode, $file) = split(/\s+/,$_,6);
 
   # ignroe directories
@@ -21,5 +26,36 @@ while (<>) {
 
   # 10 digits because comm requires lexical sort
   my($time) = sprintf("%0.10d", str2time("$date $time UTC"));
-  print "$time $size $file";
+
+  # because I unbzip2/ungzip files, must list all 3 versions of file
+  print "$time $file\n";
+  print "$time $file.bz2\n";
+  print "$time $file.gz\n";
+
+  if ($file=~s/\.tar$/.tbz/) {print "$time $file\n";}
+
+  # just for sqlite3
+  $file=~s/\'/''/g;
+  print A "INSERT INTO files (mtime, size, filename, backedup) VALUES
+ ($time, $size, '$file', 'Y');\n";
 }
+
+print A "COMMIT;\n";
+close(A);
+
+=item schema
+
+Schema of mysql tables:
+
+CREATE TABLE files (
+ mtime INT,
+ size INT,
+ filename TEXT,
+ backedup CHAR(1)
+);
+
+CREATE INDEX i1 ON files(mtime,filename);
+
+
+=cut
+
--- bc-xwd-rm.pl
+++ bc-xwd-rm.pl
@@ -10,41 +10,45 @@
 # TODO: above is probably inefficient
 require "/usr/local/lib/bclib.pl";
 
-# sshfs can be a little slow, so archiving
+# sshfs can be a little slow, so archiving...
+
+# which archives do I have/
 my($out,$err,$res) = cache_command2("echo /mnt/sshfs/XWD/*.zpaq","age=86400");
-my(@zpaq) = split(/\s+/,$out);
-map(s%/mnt/sshfs/XWD/(\d+).zpaq%$1%, @zpaq);
+my(%zpaq,%dirs);
+for $i (split(/\s+/,$out)) {
+  if ($i=~m%/mnt/sshfs/XWD/(\d{8}).zpaq%) {
+    $zpaq{$1}=1;
+  }
+}
 
 # which directories still exist (no point in testing otherwise)
-my(@dirs);
 my($out,$err,$res) = cache_command2("find /mnt/sshfs/XWD/ -maxdepth 1 -mindepth 1 -type d","age=86400");
-while ($out=~s%/mnt/sshfs/XWD/(\d{8})%%) {push(@dirs,$1);}
+while ($out=~s%/mnt/sshfs/XWD/(\d{8})%%) {$dirs{$1}=1;}
 
-my(@both) = 
+# and which have both (ie, our final check list)
+my(@flist);
+for $i (keys %zpaq) {
+  if ($dirs{$i}) {
+    push(@flist,$i);
+  }
+}
 
-debug("DIRS",@dirs,"ZPAQ",@zpaq);
-
-die "TESTING";
-
-for $i (@zpaq) {
+for $i (@flist) {
   my(%hash);
 
-  # grab the date from filename
-  unless($i=~m%/mnt/sshfs/XWD/(.*?)\.zpaq%) {warn "BAD FILE: $i"; next;}
-  my($i2) = $1;
-
   # the files I've archived
-  my($out,$err,$res) = cache_command2("zpaq list $i","age=86400");
-  while ($out=~s%$i2/pic\.($i2:.*?)\.png\.pnm%%s) {$hash{zpaq}{$1} = 1;}
+  my($out,$err,$res) = cache_command2("zpaq list /mnt/sshfs/XWD/$i.zpaq","age=86400");
+  while ($out=~s%$i/pic\.($i:.*?)\.png\.pnm%%s) {$hash{zpaq}{$1} = 1;}
 
   # the files I've OCRd
-  my($out,$err,$res)=cache_command2("find /mnt/sshfs/XWD2OCR/$i2","age=86400");
-  while ($out=~s%$i2/pic\.($i2:.*?)\.png\.txt%%s) {$hash{ocr}{$1} = 1;}
+  my($out,$err,$res)=cache_command2("find /mnt/sshfs/XWD2OCR/$i","age=86400");
+  while ($out=~s%$i/pic\.($i:.*?)\.png\.txt%%s) {$hash{ocr}{$1} = 1;}
 
   # TODO: I can probably do this better...
   for $j (keys %{$hash{zpaq}}) {
     if ($hash{ocr}{$j}) {
-      debug("J: /mnt/sshfs/XWD/$i2/$j.png");
+      print "rm /mnt/sshfs/XWD/$i/pic.$j.png\n";
+      print "rm /mnt/sshfs/XWD/$i/pic.$j.png.pnm\n";
     }
   }
 }
