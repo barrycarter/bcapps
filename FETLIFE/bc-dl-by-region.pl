436945b164b47241653e1ad3df8919ba8fca7cbe
Barry Carter <carter.barry@gmail.com>
2015-04-20 10:19:41 -0600

    Checkpoint

--- FETLIFE/bc-dl-by-region.pl
+++ FETLIFE/bc-dl-by-region.pl
@@ -9,6 +9,11 @@
 
 require "/usr/local/lib/bclib.pl";
 
+# this session id is publicly posted at
+# http://trilema.com/2015/fetlife-the-meat-market/ (somewhat
+# surprising that it still works!)
+$fl_cookie = "_fl_sessionid=9c69a3c9bb86f4b1f6ff74064e788824";
+
 for $i (@ARGV) {
   my($data) = read_file($i);
   # <h>the abbreviation for country below is in honor of Fetlife</h>
@@ -34,32 +39,31 @@ for $i (@ARGV) {
 for $i ("/countries/233", "/cities/8630", "/cities/11529") {delete $pages{$i};}
 
 # print page 1 for each URL, then page 2, etc
+# there might be more efficient ways to do this? (but fast enough for me)
+
+my(@curls);
 
 while (%pages) {
   $count++;
 
   # print all URLs that still have pages, delete those that dont
   for $i (keys %pages) {
-    if ($pages{$i}>=$count) {
-      debug("PRINT: $i -> $count");
-    } else {
-      delete $pages{$i};
-    }
+    # ignore and delete URLs that have a lower page count
+    if ($pages{$i}<$count) {delete $pages{$i}; next;}
+
+    # url for download and output filename (can't use -O will overwrite)
+    my($url) = "https://fetlife.com/$i/kinksters?page=$count";
+    my($fname) = "fetlife-$i-p$count.txt";
+
+    # this is just to make a "pretty" filename
+    $fname=~s/administrative_areas/aa-/g;
+    $fname=~s/countries/co-/g;
+    $fname=~s/(\d+)/sprintf("%0.6d",$1)/iseg;
+    $fname=~s%/%%g;
+
+    # the server I'm using does NOT have parallel, but does have xargs
+    # -P, so the 'curl' isn't actually printed below
+    print "-H 'Cookie: $fl_cookie' -o $fname '$url'\n";
+
   }
 }
-
-die "TESTING";
-
-while (<>) {
-#  debug("THUNK: $_");
-  if (m%>(.*?) Kinksters living in (.*?)<%) {
-    debug("GOT: $1 $2");
-  }
-
-  if (m%<a href=\"(.*?)/kinksters\">view more%) {
-    debug("GOT: $1");
-  }
-
-#  debug("GOT: $_");
-}
-
