07d08c8b38090215f66cf97c594177158509a124
Barry Carter <carter.barry@gmail.com>
2015-04-26 09:43:31 -0600

    Checkpoint

--- FETLIFE/bc-dl-users.pl
+++ FETLIFE/bc-dl-users.pl
@@ -10,6 +10,7 @@ require "/usr/local/lib/bclib.pl";
 # TODO: if/when this stops working, allow user to create/set
 $fl_cookie = "_fl_sessionid=9c69a3c9bb86f4b1f6ff74064e788824";
 
+my($bad);
 
 # TODO: start id should not be fixed as it is below
 
@@ -17,15 +18,23 @@ $id = 4623145;
 
 for (;;) {
   $id++;
-  if (-f "/usr/local/etc/FETLIFE/user$id.bz2" || -f "/usr/local/etc/FETLIFE/user$id") {next;}
-  debug("DLSTART: $id");
+  if (-f "/usr/local/etc/FETLIFE/user$id.bz2" || -f "/usr/local/etc/FETLIFE/user$id") {
+    debug("ALREADYHAVE: $id");
+    next;
+  }
+
   my($url) = "https://fetlife.com/users/$id";
   my($out,$err,$res) = cache_command2("curl -o /usr/local/etc/FETLIFE/user$id -H 'Cookie: $fl_cookie' 'https://fetlife.com/users/$id'", "age=86400");
   my($data) = read_file("/usr/local/etc/FETLIFE/user$id");
-  debug("RES: $res");
 
-  if (++$count%25==0) {
-    debug("DURING DEBUT STAGE, HIT ENTER TO CONTINE (every 25)");
+  # look for 10 *consecutive* bds
+  unless ($data=~/<title>/) {$bad++;} else {$bad=0;}
+
+  # TODO: improve this exit condition
+  if ($bad>10) {die "TOO MANY BAD IN A ROW";}
+
+  if (++$count%50==0) {
+    debug("DURING DEBUT STAGE, HIT ENTER TO CONTINE (every 50)");
     <STDIN>;
   }
 }
