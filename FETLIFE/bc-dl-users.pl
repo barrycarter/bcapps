ef465e3cc2e495e2f95eaf25c5c0634a69740ffc
Barry Carter <carter.barry@gmail.com>
2015-04-26 10:48:37 -0600

    Checkpoint

--- FETLIFE/bc-dl-users.pl
+++ FETLIFE/bc-dl-users.pl
@@ -11,6 +11,9 @@ require "/usr/local/lib/bclib.pl";
 $fl_cookie = "_fl_sessionid=9c69a3c9bb86f4b1f6ff74064e788824";
 
 my($bad);
+my(@have);
+my(@got);
+my(@bad);
 
 # TODO: start id should not be fixed as it is below
 
@@ -20,24 +23,40 @@ for (;;) {
   $id++;
   if (-f "/usr/local/etc/FETLIFE/user$id.bz2" || -f "/usr/local/etc/FETLIFE/user$id") {
     debug("ALREADYHAVE: $id");
+    push(@have,$id);
     next;
   }
 
   my($url) = "https://fetlife.com/users/$id";
-  my($out,$err,$res) = cache_command2("curl -o /usr/local/etc/FETLIFE/user$id -H 'Cookie: $fl_cookie' 'https://fetlife.com/users/$id'", "age=86400");
+  my($out,$err,$res) = cache_command2("curl -o /usr/local/etc/FETLIFE/user$id -H 'Cookie: $fl_cookie' 'https://fetlife.com/users/$id'", "age=864000");
   my($data) = read_file("/usr/local/etc/FETLIFE/user$id");
 
   # look for 10 *consecutive* bds
-  unless ($data=~/<title>/) {$bad++;} else {$bad=0;}
-
-  # TODO: improve this exit condition
-  if ($bad>10) {die "TOO MANY BAD IN A ROW";}
-
-  if (++$count%50==0) {
-    debug("DURING DEBUT STAGE, HIT ENTER TO CONTINE (every 50)");
-    <STDIN>;
+  unless ($data=~/<title>/) {
+    push(@bad,$id);
+    $bad++;
+  } else {
+    push(@got,$id);
+    $bad=0;
   }
+
+  if ($bad>10) {last;}
 }
+
+# TODO: delete bads, but especially last 10 bads
+
+# TODO: people, especially newbies(?) change their profiles often(?)
+
+# TODO: recheck older bad entries as well, they may have reactivated
+
+# TODO: keep old data (wiki style) so "blanking profile" trick doesn't work
+
+# NOTE: "10 day" cache will keep us from downloading the same bad many times
+
+# TODO: bzip2 others
+
+debug("PROGRAM ENDS HERE");
+
 
 
 
