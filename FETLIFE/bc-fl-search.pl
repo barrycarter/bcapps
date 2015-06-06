f3b3355ab9b37855298fc934d62e80b7f3e176de
Barry Carter <carter.barry@gmail.com>
2015-06-06 11:11:41 -0600

    Checkpoint (ugly)

--- FETLIFE/bc-fl-search.pl
+++ FETLIFE/bc-fl-search.pl
@@ -4,12 +4,41 @@
 
 require "/usr/local/lib/bclib.pl";
 
-
 print "Content-type: text/html\n\n";
 
-print "Q: $ENV{QUERY_STRING}";
+# for now
+$globopts{debug}=1;
 
-# options for age (no one is really 99, but I know that's a safe value)
+my($options) = read_file("fl-forms.txt");
+
+# TODO: this can and really should be subroutinized to create
+# arbitrary SELECT dropdowns
+
+$options=~s%<genders>(.*?)</genders>%%s;
+
+my(@genders) = split(/\n/, $1);
+
+# the wildcard option
+my(@genselect) = ("<option value=\"*\">Any Gender</option>");
+
+for $i (sort @genders) {
+  # ignore non-hash (and empty)
+  unless ($i=~/^(.*?):(.*)$/) {next;}
+  push(@genselect, "<option value=\"$1\">$2</option>");
+}
+
+$genselect="<select name='gender'>\n".join("\n",@genselect)."\n</select>\n";
+
+debug("GENDERS:",$genselect);
+
+die "TESTING";
+
+
+
+
+
+# options for age (no one is really 99, since earliest allowed birth
+# year is 1920 [which is why 95 = birth year 1920 is the "fake age" value]
 
 my(@ages);
 for $i (18..99) {push(@ages,"<option value='$i'>$i</option>");}
@@ -18,11 +47,17 @@ for $i (18..99) {push(@ages,"<option value='$i'>$i</option>");}
 @cunts = `egrep -v '#' $bclib{githome}/FETLIFE/countrylist.txt`;
 map(chomp($_),@cunts);
 
+# parse query sent by user
+
+my(@chunks)=split(/\&/,$ENV{QUERY_STRING});
+
+webug("CHUNKS",@chunks);
+
 
 # submit a query to fetlife.db.94y.info
 
 # really should use "submit=RUN+QUERY" but ok w/below
-my($query)="query=SELECT COUNT(*) FROM kinksters&submit=1";
+my($query)="query=SELECT COUNT(*) FROM kinksters";
 
 my($tmpdir) = tmpdir("bcfs");
 
