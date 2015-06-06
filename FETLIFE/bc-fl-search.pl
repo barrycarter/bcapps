ea0ca028d4e779edcded6a1caf8291426307eb08
Barry Carter <carter.barry@gmail.com>
2015-06-06 11:18:19 -0600

    About to create subroutine

--- FETLIFE/bc-fl-search.pl
+++ FETLIFE/bc-fl-search.pl
@@ -29,6 +29,50 @@ for $i (sort @genders) {
 
 $genselect="<select name='gender'>\n".join("\n",@genselect)."\n</select>\n";
 
+# roles are easier, list not a hash
+
+$options=~s%<roles>(.*?)</roles>%%s;
+
+my(@roles) = split(/\n/, $1);
+
+
+my(@roleselect) = ("<option value=\"*\">Any Gender</option>");
+
+for $i (sort @roles) {
+  # ignore non-hash (and empty)
+  unless ($i=~/^(.*?):(.*)$/) {next;}
+  push(@roleselect, "<option value=\"$1\">$2</option>");
+}
+
+$roleselect="<select name='role'>\n".join("\n",@roleselect)."\n</select>\n";
+
+# options for age (no one is really 99, since earliest allowed birth
+# year is 1920 [which is why 95 = birth year 1920 is the "fake age" value]
+
+my(@ages);
+for $i (18..99) {push(@ages,"<option value='$i'>$i</option>");}
+
+
+
+my($form) = << "MARK";
+
+<form action="/bc-fl-search.pl" method="GET"><table border>
+
+<tr><th colspan=2>UNOFFICIAL FetLife Search Form (not affiliated with FetLife)</th></tr>
+
+<tr><th>Gender</th><td>$genselect</td></tr>
+<tr><th>Age</th><td>X to X</td></tr>
+<tr><th>Role</th><td>$roleselect</td></tr><tr>
+<th>City</th><td><input type='text' name='city' /></td></tr>
+<tr><th>State</th><td><input type='text' name='state' /></td></tr>
+<tr><th>Cuntry</th><td>
+
+MARK
+;
+
+debug("FORM: $form");
+
+
 debug("GENDERS:",$genselect);
 
 die "TESTING";
@@ -36,12 +80,6 @@ die "TESTING";
 
 
 
-
-# options for age (no one is really 99, since earliest allowed birth
-# year is 1920 [which is why 95 = birth year 1920 is the "fake age" value]
-
-my(@ages);
-for $i (18..99) {push(@ages,"<option value='$i'>$i</option>");}
 
 # list of countries
 @cunts = `egrep -v '#' $bclib{githome}/FETLIFE/countrylist.txt`;
