e8344782234c0b2b16e8bfcc433e840168d76813
Barry Carter <carter.barry@gmail.com>
2015-06-09 09:27:18 -0600

    Failing, will go to return hash, sigh

--- MAPS/playground2.pl
+++ MAPS/playground2.pl
@@ -3,31 +3,37 @@
 require "/usr/local/lib/bclib.pl";
 
 
-debug(mysql("SELECT * FROM abqaddresses LIMIT 20", "test", ""));
+my(@res) = mysqlhashlist("SELECT * FROM abqaddresses LIMIT 20", "test");
 
-# testing mysql subroutine I wrote and hopefully improving it
+debug("RES",@res);
 
-sub mysql {
+=item mysqlhashlist($query,$db,$user)
+
+Run $query (should be a SELECT statement) on $db as $user, and return
+list of hashes, one for each row
+
+TODO: add error checking
+
+=cut
+
+sub mysqlhashlist {
   my($query,$db,$user) = @_;
-  unless ($user) {$user = "readonly";}
-  my($qfile) = (my_tmpfile2());
+  unless ($user) {$user="''";}
+  my(@res,$row);
+  chdir(tmpdir());
 
-  # ugly use of global here
-  $SQL_ERROR = "";
-
-  write_file($query,$qfile);
-  my($cmd) = "mysql -u $user -E $db < $qfile";
-  my($out,$err,$res,$fname) = cache_command2($cmd,"nocache=1");
-  # get rid of the row numbers + remove blank first line
-  $out=~s/^\*+\s*\d+\. row\s*\*+$//img;
-  $out = trim($out);
-
-  if ($res) {
-    warnlocal("MYSQL returns $res: $out/$err, CMD: $cmd");
-    debug("DB is $db", `pwd`);
-    $SQL_ERROR = "$res: $out/$err FROM $cmd";
-    return "";
+  write_file($query,"query");
+  # TODO: for large resultsets, loading entire output may be bad
+  my($out,$err,$res) = cache_command2("mysql -u $user -E $db < query 2> /tmp/err.txt");
+  # go through results
+  for $i (split(/\n/,$out)) {
+    # new row
+    if ($i=~/^\*+\s*(\d+)\. row\s*\*+$/) {$row = $1; next; $res[$row] = %{};}
+    unless ($i=~/^\s*(.*?):\s*(.*)/) {warn("IGNORING: $_"); next;}
+    my(%hash) = %{$res[$row]};
+    $hash{$1} = $2;
+    debug("RES $row is $res[$row]");
   }
-  return $out;
+  return @res;
 }
 
