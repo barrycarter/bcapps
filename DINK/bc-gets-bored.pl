18bb66b255e43bd29f9027520bb14347687a2155
Barry Carter <carter.barry@gmail.com>
2015-06-13 10:34:16 -0600

    Quasi working

--- DINK/bc-gets-bored.pl
+++ DINK/bc-gets-bored.pl
@@ -1,5 +1,13 @@
 #!/bin/perl
 
+# Creates freedink map tiles (assumes freedinkeditor is up and
+# running) in /tmp/dink/
+
+# TODO: lots, this is NOT a finished program
+
+# TODO: redirect the stderr of freedinkedit (or bring it up ourselves)
+# since the output is useful
+
 require "/usr/local/lib/bclib.pl";
 
 # find and raise freedink edit window
@@ -101,45 +109,3 @@ sub xdotoolkey {
   # the sleep post-keyup so next xdotool command is delayed
   system("xdotool keydown --window $win $key; sleep 0.05; xdotool keyup --window $win $key; sleep 0.25");
 }
-
-die "TESTING";
-
-# largely from
-# https://github.com/alexschrod/freedink-lua/blob/master/contrib/dinkdat_inspect.c
-# and
-# https://github.com/alexschrod/freedink-lua/blob/88ff3c183b0891abad221c3326a8f24544fb7ba4/contrib/search_script.c
-
-
-
-debug("READING FIE");
-
-my($all) = read_file("map.dat");
-
-debug("DONE READING FIE");
-
-# chunks
-
-debug(substr($all,1,31280));
-
-# short = 16 bit
-
-# tile = 2 bytes for tile, 2 bytes for hardness
-
-# 96 tiles + 1 empty tile per screen, screen = 31280 bytes
-
-# so first 388 bytes of each chunk = bitmap (+ hardness?)
-
-# experiments w/ bored:
-
-# hardwood floor = ts01.bmp, row 1, col 4
-
-# appears to be 80 bytes per tile? [which would mean 391 chunks of 80
-# bytes total]
-
-# 30 = black, 19 = wall, 6 = wall/bookcase
-
-# actually 64 tiles per supertile (50x50 each)
-
-# 12x8 layout of 96 tiles per screen,  50x50 per tile
-
-
