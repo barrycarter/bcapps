786d3750f4edec216735c0426b7e2f89b1e3b295
Barry Carter <carter.barry@gmail.com>
2015-06-26 17:24:06 -0600

    This version is working!

--- DINK/bc-image-map.pl
+++ DINK/bc-image-map.pl
@@ -10,7 +10,7 @@ my($str)="Smallwood"."\0"x13;
 
 # screen indices as 4 byte integers
 # for $i (1..511) {
-for $i (0..511) {
+for $i (0..75) {
   $str.="\x0\x0";
   if ($i<=255) {$str.=chr($i)."\x0"; next;}
   $str.=chr($i-256)."\x1";
@@ -24,17 +24,17 @@ write_file($str,"temp.dink.dat");
 # can do at most 512 tiles this way, not all 768
 
 # for $i (1..256) {
-for $i (1..150) {
+for $i (1..38) {
 
   # each value of $ts references two tile screens
   my($ts) = int(($i-1)/2);
 
   # and each of the tiles in screen 1
-  for $j (0..95) {print "\0"x20,chr($ts),chr($j),"\0"x58;}
+  for $j (0..95) {print "\0"x20,chr($j),chr($ts),"\0"x58;}
   # empty data for rest of screen 1
   print "\0"x23600;
   # tiles in screen 2
-  for $j (128..128+95) {print "\0"x20,chr($ts),chr($j),"\0"x58;}
+  for $j (128..128+95) {print "\0"x20,chr($j),chr($ts),"\0"x58;}
   # empty data for rest of screen 2
   print "\0"x23600;
 }
