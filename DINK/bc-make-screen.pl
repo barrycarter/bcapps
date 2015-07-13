05bb4d43a2126c575b4c3f87a8bde945fc2e2741
Barry Carter <carter.barry@gmail.com>
2015-07-12 18:14:56 -0600

    Checkpoint

--- DINK/bc-make-screen.pl
+++ DINK/bc-make-screen.pl
@@ -6,6 +6,25 @@ require "/usr/local/lib/bclib.pl";
 
 my($screen,$path) = @ARGV;
 
-xmessage("I have been called: $screen and $path");
+open(A,">$path");
 
+# the tiles
 
+for $y (1..8) {
+  for $x (1..12) {
+    # 20 character tile name (ignored)
+#    print A "barry carter is here";
+    print A "\0"x20;
+
+    # for now, repeat the same tile
+    # may end up doing this long term if we use background BMPs + alt hardness?
+    print A "\000\000";
+    # the other 58 chars that make up a tile
+    print A "\000"x58;
+  }
+}
+
+# this is overkill but fills the required 31280 bytes
+print A "\000"x312800;
+
+close(A);
