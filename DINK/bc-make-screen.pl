098a3250f95ad90318b4bb4803d7fa35c1f5ac92
Barry Carter <carter.barry@gmail.com>
2015-07-12 18:50:48 -0600

    Working!

--- DINK/bc-make-screen.pl
+++ DINK/bc-make-screen.pl
@@ -6,7 +6,17 @@ require "/usr/local/lib/bclib.pl";
 
 my($screen,$path) = @ARGV;
 
+# direction from $screen
+my(@dir) = ("0", "north","2","south","west","east");
+
+# figure out directory, we'll need it later
+my($dir) = $path;
+$dir=~s%/([^/]*?)$%%;
+
 open(A,">$path");
+
+# unused screen name
+# print A "\0"x19;
 
 # the tiles
 
@@ -18,13 +28,34 @@ for $y (1..8) {
 
     # for now, repeat the same tile
     # may end up doing this long term if we use background BMPs + alt hardness?
-    print A "\000\000";
+    print A "\002\005";
     # the other 58 chars that make up a tile
     print A "\000"x58;
   }
 }
 
-# this is overkill but fills the required 31280 bytes
-print A "\000"x312800;
+# unused fields + some alignment issues?
+print A "\000"x340;
+
+# sprites (which you can also script in later); sprite 0 always empty
+for $s (0..100) {print A "\000"x220;}
+
+print A "DYNAMIC";
+
+# fill to 31280 bytes
+print A "\000"x1035;
+
+close(A);
+
+my($date)=`date`;
+chomp($date);
+
+# now create the script dynamically
+
+open(A,">$dir/story/DYNAMIC.c");
+print A << "MARK";
+say_stop("I just walked direction $screen ($dir[$screen]) at $date",1);
+MARK
+;
 
 close(A);
