8ddc876ea3e70ccfb686d96fcc6ba369e09e3c0b
Barry Carter <carter.barry@gmail.com>
2015-07-16 15:35:59 -0600

    Mistake

--- DINK/bc-map2gmap.pl
+++ DINK/bc-map2gmap.pl
@@ -20,7 +20,6 @@ for $y (1..24) {
     my($num) = $1;
     # TODO: this breaks for >256 maps, being too lazy
     $num = ord(substr($num,0,1));
-    if ($num==0) {next;}
     # break into 6 tiles and resize
     for $px (0,200,400) {
       for $py (0,200) {
@@ -28,32 +27,11 @@ for $y (1..24) {
 	# TODO: MAYBE center these a bit more
 	my($xc,$yc) = ($x*3+$px/200-1,$y*2+$py/200-1);
 	my($outfile) = "7,$xc,$yc.jpg";
+	if (-f "$dir/$outfile") {next;}
+	# TODO: temp-screen0.png must exist and will be used for empty spaces
+	# probably more efficient to use symlinks for temp-screen0.png
 	my($out,$err,$res) = cache_command2("convert $dir/temp-screen$num.png -crop 200x200+$px+$py -geometry 256x256 $dir/$outfile");
       }
     }
   }
 }
-
-
-die "TESTING";
-
-# TODO: don't hardcode
-my($screendir) = "/usr/local/etc/DINK/MAPS/solstice";
-# TODO: in general, don't keep images in git, unnecessary/wasteful
-# this is where I keep things I don't need in git but do need on bcinfo3
-my($targetdir) = "/home/barrycarter/BCINFO3-PRIV/sites/MAP/IMAGES/TEST/";
-
-for $y (1..24) {
-  for $x (1..32) {
-
-    # TODO: create "blank" map as we will need this
-    if ($maps{$x}{$y} == 0) {next;}
-
-    # screen x,y creates 7,x,y (for future expansion purposes)
-    # TODO: better naming convention
-    unless (-f "$targetdir/5,$x,$y.png") {
-      system("cp $screendir/temp-screen$maps{$x}{$y}.png $targetdir/5,$x,$y.png");
-    }
-  }
-}
-
