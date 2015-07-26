3bc9b398de06102dbcc6eeb9729cd7c833a52525
Barry Carter <carter.barry@gmail.com>
2015-07-26 08:30:19 -0600

    checkpoint fail

--- DINK/bc-map2gmap.pl
+++ DINK/bc-map2gmap.pl
@@ -20,18 +20,13 @@ unless ($dir && $dinkdat) {die "Usage: $0 directory /path/to/dink.dat";}
 # about that, although the blank jpg file appears to be only 417b
 
 # <h>TONOTDO: be annoyed at how I wrote loop but never do anything about it</h>
-for $i (6,5,4,3,2,1,0) {slippy_unzoom($i);}
-die "TESTING";
+# for $i (6,5,4,3,2,1,0) {slippy_unzoom($i);}
+slippy_unzoom(6);
+# die "TESTING";
 
 my($all) = read_file($dinkdat);
 # first 20 chars are garbage
 $all=~s/^.{20}//s;
-
-# for unzooming to work properly, we must create a map whose
-# dimensions are powers of 2 (and possibly need to be equal); each x
-# screen produces 3 gmap tiles, for a total of 96; each y screen
-# produces 2 gmap tiles, total of 48; the loop counts below are higher
-# to make this 128 x and 64 y
 
 for $y (1..24) {
   for $x (1..32) {
@@ -39,6 +34,9 @@ for $y (1..24) {
     my($num) = $1;
     # TODO: this breaks for >256 maps, being too lazy
     $num = ord(substr($num,0,1));
+    #  tile #0 means no map in that space, do nothing
+    if ($num==0) {next;}
+
     # break into 6 tiles and resize
     for $px (0,200,400) {
       for $py (0,200) {
@@ -47,33 +45,11 @@ for $y (1..24) {
 	my($xc,$yc) = ($x*3+$px/200-3,$y*2+$py/200-2);
 	my($outfile) = "7,$xc,$yc.jpg";
 	if (-f "$dir/$outfile") {next;}
-
-	# for tile #0, symlink (don't copy) to blank.jpg (which must exist)
-	if ($num==0) {system("ln -s $dir/blank.jpg $dir/$outfile");next;}
-
-	# TODO: temp-screen0.png must exist and will be used for empty spaces
-	# probably more efficient to use symlinks for temp-screen0.png
 	my($out,$err,$res) = cache_command2("convert $dir/temp-screen$num.png -crop 200x200+$px+$py -geometry 256x256 $dir/$outfile");
       }
     }
   }
 }
-
-# the above will create x tiles 0-95, y tiles 0-47; below for test
-
-for $yc (48..127) {
-  for $xc (0..127) {
-    system("ln -s $dir/blank.jpg $dir/7,$xc,$yc.jpg");
-  }
-}
-
-for $xc (96..127) {
-  for $yc (0..47) {
-    system("ln -s $dir/blank.jpg $dir/7,$xc,$yc.jpg");
-  }
-}
-
-# TODO: fill in remainder of 128x128 grid here
 
 # create level n slippy tiles (from level n+1 tiles, which must already exist)
 
@@ -83,19 +59,20 @@ for $xc (96..127) {
 sub slippy_unzoom {
   # $dir = directory with the level n+1 slippy tiles
   my($n,$dir) = @_;
+  my($z) = $n+1;
 
   for $x (0..2**$n-1) {
     for $y (0..2**$n-1) {
-      my(@tiles);
-      # which tiles to combine
-      my($z) = $n+1;
-      for $ty (2*$x,2*$x+1) {
-	for $tx (2*$y,2*$y+1) {
-	  push(@tiles, "'$z,$tx,$ty.jpg'");
-	}
-      }
+      # this is less efficient than a loop, but easier to understand
+      # TODO: restore it to loop later?
+      my($nw) = join(",",$z,2*$x,2*$y);
+      my($ne) = join(",",$z,2*$x+1,2*$y);
+      my($sw) = join(",",$z,2*$x,2*$y+1);
+      my($se) = join(",",$z,2*$x+1,2*$y+1);
+      debug("$nw/$ne/$se/$sw");
+
       # TODO: figure out why this y,x not x,y
-      my($cmd) = "montage -geometry 128x128+0+0 ".join(" ",@tiles)." -tile 2x2 $n,$y,$x.jpg";
+      my($cmd) = "montage -geometry 128x128+0+0".join(" ",@tiles)." -tile 2x2 $n,$y,$x.jpg";
       print "$cmd\n";
     }
   }
