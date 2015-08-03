2bbd3693d288833ce69437b2f79a8e8a80db2bfd
Barry Carter <carter.barry@gmail.com>
2015-08-03 09:56:33 -0600

    Checkpoint

--- NAGIOS/bc-nagios-test.pl
+++ NAGIOS/bc-nagios-test.pl
@@ -92,6 +92,59 @@ sub func {
   }
 }
 
+=item bc_check_file_of_files_age($file)
+
+Given a file formatted like recentfiles.txt, check that all files are
+sufficiently recent
+
+=cut
+
+sub bc_check_file_of_files_age {
+  my($file) = @_;
+
+  $globopts{debug}=1;
+
+  my(@tests) = `egrep -v '^#|^\$' $file`;
+
+  for $i (@tests) {
+    chomp($i);
+
+    # file glob MUST be quoted
+    unless ($i=~s/\"(.*?)\"//) {
+      print "$file contains unquoted line: $i\n";
+      return 2;
+    }
+
+    my($w,$c) = split(/\s+/,$i);
+    my($files) = $1;
+
+    my($out,$err,$res) = cache_command("ls -1tr $files | head -1 | xargs stat -c '%Y'");
+
+    if ($res) {
+      print "Filespec $i returned error: $err\n";
+      return 2;
+    }
+
+    my($fileage) = time()-$out;
+    if ($fileage > $c) {
+      print "Filespec $i critical: $fileage\n";
+      return 2;
+    }
+
+    if ($fileage > $w) {
+      print "Filespec $i warning: $fileage\n";
+      return 1;
+    }
+  }
+
+  # if ALL tests pass...
+  return 0;
+}
+
+
+
+
+
 =item bc_check_mounts(@mountpoints)
 
 Given a list of mountpoints, write a random tempfile to each, read it
--- bc-supergrep.pl
+++ bc-supergrep.pl
@@ -14,5 +14,53 @@
 # Additionally, supports the "include" protocol: if a file "includes"
 # another file, the included file is grepped as well, recursively
 
+# TODO: not crazy about "##include 'full-path-to-filename'" as include protocol
+
 require "/usr/local/lib/bclib.pl";
+
+my($fileq) = 0;
+
+# parse arguments until we find a file
+
+for $i (@ARGV) {
+
+  # is it a file
+  if (-f $i) {
+    # if fileq not set, set it
+    $fileq = 1;
+    push(@files,$i);
+    next;
+  }
+
+  # not a file
+  if ($fileq) {die "Have already seen a file, no more phrases allowed!";}
+
+  push(@phrases,$i);
+}
+
+# search for inclusions (allowing for recursion and self-looping)
+
+for $i (@files) {
+  my(@incs) = `egrep '^## include \' $i`;
+  for $j (@incs) {
+    $j=~s/\"(.*?)\"//;
+    # TODO: need to use hash here to allow for recursion and looping
+    push(@files,$1);
+  }
+}
+
+my($files) = join(" ",@files);
+
+# construct fgrep chain; first phrase is normal grep
+my($fphrase) = shift(@phrases);
+my($cmd) = "fgrep -i '$fphrase' $files";
+
+# other phrases as piped greps
+for $i (@phrases) {
+  $cmd .= "|fgrep -i '$i'";
+}
+
+debug($cmd);
+system($cmd);
+
 
