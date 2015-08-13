a3bef2c7f85dd12ceca41d6cb4440d0aeeed4d9f
Barry Carter <carter.barry@gmail.com>
2015-08-13 11:25:06 -0600

    Working, about to cleanup

--- ASTRO/playground16.m
+++ ASTRO/playground16.m
@@ -1,24 +1,37 @@
 (* dump the daily positions of planets, with help from bc-dump-cheb.pl *)
 
-jd2params[jd_,planet_] := Module[{jd2,chunk,days,t},
+chebval[t_,list_] := Table[ChebyshevT[n,t],{n,0,Length[list]-1}].list
+
+posxyz[jd_,planet_] := Module[{jd2,chunk,days,t},
+
+   (* special case for Earth sigh, below is EMRAT1 *)
+   If[planet==earth, Return[
+ posxyz[jd,earthmoon]-50000000000000/4115028453709531*posxyz[jd,moongeo]]];
+
    (* normalize to boundary *)
    jd2 = jd-33/2;
+
    (* days in a given chunk *)
    days = 32/info[planet][chunks];
+
    (* which chunk *)
    chunk = Floor[Mod[jd2,32]/days]+1;
+
    (* where in chunk *)
    t = Mod[jd2,days]/days*2-1;
-   {Quotient[jd2,32]*32+33/2,chunk,t}
+
+   (* and Chebyshev *)
+   Table[chebval[t,pos[planet][Quotient[jd2,32]*32+33/2][[chunk]][[i]]],
+    {i,1,3}]
 ];
 
-(* Chebyshev with memoization *)
+
+
+(* Chebyshev with memoization [which is actually unnecessary, grumble] *)
 
 cheb[n_,t_] := cheb[n,t] = ChebyshevT[n,t];
 
 (* list of cheb at t w/ coeffs *)
-
-chebval[t_,list_] := Table[cheb[n,t],{n,0,Length[list]-1}].list
 
 jd2params[2457248.209699,mercury]
 
