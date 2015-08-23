a6aa7bac6a20ebc073950ccf99dce7087d813fa3
Barry Carter <carter.barry@gmail.com>
2015-08-23 17:14:08 -0600

    Checkpoint

--- ASTRO/bc-ecliptic-map.pl
+++ ASTRO/bc-ecliptic-map.pl
@@ -4,21 +4,23 @@
 
 require "/usr/local/lib/bclib.pl";
 
-my($w,$h) = (3600,225);
+$factor = 3;
+my($w,$h) = (3600,225*$factor);
 
-print "new\nsize 3600,113\nsetpixel 0,0,0,0,0\n";
+print "new\nsize $w,$h\nsetpixel 0,0,0,0,0\n";
 
 my($all) = read_file("$bclib{githome}/ASTRO/eclipticlong.txt");
 
 while ($all=~s/\{+(.*?),\s*\{(.*?)\,\s*(.*?)\},\s*(.*?)\}//) {
   my($name,$eclong,$eclat,$mag) = ($1,$2,$3,$4);
+  $name=~s/\"//g;
 
   # exclude too far
 #  if (abs($eclat)>360/64) {next;}
 
   my($x) = $w*(-$eclong/360+1/2);
   # yes, below is really $w, not $h, for rectangularity
-  my($y) = -$w*$eclat/360+$h/2;
+  my($y) = -$factor*$w*$eclat/360+$h/2;
 #  my($y) = round(113-($eclat/(360/64)*113/2+113/2));
   debug("$eclong/$eclat -> $x,$y");
   my($r) = round(6-$mag);
@@ -28,8 +30,12 @@ while ($all=~s/\{+(.*?),\s*\{(.*?)\,\s*(.*?)\},\s*(.*?)\}//) {
   # fly oddness
 #  if ($y+$r > 113) {next;}
 
+  # testing limit
+  if ($mag<=3.5) {
+    print "string 0,0,255,$x,$y,tiny,$name\n";
+  }
+
   print "fcircle $x,$y,$r,255,255,255\n";
-  print "string 255,255,255,$x,$y,tiny,$name\n";
 
 #  debug("GOT: $name/$eclong/$eclat/$mag");
 }
