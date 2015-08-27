e37700541a87e3f11a1ea5a9db9d374631fa5be5
Barry Carter <carter.barry@gmail.com>
2015-08-27 13:39:58 -0600

    Checkpoint

--- ASTRO/bc-conjunct-table.m
+++ ASTRO/bc-conjunct-table.m
@@ -5,6 +5,10 @@ outfile = "/home/barrycarter/SPICE/KERNELS/mseps"<>"-"<>
 
 planets={mercury,venus,mars,jupiter,saturn,uranus};
 
+(* sets of at least two planets *)
+
+conjuct1 = Subsets[planets,{2,Length[planets]}];
+
 (* Given a commutative function and a list, find the maximum of the
 function when applied pairwise to members of the list *)
 
@@ -16,31 +20,32 @@ maxflist[f_,list_] := Max[Table[Apply[f,x],
 
 angsep[p1_,p2_] := VectorAngle[earthvector2[jd,p1],earthvector2[jd,p2]];
 
-(* daily separations for a list of planets (ie, max distance of any 2) *)
-
-(****** TODO: 7 is just a test below !!! ****)
-
-dailysep[list_] := Table[maxflist[angsep,list],
-{jd,info[jstart],info[jstart]+7}]
-
-planpairs = Flatten[Table[{planets[[i]],planets[[j]]},
-{i,1,Length[planets]-1},{j,i+1,Length[planets]}],1];
-
-seps[{p1_,p2_}] := seps[{p1,p2}] = Table[{jd,VectorAngle[
-earthvector2[jd,p1],earthvector2[jd,p2]]}, {jd,info[jstart],info[jend]}];
-
-(* this just assigns the 15 pairs of p1,p2, return value ignored *)
-
-Table[seps[i],{i,planpairs}];
-
 (* Given a table of separations, find local minima, sorted *)
 
 minseps[tab_] := Sort[Table[{i,tab[[i]]}, {i,Select[Range[2,Length[tab]-1], 
 tab[[#,2]] <= Min[tab[[#-1,2]],tab[[#+1,2]]] &]}],#1[[2,2]] < #2[[2,2]] &];
 
-mseps[{p1_,p2_}] := mseps[{p1,p2}] = minseps[seps[{p1,p2}]];
+minseps[tab_] := Table[tab[[i]], {i,Select[Range[2,Length[tab]-1], 
+tab[[#,2]] <= Min[tab[[#-1,2]],tab[[#+1,2]]] &]}];
 
-Table[mseps[i],{i,planpairs}];
+(* minimum separations for given list of planets *)
 
-DumpSave[outfile,{mseps,info}];
+(****** TODO: 7 is just a test below !!! ****)
+
+minangdists[list_] := minangdists[list] = Module[{t},
+
+ (* the daily separations *)
+ t = Table[{jd,maxflist[angsep,list]},{jd,info[jstart],info[jstart]+7777}];
+
+ (* local minima and the list sent in*)
+ Return[minseps[t]];
+];
+
+(* and run that for all of them *)
+
+Table[minangdists[i],{i,conjuct1}];
+
+(* TODO: restore below *)
+
+DumpSave[outfile,{minangdists,info}];
 
