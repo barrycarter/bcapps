bf395e25dc74b00fcd39e0d04930f6bc888e60cc
Barry Carter <carter.barry@gmail.com>
2015-08-31 09:41:22 -0600

    Checkpoint: could I have used Mathematica's Plot function to find conjunctions faster?

--- ASTRO/playground23.m
+++ ASTRO/playground23.m
@@ -1,20 +1,64 @@
+(* graphing conjunctions JFF *)
+
+p = Plot[earthangle[jd,mercury,venus],{jd,info[jstart],info[jend]}];
+
+points = p[[1,1,3,2,1]];
+
+minin = Select[Range[2,Length[points]-1], 
+points[[#,2]] < points[[#+1,2]] && points[[#,2]] < points[[#-1,2]] &];
+
+minpts = Table[points[[i]],{i,minin}];
+
+min2 = Select[minpts, #[[2]]<6*Degree&];
+
+(* finds 260 conjunctions *)
+
+true = trueminseps[{mercury,venus}];
+
+(* 2.4516196051509483*10^6 is first true conjunction, plot misses it *)
+
+
+
+
+delta = .001;
+
+Plot[
+ (earthangle[jd+delta,mercury,venus]-
+ earthangle[jd-delta,mercury,venus])
+ /2/delta,
+{jd,info[jstart],info[jstart]+365*10}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+(* an attempt to inline a function so Mathematica treats it as a pure
+function *)
+
+posxyz[jd_,planet_] := Module[{jd2,chunk,days,t},
+
+   (* which chunk *)
+   chunk = Floor[Mod[(jd-33/2),32]/(32/info[planet][(Floor[Mod[(jd-33/2),32]/(32/info[planet][chunks])]+1)s])]+1;
+
+   (* where in chunk *)
+   t = Mod[(jd-33/2),(32/info[planet][(Floor[Mod[(jd-33/2),32]/(32/info[planet][chunks])]+1)s])]/(32/info[planet][chunks])*2-1;
+
+   (* and Chebyshev *)
+   Table[chebyshev[pos[planet][Quotient[(jd-33/2),32]*32+33/2][[chunk]][[i]],t],
+    {i,1,3}]
+];
+
+
+
 (* an approach using pos[] and piecewise *)
-
-conds[p_] := Table[t>=i && t<=i+32/info[p][chunks],
- {i,info[jstart],info[jend]-1,32/info[p][chunks]}];
-
-polys[p_] := 
-Flatten[
-Table[chebyshev[pos[p][u][[i,j]],
-      Mod[t-info[jstart],32/info[p][chunks]]/
-      (32/info[p][chunks])*2-1],
-{u,info[jstart],info[jend]-1,32},
-{i,1,Length[pos[p][u]]},
-{j,1,3}
-],1];
-
-posxyz[p_,t_] := posxyz[p,t] = Piecewise[
-Table[{polys[p][[i]],conds[p][[i]]},{i,1,Length[polys[p]]}]];
 
 (* need rational values for pos[] *)
 
@@ -26,14 +70,28 @@ posfxyz[p_] := Module[{conds,polys},
  conds = Table[t>=i && t<=i+32/info[p][chunks],
  {i,info[jstart],info[jend]-1,32/info[p][chunks]}];
 
-Print[conds];
-
- polys = Flatten[Table[chebyshev[pos[p][u][[i,j]],
+ polys = N[
+Flatten[Table[chebyshev[pos[p][u][[i,j]],
       Mod[t-info[jstart],32/info[p][chunks]]/(32/info[p][chunks])*2-1],
-{u,info[jstart],info[jend]-1,32},{i,1,Length[pos[p][u]]},{j,1,3}],1];
+{u,info[jstart],info[jend]-1,32},{i,1,Length[pos[p][u]]},{j,1,3}],1]
+];
 
- Return[Piecewise[Table[{polys[[i]],conds[[i]]},{i,1,Length[polys[p]]}]]];
+ Return[Piecewise[Table[{polys[[i]],conds[[i]]},{i,1,Length[polys]}]]];
 ]
+
+f[t_] = posfxyz[earthmoon];
+
+test[t_] = f;
+
+
+
+
+
+(******* TESTS START HERE ********)
+
+
+
+
 
 info[jend] = info[jstart]+32*2
 
