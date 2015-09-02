f17d789d60aceaa947f88f0483a5d607de7b26f3
Barry Carter <carter.barry@gmail.com>
2015-09-02 17:39:44 -0600

    Still working; improved to accept parameters

--- ASTRO/example1.c
+++ ASTRO/example1.c
@@ -19,18 +19,18 @@ int main( int argc, char **argv )
   SpiceChar         begstr [ TIMLEN ];
   SpiceChar         endstr [ TIMLEN ];
 
-  SpiceChar       * targ1  = "MOON";
+  SpiceChar       * targ1  = argv[1];
   SpiceChar       * frame1 = "NULL";
-  SpiceChar       * shape1 = "SPHERE";
+  SpiceChar       * shape1 = "POINT";
 
-  SpiceChar       * targ2  = "EARTH";
+  SpiceChar       * targ2  = argv[2];
   SpiceChar       * frame2 = "NULL";
-  SpiceChar       * shape2 = "SPHERE";
+  SpiceChar       * shape2 = "POINT";
 
   SpiceChar       * abcorr = "NONE";
-  SpiceChar       * relate = "LOCMAX";
+  SpiceChar       * relate = "LOCMIN";
 
-  SpiceChar       * obsrvr = "SUN";
+  SpiceChar       * obsrvr = "EARTH";
 
   SpiceInt          count;
   SpiceInt          i;
@@ -38,12 +38,12 @@ int main( int argc, char **argv )
 
   furnsh_c( "standard.tm" );
     
-  str2et_c( "2007 JAN 01", &begtim );
-  str2et_c( "2008 JAN 01", &endtim );
+  str2et_c( "2000 JAN 01", &begtim );
+  str2et_c( "2100 JAN 01", &endtim );
 
   wninsd_c ( begtim, endtim, &cnfine );
 
-  step   = 6.*spd_c();
+  step   = 1.*spd_c();
   adjust = 0.;
   refval = 0.;
 
@@ -51,27 +51,18 @@ int main( int argc, char **argv )
 	    relate, refval, adjust, step, MAXWIN, &cnfine, &result);
   count = wncard_c( &result );
 
-    if (count == 0 )
-      {
-        printf ( "Result window is empty.\n\n" );
-      }
-    else
-      {
-        for ( i = 0;  i < count;  i++ )
-   {
+  for ( i = 0;  i < count;  i++ ) {
 
    wnfetd_c ( &result, i, &beg, &end );
-
-               timout_c ( beg, TIMFMT, TIMLEN, begstr );
-               timout_c ( end, TIMFMT, TIMLEN, endstr );
-
-               printf ( "Interval %d\n", i + 1);
-               printf ( "Beginning TDB %s \n", begstr );
-               printf ( "Ending TDB    %s \n", endstr );
-
-   }
-      }
-
+   
+   timout_c ( beg, TIMFMT, TIMLEN, begstr );
+   timout_c ( end, TIMFMT, TIMLEN, endstr );
+   
+   printf ( "Interval %d\n", i + 1);
+   printf ( "Beginning TDB %s \n", begstr );
+   printf ( "Ending TDB    %s \n", endstr );
+   
+  }
          kclear_c();
          return( 0 );
 }
