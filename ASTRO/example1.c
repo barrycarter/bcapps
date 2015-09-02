42eb9eab7515f9c231f43fb0d23758cb6150a10d
Barry Carter <carter.barry@gmail.com>
2015-09-02 17:36:00 -0600

    Cleaning up, still working

--- ASTRO/example1.c
+++ ASTRO/example1.c
@@ -1,26 +1,20 @@
-      #include <stdio.h>
-      #include <stdlib.h>
-      #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-      #include "SpiceUsr.h"
+#include "SpiceUsr.h"
 
-      #define       MAXWIN    1000
-      #define       TIMFMT    "YYYY-MON-DD HR:MN:SC.###### (TDB) ::TDB ::RND"
-      #define       TIMLEN   41
+#define       MAXWIN    1000
+#define       TIMFMT    "YYYY-MON-DD HR:MN:SC.###### (TDB) ::TDB ::RND"
+#define       TIMLEN   41
 
 int main( int argc, char **argv )
 {
 
-    SPICEDOUBLE_CELL ( result, 2*MAXWIN );
+  SPICEDOUBLE_CELL ( result, 2*MAXWIN );
   SPICEDOUBLE_CELL ( cnfine, 2       );
 
-  SpiceDouble       begtim;
-  SpiceDouble       endtim;
-  SpiceDouble       step;
-  SpiceDouble       adjust;
-  SpiceDouble       refval;
-  SpiceDouble       beg;
-  SpiceDouble       end;
+  SpiceDouble begtim, endtim, step, adjust, refval, beg, end;
 
   SpiceChar         begstr [ TIMLEN ];
   SpiceChar         endstr [ TIMLEN ];
@@ -42,45 +36,31 @@ int main( int argc, char **argv )
   SpiceInt          i;
 
 
-	   furnsh_c( "standard.tm" );
+  furnsh_c( "standard.tm" );
+    
+  str2et_c( "2007 JAN 01", &begtim );
+  str2et_c( "2008 JAN 01", &endtim );
 
-	   str2et_c( "2007 JAN 01", &begtim );
-         str2et_c( "2008 JAN 01", &endtim );
+  wninsd_c ( begtim, endtim, &cnfine );
 
-         wninsd_c ( begtim, endtim, &cnfine );
+  step   = 6.*spd_c();
+  adjust = 0.;
+  refval = 0.;
 
-	     step   = 6.*spd_c();
-	   adjust = 0.;
-	   refval = 0.;
+  gfsep_c ( targ1, shape1, frame1, targ2, shape2, frame2, abcorr, obsrvr,
+	    relate, refval, adjust, step, MAXWIN, &cnfine, &result);
+  count = wncard_c( &result );
 
-	   gfsep_c ( targ1,
-		     shape1,
-		     frame1,
-		     targ2,
-		     shape2,
-		     frame2,
-		     abcorr,
-		     obsrvr,
-		     relate,
-		     refval,
-		     adjust,
-		     step,
-		     MAXWIN,
-		     &cnfine,
-		     &result );
+    if (count == 0 )
+      {
+        printf ( "Result window is empty.\n\n" );
+      }
+    else
+      {
+        for ( i = 0;  i < count;  i++ )
+   {
 
-         count = wncard_c( &result );
-
-	   if (count == 0 )
-	     {
-	       printf ( "Result window is empty.\n\n" );
-	     }
-	   else
-	     {
-	       for ( i = 0;  i < count;  i++ )
-		 {
-
-		 wnfetd_c ( &result, i, &beg, &end );
+   wnfetd_c ( &result, i, &beg, &end );
 
                timout_c ( beg, TIMFMT, TIMLEN, begstr );
                timout_c ( end, TIMFMT, TIMLEN, endstr );
@@ -89,8 +69,8 @@ int main( int argc, char **argv )
                printf ( "Beginning TDB %s \n", begstr );
                printf ( "Ending TDB    %s \n", endstr );
 
-		 }
-	     }
+   }
+      }
 
          kclear_c();
          return( 0 );
