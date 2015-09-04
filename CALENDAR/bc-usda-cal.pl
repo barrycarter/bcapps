b881e245470d4f928f13295951ad280eb93d7595
Barry Carter <carter.barry@gmail.com>
2015-09-04 11:12:38 -0600

    Checkpoint save of working stellarium script

--- CALENDAR/bc-usda-cal.pl
+++ CALENDAR/bc-usda-cal.pl
@@ -22,7 +22,7 @@ for $i (1..12) {$months{$months[$i]}=$i;}
 my($reg) = join("|",@months[1..12]);
 
 # deordinalize
-my(%ord) = ("first" => 1, "second" => 2, "third" => 3, "fourth" => 4);
+my(%ord) = ("first"=>1,"second"=>2,"third"=>3,"fourth"=>4,"last"=>-1);
 
 debug("REG: $reg");
 
@@ -35,7 +35,7 @@ while ($all=~s%<li>(.*?)\s*\((.*?)\).%%) {
   my($ev,$da)=($1,$2);
 
   # ignore variable (non-computable) events
-  if ($da=~/varies/i) {
+  if ($da=~/varies|changes/i) {
     debug("IGNORING VARIABLE: $ev/$da");
     next;
   }
@@ -45,19 +45,34 @@ while ($all=~s%<li>(.*?)\s*\((.*?)\).%%) {
   # required by format
   my($uid) = sha1_hex("$da $day $ev barrycarter");
 
-  # week long events
-  if ($da=~s/^(.*?)\s+week\s+of\s+($reg)$//is) {
-    my($ord,$mon) = ($1,$2);
-    my($dtstart) = sprintf("2015%02d01T000000",$months{$mon});
-    debug("WEEK: $ord/$mon");
-# TODO: this creates a spurious event on 1 Jan 2015
+  # TODO: "last week in April"/etc I probably have wrong
+  # week long events or events on nth weekday of month
+  debug("DA: $da");
+  if ($da=~s/^(.*?)\s+(.*?)\s+(of|in)\s+($reg)$//is) {
+    my($ord,$wday,$mon) = ($1,$2,$4);
+
+    # duration is 1 day, unless a week
+    my($duration) = "P1D";
+
+    # fixup the ordinal
+    $ord = $ord{lc($ord)};
+    # and the month
+    $mon = $months{$mon};
+    # and the weekday
+    if ($wday=~/week/) {$wday="Sunday"; $duration="P1W";}
+    $wday = uc(substr($wday,0,2));
+
+    debug("W: -> $ord|$wday|$mon");
+#    my($dtstart) = sprintf("2015%02d01T000000",$months{$mon});
+
+# TODO: this creates a spurious event on 1 Jan 2000
     print A << "MARK";
 BEGIN:VEVENT\r
 SUMMARY:$ev\r
 UID:$uid\r
-DTSTART:20150101T000000\r
-RRULE:FREQ=MONTHLY;BYMONTH=$months{$mon};BYDAY=$ord{$ord}SU\r
-DURATION:P1W\r
+DTSTART:20000101T000000\r
+RRULE:FREQ=MONTHLY;BYMONTH=$mon;BYDAY=$ord$wday\r
+DURATION:$duration\r
 END:VEVENT\r
 MARK
 ;
@@ -89,9 +104,9 @@ MARK
 BEGIN:VEVENT\r
 SUMMARY:$ev\r
 UID:$uid\r
-DTSTART:$dtstart\r
-DURATION: 1d\r
-RRULE:FREQ=YEARLY;\r
+DTSTART:20000101T000000\r
+DURATION: P1D\r
+RRULE:FREQ=YEARLY;BYMONTH=$months{$da};BYDAY=$day\r
 END:VEVENT\r
 MARK
 ;
