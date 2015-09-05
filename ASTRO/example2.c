6f7e186dbda1e65ae8438126b7d0de582a347033
Barry Carter <carter.barry@gmail.com>
2015-09-05 05:49:14 -0600

    Checkpoint

--- ASTRO/example2.c
+++ ASTRO/example2.c
@@ -20,9 +20,7 @@ void main() {
   for ( i = 0;  i < MAXITR;  i++ ) {
     et  =  ET0 + i*STEP;
     spkezp_c ( TARGET, et, FRAME, ABCORR, OBSERVER,  pos,  &lt);
-    printf( "\net = %20.10f\n\n",                 et     );
-    printf( "J2000 x-position (km):   %20.10f\n", pos[0] );
-    printf( "J2000 y-position (km):   %20.10f\n", pos[1] );
-    printf( "J2000 z-position (km):   %20.10f\n", pos[2] );
+    printf("%0.f %.9f %.9f %.9f\n",et,pos[0],pos[1],pos[2]);
+    //    printf("%20.10f %20.10f %20.10f %20.10f\n",et,pos[0],pos[1],pos[2]);
   }
 }
