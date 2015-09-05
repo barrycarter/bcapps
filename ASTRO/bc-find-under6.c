da5c82fda6bda60937118dbdcc2fec369ebb91a8
Barry Carter <carter.barry@gmail.com>
2015-09-05 11:13:57 -0600

    Checkpoint, just did something silly and unnecessary, sigh

--- ASTRO/bc-find-under6.c
+++ ASTRO/bc-find-under6.c
@@ -6,38 +6,41 @@
 #include <string.h>
 #include "SpiceUsr.h"
 
-#define       MAXWIN    1000
-#define       TIMFMT    "YYYY-MON-DD HR:MN:SC.###### (TDB) ::TDB ::RND"
-#define       TIMLEN   41
+#define       MAXWIN    10000
 
 int main( int argc, char **argv )
 {
 
   SPICEDOUBLE_CELL(result,2*MAXWIN );
   SPICEDOUBLE_CELL(cnfine,2);
-  int i;
+  SpiceInt i;
+  SpiceDouble j;
 
   // which ephermis's to use
   furnsh_c( "/home/barrycarter/BCGIT/ASTRO/standard.tm" );
 
+  // TOL too small otherwise; this is one second
+  gfstol_c(1.);
+
   // SPICE window for all DE431
   //  wninsd_c (-479695089600.+86400*468, 479386728000., &cnfine );
 
-  // SPICE window testing
-  wninsd_c (0, 86400.*365.2425*500, &cnfine );
+  for (j=-479654654400.; j<479386728000.; j+= 15778476000.) {
+
+    // SPICE window creation
+    wninsd_c (j, j+15778476000., &cnfine );
     
-  // I sent these as: observer b1 b2, thus the odd argv order below
-  // 0.10471975511965977462 radians = 6 degrees
-  gfsep_c(argv[2],"POINT","J2000",argv[3],"POINT","J2000","NONE",argv[1],
-	  "<", 0.10471975511965977462,0,1.*spd_c(),10000,&cnfine,&result);
+    // I send these as: observer b1 b2, thus the odd argv order below
+    // 0.10471975511965977462 radians = 6 degrees
+    gfsep_c(argv[2],"POINT","J2000",argv[3],"POINT","J2000","NONE",argv[1],
+	  "<", 0.10471975511965977462,0,86400.,10000,&cnfine,&result);
 
-  SpiceInt count = wncard_c( &result );
+    SpiceInt count = wncard_c( &result );
+    SpiceDouble begtim, endtim;
 
-  SpiceDouble begtim, endtim;
-
-  for (i=0; i<count; i++) {
-    wnfetd_c ( &result,i,&begtim,&endtim);
-    printf("%f,%f\n",begtim,endtim);
-
+    for (i=0; i<count; i++) {
+      wnfetd_c ( &result,i,&begtim,&endtim);
+      printf("%f,%f\n",begtim,endtim);
+    }
   }
 }
