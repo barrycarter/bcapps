e3c3dc180192cd320b1a07cb93dfbacfea9bb791
Barry Carter <carter.barry@gmail.com>
2015-09-06 09:31:49 -0600

    About to break more

--- ASTRO/bc-find-under6.c
+++ ASTRO/bc-find-under6.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include "SpiceUsr.h"
 
-#define MAXWIN 10000
+#define MAXWIN 100000
 
 int main( int argc, char **argv )
 {
@@ -23,26 +23,29 @@ int main( int argc, char **argv )
   // TOL too small otherwise; this is one second
   gfstol_c(1.);
 
-  // the overlap below is intentional, to catch corner cases
-  // Could run the program for all of DE431 at once, but below better
-  // for debugging (and uses less memory?)
-  for (j=-479654654400.; j<479386728000.; j+= 0.9*15778476000.) {
+  // SPICE window for all DE431
+  // wninsd_c (-479695089600.+86400*468, 479386728000., &cnfine);
 
-    // SPICE window creation
-    wninsd_c (j, j+15778476000., &cnfine );
+  // FOR TESTING PURPOSES ONLY!!!!
+  wninsd_c (946684800., 2147483648., &cnfine);
     
-    // I send these as: observer b1 b2, thus the odd argv order below
-    // 0.10471975511965977462 radians = 6 degrees
+  // I send these as: observer b1 b2, thus the odd argv order below
+  // 0.10471975511965977462 radians = 6 degrees
 
-    gfsep_c(argv[2],"POINT","J2000",argv[3],"POINT","J2000","NONE",argv[1],
+  gfsep_c(argv[2],"POINT","J2000",argv[3],"POINT","J2000","NONE",argv[1],
 	  "<", 0.10471975511965977462,0,86400.,MAXWIN,&cnfine,&result);
 
-    SpiceInt count = wncard_c( &result );
-    SpiceDouble begtim, endtim;
+  SpiceInt count = wncard_c(&result);
+  SpiceDouble begtim, endtim, mintim;
 
-    for (i=0; i<count; i++) {
-      wnfetd_c ( &result,i,&begtim,&endtim);
-      printf("%f,%f\n",begtim,endtim);
-    }
+  for (i=0; i<count; i++) {
+    wnfetd_c ( &result,i,&begtim,&endtim);
+    printf("%s %s %s 6deg %f %f\n",argv[1],argv[2],argv[3],begtim,endtim);
+
+    // also find min separation in this interval
+    // TODO: use of ABSMIN below may miss some "conjunctions"
+    //    gfsep_c(argv[2],"POINT","J2000",argv[3],"POINT","J2000","NONE",argv[1],
+    //  "ABSMIN", 0,0,86400.,MAXWIN,&cnfiner,&result);
   }
 }
+
