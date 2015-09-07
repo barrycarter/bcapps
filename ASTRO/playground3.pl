7c7c4c1483d1ce9ce0d9d9e2bf331a811c624864
Barry Carter <carter.barry@gmail.com>
2015-09-07 09:51:09 -0600

    Checkpoint

--- ASTRO/playground3.pl
+++ ASTRO/playground3.pl
@@ -2,18 +2,49 @@
 
 require "/usr/local/lib/bclib.pl";
 
-# Julian Dates, the adventure continues
 
-# lets add multiple 400 year periods (400 years = 146097 days)
+=item jd2ymdhms($jd)
 
-while (<>) {
-  chomp;
-  my($modjd)= ($_-2451543.5)%146097+2451543.5;
-  my($date) = Astro::Nova::get_date($modjd);
+Given a Julian date, return the year, month, date, hour, minute, and
+second, in the same way that
+http://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/etcal_c.html
+would
+
+=cut
+
+sub jd2ymdhms {
+  my($jd) = @_;
+
+  # "reduce" this date to 2000-2399, by adding/subtracting 400 year periods
+  # JD 2451543.5 = 1999-12-31 00:00:00, day 0 of year 2000
+
+  # how many 400 year periods we add/subtract
+  my($div) = int(($jd-2451543.5)/146097);
+
+  # how many days are leftover
+  my($newjd) = ($jd-2451543.5)%146097+2451543.5;
+
+  # compute for newjd
+  my($date) = Astro::Nova::get_date($newjd);
   my(@date) = ($date->get_years(), $date->get_months(),
 	     $date->get_days(), $date->get_hours(),
 	     $date->get_minutes(), $date->get_seconds());
-  debug("$_ ->".join(" ",@date));
+
+  # fix the year, otherwise all good
+  $date[0] += 400*($div-1)-1;
+
+  return @date;
+}
+
+
+# Julian Dates, the adventure continues
+
+while (<>) {
+  chomp;
+
+  # just testing, format is year jd
+  my($year,$jd) = split(/\s+/,$_);
+  debug("$year ($jd) ->".join(" ",jd2ymdhms($jd)));
 }
 
 die "TESTING";
