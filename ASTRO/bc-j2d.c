34291e37ef8ac1b41cb3afc668844b61a1fa4adf
Barry Carter <carter.barry@gmail.com>
2015-09-06 21:03:38 -0600

    Checkpoint

--- ASTRO/bc-j2d.c
+++ ASTRO/bc-j2d.c
@@ -8,7 +8,15 @@
 SpiceChar s[255];
 
 int main( int argc, char **argv ) {
-  etcal_c(0,255,s);
+
+  furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
+
+  SpiceDouble jd = 1721423.5;
+
+  et2utc_c ((jd-2451545.)*86400, "C", 0, 255, s);
   printf("%s\n",s);
+
+  //  etcal_c((jd-2451545.)*86400.,255,s);
+  //  printf("%s\n",s);
 }
 
