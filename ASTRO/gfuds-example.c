3a24ea6288912de19be93ebbef73e128e434b532
Barry Carter <carter.barry@gmail.com>
2015-09-09 08:22:24 -0600

    Still working...

--- ASTRO/gfuds-example.c
+++ ASTRO/gfuds-example.c
@@ -1,15 +1,12 @@
-      #include <stdio.h>
-      #include <stdlib.h>
-      #include <string.h>
-   
-      #include "SpiceUsr.h"
-      #include "SpiceZfc.h"
-   
-   
-      #define       MAXWIN    20000
-      #define       TIMFMT    "YYYY-MON-DD HR:MN:SC.###"
-      #define       TIMLEN    41
-      #define       NLOOPS    7
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "SpiceUsr.h"
+#include "SpiceZfc.h"
+#define       MAXWIN    20000
+#define       TIMFMT    "YYYY-MON-DD HR:MN:SC.###"
+#define       TIMLEN    41
+#define       NLOOPS    7
    
 void    gfq     ( SpiceDouble et, SpiceDouble * value );
 void    gfdecrx ( void ( * udfuns ) ( SpiceDouble    et,
@@ -20,32 +17,15 @@ void    gfdecrx ( void ( * udfuns ) ( SpiceDouble    et,
 doublereal dvnorm_(doublereal *state);
    
    
-int main( int argc, char **argv )
-{
-   
-         /*
-  Create the needed windows. Note, one interval
-    consists of two values, so the total number
-         of cell values to allocate is twice
-         the number of intervals.
-         */
-    SPICEDOUBLE_CELL ( result, 2*MAXWIN );
+int main( int argc, char **argv ) {
+
+  SPICEDOUBLE_CELL ( result, 2*MAXWIN );
   SPICEDOUBLE_CELL ( cnfine, 2        );
    
-  SpiceDouble       begtim;
-  SpiceDouble       endtim;
-  SpiceDouble       step;
-  SpiceDouble       adjust;
-  SpiceDouble       refval;
-  SpiceDouble       beg;
-  SpiceDouble       end;
-   
+  SpiceDouble       begtim,endtim,step,adjust,refval,beg,end;
   SpiceChar         begstr [ TIMLEN ];
   SpiceChar         endstr [ TIMLEN ];
-   
-  SpiceInt          count;
-  SpiceInt          i;
-  SpiceInt          j;
+  SpiceInt          count,i,j;
    
   ConstSpiceChar * relate [NLOOPS] = { "=",
 				       "<",
@@ -56,175 +36,79 @@ int main( int argc, char **argv )
                                               "ABSMAX"
   };
    
-  printf( "Compile date %s, %s\n\n", __DATE__, __TIME__ );
+  
+  furnsh_c( "standard.tm" );
    
-         /*
-         Load kernels.
-         */
-	   furnsh_c( "standard.tm" );
+  str2et_c( "2007 JAN 01", &begtim );
+  str2et_c( "2007 APR 01", &endtim );
+  wninsd_c ( begtim, endtim, &cnfine );
    
-         /*
-         Store the time bounds of our search interval in the `cnfine'
-         confinement window.
-         */
-         str2et_c( "2007 JAN 01", &begtim );
-         str2et_c( "2007 APR 01", &endtim );
+  step   = spd_c();
+  adjust = 0.;
+  refval = .3365;
    
-         wninsd_c ( begtim, endtim, &cnfine );
+  for ( j = 0;  j < NLOOPS;  j++ ) {
+    printf ( "Relation condition: %s \n",  relate[j] );
+    
+    gfuds_c ( gfq, gfdecrx, relate[j], refval, adjust, step,
+	      MAXWIN, &cnfine, &result );
+
+    count = wncard_c( &result );
    
-         /*
-         Search using a step size of 1 day (in units of seconds). The reference
-         value is .3365 km/s. We're not using the adjustment feature, so
-         we set `adjust' to zero.
-         */
-         step   = spd_c();
-         adjust = 0.;
-         refval = .3365;
+    if (count == 0 )
+      {
+	printf ( "Result window is empty.\n\n" );
+      } else {
+      for ( i = 0;  i < count;  i++ )
+	{
    
-         for ( j = 0;  j < NLOOPS;  j++ )
-            {
+	  wnfetd_c ( &result, i, &beg, &end );
+	  
+	  timout_c ( beg, TIMFMT, TIMLEN, begstr );
+	  timout_c ( end, TIMFMT, TIMLEN, endstr );
+	  
+	  printf ( "Start time, drdt = %s \n", begstr );
+	  printf ( "Stop time,  drdt = %s \n", endstr );
+	  
+	}
+      
+    }
    
-            printf ( "Relation condition: %s \n",  relate[j] );
+    printf("\n");
    
-            /*
-            Perform the search. The SPICE window `result' contains
-            the set of times when the condition is met.
-            */
-   
-	   gfuds_c ( gfq,
-		     gfdecrx,
-		     relate[j],
-		     refval,
-		     adjust,
-		     step,
-		     MAXWIN,
-		     &cnfine,
-		     &result );
-   
-	 count = wncard_c( &result );
-   
-            /*
-            Display the results.
-            */
-	      if (count == 0 )
-		{
-		  printf ( "Result window is empty.\n\n" );
-		}
-	      else
-		{
-		  for ( i = 0;  i < count;  i++ )
-		    {
-   
-                  /*
-                  Fetch the endpoints of the Ith interval
-                  of the result window.
-                  */
-		    wnfetd_c ( &result, i, &beg, &end );
-   
-                  timout_c ( beg, TIMFMT, TIMLEN, begstr );
-                  timout_c ( end, TIMFMT, TIMLEN, endstr );
-   
-                  printf ( "Start time, drdt = %s \n", begstr );
-                  printf ( "Stop time,  drdt = %s \n", endstr );
-   
-		    }
-   
-		}
-   
-            printf("\n");
-   
+  }
+  
+  kclear_c();
+  return( 0 );
 }
-   
-kclear_c();
-return( 0 );
-}
-   
-   
-   
-      /*
-      The user defined functions required by GFUDS.
-   
-         gfq     for udfuns
-         gfdecrx for udqdec
-      */
-   
-   
-   
-      /*
-      -Procedure Procedure gfq
-      */
-   
-      void gfq ( SpiceDouble et, SpiceDouble * value )
-   
-      /*
-      -Abstract
-   
-      User defined geometric quantity function. In this case,
-         the range rate from the sun to the Moon at TDB time `et'.
-   
-      */
-         {
+
+void gfq ( SpiceDouble et, SpiceDouble * value ) {
    
          /* Initialization */
-         SpiceInt             targ   = 301;
-         SpiceInt             obs    = 10;
+  SpiceInt             targ   = 301;
+  SpiceInt             obs    = 10;
+  
+  SpiceChar          * ref    = "J2000";
+  SpiceChar          * abcorr = "NONE";
    
-         SpiceChar          * ref    = "J2000";
-         SpiceChar          * abcorr = "NONE";
+  SpiceDouble          state [6];
+  SpiceDouble          lt;
    
-         SpiceDouble          state [6];
-         SpiceDouble          lt;
+  
+  spkez_c ( targ, et, ref, abcorr, obs, state, &lt );
    
-         /*
-         Retrieve the vector from the Sun to the Moon in the J2000
-         frame, without aberration correction.
-         */
-         spkez_c ( targ, et, ref, abcorr, obs, state, &lt );
+  *value = dvnorm_( state );
    
-         /*
-         Calculate the scalar range rate corresponding the
-        `state' vector.
-         */
-   
-	  *value = dvnorm_( state );
-   
-return;
+  return;
 }
    
-   
-   
-      /*
-      -Procedure gfdecrx
-      */
-   
-      void gfdecrx ( void ( * udfuns ) ( SpiceDouble    et,
-                                         SpiceDouble  * value ),
-                     SpiceDouble    et,
-                     SpiceBoolean * isdecr )
-   
-      /*
-      -Abstract
-   
-         User defined function to detect if the function derivative
-         is negative (the function is decreasing) at TDB time `et'.
-      */
-         {
+void gfdecrx ( void ( * udfuns ) ( SpiceDouble    et,
+				   SpiceDouble  * value ),
+	       SpiceDouble    et,
+	       SpiceBoolean * isdecr ) {
    
          SpiceDouble         dt = 10.;
+	 uddc_c( udfuns, et, dt, isdecr );
    
-         /*
-         Determine if "udfuns" is decreasing at `et'.
-   
-         uddc_c   - the GF function to determine if
-                    the derivative of the user defined
-                    function is negative at `et'.
-   
-         uddf_c   - the SPICE function to numerically calculate the
-                    derivative of "udfuns" at `et' for the
-                    interval [et-dt, et+dt].
-         */
-   
-		    uddc_c( udfuns, et, dt, isdecr );
-   
-return;
+	 return;
 }
