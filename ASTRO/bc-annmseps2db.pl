31f18b82825455cc883f7975ea4ace2418b557c0
Barry Carter <carter.barry@gmail.com>
2015-09-08 20:49:35 -0600

    Checkpoint

--- ASTRO/bc-annmseps2db.pl
+++ ASTRO/bc-annmseps2db.pl
@@ -5,17 +5,25 @@
 
 require "/usr/local/lib/bclib.pl";
 
-warn "Ignoring 2+ planet conjunctions for now";
+# this is the dump file for all conjunctions, including 3+ planets
+open(A,">/tmp/conjuncts.txt");
+# and mysql queries
+open(B,">/tmp/ams2-queries.txt");
+print B "BEGIN;\n";
 
 for $i (glob "/home/barrycarter/SPICE/KERNELS/annmsepsdump*.txt") {
+  debug("READING: $i");
   my($all) = read_file($i);
+
+  # remove empty sep lists
+  while ($all=~s/(annminsep\[\{[a-z\s\,]+\}\] \= \{\})//) {debug("IGNORING: $1");}
 
   while ($all=~s/annminsep\[\{(.*?)\}\]\s*=\s*\{(\{.*?\})\}//s) {
 
     my($planets,$data) = ($1,$2);
-    my(@planets) = split(/\,\s*/s,$planets);
 
-    if (scalar(@planets)>2) {next;}
+    my(@planets) = split(/\,\s*/s,$planets);
+    my($numplans) = scalar(@planets);
 
     while ($data=~s/\{(.*?)\}//s) {
       my($jd, $sep, $sun, $star, $ssep) = split(/\,\s*/s,$1);
@@ -23,20 +31,86 @@ for $i (glob "/home/barrycarter/SPICE/KERNELS/annmsepsdump*.txt") {
       # get calendar date from Julian date
       # MySQL can't handle years < 100 so separate into fields
       $jd=~s/\*\^/e/;
-      my(@date) = jd2ymdhms($jd);
-      debug("$jd ->",join(" ",@date));
+      my(@date) = jd2mixed_ymdhms($jd);
 
-      debug("$planets/$jd/$sep/$sun/$star/$ssep");
+      print A join(",",(join("+",@planets),$jd,@date[0..2],join(":",@date[3..5]),$sep,$sun,$star,$ssep)),"\n";
+
+      my(@printlist) = (@planets,$jd,@date[0..2],join(":",@date[3..5]),$sep,$sun,$star,$ssep);
+      map($_="'$_'",@printlist);
+      my($print) = join(", ",@printlist);
+
+      # build up field list (this is ugly and non-normalized)
+      my($flist) = "p1";
+      for $j (2..$numplans) {$flist.=", p$j";}
+
+      print B "INSERT INTO p$numplans ($flist, jd, year, month, day, time, sep, solarsep, star, starsep) VALUES ($print);\n";
 
       # TODO: turn this into an INSERT statement
-      # TODO: breakup into year, month, day for searching
       # TODO: this won't work if more than 2 planets, fix
       # NOTE: including JD in final result can't use MySQL date, but also
       # doing year/month/day breakdown for ease of use
-      print join("\t",@planets,$jd,@date[0..2],@date[3..5],$sep,$sun,$star,$ssep),"\n";
+      if (scalar(@planets)>2) {next;}
 
+#      print join("\t",@planets,$jd,@date[0..2],join(":",@date[3..5]),$sep,$sun,$star,$ssep),"\n";
+      # for the dump file, we glue planets using plus since number can vary
     }
   }
 }
 
+print B "COMMIT;\n";
 
+close(B);
+
+=item schema
+
+-- The schema of the MySQL tables:
+
+CREATE TABLE p2 (
+p1 TEXT, p2 TEXT, 
+jd DOUBLE, year INT, month INT, day INT, time TIME,
+sep DOUBLE, solarsep DOUBLE, star TEXT, starsep DOUBLE
+);
+
+-- The others are identical, but with more planets
+
+CREATE TABLE p3 (
+p1 TEXT, p2 TEXT, p3 TEXT, 
+jd DOUBLE, year INT, month INT, day INT, time TIME,
+sep DOUBLE, solarsep DOUBLE, star TEXT, starsep DOUBLE
+);
+
+CREATE TABLE p4 (
+p1 TEXT, p2 TEXT, p3 TEXT, p4 TEXT,
+jd DOUBLE, year INT, month INT, day INT, time TIME,
+sep DOUBLE, solarsep DOUBLE, star TEXT, starsep DOUBLE
+);
+
+CREATE TABLE p5 (
+p1 TEXT, p2 TEXT, p3 TEXT, p4 TEXT, p5 TEXT, 
+jd DOUBLE, year INT, month INT, day INT, time TIME,
+sep DOUBLE, solarsep DOUBLE, star TEXT, starsep DOUBLE
+);
+
+-- there is only one conjunction of all 6 planets
+
+CREATE TABLE p6 (
+p1 TEXT, p2 TEXT, p3 TEXT, p4 TEXT, p5 TEXT, p6 TEXT,
+jd DOUBLE, year INT, month INT, day INT, time TIME,
+sep DOUBLE, solarsep DOUBLE, star TEXT, starsep DOUBLE
+);
+
+-- indexes are also similar
+
+CREATE INDEX p11 ON p2(p1(10));
+CREATE INDEX p12 ON p2(p2(10));
+CREATE INDEX p13 ON p2(jd);
+CREATE INDEX p14 ON p2(year);
+CREATE INDEX p15 ON p2(month);
+CREATE INDEX p16 ON p2(day);
+CREATE INDEX p17 ON p2(time);
+CREATE INDEX p18 ON p2(sep);
+CREATE INDEX p19 ON p2(solarsep);
+CREATE INDEX p1a ON p2(star(10));
+CREATE INDEX p1b ON p2(starsep);
+
+=cut
--- ASTRO/playground3.pl
+++ ASTRO/playground3.pl
@@ -2,7 +2,6 @@
 
 require "/usr/local/lib/bclib.pl";
 
-
 # Julian Dates, the adventure continues
 
 while (<>) {
@@ -41,90 +40,3 @@ for $y (-13200..-4000) {
 # $rd = cjdn_to_rd($cjdn, $cjdf);
 
 # debug(strftime("%Y-%m-%d",gmtime(-999999999999)));
-
-=item jd2proleptic_julian_ymdhms($jd)
-
-Given a Julian date, returns the proleptic Julian year/month/day etc.
-
-Proleptic Julian = assumes the Julian calendar (1 leap year every 4
-years w/ no variance) is always used
-
-See jd2mixed_ymdhms() for caveats
-
-=cut
-
-sub jd2proleptic_julian_ymdhms {
-  my($jd) = @_;
-
-  # The Julian calendar repeats every 4*365+1 = 1461 days
-  # using 2000-2003 as "reference date"
-  # [2000 1 1 = JD 2451544.500000 = Unix 946684800, so 2451543.500000 = day 0]
-  # how many "chunks" of 1461 days ago is/was this?
-  # 14 to compensate for Gregorian reformation
-  my($yrs) = ($jd-2451543.500000-14.);
-  # how many days into this chunk?
-  my($chunks,$days) = (floor($yrs/1461),fmodp($yrs,1461));
-  my(@gm) = gmtime(946684800+$days*86400);
-  # adjust the year (gmtime returns years-1900, thus the adjustment below)
-  $gm[5] += 1900+4*$chunks;
-  # gmtime returns month-1, so...
-  $gm[4]++;
-  return(reverse(@gm[0..5]));
-}
-
-=item jd2proleptic_gregorian_ymdhms($jd)
-
-Given a Julian date, returns the proleptic Gregorian year/month/day etc.
-
-Proleptic Gregorian = assumes the Gregorian calendar (1 leap year every 4
-years except every 100 years except every 400 years) is always used
-
-See jd2mixed_ymdhms() for caveats
-
-=cut
-
-sub jd2proleptic_gregorian_ymdhms {
-
-  my($jd) = @_;
-
-  # "reduce" this date to 2000-2399, by adding/subtracting 400 year periods
-  # JD 2451543.5 = 1999-12-31 00:00:00, day 0 of year 2000
-  my($yrs) =$jd-2451543.500000;
-
-  # The Gregorian calendar repeats every 400 years = 146097 days
-  my($chunks,$days) = (floor($yrs/146097),fmodp($yrs,146097));
-  # compute for newjd (add to days to bring back into 2000-2399 era)
-  my($date) = Astro::Nova::get_date($days+2451543.5);
-  my(@date) = ($date->get_years(), $date->get_months(),
-	     $date->get_days(), $date->get_hours(), $date->get_minutes,
-	      $date->get_seconds);
-  # adjust
-  $date[0] += 400*$chunks;
-  return @date;
-}
-
-=item jd2mixed_ymdhms($jd)
-
-Returns either the Gregorian year/month/date/etc or the Julian one,
-depending on whether is it before or after the Reformation:
-
-TODO: allow user to choose Reformation date, one below is per NASA:
-
-http://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/time.html#Calendars
-
-Julian 1582 Oct 5 = Gregorian 1582 Oct 15 = JD 2299160.500000
-
-Caveats: following the astronomical convention that 1BC is year 0, 2BC
-is year -1, and so on, since Im mostly doing this for Stellarium dates:
-
-http://www.stellarium.org/wiki/index.php/FAQ#.22There_is_no_year_0.22.2C_or\
-_.22BC_dates_are_a_year_out.22
-
-=cut
-
-sub jd2mixed_ymdhms {
-  my($jd) = @_;
-
-  if ($jd<2299160.5) {return jd2proleptic_julian_ymdhms($jd);}
-  return jd2proleptic_gregorian_ymdhms($jd);
-}
