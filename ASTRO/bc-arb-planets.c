851dc73fa68109463dd1e2d8f4d299e0e427dce0
Barry Carter <carter.barry@gmail.com>
2015-09-12 14:51:57 -0600

    Checkpoint

--- ASTRO/bc-arb-planets.c
+++ ASTRO/bc-arb-planets.c
@@ -6,6 +6,14 @@
 #define MAXWIN 200000
 #define SIXDEGREES 0.10471975511965977462
 
+// prototypes
+// gfq = function that returns scalar value of interest
+void gfq (SpiceDouble et, SpiceDouble * value );
+// gfdecrx = function that determines whether gfq is decreasing
+void gfdecrx (void (*udfuns) (SpiceDouble et, SpiceDouble *value ),
+		 SpiceDouble et, SpiceBoolean * isdecr );
+void findmins(SpiceDouble beg, SpiceDouble end);
+
 // Usage: $0 naif-id-of-planet naif-id-of-planet  naif-id-of-planet  ...
 
 static SpiceInt planets[6];
@@ -15,23 +23,12 @@ static SpiceInt planetcount;
 double et2jd(double d) {return 2451544.5+d/86400.;}
 double unix2et(double d) {return d-946684800.;}
 
-// gfq = function that returns scalar value of interest
-void gfq (SpiceDouble et, SpiceDouble * value );
-// gfdecrx = function that determines whether gfq is decreasing
-void gfdecrx (void (*udfuns) (SpiceDouble et, SpiceDouble *value ),
-		 SpiceDouble et, SpiceBoolean * isdecr );
-
 int main( int argc, char **argv ) {
 
-  SpiceInt i,j,count,count2;
+  SpiceInt i,j,count;
   SPICEDOUBLE_CELL(result, 2*MAXWIN);
-  SPICEDOUBLE_CELL(result2,2*MAXWIN );
-  // I use the two below as windows
   SPICEDOUBLE_CELL(cnfine,2);
-  SPICEDOUBLE_CELL(cnfine2,2);
-  // I use this as just a cell
-  SPICEDOUBLE_CELL(cell,2);
-  SpiceDouble beg, end, beg2, end2;
+  SpiceDouble beg, end;
 
   // fill the static planets array
   for (i=1; i<argc; i++) {planets[i] = atoi(argv[i]);}
@@ -56,31 +53,7 @@ int main( int argc, char **argv ) {
     wnfetd_c(&result,i,&beg,&end);
     printf("6deg %f %f\n",et2jd(beg),et2jd(end));
 
-    // TODO: this is hideous coding (append/remove, must be a better way)
-    //    appndd_c(beg,&cnfine2);
-    //    appndd_c(end,&cnfine2);
-    //    wnvald_c(2,2,&cnfine2);
-    
-    wninsd_c(beg,end,&cnfine2);
-
-    printf("SIZE1: %d\n",card_c(&cnfine2));
-
-    // find min *separations* (maybe more than one!) in this window
-    gfuds_c(gfq,gfdecrx,"LOCMIN",0.,0.,86400.,MAXWIN,&cnfine2,&result2);
-    count2 = wncard_c(&result2);
-
-    for (j=0; j<count2; j++) {
-      wnfetd_c(&result2,j,&beg2,&end2);
-      // this is cheating, but I'm not using &end, so...
-      gfq(beg,&end);
-      // TODO: compute minimal solar distance
-      printf ("%f %f\n",et2jd(beg2),end2/pi_c()*180.);
-    }
-
-    removd_c(beg,&cnfine2);
-    removd_c(end,&cnfine2);
-    printf("SIZE2: %d\n",card_c(&cnfine2));
-
+    // findmins(beg,end);
   }
   return 0;
 }
@@ -90,23 +63,30 @@ void gfq ( SpiceDouble et, SpiceDouble *value ) {
   // max separation between all non-0 planets in planets
   // TODO: get min solar separation (not necessarily here)
   SpiceInt i,j;
-  SpiceDouble sep, lt;
-  SpiceDouble position[planetcount][2];
+  SpiceDouble sep, lt, max;
+  SpiceDouble position[6][3];
+  SpiceDouble test[3];
 
   // compute the Earth positions first for efficiency
   for (i=1; i<=planetcount; i++) {
-    spkezp_c(planets[i], et, "J2000", "LT+S", 399, position[i], &lt);
+    //    spkezp_c(planets[i], et, "J2000", "LT+S", 399, position[i], &lt);
+    spkezp_c(planets[i], et, "J2000", "NONE", 3, test, &lt);
+    // this is stupid
+    for (j=0; j<=2; j++) {position[i][j] = test[j];}
   }
-
-  *value = 0;
 
   // and now the angle diffs (keep only min)
   for (i=1; i<=planetcount; i++) {
     for (j=i+1; j<=planetcount; j++) {
+      printf("I3: %d %f -> %f %f %f %f\n",planets[i],et2jd(et),position[i][0],position[i][1],position[i][2],position[i][3]);
+      //      printf("I3: %d %f -> %f %f %f %f\n",planets[i],et2jd(et),test[0],test[1],test[2],test[3]);
       sep = vsep_c(position[i],position[j]);
-      if (sep>*value) {*value=sep;}
+      if (sep>max) {max=sep;}
     }
   }
+
+  if (max<SIXDEGREES){printf("ABOUT TO RETURN: %f -> %f\n",et2jd(et),max);}
+  *value=max;
   return;
 }
  
@@ -116,4 +96,30 @@ void gfdecrx (void(* udfuns)(SpiceDouble et,SpiceDouble * value),
  uddc_c( udfuns, et, dt, isdecr );
  return;
 }
+
+// Finds and prints (icky) local mins of gfq between beg and end
+
+void findmins(SpiceDouble beg, SpiceDouble end) {
+
+  // we expect very few conjunctions per 6 degree interval, 100 is overkill
+  SPICEDOUBLE_CELL(result2, 200);
+  SPICEDOUBLE_CELL(cnfine2,2);
+  SpiceDouble beg2, end2;
+  SpiceInt count,i;
+  // TODO: if intervals are bad minwise, expand them and trim?
+  wninsd_c(beg,end,&cnfine2);
+  // find local mins
+  gfuds_c(gfq,gfdecrx,"LOCMIN",0.,0.,86400.,MAXWIN,&cnfine2,&result2);
+  count = wncard_c(&result2); 
+  for (i=0; i<count; i++) {
+    wnfetd_c(&result2,i,&beg2,&end2);
+    printf("min %f %f\n",et2jd(beg2),et2jd(end2));
+  }
+
+  // SPICEDOUBLE_CELLs are static (as per SpiceCel.h, so must clear)
+  removd_c(beg,&cnfine2);
+  removd_c(end,&cnfine2);
+  return;
+}
+
 
