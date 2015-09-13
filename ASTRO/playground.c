0ad3a753c277a5a68ae658dec729bebeaecac2ac
Barry Carter <carter.barry@gmail.com>
2015-09-12 18:10:29 -0600

    All good for now

--- ASTRO/playground.c
+++ ASTRO/playground.c
@@ -14,16 +14,14 @@ double jd2et(double d) {return 86400.*(d-2451545.);}
 
 // icky defining functions first
 
-SpiceDouble *posxyz(double time, int planet) {
+void posxyz(double time, int planet, SpiceDouble position[3]) {
   SpiceDouble lt;
-  static SpiceDouble position[3];
   spkezp_c(planet, jd2et(time),"J2000","NONE",0,position,&lt);
-  return position;
 }
 
 int main (int argc, char **argv) {
 
-  SpiceDouble *pos, *pos2;
+  SpiceDouble pos[3], pos2[3];
   SpiceDouble lt;
 
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
@@ -33,8 +31,9 @@ int main (int argc, char **argv) {
     int planet = atoi(argv[3]);
     //    spkezp_c(planet,time,"J2000","NONE",0,position,&lt);
     //    spkgps_c(planet,time,"J2000",0,position,&lt);
-    pos = posxyz(atof(argv[2]),atoi(argv[3]));
-    //    pos2 = posxyz(2450000,6);
+    posxyz(atof(argv[2]),atoi(argv[3]),pos);
+    posxyz(2450000,6,pos2);
+    printf("%f -> %f %f %f\n",time,pos2[0],pos2[1],pos2[2]);
     printf("%f -> %f %f %f\n",time,pos[0],pos[1],pos[2]);
   }
 
