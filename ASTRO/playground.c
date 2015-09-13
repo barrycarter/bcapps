c0582c97342c803eceb44d5b7d54bdd42edb99e1
Barry Carter <carter.barry@gmail.com>
2015-09-12 18:05:07 -0600

    Checkpoint

--- ASTRO/playground.c
+++ ASTRO/playground.c
@@ -12,9 +12,18 @@
 double et2jd(double d) {return 2451545.+d/86400.;}
 double jd2et(double d) {return 86400.*(d-2451545.);}
 
+// icky defining functions first
+
+SpiceDouble *posxyz(double time, int planet) {
+  SpiceDouble lt;
+  static SpiceDouble position[3];
+  spkezp_c(planet, jd2et(time),"J2000","NONE",0,position,&lt);
+  return position;
+}
+
 int main (int argc, char **argv) {
 
-  SpiceDouble position[3];
+  SpiceDouble *position;
   SpiceDouble lt;
 
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
@@ -22,8 +31,9 @@ int main (int argc, char **argv) {
   if (!strcmp(argv[1],"posxyz")) {
     double time = jd2et(atof(argv[2]));
     int planet = atoi(argv[3]);
-    spkezp_c(planet,time,"J2000","NONE",0,position,&lt);
+    //    spkezp_c(planet,time,"J2000","NONE",0,position,&lt);
     //    spkgps_c(planet,time,"J2000",0,position,&lt);
+    position = posxyz(atof(argv[2]),atoi(argv[3]));
     printf("%f -> %f %f %f\n",time,position[0],position[1],position[2]);
   }
 
