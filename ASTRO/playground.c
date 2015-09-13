4231a4626dda99765a78ed81302bcdc40f91bd1a
Barry Carter <carter.barry@gmail.com>
2015-09-12 18:25:37 -0600

    Checkpoint: some fixes

--- ASTRO/playground.c
+++ ASTRO/playground.c
@@ -19,22 +19,48 @@ void posxyz(double time, int planet, SpiceDouble position[3]) {
   spkezp_c(planet, jd2et(time),"J2000","NONE",0,position,&lt);
 }
 
+void earthvector(double time, int planet, SpiceDouble position[3]) {
+  SpiceDouble lt;
+  // TODO: make this 399 for production
+  spkezp_c(planet, jd2et(time),"J2000","NONE",3,position,&lt);
+}
+
+double earthangle(double time, int p1, int p2) {
+  SpiceDouble pos[3], pos2[3];
+  earthvector(time, p1, pos);
+  earthvector(time, p2, pos2);
+  printf("P1: %f -> %f %f %f\n",time,pos[0],pos[1],pos[2]);
+  printf("P2: %f -> %f %f %f\n",time,pos2[0],pos2[1],pos2[2]);
+  return vsep_c(pos,pos2);
+}
+
 int main (int argc, char **argv) {
 
-  SpiceDouble pos[3], pos2[3];
+  SpiceDouble pos[3];
   SpiceDouble lt;
 
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
 
   if (!strcmp(argv[1],"posxyz")) {
-    double time = jd2et(atof(argv[2]));
+    double time = atof(argv[2]);
     int planet = atoi(argv[3]);
-    //    spkezp_c(planet,time,"J2000","NONE",0,position,&lt);
-    //    spkgps_c(planet,time,"J2000",0,position,&lt);
-    posxyz(atof(argv[2]),atoi(argv[3]),pos);
-    posxyz(2450000,6,pos2);
-    printf("%f -> %f %f %f\n",time,pos2[0],pos2[1],pos2[2]);
+    posxyz(time,planet,pos);
     printf("%f -> %f %f %f\n",time,pos[0],pos[1],pos[2]);
-  }
+  };
+
+  if (!strcmp(argv[1],"earthvector")) {
+    double time = atof(argv[2]);
+    int planet = atoi(argv[3]);
+    earthvector(time,planet,pos);
+    printf("%f -> %f %f %f\n",time,pos[0],pos[1],pos[2]);
+  };
+
+  if (!strcmp(argv[1],"earthangle")) {
+    double time = atof(argv[2]);
+    int p1 = atoi(argv[3]);
+    int p2 = atoi(argv[4]);
+    SpiceDouble sep = earthangle(time,p1,p2);
+    printf("%f -> %f\n",time,sep);
+  };
 
 }
