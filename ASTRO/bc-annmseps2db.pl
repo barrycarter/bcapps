95f165a83a42606ea02761860e6dab5497327e12
Barry Carter <carter.barry@gmail.com>
2015-09-14 09:14:39 -0600

    Checkpoint (not working)

--- ASTRO/bc-annmseps2db.pl
+++ ASTRO/bc-annmseps2db.pl
@@ -4,6 +4,9 @@
 
 require "/usr/local/lib/bclib.pl";
 
+# NOTE: we dont print Earth, but need Mars in position 4, so...
+@plist = ("", "Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus");
+
 # this is the dump file for all conjunctions, including 3+ planets
 open(A,">/tmp/conjuncts.txt");
 # and mysql queries
@@ -17,9 +20,10 @@ my(@planets,@range);
 while (<C>) {
 
   # new set of planets?
+  # TODO: convert to names when putting in db
   if (/CONJUNCTIONS FOR.*planets: (.*?)$/i) {
-    @planets = ();
-    for $i (split(/\s+/,$1)) {if ($i) {push(@planets,$i);}}
+    @planets=split(/\s+/,$1);
+    map($_=$plist[$_],@planets);
     next;
   }
 
@@ -28,7 +32,7 @@ while (<C>) {
 
   # the min line where we actually do print stuff
   my($m,$jd,$sep,$sunsep) = split(/\s+/, $_);
-  my($numplans) = scalar(@planets);
+
   my(@date) = jd2mixed_ymdhms($jd);
   # the start and end dates for the range containing this conjunction
   my(@sdate) = jd2mixed_ymdhms($range[0]);
@@ -36,21 +40,14 @@ while (<C>) {
 
   # cleanup hms
   my($hms) = sprintf("%02d:%02d:%02d",@date[3..5]);
-  debug("HMS: $hms");
 
-  debug("DATE",@date);
-
-  # build up field list (this is ugly and non-normalized)
-  my($flist) = "p1";
-  for $j (2..$numplans) {$flist.=", p$j";}
-
-  my(@printlist) = (@planets,$jd,@date[0..2],join(":",@date[3..5]),$sep,$sun,$star,$sunsep);
+  my(@printlist) = (@planets,$jd,@date[0..2],$hms,$sep,$sunsep);
   map($_="'$_'",@printlist);
   my($print) = join(", ",@printlist);
 
   # TODO: printing to stdout just for testing
-  print "INSERT INTO p$numplans 
-($flist, jd, year, month, day, time, sep, solarsep) VALUES ($print);\n";
+  print "INSERT INTO p6 (p1, p2, p3, p4, p5, p6, jd, year, month, day, 
+         time, sep, solarsep) VALUES ($print);\n";
 
   print A join(",",(join("+",@planets),$jd,@date[0..2],join(":",@date[3..5]),$sep,$sunsep)),"\n";
 
@@ -112,10 +109,13 @@ close(B);
 
 -- The schema of the MySQL table:
 
+-- (using DATETIME for sdate and edate even though they exceed ranges)
+-- TODO: change to filled text if this doesn't work
+
 CREATE TABLE conjunctions (
 p1 TEXT, p2 TEXT, p3 TEXT, p4 TEXT, p5 TEXT, p6 TEXT,
 jd DOUBLE, year INT, month INT, day INT, time TIME,
-sep DOUBLE, solarsep DOUBLE);
+sep DOUBLE, sunsep DOUBLE, sdate DATETIME, edate DATETIME);
 
 CREATE INDEX p1 ON conjunctions(p1(10));
 CREATE INDEX p2 ON conjunctions(p2(10));
@@ -129,6 +129,8 @@ CREATE INDEX p9 ON conjunctions(month);
 CREATE INDEX pa ON conjunctions(day);
 CREATE INDEX pb ON conjunctions(time);
 CREATE INDEX pc ON conjunctions(sep);
-CREATE INDEX pd ON conjunctions(solarsep);
+CREATE INDEX pd ON conjunctions(sunsep);
+CREATE INDEX pe ON conjunctions(sdate);
+CREATE INDEX pf ON conjunctions(edate);
 
 =cut
