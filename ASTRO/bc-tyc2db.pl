8789f1b671f0dd5ae89d8d3caf990e554367dd97
Barry Carter <carter.barry@gmail.com>
2015-10-04 13:10:23 -0600

    Seriously frustrated; tycho omits bright stars (except in supplements)

--- ASTRO/bc-tyc2db.pl
+++ ASTRO/bc-tyc2db.pl
@@ -1,1 +1,41 @@
 #!/bin/perl
+
+# I'm having issues with the Hipparcos catalog (bad version/compile of
+# scat?), so using the Tycho catalog and doing the equatorial to
+# ecliptic translations myself (sigh)
+
+require "/usr/local/lib/bclib.pl";
+
+# downloaded as tar from http://vizier.u-strasbg.fr/viz-bin/Cat?I/259
+
+# there are two versionf of Tycho, confirmed this one IS J2000
+
+open(A,"zcat /home/barrycarter/SPICE/KERNELS/I259/tyc2.dat.??.gz|");
+
+while (<A>) {
+
+  my($pflag) = get_chars($_,14,15);
+  my($ra) = get_chars($_,16,28);
+  my($dec) = get_chars($_,29,41);
+  my($mag) = get_chars($_,124,130);
+  my($hip) = get_chars($_,143,148);
+
+  # trim spaces
+  map(s/\s//g, ($ra,$dec,$mag,$hip));
+
+  # TODO: restore this to 6.5, lowered for testing
+  # ignore faint and blank ra/dec
+  if ($mag>1.5 || length($ra)==0 || length($dec)==0 || length($mag)==0) {next;}
+
+  debug("$ra/$dec/$mag/$hip/$pflag");
+}
+
+# wrapper around substr so I can match tycho2-guide.pdf intentionally
+# snips pipe character at end since I don't need it (this also means
+# this subroutine, as written, is not valid for general use)
+
+sub get_chars {
+  my($str,$x,$y) = @_;
+  return substr($str,$x-1,$y-$x);
+}
+
--- ASTRO/playground27.m
+++ ASTRO/playground27.m
@@ -1,12 +1,11 @@
 (* does rotating around x axis simply change "declination" by sin of azimuth? *)
 
-(* simple equ2ecl conversion? *)
+(* suppose Im doing ecl2equ, note that ecl is xy plane so phi/z is 0 *)
 
-(* but longitude doubtless changes, so this probably won't work... *)
-
-exp0 = Simplify[Apply[xyz2sph,rotationMatrix[x,e].sph2xyz[th,ph,1]],
+exp0 = Simplify[Apply[xyz2sph,rotationMatrix[x,e].sph2xyz[th,0,1]],
 Element[{e,th,ph},Reals]
 ]
+
 
 exp1 = Simplify[exp0 /. ArcTan[x_,y_] -> ArcTan[y/x],
 Element[{e,th,ph},Reals]]
