d080b3e9c96a189d59f4ce5129b6b22c26e00799
Barry Carter <carter.barry@gmail.com>
2015-11-02 20:15:11 -0700

    Checkpoint

--- ASTRO/bc-zdump2abbrev.pl
+++ ASTRO/bc-zdump2abbrev.pl
@@ -10,10 +10,24 @@
 
 require "/usr/local/lib/bclib.pl";
 
+my(%latest,%abbrev);
+
 open(A,"bzcat $bclib{githome}/ASTRO/zdump.txt.srt.bz2|");
 
 while (<A>) {
-  debug("GOT: $_");
+
+  # we need: filename, UTC year, abbrev
+  my(@data) = split(/\s+/,$_);
+
+  my($fname, $utcy, $abbrev) = @data[0,5,13];
+  $fname=~s%/usr/share/zoneinfo/%%;
+
+  # if I already have later abbrev for this tz, ignore
+  if ($latest{$fname} > $utcy) {next;}
+
+  $latest{$fname} = $utcy;
+  $abbrev{$fname} = $abbrev;
 }
 
+for $i (keys %abbrev) {print "$i $abbrev{$i}\n";}
 
