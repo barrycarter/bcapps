d5b50572ae3f2b3615687134309d58675aa50bae
Barry Carter <carter.barry@gmail.com>
2015-11-10 15:26:55 -0700

    Checkpoint (but can't post to sandbox yet)

---
+++ CALENDAR/bc-meetup-api.pl
@@ -0,0 +1,17 @@
+#!/bin/perl
+
+# this program, unlike ../bc-meetup.pl, uses the Meetup API to
+# schedule events, more specifically, events held at the
+# Multigenerational centers in the City of Albuquerque
+
+require "/usr/local/lib/bclib.pl";
+require "/home/barrycarter/bc-private.pl";
+
+my($url) = "https://api.meetup.com/2/event";
+
+my($postdata) = "group_urlname=Meetup-API-Testing&name=Big+Barry+Birthday+Bash&key=$private{meetup}{key}";
+
+my($out,$err,$res) = cache_command("curl -d '$postdata' '$url'","age=1800");
+
+debug("OUT: $out");
+
