b17f20b048ffc4c0c31bab1b278f39597f2c8788
Barry Carter <carter.barry@gmail.com>
2015-11-11 12:19:20 -0700

    Checkpoint

--- CALENDAR/bc-parse-mmmc.pl
+++ CALENDAR/bc-parse-mmmc.pl
@@ -14,6 +14,8 @@
 
 # TODO: check existing events to avoid overwrite
 
+# TODO: announce meetups if/when I have an actual group
+
 # TODO: limit events to 15m alignment IF meetup.com can't handle
 # otherwise (they allow millisecond timing, but don't actually appear
 # to support it)
@@ -22,24 +24,51 @@ require "/usr/local/lib/bclib.pl";
 require "/home/barrycarter/bc-private.pl";
 
 # TODO: change gname when not testing
+my($gname,$status) = ("Meetup-API-Testing", "published");
+# my ($gname,$status) = ("Albuquerque-Multigenerational-Center-Events-unofficial","draft");
 
-my($gname) = "Meetup-API-Testing";
-# my ($gname) = "Albuquerque-Multigenerational-Center-Events-unofficial";
+my($desc) = "
 
+This event was copied directly from an Albuquerque area
+multigenerational center event. For more information on this event and
+Albuquerque's multigenerational centers, please contact:
+
+https://www.cabq.gov/seniors/centers/north-domingo-baca-multigenerational-center (offical site for North Domingo Baca Multigenerational Center)
+
+https://www.cabq.gov/seniors/centers/manzano-mesa-multigenerational-center (official site for Manzano Mesa Multigenerational Center)
+
+http://manzanomesacenter.com/ (unofficial site for Manzano Mesa Multigenerational Center)
+
+The owner of this meetup group is not affiliated with the City of
+Albuquerque, and has no further information on this event, sorry.
+
+Although this event is listed in an official City of Albuquerque
+publication or website, there is no guarantee that it will actually
+occur. Please contact the venue directly if you need to be sure.
+
+You do need to RSVP for this event to attend; however, you may RSVP if
+you choose to do so.
+
+";
+
+# convert weekdays to number
 for $i (0..6) {$wday{substr("xmtwrfs",$i,1)} = $i;}
-
+# convert spreadsheet to array
 my(@arr) = gnumeric2array("$bclib{githome}/CALENDAR/mmmc.gnumeric");
-
 # the first row is headers
 my(@headers) = @{$arr[0]};
 
-# remaining rows
+debug("HEADERS",@headers);
+
+# remaining rows are events
 
 for $i (1..$#arr) {
 
   # create hash
   my(%hash) = ();
   for $j (0..$#headers) {$hash{lc($headers[$j])} = $arr[$i][$j];}
+
+  $hash{name} = urlencode($hash{name});
 
   for $j (split(/\n/,$hash{when})) {
     my(@dates) = parseDateString($j,11,2015);
@@ -48,6 +77,18 @@ for $i (1..$#arr) {
       my($st,$en) = @{$k};
 
       # now, cruft the meetup itself
+
+      # the description will always contain $desc above, but may have more
+      my($edesc);
+
+      if ($hash{cost}) {$edesc .= "Cost: $hash{cost}\n\n";}
+      if ($hash{location}) {$edesc .= "Location: $hash{location}\n\n";}
+      if ($hash{contact}) {$edesc .= "Contact: $hash{contacts}\n\n";}
+      if ($hash{comments}) {$edesc .= "Comments: $hash{comments}\n\n";}
+
+      $edesc .= $desc;
+      $edesc=~s/\n\n/<br><br>\n/sg;
+      $edesc = urlencode($edesc);
 
       my($pstr) = "time=".$st*1000;
 
@@ -58,10 +99,10 @@ for $i (1..$#arr) {
 
       # these are fixed for all meetups I post
       # TODO: using draft for now
-      $pstr .= "&group_urlname=$gname&key=$private{meetup}{key}&guest_limit=999&publish_status=draft&venue_visibility=public&venue_id=710375";
+      $pstr .= "&group_urlname=$gname&key=$private{meetup}{key}&guest_limit=999&publish_status=$status&venue_visibility=public&venue_id=710375";
 
       # TODO: add non-fixed data to post: name, description
-      $pstr .= "&name=this+event+needs+a+name&description=needs+a+description&simple_html_description=what+the+heck+is+a+simple+html+desc";
+      $pstr .= "&name=$hash{name}&description=$edesc";
 
       # for now, just print the command, don't actually run it
       print "curl -d '$pstr' 'https://api.meetup.com/2/event'\n";
@@ -79,6 +120,17 @@ sub parseDateString {
 
   my($str,$mon,$yr) = @_;
   my($stime,$etime,@res);
+
+  # handle "stardate" format
+  if ($str=~s/^(\d{8}):(.*)$//) {
+    my($sdate,$time) = ($1,$2);
+    ($stime,$etime) = split(/\-/,$time);
+    # ok to overwrite here, not going thru loop below
+    $stime = str2time("$sdate $stime MST7MDT");
+    $etime = $etime?str2time("$sdate $etime MST7MDT"):"";
+    # this is a one-element list whose element is a list of two elements
+    return [$stime,$etime];
+  }
 
   # need 2 digit months for matching
   $mon = sprintf("%02d",$mon);
@@ -111,7 +163,7 @@ sub parseDateString {
       unless ($sdate=~/^$yr$mon/) {next;}
 
       # TODO: figure out what to do w/ no start time
-      unless ($stime) {warn "NO STIME ($stime), IGNORING"; next;}
+      unless ($stime) {warn "NO STIME ($str), IGNORING"; next;}
       # unix start and end times
       my($ustime) = str2time("$sdate $stime MST7MDT");
       my($uetime) = $etime?str2time("$sdate $etime MST7MDT"):"";
