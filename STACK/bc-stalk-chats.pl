ac82edc7795d3412b55ec371b82df5f9c806aaa8
Barry Carter <carter.barry@gmail.com>
2015-12-12 12:57:22 -0700

    Checkpoint: not working

---
+++ STACK/bc-stalk-chats.pl
@@ -0,0 +1,27 @@
+#!/bin/perl
+
+# Stalks given chat rooms per http://meta.stackexchange.com/questions/218343/how-do-the-stack-exchange-websockets-work-what-are-all-the-options-you-can-send/218443#218443
+
+require "/usr/local/lib/bclib.pl";
+
+# http://chat.stackexchange.com/rooms/36/mathematics is test
+
+my($rid) = 36;
+my($out,$err,$res) = cache_command2("curl -L http://chat.stackexchange.com/rooms/$rid","age=3600");
+
+unless ($out=~s%<input id="fkey" name="fkey" type="hidden" value="(.*?)" />%%s) {die "NO FKEY";}
+
+my($fkey) = $1;
+
+# get events
+
+# fixed timestamp for testing
+my($ts) = 1449949857-600;
+
+# TODO: normally, dont cache this
+# TODO: can I do multiple roomids here, eg r1=foo&r2=foo.. ?
+($out,$err,$res) = cache_command2("curl --trace-ascii - -H 'Referer: http://chat.stackexchange.com/rooms/$rid' -H 'X-Requested-With: XMLHttpRequest' -H 'User-Agent: Fauxzilla' -b 'x=0' -d 'fkey=$fkey&r$rid=$ts' http://chat.stackexchange.com/events", "age=3600");
+
+# debug("FK: $fkey");
+debug("OUT: $out","ERR: $err");
+
