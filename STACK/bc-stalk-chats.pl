715aabcc133190536ad861aeb352a3b49ff729ef
Barry Carter <carter.barry@gmail.com>
2015-12-12 19:04:27 -0700

    Checkpoint.

--- STACK/bc-stalk-chats.pl
+++ STACK/bc-stalk-chats.pl
@@ -4,21 +4,48 @@
 
 require "/usr/local/lib/bclib.pl";
 
-# http://chat.stackexchange.com/rooms/36/mathematics is test
+# TODO: don't hardcode these rooms AND allow changes while program is
+# running (perhaps as a mini-IRC server)
 
-my($rid) = 36;
+@rooms = (118, 198, 21, 30332, 35, 18, 36, 8595, 30985);
+
+# note that this doesn't change @rooms, which is important
+my($rstr) = join("&",map("r$_=0",@rooms));
+
+# "1" below is just for convenience, we can get fkey in any room
 my($out,$err,$res) = cache_command2("curl -L http://chat.stackexchange.com/rooms/1","age=3600");
-
 unless ($out=~s%<input id="fkey" name="fkey" type="hidden" value="(.*?)" />%%s) {die "NO FKEY";}
-
 my($fkey) = $1;
 
-my($rids) = "r36=0&r49=0&r24938=0";
+# cruft the command
+my($cmd) = "curl -d 'fkey=$fkey&$rstr' http://chat.stackexchange.com/events";
+
+# cache during testing only
 
 # get recent events
-($out,$err,$res) = cache_command2("curl -d 'fkey=$fkey&$rids' http://chat.stackexchange.com/events", "age=3600");
+# TODO: this will eventually be a loop
 
-debug("OUT: $out");
+($out,$err,$res) = cache_command2($cmd,"age=3600");
+
+my(%hash) = %{JSON::from_json($out)};
+
+my(%msgs);
+
+for $i (keys %hash) {
+  for $j (keys %{$hash{$i}}) {
+    # the d and t values are probably important, but not to me at the moment
+    for $k (@{$hash{$i}{$j}}) {
+      # TODO: ignore non-1 event_type?
+      # TODO: this is just a temporary "prettyprint"
+#      my(%hash) = %{$k};
+      $msgs{$k->{id}} = join("|", $k->{room_name}, $k->{user_name}, $k->{time_stamp}, $k->{content});
+    }
+  }
+}
+
+debug(unfold(\%msgs));
+
+# debug(var_dump("hash",\%hash));
 
 die "TESTING";
 
