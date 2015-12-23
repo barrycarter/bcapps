ca4748506b17f1cdcba831b30731a8d865293ac6
Barry Carter <carter.barry@gmail.com>
2015-12-23 09:00:03 -0700

    Checkpoint (still not really working)

--- ASTRO/playground3.c
+++ ASTRO/playground3.c
@@ -8,11 +8,12 @@
 
 int main (int argc, char **argv) {
 
-  SpiceDouble lt, v[3];
+  // t=19037 is the closest second to the epoch where GMST is 0
+  SpiceDouble lt, v[3], t=19037;
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/test.tm");
-  spkezp_c(10,0,"IAU_EARTH","NONE",399,v,&lt);
+  spkezp_c(10,t,"IAU_EARTH","NONE",399,v,&lt);
   printf("IAU_EARTH: %f %f %f\n",v[0],v[1],v[2]);
-  spkezp_c(10,0,"J2000","NONE",399,v,&lt);
+  spkezp_c(10,t,"J2000","NONE",399,v,&lt);
   printf("J2000: %f %f %f\n",v[0],v[1],v[2]);
   return 0;
 }
