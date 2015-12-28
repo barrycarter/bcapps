e9c77b21895e5b9c05d88162435b9fa81edc48be
Barry Carter <carter.barry@gmail.com>
2015-12-27 18:56:57 -0700

    Checkpoint

--- ASTRO/playground5.c
+++ ASTRO/playground5.c
@@ -11,17 +11,40 @@ int main(void) {
 
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
 
-  double *results = bcriset(30*rpd_c(),0*rpd_c(), 0, 1448953200, 1454310000,
+  double stime = 1448953200, etime = 1454310000;
+  double lat=40., lon=-100., dl, noon, mindl, minnoon;
+
+  double *results = bcriset(lat*rpd_c(),lon*rpd_c(), 0, stime, etime,
 			    "Sun", -5/6.*rpd_c(), ">");
 
-  // this is weird because I don't know actual size of results?
-  for (int i=0; i<=100; i++) {
+  mindl = 0.;
+  minnoon = 0.;
+
+  // we intentionally ignore first result, expect it to be wrong
+  for (int i=2; i<=100; i++) {
+
+    // if we start seeing 0s, we are out of true answers
     if (results[2*i] < .001) {break;}
 
-    // this is: time of noon and length of day
-    printf("%f %f %f %f\n",30.,0.,
-	   (results[2*i]+results[2*i+1])/2, results[2*i+1]-results[2*i]);
+    // if the end result is too close to etime, result is inaccurate
+    if (abs(results[2*i+1]-etime)<1) {continue;}
+
+    // length of day and "noon"
+    dl = results[2*i+1]-results[2*i];
+    noon = (results[2*i+1]+results[2*i])/2;
+
+    // if day length is less than mindl (or mindl = 0), set mindl and
+    // minnoon to current
+
+    if (mindl < .01 || dl < mindl) {
+      mindl = dl;
+      minnoon = noon;
+    }
   }
+
+  // this is: minimal time of noon and length of day
+  printf("%f %f %f %f\n", lat, lon, minnoon, mindl);
+
 
   return 0;
 }
