fe71f95f384853e91004f38b8c37e664225becf8
Barry Carter <carter.barry@gmail.com>
2015-12-27 19:12:51 -0700

    Checkpoint save

--- ASTRO/playground5.c
+++ ASTRO/playground5.c
@@ -12,44 +12,48 @@ int main(void) {
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
 
   double stime = 1448953200, etime = 1454310000;
-  double lat=40., lon=100., dl, noon, mindl, minnoon;
+  double lat, lon, dl, noon, mindl, minnoon;
 
-  double *results = bcriset(lat*rpd_c(),lon*rpd_c(), 0, stime, etime,
-			    "Sun", -5/6.*rpd_c(), ">");
+  for (lat = 20.; lat<=70.; lat+=5.) {
+    for (lon = -180.; lon <= 180.; lon+=15.) {
 
-  mindl = 0.;
-  minnoon = 0.;
+      double *results = bcriset(lat*rpd_c(),lon*rpd_c(), 0, stime, etime,
+				"Sun", -5/6.*rpd_c(), ">");
 
-  // we intentionally ignore first result, expect it to be wrong
-  for (int i=2; i<=100; i++) {
+      mindl = 0.;
+      minnoon = 0.;
 
-    // if we start seeing 0s, we are out of true answers
-    if (results[2*i] < .001) {break;}
+      // we intentionally ignore first result, expect it to be wrong
+      for (int i=2; i<=100; i++) {
 
-    // if the end result is too close to etime, result is inaccurate
-    if (abs(results[2*i+1]-etime)<1) {continue;}
+	// if we start seeing 0s, we are out of true answers
+	if (results[2*i] < .001) {break;}
 
-    // length of day and "noon"
-    dl = results[2*i+1]-results[2*i];
-    noon = (results[2*i+1]+results[2*i])/2;
+	// if the end result is too close to etime, result is inaccurate
+	if (abs(results[2*i+1]-etime)<1) {continue;}
 
-    // if day length is less than mindl (or mindl = 0), set mindl and
-    // minnoon to current
+	// length of day and "noon"
+	dl = results[2*i+1]-results[2*i];
+	noon = (results[2*i+1]+results[2*i])/2;
 
-    if (mindl < .01 || dl < mindl) {
-      mindl = dl;
-      minnoon = noon;
-    }
-  }
+	// if day length is less than mindl (or mindl = 0), set mindl and
+	// minnoon to current
+
+	if (mindl < .01 || dl < mindl) {
+	  mindl = dl;
+	  minnoon = noon;
+	}
+      }
   
   // the start of December for this longitude (1448928000 = GMT
   // midnight Dec 1)
-  double sod = 1448928000-240.*lon;
+      double sod = 1448928000-240.*lon;
 
-  // this is: minimal time of noon and length of day
-  printf("%f %f %f %f %f\n", lat, lon, minnoon, mindl, 1+(minnoon-sod)/86400);
-
-
+      // this is: minimal time of noon and length of day
+      printf("%f %f %f %f %f\n", lat, lon, minnoon, mindl,
+	     1+(minnoon-sod)/86400);
+    }
+  }
   return 0;
 }
 
