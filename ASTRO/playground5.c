4b040769afbc8ffb404e597fec621b99f944cb74
Barry Carter <carter.barry@gmail.com>
2016-01-04 17:38:37 -0700

    Checkpoint

--- ASTRO/playground5.c
+++ ASTRO/playground5.c
@@ -10,33 +10,33 @@ int main (int argc, char **argv) {
 
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
 
-  // change error handling to see if we can do try/catch
+  // change error handling
   erract_c("SET", 0, "RETURN");
-
-
-  
-  spkezp_c(10,unix2et(-365.2425*86400*8),"ITRF93","CN+S",399,v,&lt);
-
-  if (failed_c()) {printf("You have failed\n");}
-
-  printf("Program continues\n");
-
-  
-
-
-  exit(0);
-
-
-  // this shouldn't work
-  printf("%f\n", unix2et(-86400*365.2425*7));
-  spkezp_c(10,-1167626622,"ITRF93","CN+S",399,v,&lt);
-  printf("%f %f %f\n",v[0],v[1],v[2]);
 
   // sun's z position
   void solarzed (SpiceDouble unixtime, SpiceDouble *value) {
     SpiceDouble v[3], lt;
-    // TODO: can't use ITRF93 here because it doesn't go back far enough
+
+    // NOTE: trying ITRF93 first and checking for error is
+    // inefficient, since ITRF93 only covers a few dozen years; more
+    // efficient would be finding the actual times ITRF93 is valid
+    // (which the filenames and comments clearly state, although
+    // earth_720101_070426.bpc actually goes back to 1963 or so), but
+    // I want to show how clever I am with error handling
+
+    // try ITRF93 first
     spkezp_c(10,unix2et(unixtime),"ITRF93","CN+S",399,v,&lt);
+
+    // I should be checking for a specific error, but this is close enough
+    if (failed_c()) {
+      // fallback on IAU_EARTH
+      spkezp_c(10,unix2et(unixtime),"IAU_EARTH","CN+S",399,v,&lt);
+      // and reset the error message
+      reset_c();
+      // this printf will let me use a better approach later
+      printf("NO ITRF93 FOR: %f %f\n",unix2et(unixtime),unixtime);
+    }
+
     *value = v[2];
   }
 
@@ -46,7 +46,9 @@ int main (int argc, char **argv) {
   }
 
   // find when 0
+  // TODO: find max and min for solstices
   wninsd_c(-86400*365.2425*7,1451631600+86400*365.2425*20,&cnfine);
+
   gfuds_c(solarzed,gfdecrx,"=",0,0,86400,10000,&cnfine,&result);
 
   int count = wncard_c( &result );
