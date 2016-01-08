f089419f1a31fb4332dc57edcaea4b25e31af217
Barry Carter <carter.barry@gmail.com>
2016-01-07 18:21:30 -0700

    Checkpoint

--- ASTRO/playground37.m
+++ ASTRO/playground37.m
@@ -3,6 +3,12 @@
 (* Mathematica takes forever to compute this, so computing it once and
 storing it *)
 
+(* START SKIP FROM HERE TO NEXT "SKIP" IF USING MX FILE BELOW:
+
+math -initfile /home/barrycarter/20160107/magpos.mx
+
+ *)
+
 pos[s_] := pos[s] = AstronomicalData[s, "PositionLightYears"];
 
 (* this just forces evaluation of above, doesn't actually do anything *)
@@ -23,11 +29,25 @@ Table[oc[p],{p, AstronomicalData["Exoplanet"]}];
 
 DumpSave["/home/barrycarter/20160107/magpos.mx", {mag,pos,stars,exos,oc}];
 
+(* END SKIP FROM HERE *)
 
 (* the distance between stars s1 and s2 *)
 
-dist[s1_,s2_] :=  Norm[AstronomicalData[s1, "PositionLightYears"] - 
- AstronomicalData[s2, "PositionLightYears"]];
+dist[s1_,s2_] :=  Norm[pos[s1]-pos[s2]];
+
+(* magnitude of s2 as viewed from s1 *)
+
+magXY[s1_,s2_] := mag[s2] - Log[10,32.6/dist[s1,s2]]*5;
+
+(* brightness of all stars (excluding primary?) from given exoplanet *)
+
+brightstars[p_] := Sort[Table[{s1, magXY[oc[p], s1]}, {s1,stars}], 
+ #1[[2]] < #2[[2]] &];
+
+test = brightstars[exos[[8]]];
+
+test2 = Sort[test, #1[[2]] < #2[[2]] &];
+
 
 (* convert absolute magnitude and distance in light years to apparent
 magnitude *)
