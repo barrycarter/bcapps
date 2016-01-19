f187c0597d31cfd740084be4c77929486a064bdb
Barry Carter <carter.barry@gmail.com>
2016-01-19 13:15:52 -0700

    Checkpoint

--- MATHEMATICA/bc-solve-mathematica-104178.m
+++ MATHEMATICA/bc-solve-mathematica-104178.m
@@ -1,13 +1,14 @@
 (* lookup nearest physical constants; highly inefficient, loops thru
 entire list *)
 
+(* /tmp/math.txt is the output of "bc-solve-mathematica-104178.pl nist-constants.txt" *)
+
+<< /tmp/math.txt
 nearestPhysicalConstant[x_] := Module[{},
  Return[
 Sort[Table[{i, MantissaExponent[x][[1]] - MantissaExponent[i[[2]]][[1]]},
  {i, physicalConstants}],  Abs[#1[[2]]] < Abs[#2[[2]]] &]]];
 
-
-test[x_] := 
-Sort[Table[{i, MantissaExponent[x][[1]] - MantissaExponent[i[[2]]][[1]]}, 
- {i, physicalConstants}]
+DumpSave["/home/barrycarter/BCGIT/MATHEMATICA/nearestPhysicalConstant.mx",
+{nearestPhysicalConstant, physicalConstants}];
 
