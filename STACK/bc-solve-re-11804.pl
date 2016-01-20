a5fb07fa5f4e3670ee12c28a0f4ea63289d8c0d9
Barry Carter <carter.barry@gmail.com>
2016-01-20 10:47:53 -0700

    Checkpoint

--- STACK/bc-solve-re-11804.pl
+++ STACK/bc-solve-re-11804.pl
@@ -3,5 +3,23 @@
 require "/usr/local/lib/bclib.pl";
 
 while (<>) {
-  debug($_);
+
+  chomp;
+  my(@data) = split(/\s+/, $_);
+  map(s/[h\s]//g, @data);
+  my($ck) = hex(pop(@data));
+#  debug("DATA", @data);
+
+  my(@test) = (32,16,8,2,4,1);
+
+  # note that this is NOT hex(string) because two digit pairs (base
+  # 256) are treated as a single number
+
+  $sum = 0;
+  for $i (0..$#test) {
+    $sum += $test[$i]*$data[$i];
+  }
+
+  debug("SUM: $sum vs $ck");
+
 }
