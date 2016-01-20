6147514885581acebdce306cfaa3570261d20c06
Barry Carter <carter.barry@gmail.com>
2016-01-20 09:05:55 -0700

    Not quite working

--- playground.java
+++ playground.java
@@ -8,60 +8,33 @@ public class playground {
     public static void main(String[] args) {
 
         int choice = 0;
-        double weight;
-        double earth = 9.81;
-        double moon = 1.622;
-        double mercury = 3.7;
-        double venus = 8.87;
-        double jupiter = 24.79;
+	// -1 to make sure its negative
+        double weight = -1.;
+	// mercury, venus, earth, moon, jupiter
+	double[] gravs = {0, 3.7/9.81, 8.87/9.81, 1.622/9.81, 24.79/9.81};
+	String[] names = {"", "Mercury", "Venus", "Moon", "Jupiter"};
 
         System.out.println("Welcome to Planetary Weight Calculator: ");
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your weight in pounds (lb): ");
-        weight = input.nextDouble();
-        while (weight <= 0) {
-            System.out.println("Invalid Input - Plase enter a value higher than 0 ");
-            System.out.print("Enter your weight in pounds (lb): ");
-            weight = input.nextDouble();
-        }
-        System.out.println("/////MENU/////");
-        System.out.println("1 - Moon");
-        System.out.println("2 - Mercury");
-        System.out.println("3 - Venus");
-        System.out.println("4 - Jupiter");
-        System.out.print("Pick a number 1-4: ");
-        choice = input.nextInt();
-        while (choice <= 0 || choice >= 5) {
-            System.out.println("/////MENU/////");
-            System.out.println("1 - Moon");
-            System.out.println("2 - Mercury");
-            System.out.println("3 - Venus");
-            System.out.println("4 - Jupiter");
-            System.out.print("Pick a number 1-4: ");
-            choice = input.nextInt();
-        }
-        switch (choice) {
-	case 1:
-	    System.out.println("You selected Moon, your weight on Moon is: ");
-	    System.out.println(weight * (moon / earth) + " Lbs");
-	    break;
-	case 2:
-	    System.out.println("You selected Mercury, your weight on Mercury is: ");
-	    System.out.println(weight * (mercury / earth) + " Lbs");
-	    break;
-	case 3:
-	    System.out.println("You selected Venus, your weight on Venus is: ");
-	    System.out.println(weight * (venus / earth) + " Lbs");
-	    break;
-	case 4:
-	    System.out.println("You selected Jupiter, your weight on Jupiter is: ");
-	    System.out.println(weight * (jupiter / earth) + " Lbs");
-	    break;
-        }
 
+	do {
+	    System.out.print("Enter your weight in pounds (lb): ");
+            weight = input.nextDouble();
+	    if (weight <= 0) {
+		System.out.println("Invalid Input - Plase enter a value higher than 0 ");
+	    }
+	} while (weight <= 0);
+
+	do {
+	    System.out.println("/////MENU/////");
+	    for (int i=0; i<=3; i++) {
+		System.out.format("%d %s\n", i, names[i]);
+	    }
+	    choice = input.nextInt();
+	} while (choice <= 0 || choice >=4);
+
+	System.out.format("You selected %s, your weight on %s is %f lbs\n", names[choice], names[choice], weight*gravs[choice]);
         System.out.println("Thank you for using the Planetary Weight Calculator");
         System.out.println("Hope you will use it again! ");
-
     }
-
 }
