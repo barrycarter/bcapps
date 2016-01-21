3297d93879e09d0e12c0c5e74aac5881013148a5
Barry Carter <carter.barry@gmail.com>
2016-01-21 10:08:15 -0700

    Checkpoint

--- STACK/bc-solve-stats-191220.pl
+++ STACK/bc-solve-stats-191220.pl
@@ -13,16 +13,27 @@ while (<>) {
   push(@x,$1); push(@y,$2);
 }
 
+# look at diffs
+
+for $i (1..$#x) {
+  my($xdiff) = $x[$i]-$x[$i-1];
+  my($ydiff) = $y[$i]-$y[$i-1];
+  my($dist2) = $xdiff**2+$ydiff**2;
+  print "$dist2\n";
+}
+
+die "TESTING";
+
 # this lets us look at a few points at a time
-@x = @x[0..200];
-@y = @y[0..200];
+@x = @x[0..50];
+@y = @y[0..50];
 
 my($minx,$maxx,$miny,$maxy) = (min(@x),max(@x),min(@y),max(@y));
 
 for $i (0..$#x) {
-  my($px) = ($x-$minx)/($maxx-$minx);
-  my($py) = ($y-$miny)/($maxy-$miny);
+  my($px) = ($x[$i]-$minx)/($maxx-$minx);
+  my($py) = ($y[$i]-$miny)/($maxy-$miny);
   my($qx) = $px*600;
   my($qy) = $py*600;
-  print "string 255,0,0,$qx,$qy,tiny,$pt\n";
+  print "string 255,0,0,$qx,$qy,tiny,$i\n";
 }
