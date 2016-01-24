056b5621ef852f9171b882c075a171fe01b4dddd
Barry Carter <carter.barry@gmail.com>
2016-01-24 12:31:37 -0700

    Checkpoint: found and fixed array miscount error

--- ASTRO/bc-solve-physics-197481.c
+++ ASTRO/bc-solve-physics-197481.c
@@ -4,89 +4,41 @@
 #include "SpiceUsr.h"
 #include "SpiceZfc.h"
 #define MAXWIN 200000
-#define SIXDEGREES 0.10471975511965977462
-
-// prototypes
-// gfq = function that returns scalar value of interest
-void gfq (SpiceDouble et, SpiceDouble * value );
-// gfdecrx = function that determines whether gfq is decreasing
-void gfdecrx (void (*udfuns) (SpiceDouble et, SpiceDouble *value ),
-		 SpiceDouble et, SpiceBoolean * isdecr );
-void findmins(SpiceDouble beg, SpiceDouble end);
+#include "/home/barrycarter/BCGIT/ASTRO/bclib.h"
 
 // Usage: $0 naif-id-of-planet naif-id-of-planet  naif-id-of-planet  ...
 
 static SpiceInt planets[6];
-static SpiceInt planetcount;
-
-// actually declaring entire functions here, not just prototype
-double et2jd(double d) {return 2451545.+d/86400.;}
-double jd2et(double d) {return 86400.*(d-2451545.);}
-double unix2et(double d) {return d-946684800.;}
-
-int main( int argc, char **argv ) {
-
-  SpiceInt i,j,count;
-  SPICEDOUBLE_CELL(result, 2*MAXWIN);
-  SPICEDOUBLE_CELL(cnfine,2);
-  SpiceDouble beg, end;
-
-  // fill the static planets array
-  for (i=1; i<argc; i++) {planets[i] = atoi(argv[i]);}
-  planetcount = argc-1;
-
-  // 1 second tolerance (serious overkill, but 1e-6 is default, worse!)
-  gfstol_c(1.);
-
-  furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
-
-  // DE431 limits
-  //  wninsd_c (-479695089600.+86400*468, 479386728000., &cnfine);
-
-  // 1970 to 2038 (all "Unix time") for testing
-  wninsd_c(unix2et(0),unix2et(2147483647),&cnfine);
-
-  // find under 6 degrees...
-  gfuds_c(gfq,gfdecrx,"<",SIXDEGREES,0.,86400.,MAXWIN,&cnfine,&result);
-  count = wncard_c(&result); 
-
-  for (i=0; i<count; i++) {
-    wnfetd_c(&result,i,&beg,&end);
-    printf("6deg %f %f\n",et2jd(beg),et2jd(end));
-
-    // findmins(beg,end);
-  }
-  return 0;
-}
+static int planetcount;
 
 void gfq ( SpiceDouble et, SpiceDouble *value ) {
 
+  printf("GFQ CALLED: %f, pc is: %d\n",et,planetcount);
+
   // max separation between all non-0 planets in planets
-  // TODO: get min solar separation (not necessarily here)
-  SpiceInt i,j;
-  SpiceDouble sep, lt, max;
-  SpiceDouble position[planetcount][3];
+  int i,j;
+  SpiceDouble sep, lt, max=0.;
+  SpiceDouble position[planetcount+1][3];
   // TODO: why do I need a temp var here?
   SpiceDouble temp[3];
 
-  // compute the Earth positions first for efficiency
+  // compute positions
   for (i=1; i<=planetcount; i++) {
-    spkezp_c(planets[i], et, "J2000", "LT+S", 399, temp, &lt);
+    spkezp_c(planets[i], et, "J2000", "LT+S", 10, temp, &lt);
     // copy from temp var (argh!)
     for (j=0; j<=2; j++) {position[i][j] = temp[j];}
   }
 
+
   // and now the angle diffs (keep only min)
   for (i=1; i<=planetcount; i++) {
     for (j=i+1; j<=planetcount; j++) {
-      //      printf("I3: %d %f -> %f %f %f %f\n",planets[i],et2jd(et),position[i][0],position[i][1],position[i][2],position[i][3]);
-      //      printf("I3: %d %f -> %f %f %f %f\n",planets[i],et2jd(et),test[0],test[1],test[2],test[3]);
+      printf("J: %d\n",j);
       sep = vsep_c(position[i],position[j]);
       if (sep>max) {max=sep;}
     }
   }
 
-  //  if (max<SIXDEGREES){printf("ABOUT TO RETURN: %f -> %f\n",et2jd(et),max);}
   *value=max;
   return;
 }
@@ -123,4 +75,39 @@ void findmins(SpiceDouble beg, SpiceDouble end) {
   return;
 }
 
+int main( int argc, char **argv ) {
+
+  SpiceInt i,count;
+  SPICEDOUBLE_CELL(result, 2*MAXWIN);
+  SPICEDOUBLE_CELL(cnfine,2);
+  SpiceDouble beg, end;
+
+  // fill the static planets array
+  for (i=1; i<argc; i++) {planets[i] = atoi(argv[i]);}
+  planetcount = argc-1;
+
+  printf("PLANETS: %d %d %d %d\n",planets[0],planets[1],planets[2],planetcount);
+
+  // 1 second tolerance (serious overkill, but 1e-6 is default, worse!)
+  gfstol_c(1.);
+
+  furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
+
+  // DE431 limits
+  //  wninsd_c (-479695089600.+86400*468, 479386728000., &cnfine);
+
+  // 1970 to 2038 (all "Unix time") for testing
+  wninsd_c(unix2et(0),unix2et(2147483647),&cnfine);
+
+  // find under 6 degrees...
+  gfuds_c(gfq,gfdecrx,"<",6.*rpd_c(),0.,86400.,MAXWIN,&cnfine,&result);
+  count = wncard_c(&result); 
+
+  for (i=0; i<count; i++) {
+    wnfetd_c(&result,i,&beg,&end);
+    printf("6deg %f %f\n",et2jd(beg),et2jd(end));
+    findmins(beg,end);
+  }
+  return 0;
+}
 
