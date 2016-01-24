e25beb6cc03a987a8d2b01e8aa6e1072e138d334
Barry Carter <carter.barry@gmail.com>
2016-01-24 12:05:27 -0700

    Checkpoint

---
+++ MATHEMATICA/bc-solve-physics-197481.m
@@ -0,0 +1,17 @@
+(*
+
+This is an interesting question. I've previously computed planetary
+conjunctions *as viewed from Earth*:
+https://astronomy.stackexchange.com/questions/11141 but not as viewed
+from the Sun, or from an observer looking down at the solar system and
+looking for straightish lines.
+
+It turns out this is much easier to compute, since the planets follow
+nearly circular orbits around the Sun.
+
+TODO: note significantly more accurate calculations with CSPICE
+
+*)
+
+AstronomicalData["Jupiter", "Properties"]
+
