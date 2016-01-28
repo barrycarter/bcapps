a89d9e4fdf704158baf26da9b03b5db735c9b649
Barry Carter <carter.barry@gmail.com>
2016-01-28 09:26:57 -0700

    Checkpoint

--- MATHEMATICA/bc-solve-physics-232097.m
+++ MATHEMATICA/bc-solve-physics-232097.m
@@ -6,28 +6,32 @@ http://physics.stackexchange.com/questions/14700/the-time-that-2-masses-will-col
 
 *)
 
-DSolve[{
- x1[0] == 0, x2[0] == d, x1'[0] == 0, x2'[0] == 0,
- x1''[t] == 1/(x1[t]-x2[t])^2,
- x2''[t] == -1/(x2[t]-x1[t])^2
-}, {x1[t],x2[t]}, t]
+(* observer in center, 2*d = distance between objects *)
 
-DSolve[{
- x1''[t] == 1/(x1[t]-x2[t])^2,
- x2''[t] == -1/(x1[t]-x2[t])^2
-}, {x1[t],x2[t]}, t]
-
-sol[g_, d_] :=
+sol[g_, d_] := sol[g,d] = 
 NDSolve[{
- x1[0] == 0, x2[0] == d, x1'[0] == 0, x2'[0] == 0,
+ x1[0] == -d, x2[0] == d, x1'[0] == 0, x2'[0] == 0,
  x1''[t] == g/(x1[t]-x2[t])^2,
  x2''[t] == -g/(x2[t]-x1[t])^2
-}, {x1,x2}, {t,0,50}]
+}, {x1,x2}, {t,0,10^6}]
 
-test[t_] = sol[1,10][[1,1,2]][t]
+coll[g_,d_] := FindRoot[sol[g,d][[1,1,2]][t]==0, {t,0,4*d}][[1,2]]
 
-DSolve[{x[0] == d, x'[0] == 0, x''[t] == x[t]^-2}, x, t]
-DSolve[{x''[t] == x[t]^-2}, x, t]
+(* using Mathematica's "stiffness limit" instead *)
+
+col2[g_,d_] := sol[g,d][[1,1,2,1,1,2]]
+
+Plot[col2[1,d],{d,0,20}]
+
+tab = Table[{d,col2[1,d]},{d,1,100}]
+
+FindFit[tab, x^a, {a}, x]
+
+Plot[{x^1.68187, col2[1,x]},{x,0,100}]
+
+FindFit[Take[tab,50], x^a, {a}, x]
+
+tab2 = Table[{d,col2[1,d]},{d,1,500}]
 
 (*
 
@@ -51,6 +55,20 @@ ListPlot[{
 {1,0.785398}, {2,2.22144}, {3,4.08105}, {4,6.28318}, {8,17.7715}, {16,20}
 }]
 
+(* even simpler case *)
 
+eqs = {x[0] == d, x'[0] == 0, x''[t] == x[t]^-2}
+
+(* mathematica loses solutions w/ boundary conditions *)
+
+eqs = {x''[t] == x[t]^-2}
+s = DSolve[eqs,x,t]
+form = s[[1]]
+
+form[[1,1]] == form[[2,1]]
+
+(* WRONG: convinced C[2] is 0 *)
+
+s2 = form[[1,1]] == form[[2,1]] /. C[2] -> 0
 
 
