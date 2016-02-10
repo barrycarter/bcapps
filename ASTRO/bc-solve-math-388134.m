c36423ad3c072cfc6449b9391f6acb8c07dd8050
Barry Carter <carter.barry@gmail.com>
2016-02-10 11:26:37 -0700

    Checkpoint

--- ASTRO/bc-solve-math-388134.m
+++ ASTRO/bc-solve-math-388134.m
@@ -12,13 +12,21 @@ ang = 45*Degree;
 
 f = Sqrt[a^2-b^2];
 
-RegionPlot[Norm[{x,y}-{-f,0}] + Norm[{x,y}-{f,0}] <= 4, {x,0,2}, {y,0,1}]
+pp = RegionPlot[Norm[{x,y}-{-f,0}]+Norm[{x,y}-{f,0}] <= 4 && y/x<b/a*Tan[ang],
+ {x,0,2}, {y,0,1}, ColorFunction -> Function[{x,y}, RGBColor[{1,0,0,0.1}]]]
+
 
 RegionPlot[{x<a*Cos[ang] && y<b*Sin[ang]}, {x,0,2},{y,0,1}]
 
 pp = RegionPlot[{y/x < b/a*Tan[ang]}, {x,0,a*Cos[ang]},{y,0,b*Sin[ang]}]
 
+Graphics[Disk[{0,0}, {a,b}, {0,ang}]]
+
+
 g1 = Graphics[{
+ RGBColor[{1,0,0,0.1}],
+ Disk[{0,0}, {a,b}, {0,ang}],
+ RGBColor[{0,0,0}],
  Circle[{0,0}, {a,b}],
  Line[{{-a,0}, {a,0}}],
  Line[{{0,-b}, {0,b}}],
