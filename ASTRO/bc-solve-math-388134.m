d91036fec36a67eed4a2c555b391d3519dbaaa34
Barry Carter <carter.barry@gmail.com>
2016-02-10 17:19:14 -0700

    About to change ellipse, it's excessive at the moement, even for demonstration

--- ASTRO/bc-solve-math-388134.m
+++ ASTRO/bc-solve-math-388134.m
@@ -12,28 +12,47 @@ ang = 45*Degree;
 
 f = Sqrt[a^2-b^2];
 
-pp = RegionPlot[Norm[{x,y}-{-f,0}]+Norm[{x,y}-{f,0}] <= 4 && y/x<b/a*Tan[ang],
- {x,0,2}, {y,0,1}, ColorFunction -> Function[{x,y}, RGBColor[{1,0,0,0.1}]]]
-
-
-RegionPlot[{x<a*Cos[ang] && y<b*Sin[ang]}, {x,0,2},{y,0,1}]
-
-pp = RegionPlot[{y/x < b/a*Tan[ang]}, {x,0,a*Cos[ang]},{y,0,b*Sin[ang]}]
-
-Graphics[Disk[{0,0}, {a,b}, {0,ang}]]
-
-
 g1 = Graphics[{
  RGBColor[{1,0,0,0.1}],
  Disk[{0,0}, {a,b}, {0,ang}],
- RGBColor[{0,0,0}],
+ RGBColor[{0,0,0, 1}],
+ Text[Style["\[Theta]", Large], {a/7.5,b/15}],
+ Text[Style["\[Theta]ab", FontSize->50], {1,0.25}],
+ Text[Style["a", FontSize->50], {1,-0.07}],
+ Text[Style["b", FontSize->50], {-0.07,0.5}],
  Circle[{0,0}, {a,b}],
- Line[{{-a,0}, {a,0}}],
- Line[{{0,-b}, {0,b}}],
+ Circle[{0,0}, a/10., {0,ArcTan[a*Cos[ang], b*Sin[ang]]}],
+ Line[{{-a,0}, {0,0}}],
+ Line[{{0,-b}, {0,0}}],
+ Arrowheads[{-.02,.02}],
+ Arrow[{{0,0}, {a,0}}],
+ Arrow[{{0,0}, {0,b}}],
  RGBColor[{1,0,0}],
- Line[{{0,0}, {a*Cos[ang], b*Sin[ang]}}]
+ Line[{{0,0}, {a*Cos[ang], b*Sin[ang]}}],
 }]
 
-Show[g1,pp]
+Show[g1]
+showit
+
+g2 = Graphics[{
+ RGBColor[{0,0,1,0.1}],
+ Polygon[{{0,0}, {Sqrt[a^2-b^2], 0}, {a*Cos[ang], b*Sin[ang]}}],
+ RGBColor[{1,0,0,0.1}],
+ Disk[{0,0}, {a,b}, {0,ang}],
+ RGBColor[{0,0,0, 1}],
+ Text[Style["\[Theta]", Large], {a/7.5,b/15}],
+ Text[Style["\[Theta]ab", FontSize->50], {1,0.25}],
+ Text[Style["a", FontSize->50], {1,-0.07}],
+ Text[Style["b", FontSize->50], {-0.07,0.5}],
+ Circle[{0,0}, {a,b}],
+ Circle[{0,0}, a/10., {0,ArcTan[a*Cos[ang], b*Sin[ang]]}],
+ Line[{{-a,0}, {0,0}}],
+ Line[{{0,-b}, {0,0}}],
+ Arrowheads[{-.02,.02}],
+ Arrow[{{0,0}, {a,0}}],
+ Arrow[{{0,0}, {0,b}}],
+ RGBColor[{1,0,0}],
+ Line[{{0,0}, {a*Cos[ang], b*Sin[ang]}}],
+}]
 
 
