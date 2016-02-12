ebe6f950b58431eaf26b06dd6c73a598d371cdb9
Barry Carter <carter.barry@gmail.com>
2016-02-12 14:59:28 -0700

    Checkpoint

--- STACK/bc-solve-1650965.m
+++ STACK/bc-solve-1650965.m
@@ -1,25 +1,21 @@
 (*
 
-Induction: give each new node the lowest number possible.
-
-In this case, a node with number 'n' must have a given subset of nodes:
-
-If a node is numbered 4, it must have children with numbers 1, 2, 3.
-
-This is subset 7 (2^2 + 2 + 1)
-
-f[4] = {7}
-
-If numbered 3, childset must be {1,2} or {1,2,4}
-
-f[3] = {3, 11}
-
-IF 2, {{1},{1,3},{1,4},{1,3,4}}
-
-f[2] = {1, 5, 9, 13}
-
-If 1, {{2}, {3}, {4}, {2,3}, {2,4}, {3,4}, {2,3,4}}
-
-f[1] = {2,4,8,6,10,12,14}
+When adding nodes, give each new node the lowest possible color
+number. A node's color is determined entirely by the nodes it is
+connecting to, as below (binary)
 
 *)
+
+(* no subnodes, I get 1 *)
+
+f[0] = 1;
+
+(* 0001, I get 2 *)
+
+f[1] = 2;
+
+(* 0010, I get 1 *)
+
+f[2] = 1;
+
+(* 
