5cbf366181268c55e756c01ca750d64dbd61d1f1
Barry Carter <carter.barry@gmail.com>
2016-02-16 07:53:54 -0700

    Checkpoint

--- STACK/bc-solve-codereview-119661.php
+++ STACK/bc-solve-codereview-119661.php
@@ -1,37 +1,37 @@
 <?
 
-function calculateWeightedAverage($ratings)
+// It sorts them and then applies a weight to the upper third (count /
+// 3) and lower third items in the array, giving majority weight to
+// values in the middle. For instance, if the votes were [1, 4, 7],
+// the weight of the 1 and 7 would be decreased before calculating the
+// average. If it were [2, 2, 3, 4, 4, 7] the weights of the 2, 2,
+// second 4, and 7 would be lowered. - user1960364 Feb 11 at 22:14
+
+protected function calculateWeightedAverage($ratings)
 {
+  $sum = 0;
+  $rating = 0;
+
   $values = array_values($ratings);
   sort($values);
 
   $count = count($values);
-  $weights = array_fill(0, $count, 1);
-
   $out = (int) ($count / 3);
   $out2 = pow($out + 1, 2);
 
-  $max = $count - 1;
-  $min = 0;
-
-  echo "VALUES: $count $out $out2 $max $min\n";
-
-  for ($i = 0; $i < $out; $i++) {
-    $j = $i + 1;
-    $weights[$min + $i] = $j * $j / $out2;
-    $weights[$max - $i] = $j * $j / $out2;
-    echo "SETTING: $i $j weights of $min + $i to $j * $j / $out2\n";
+  for ($i=0; $i<$count; $i++) {
+    if ($i < $out) {
+      $weights[$i] = pow(($i+1),2)/$out2;
+    } elseif ($i > $count-$out-1) {
+      $weights[$i] = pow($count-$i,2)/$out2;
+    } else {
+      $weights[$i] = 1;
+    }
+    $sum += $weights[$i];
   }
 
-  $sum = array_sum($weights);
-
   for ($i = 0; $i < $count; $i++) {
-    $weights[$i] /= $sum;
-  }
-
-  $rating = 0;
-  for ($i = 0; $i < $count; $i++) {
-    $rating += $values[$i] * $weights[$i];
+    $rating += $values[$i] * $weights[$i]/$sum;
   }
 
   return $rating;
