c0057f772d6895383f91e8270d3031719c3686a6
Barry Carter <carter.barry@gmail.com>
2016-02-18 13:52:43 -0700

    Checkpoint

--- MATHEMATICA/bc-planar.m
+++ MATHEMATICA/bc-planar.m
@@ -9,12 +9,13 @@ as raw points
 f[x_,y_] = 3*x+2*y+15;
 
 p0 = Plot3D[f[x,y], {x,-5,5}, {y,-5,5}, Mesh -> None,
- ColorFunction -> Function[{x,y,z}, Hue[1,0.5,1]]]
+ ColorFunction -> Function[{x,y,z}, Hue[1,0.5,1,0.2]]]
 
 g0 = Graphics3D[{
 PointSize[.02],
-Point[{3,3,f[3,3]}]
+Point[{-2,-2,f[-2,-2]}],
+Arrow[Tube[{{-2,-2,f[-2,-2]}, {-2,-2,25}},.05]]
 }]
 
-Show[{p0,g0}, Axes -> False, Boxed -> False]
+Show[{p0,g0}, Boxed -> False, PlotRange -> {{-5,5},{-5,5},{-10,55}}]
 showit
