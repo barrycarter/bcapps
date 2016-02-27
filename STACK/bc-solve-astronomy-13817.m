4cc8946e21bf07ac65f6330a51444e7f37afc198
Barry Carter <carter.barry@gmail.com>
2016-02-27 16:15:26 -0700

    Checkpoint

--- STACK/bc-solve-astronomy-13817.m
+++ STACK/bc-solve-astronomy-13817.m
@@ -7,7 +7,34 @@ This doesn't fully answer your question.
 As you accelerate away from star 1 at 10m/s^2, Newtonian mechanics
 would give your velocity at time t as 10*t.
 
+travels distance u in 1 second
+
+your distance u = u/factor[v], factor[v], or u + v - uv^2
+
+accel = 10/300/10^6
+
+Solve[Tanh[accel*t] == .6, t]
+
+I say: .5 light seconds in 1 second
+
+converted: 0.433013 light seconds in 1.1547s or 
 
 
-factor[v_] = (1-v^2)^-1
+factor[v_] = (1-v^2)^(-1/2)
+
+Plot[1-v^2,{v,0,1}]
+
+DSolve[{v'[t] == u*(1-v[t]^2),v[0]==0},v[t],t]
+
+u = 10/300000000
+
+Plot[Tanh[t*u]*300000000,{t,0,30000000},PlotRange->All]
+
+f[u_] = u + u*(1-u^2)
+
+RSolve[{
+ a[n+1] == a[n] + a[n]*(1-a[n]^2),
+ a[0] == 2
+},
+a[n],n]
 
