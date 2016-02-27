1aa9fe87793aafccd4d3702ec6fd940d0a62350c
Barry Carter <carter.barry@gmail.com>
2016-02-27 08:34:44 -0700

    Still wrong

--- MATHEMATICA/bc-relativity.m
+++ MATHEMATICA/bc-relativity.m
@@ -2,15 +2,15 @@
 
 Simple relativity:
 
-A shoots light at B, 1 light second away moving at .9c
+A shoots light at B, 0.9 light second away moving at .9c
 
-A sees light reach B at 10s, 10ls away
+A sees light reach B at 9s, 9ls away
 
-B sees light at 2s, 1.9ls away
+B sees light t seconds later at distance 0, with ???
 
 Thus, A to B transform is:
 
-{10, 10} -> {1.9, 2}
+{9, 9} -> {0, t}
 
 *)
 
@@ -21,3 +21,12 @@ Sqrt[1-v^2]
 (* time dilation *)
 
 1/Sqrt[1-v^2]
+
+(* using pqrs to avoid conflicting vars *)
+
+m = {{p,q},{r,s}}
+
+Solve[{
+ m.{9,9} == {0,t}, 
+ m.m == {{-1,0},{0,1}}
+}, {p,q,r,s}]
