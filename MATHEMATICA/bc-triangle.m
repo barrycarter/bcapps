c5035ee9c4d9a27863f25926279c9821e33650c8
Barry Carter <carter.barry@gmail.com>
2016-02-28 06:57:47 -0700

    Checkpoint

--- MATHEMATICA/bc-triangle.m
+++ MATHEMATICA/bc-triangle.m
@@ -1,11 +1,22 @@
 (*
 
-Yet another attempt to find triangle formulas, this time using
-psuedo objects (a,b,c are the complex vertices of our triangle):
+Yet another attempt to find triangle formulas, this time using psuedo
+objects (all work is in complex plane):
 
-line[z1,z2]: the line (segment) from complex z1 to z2
+triangle[{z1,z2,z3}]: the triangle with vertices z1, z2, z3
+line[{z1,z2}]: the line (segment) from z1 to z2
 
 *)
+
+angle[line[{z1_,z2_}],line[{z3_,z4_}]] = Arg[z2-z1]-Arg[z4-z3]
+
+lines[triangle[{z1_,z2_,z3_}]] = {line[{z2,z3}], line[{z1,z3}], line[{z1,z2}]};
+
+
+
+{Arg[z2-z1]-Arg[z3-z1], 
+Arg[z1-z2]-Arg[z3-z2],  Arg[z1-z3]-Arg[z2-z3]};
+
 
 
 
