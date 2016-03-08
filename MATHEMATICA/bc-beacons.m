0529b2b7d62442e43cbe9093d6cd7147bcb62edb
Barry Carter <carter.barry@gmail.com>
2016-03-08 12:26:30 -0700

    Checkpoint

--- MATHEMATICA/bc-beacons.m
+++ MATHEMATICA/bc-beacons.m
@@ -2,7 +2,11 @@
 
 TODO: consider publishing, but currently just a playground
 
-we assume addition formula and all speeds as fraction of light
+we assume addition formula, dilation formula and all speeds as
+fraction of light
+
+we assume increase of dv every one time unit, but don't specify time
+unit for now
 
 *)
 
@@ -12,7 +16,7 @@ dilationFactor[v_] = Sqrt[1-v^2]
 
 addVelocity[dv,dv]
 
-conds = {dv>0, dt>0, n>0, dv<1, Element[n, Integers]}
+conds = {dv>0, n>0, dv<1, Element[n, Integers]}
 
 (* velocity of nth beacon as measured from beacon 0 *)
 
@@ -21,14 +25,25 @@ FullSimplify[v[n] /.
 RSolve[{v[0] == 0, v[n] == (dv+v[n-1])/(1+dv*v[n-1])}, v[n], n][[1]],
 conds]
 
-(* distance of nth beacon as measured from beacon 0 *)
-
-distance[n_] = FullSimplify[Sum[velocity[i]*dt,{i,0,n}], conds]
-
 (* time between nth and n+1st beacon drop based on time dilation, from
 beacon 0 *)
 
-timeBetween[n_] = FullSimplify[dt/Sqrt[1-velocity[n]^2], conds]
+timeBetween[n_] = FullSimplify[1/Sqrt[1-velocity[n]^2], conds]
+
+(* distance the nth beacon travels before the next beacon is dropped *)
+
+distanceTraveled[n_] = FullSimplify[timeBetween[n]*velocity[n],conds]
+
+(* distance of nth beacon as measured from beacon 0; could not get this!!!
+
+distance[n_] = FullSimplify[Sum[distanceTraveled[i],{i,0,n-1}], conds]
+
+*)
+
+(* time OF the nth drop *)
+
+timeOf[n_] = FullSimplify[Sum[timeBetween[i],{i,0,n-1}],conds]
+
 
 
 
