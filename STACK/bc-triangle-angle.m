01610d36552a8dcb32cfd833cc50f37b0733a4c0
Barry Carter <carter.barry@gmail.com>
2016-03-10 07:26:48 -0700

    Checkpoint

--- STACK/bc-triangle-angle.m
+++ STACK/bc-triangle-angle.m
@@ -6,24 +6,79 @@ http://math.stackexchange.com/questions/1688586/finding-an-angle-in-a-triangle-w
 
 Here's a purely analytical approach.
 
-Use the triangle to define a complex number grid such that N is at the
-origin (0 + 0I) and A is at positive real number `an`
+TODO: verbiage
 
-We know B's distance from N (call it `bn`), we have:
+N -> origin
 
-Norm[b] == bn
+A -> {an,0}
 
-TODO: verbosity
+B -> {bx,by} (unknown, but norm is known)
 
-Norm[s] == ns
-Norm[s-an] == as
-Norm[s-bn] == bs
+S -> {sx,sy} (unknown, but three norms known)
+
+lens: {ns, as, bs} and {an, bn} (note diagram gives sn, sa, sb)
+
+conds = {an > 0, bn > 0, ns > 0, as > 0, bs > 0, sy > 0, 
+ Element[{sx,bx,by}, Reals]}
+
+TODO: disclaim sy>0
+
+sol = FullSimplify[Solve[{sx^2 + sy^2 == ns^2, (sx-an)^2 + sy^2 == as^2}, 
+ {sx,sy}, Reals], conds]
+
+sxsol = sol[[1,1,2,1]]
+
+sysol = Sqrt[ns^2 - (an^2 - as^2 + ns^2)^2/(4*an^2)]
+
+(* NOTE: below is actually plusminus)
+
+sy = Sqrt[ns^2 - (an^2 - as^2 + ns^2)^2/(4*an^2)] 
+
+sol2 = FullSimplify[
+Solve[{bx^2 + by^2 == bn^2, (bx-sx)^2 + (by-sy)^2 == bs^2}, {bx,by}],
+conds]
+
+(* using negative sy *)
+
+sol3 = FullSimplify[
+Solve[{bx^2 + by^2 == bn^2, (bx-sx)^2 + (by+sy)^2 == bs^2}, {bx,by}],
+conds]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Solve[{
- Norm[s] == ns
- Norm[s-an] == as
- Norm[s-bn] == bs
-}, s, Complexes]
+ sx^2 + sy^2 == ns^2,
+ (sx-an)^2 + sy^2 == as^2,
+ (sx-bx)^2 + (sy-by)^2 == bs^2,
+ bx^2 + by^2 == bn^2
+}, {sx,sy,bx,by}, Reals]
+
+
+
+Solve[{
+ Norm[{sx,sy}]^2 == ns^2,
+ Norm[{sx,sy} - {an,0}]^2 == as^2,
+ Norm[{sx,sy} - {bx,by}]^2 == bs^2,
+ Norm[{bx,by}]^2 == bn^2
+}, {sx,sy,bx,by}, Reals]
+
+
+
 
 
 
