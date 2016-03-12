de6b6d2f4e0700337fe80ed002d67955a5692aca
Barry Carter <carter.barry@gmail.com>
2016-03-12 09:39:23 -0700

    Checkpoint: easier than I thought!

--- STACK/bc-accel.m
+++ STACK/bc-accel.m
@@ -1,10 +1,96 @@
+(*
+
+Subject: Error in deriving relativity velocity addition formula
+
+$D$ is traveling at .995c with respect to $C$ who is traveling at
+.995c with respect to $B$ all in the same direction. We want to
+compute $D$'s velocity as observed from $B$. Note that the Lorentz
+contraction/time dilation factor for .995c is $\sqrt{1-0.995^2}$ which
+I will approximate as 1/10. Now:
+
+  - $C$ announces "my clock reads 10 seconds, and $D$ is .995*10 or
+  9.95 light seconds ahead of me".
+
+  - $B$ converts this reading to his own frame of reference:
+
+    - 10 seconds of $C$ time is 100 seconds of my time.
+
+    - 9.95 light seconds of $C$ distance is 0.995 light seconds of my distance.
+
+    - Thus, the distance between $C$ and $D$ is 0.995 light seconds
+    when my clock reads 100 seconds.
+
+    - Since I know $C$ is 99.5 light seconds away at 100 seconds, and
+    I know (from above) that $D$ is .995 light seconds further out.
+
+    - Thus $D$ is 99.5+.995 = 100.495 light seconds away from me at
+    100 seconds.
+
+    - However, this would mean $D$ is traveling faster than light in
+    my reference frame (100.495 light seconds in 100 seconds =
+    1.00495c), which is impossible.
+
+What have I done wrong?
+
+I know the correct formula for relativistic velocity addition (which
+would give $D$ a speed of 0.999987c), but just want to know why I
+didn't get that answer.
+
+I also realize there are several similar questions, but I don't think
+there's one that exactly addresses this (mis)-derivation.
+
+END OF Q HERE
+
+I am observing something at v0*t and its observing something at u0*t,
+what is my observed position for it?
+
+thing: at time t, I observer position u0*t
+
+me: at time t/Sqrt[1-v0^2], I observe u0*t*Sqrt[1-v0^2]
+
+t*u0*(1-v0^2) = where I see it?
+
+example:
+
+Obs: at 5 seconds, it's at 5*3 = 15 units away
+
+Me: at 5/Sqrt[1-v0^2] seconds, its 15*Sqrt[1-v0^2] away
+
+so 3*(1-v0^2) is the velo for me (assuming I was in obs frame)
+
+at time = 10 seconds, I report object is 9 light seconds away
+
+me: 22.9416 seconds, distance of object from you is 3.92 light seconds and obs is 20.6474 light seconds, so
+
+22.9416 seconds -> 24.5674 light seconds -> 
+
+other way:
+
+10 seconds of my time: B is 9 light seconds away and 4.3589 have
+elapsed on his clock; he thus sees C 3.923 light seconds away
+
+for me thats 1.71 light seconds
+
+
+
+
+
+
+
+
+
+
+*)
+
+
+
 (* formulas start here *)
+
+BELOW THIS IS WRONG; MUST ACCOUNT FOR TIME DIFF TOO
 
 stationReladist[a_,s0_,v0_,t_]= 
  s0+t*v0+((-1 + Sqrt[1 + a^2*t^2])*Sqrt[1 - v0^2])/a
 stationNewtdist[a_,s0_,v0_,t_] = s0 + v0*t + a*t^2/2
-
-BELOW THIS IS WRONG; MUST ACCOUNT FOR TIME DIFF TOO
 
 g = 98/10/299792458;
 y2s = 31556952;
---
+++ STACK/bc-subsolar.m
@@ -0,0 +1,43 @@
+(*
+
+Let's see if we can find an **approximate** formula for where on Earth
+the Sun is overhead (ie, the "subsolar" point).
+
+Let $t$ be the number of days since the last spring equinox, $y$ be
+the number of days in a year (about 365.2425 days), and $\epsilon$ be
+the obliquity of the ecliptic (about 23.44 degrees). We will subsitute
+in numbers for $y$ and and $\epsilon$ later.
+
+The declination of the sun follows (roughly) a sine wave over the
+year, so we can estimate the solar declination as:
+
+$\epsilon  \sin \left(\frac{2 \pi  t}{y}\right)$
+
+This is also the latitude where the sun will be overhead.
+
+The sun's right ascension increases linearally (very roughly
+speaking), and we can estimate it in radians as:
+
+$2 \pi  t$
+
+Since the Earth rotates, the longitude of the subsolar point changes
+with time: it completes a 360 degree (2 pi radians) circle
+approximately every 24 hours.
+
+Since we're approximating, let's assume the spring equinox occurred at
+exactly noon UTC (which introduces an error of up to 12 hours).
+
+
+
+
+
+TODO: ask someone to improve
+
+
+
+
+
+
+Since we're
+approximating, let's also assume the spring equinox occurred exactly
+at Greenwich noon.
