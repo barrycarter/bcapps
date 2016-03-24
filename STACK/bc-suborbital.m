59cf334833f9a87d55ddb6699fdff1a062739255
Barry Carter <carter.barry@gmail.com>
2016-03-24 10:15:38 -0600

    Checkpoint

--- STACK/bc-suborbital.m
+++ STACK/bc-suborbital.m
@@ -2,6 +2,26 @@
 
 http://space.stackexchange.com/questions/14537/how-will-a-suborbital-flight-country-to-country-work
 
+
+TODO: when TeXing up, consider replacing lat/lon 
+lat = Subscript[s,"lat"]
+lon = Subscript[s,"lon"]
+
+On a rotating spherical Earth with radius $r$, the Cartesian
+coordinates of a location with latitude $s_{\text{lat}}$ and longitude
+$s_{\text{lon}}$ a time $t$ are:
+
+s[t_,lat_,lon_] = {r*Cos[lat]*Cos[(t-lon)/2/Pi], r*Cos[lat]*Sin[(t-lon)/2/Pi], 
+ r*Sin[lat]}
+
+
+
+
+
+
+
+TODO: disclaimers, non elliptical, 0 elevation, not an answer, will crash, no air ressit (prob important because khan? line mentioned)
+
 *)
 
 g = Graphics3D[{
@@ -13,8 +33,14 @@ g = Graphics3D[{
  Point[{0,1,0}],
 }];
 
-
-
 Show[g, Boxed -> False]
 showit
+
+r = Sqrt[x[t]^2+y[t]^2+z[t]^2];
+
+DSolve[{
+ x''[t] == -g*((r^2*x[t])/(x[t]^2 + y[t]^2 + z[t]^2)^(3/2)),
+ y''[t] == -g*((r^2*y[t])/(x[t]^2 + y[t]^2 + z[t]^2)^(3/2)),
+ z''[t] == -g*((r^2*z[t])/(x[t]^2 + y[t]^2 + z[t]^2)^(3/2))
+}, {x[t],y[t],z[t]}, t]
 
