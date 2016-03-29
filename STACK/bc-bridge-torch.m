3b7a9d95e5e9ec9c14e6e500abb306ee1469b902
Barry Carter <carter.barry@gmail.com>
2016-03-29 08:59:36 -0600

    Checkpoint

--- STACK/bc-bridge-torch.m
+++ STACK/bc-bridge-torch.m
@@ -8,9 +8,72 @@ graphs. So let's try a hard way.
 I define a state $S$ as the set of people on the left ("wrong") side
 of the bridge. There are 32 possible states, although I don't think
 all of them are reachable. The initial state is `{t, b, f, m, g}` and
-the winning state is `{}`.
+the "winning" state is `{}`.
 
-Given a state $S$, I define a "trip" as follows:
+Given a state $i$, I define a "trip" as follows:
+
+  - $j$, a subset of one or two people from $i$ who cross the bridge
+  (left to right) with the flashlight. This is `j = Subsets[i,{1,2}]`
+
+  - $k$, a set of one or two people who cross the bridge (right to
+  left) to return the flashlight. The 1 or 2 people who cross back are
+  some subset of:
+
+    - the ones who just crossed over (ie, $j$) *or*
+
+    - the ones already on the other side (ie, the complement of $i$)
+
+I believe the second condition is crucial to solving the problem. Note
+that if 2 people cross back, they need not be both from $j$ or both
+from the complement of $i$. 
+
+Thus, this is `k = Subsets[Union[Complement[people, i],j], {1,2}]`
+
+Note that my definition of "trip" assumes the flashlight will always
+be returned, even if everybody is safely on the right side of the
+bridge. I will correct for this later.
+
+For now, let's enumerate all trips, by looping through all values of
+i, j, and k above. For convenience, we'll also record the state after
+the trip is complete, which is:
+
+`Union[Complement[i, j], k]`
+
+<pre><code>
+(* some setup; we won't use 'times' or 'f' now, but will need them later *)
+people = {"t", "b", "f", "m", "g"};
+times = {1, 3, 6, 8, 12};
+Table[f[people[[i]]] = times[[i]], {i,1,Length[people]}];
+states = Subsets[people];
+
+allTrips = Flatten[Table[{i,j,k, Union[Complement[i, j], k]}, 
+ {i, states}, {j, Subsets[i,{1,2}]}, 
+ {k, Subsets[Union[Complement[people, i],j], {1,2}]}
+],2];
+
+</pre></code>
+
+Note that `Length[allTrips]` is 1180.
+
+To build a graph, we now connect initial and final states, and compute
+the edge length. This is fairly straightforward: we take the max of
+the left-to-right crossers time plus max of the right-to-left crossers
+time, with one exception: if the left-to-right crossing leaves no one
+on the left side, we ignore the right-to-left crossing. This special
+case changes both the length of the trip and the final state.
+
+<pre><code>
+
+edges = Table[{i[[1]], If[Sort[i[[2]]] == Sort[i[[1]]], {}, i[[4]]],
+ Max[Map[f,i[[1]]]]+If[Sort[i[[2]]] == Sort[i[[1]]], 0, Max[Map[f, i[[4]]]]]},
+{i, allTrips}];
+
+</code></pre>
+
+edges2 = DeleteDuplicates[Table[StringJoin[Sort[i[[1]]]]<>"x" <->
+StringJoin[Sort[i[[2]]]]<>"x", {i,edges}]]
+
+
 
   - One or two people from $S$ cross the bridge (left to right) with
   the flashlight.
@@ -83,7 +146,13 @@ allTrips = Flatten[Table[
 
 </pre></code>
 
+There are 1180 possible trips. We now assign lengths to each of these
+trips. This is fairly straightforward:
 
+  - If the left side is empty after the left-to-right trip, the length
+  is the length of the left-to-right crossing.
+
+  - 
 
 TODO: request golf
 
