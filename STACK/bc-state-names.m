895e4c9f9d987cdca4c3079f1d590c731c2024f5
Barry Carter <carter.barry@gmail.com>
2016-03-29 21:27:50 -0600

    Checkpoint

--- STACK/bc-state-names.m
+++ STACK/bc-state-names.m
@@ -16,6 +16,22 @@ usa[[1,6,2,1]] is text Alabama
 
 usa[[1,2,2,1,1]] is quasi-centroid of Alabama
 
+cheating: state 2 is alaska, state 11 is HI
+
+states = Table[i, {i,Flatten[{1,Range[3,10],Range[12,50]}]}];
+
+test2120 = Table[{
+ EdgeForm[Thin],
+ RGBColor[Random[], Random[], Random[]],
+ usa[[1,2,2,i,1]],
+ Text["hello", usa[[1,2,2,i,1,1]]],
+ Opacity[0.1],
+ usa[[1,2,2,i]]
+}, {i,states}]
+
+Graphics[test2120]
+showit
+
 Graphics[{
  usa[[1,2,2,1,1]],
  Text["hello", usa[[1,2,2,1,1,1]]],
