fe6a8b362525a8278c0bf8947930ce25aefee6ff
Barry Carter <carter.barry@gmail.com>
2016-03-31 09:02:37 -0600

    Checkpoint

---
+++ STACK/bc-cartograms.m
@@ -0,0 +1,77 @@
+(* this starts out as an exact copy of
+http://mathematica.stackexchange.com/a/16138/1722 *)
+
+ClearAll["Global`*"]
+usa = Import[
+   "http://code.google.com/apis/kml/documentation/us_states.kml", 
+   "Data"];
+popdata = 
+  Import["https://www.census.gov/geo/reference/docs/cenpop2010/CenPop2010_Mean_ST.txt", "CSV"][[2 ;;, 2 ;; 3]];
+popdata = Thread[popdata[[All, 1]] -> popdata[[All, 2]]];
+stateabbrev = Import["http://goo.gl/5wC23"][[All, {1, -1}]];
+stateabbrev = Thread[stateabbrev[[All, 2]] -> stateabbrev[[All, 1]]];
+presresults = 
+  Import["http://www.fec.gov/pubrec/fe2008/tables2008.xls"];
+electoralVotes = 
+  Thread[presresults[[3, 5 ;; 55, 1]] -> 
+     Total /@ (presresults[[3, 5 ;; 55, {2, 3}]] /. "" -> 0)] /. 
+   stateabbrev;
+presresults = presresults[[3, 5 ;; 55, {1, 4, 5}]] /. stateabbrev;
+transform[s_] := StringTrim[s, Whitespace ~~ "(" ~~ ___ ~~ ")"]
+
+polygons = 
+  Thread[transform[
+     "PlacemarkNames" /. usa[[1]]] -> ("Geometry" /. usa[[1]])];
+stateNames = polygons[[All, 1]];
+stateNames = 
+  Extract[stateNames, 
+   Position[stateNames, x_ /; x != "Alaska" && x != "Hawaii"]];
+stateColors = 
+  Flatten[{#[[1]] -> If[#[[2]] > #[[3]], Blue, Red]} & /@ 
+    presresults];
+
+area[pts_] := 
+  Plus @@ (ListCorrelate[{1, 1}, First /@ pts, 
+       1] ListCorrelate[{-1, 1}, Last /@ pts, 1])/2;
+com[pts_] := Module[{moments, thearea},
+   moments = (1/6) {
+      Plus @@ ((#1^2 + #1 #2 + #2^2 & @@ ({RotateLeft[#], #} &@(First \
+/@ pts))) ListCorrelate[{-1, 1}, Last /@ pts, 1]),
+      -Plus @@ ((#1^2 + #1 #2 + #2^2 & @@ ({RotateLeft[#], #} &@(Last \
+/@ pts))) ListCorrelate[{-1, 1}, First /@ pts, 1]) };
+   thearea = area[pts];
+   Return@If[thearea == 0.0, Mean[pts], moments/thearea]]; 
+com2[pts_, weights_] := Module[{},
+   Return[Total[weights*pts]/Total[weights]]]; 
+
+origdata = (stateNames /. polygons)[[All, 2 ;;, 1]];
+newdata = origdata;
+nits = 10;
+nstates = Length@stateNames;
+weights = (stateNames /. popdata)/Total[stateNames /. popdata];
+For[j = 1, j <= nits*nstates(*Length@origdata*),
+  i = Mod[j - 1, nstates] + 1;
+  tempdata = newdata;
+  (*compts=Map[com,newdata,{2}];*)
+
+  polyarrs = Map[area, newdata, {2}];
+  statearrs = Total /@ polyarrs;
+  allarr = Total@statearrs;
+  comall = 
+   Table[com2[com /@ newdata[[i]], polyarrs[[i]]], {i, 
+     Length@origdata}];
+  norms = Map[Norm[(# - comall[[i]])] &, tempdata, {3}];
+  exp = Tanh[Log[1/weights[[i]] statearrs[[i]]/allarr]]/2;
+  newdata = 
+   Map[(# - comall[[i]]) &, 
+     tempdata, {3}] (1 - exp Exp[-(norms/Max[norms[[i]]])^2]);
+  newdata *= Sqrt[
+   Total[Total /@ Map[area, tempdata, {2}]]/
+    Total[Total /@ Map[area, newdata, {2}]]];
+  j++];
+plotcolors = stateNames /. stateColors;
+Show[Graphics[
+  Table[{EdgeForm[Directive[Black, AbsoluteThickness[0.5]]], 
+    plotcolors[[i]], Polygon[newdata[[i]]]}, {i, Length@newdata}]], 
+ AspectRatio -> Automatic]
+
--- STACK/bc-odd-function.m
+++ STACK/bc-odd-function.m
@@ -2,6 +2,63 @@
 
 http://math.stackexchange.com/questions/1720741/closed-form-of-int-0-pi-x3-ln82-sinx-dx
 
+EDIT: This doesn't answer your question but may be helpful.
+
+From http://math.stackexchange.com/questions/674769 (and many other
+sources) note that:
+
+$\sin(x) =  x\prod_{n=1}^\infty \left(1-\frac{x^2}{n^2\pi^2}\right)$
+
+and thus:
+
+$
+\log (\sin (x))=\log (x) +\sum _{n=1}^{\infty } \log \left(1-\frac{x^2}{n^2 \pi
+^2}\right)
+$
+
+The partial sum turns out to be:
+
+
+
+$
+\log(x) +   \sum _{n=1}^k \log \left(1-\frac{x^2}{n^2 \pi ^2}\right) \to
+   \log \left(\sin (x) \Gamma \left(k-\frac{x}{\pi }+1\right) \Gamma
+    \left(k+\frac{x}{\pi }+1\right)\right)-2 \text{log$\Gamma $}(k+1)
+$
+
+(after massive simplification).
+
+We can thus write the kth approximation to the integrand as:
+
+$
+   x^3 \left(-2 \text{log$\Gamma $}(k+1)+\log \left(\sin (x) \Gamma
+    \left(k-\frac{x}{\pi }+1\right) \Gamma \left(k+\frac{x}{\pi
+    }+1\right)\right)+\log (2)\right)^8
+$
+
+(note that the log(2) accounts for the 2*x we were ignoring earlier)
+
+Sum[Log[1-x^2/n^2/Pi^2],{n,1,Infinity}]
+
+x^3*(Log[2] + Log[x] + Sum[Log[1-x^2/n^2/Pi^2],{n,1,k}])^8
+
+Simplify[Coefficient[Normal[Series[f[x],{x,0,20}]],x,7],conds]
+
+Integrate[Log[2*Sin[x]]^8,{x,0,Pi}]
+
+
+
+
+conds = {x>0,x<Pi,Element[k,Integers],k>1}
+
+Log[Gamma[1 + k - x/Pi]*Gamma[1 + k + x/Pi]*Sin[x]] - 2*LogGamma[1 + k]
+
+x^3* (Log[Gamma[1 + k - x/Pi]*Gamma[1 + k + x/Pi]*Sin[x]] -
+2*LogGamma[1 + k] + Log[2])^8
+
+
+
+
 odd in the sense of unusual, not f(-x) = -f(x)
 
 f[x_] = x^3*Log[2*Sin[x]]^8
@@ -30,3 +87,33 @@ NIntegrate[x^3*Log[2*Sin[x]]^8,{x,0,Pi}, WorkingPrecision -> 50,
 AccuracyGoal -> 40, PrecisionGoal -> 40]
 
 624509.97425476323973864321155907856915353495760746
+
+(uv)' = uv'+vu'
+
+uv = int uv' + int vu'
+
+int uv' = uv - int vu'
+
+binomial helps?
+
+Integrate[Log[Sin[x/2]]^7, x]
+
+g[x_] = Expand[x^3*(Log[4]+Log[Sin[x/2]]+Log[Cos[x/2]])^8]
+
+test2058 = Apply[List,g[x]]
+
+test2059 = Table[Integrate[i,{x,0,Pi}], {i,test2058}];
+
+
+2*Sin[x] -> 4*Sin[x/2]*Cos[x/2] -> 
+
+Log[2*Sin[x]] = 2*Log[x] + 2*Sum[Log[1-x^2/n^2/Pi^2],{n,1,Infinity}]
+
+2*Sum[Log[1-x^2/n^2/Pi^2],{n,1,k}]
+
+x^3*(Log[2]+2*Log[x] + 2*Sum[Log[1-x^2/n^2/Pi^2],{n,1,k}])
+
+x^3*Log[1-x^2/n^2/Pi^2]
+
+
+
