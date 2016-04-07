a37edba8a06d360f9197a7254ebb768d8c47ecd9
Barry Carter <carter.barry@gmail.com>
2016-04-07 09:04:01 -0600

    Checkpoint

--- STACK/bc-andromeda.m
+++ STACK/bc-andromeda.m
@@ -4,13 +4,27 @@ translate[v_,x_,t_] = {x-v*t, t-v*x}/Sqrt[1-v^2]
 
 (*
 
-DISCLAIMER: This argument may be considered non-standard.
+[[image]]
 
 You could argue that simultaneity is just a way to work around the
-Lorentz contraction.
+Lorentz contraction:
+
+  - In the image above, a ship (Black) passes a planet (Red) traveling 0.8c. 
+
+  - Both are seeing the light that left Earth in the year 2000.
+
+  - Red notes the distance to Earth is 10 light years, and correctly
+  concludes it is now 2010 on Earth.
+
+  - Black notes that, by the Lorentz contraction, Earth is only 6
+  light years away, and incorrectly concludes that the Earth year must
+  be 2006.
+
+  
+
 
 circs = Table[{
- Circle[{10,0},i+1, {135*Degree, 225*Degree}]
+ Circle[{10,0},i+1]
 },{i,0,9}];
 
 texts = Table[{
@@ -21,14 +35,19 @@ g2 = Graphics[{
  PointSize[.02], Arrowheads[.02],
  RGBColor[{0,0,1}], Point[{10,0}],
  RGBColor[{1,0,0}], Point[{0,0}],
- RGBColor[{0,0,0}], Point[{0,0.2}], 
- Arrow[{{0,.2},{.98,.2}}],
- Text[Style["0.8c", FontSize -> 15], {.49, .4}],
+ RGBColor[{0,0,0}], Point[{0,0.25}], 
+ Arrow[{{0,.25},{.75,.25}}],
+ Text[Style["0.8c", FontSize -> 20], {.75/2, .5}],
+ Text[Style["Earth", FontSize -> 20], {10, .25}],
+ Text[Style["2010", FontSize -> 20], {10, -.25}],
+ Rotate[Text[Style["2000", FontSize -> 20], {-0.5, 0.125}], 90*Degree],
+ Arrow[{{-0.5, 0.50},{0,1.5}}],
+ Arrow[{{-0.5, -0.30},{0,-1.5}}],
  texts,
  RGBColor[{1,1,0}], circs
 
 }];
-Show[g2, AspectRatio -> 1, PlotRange -> {{0,10}, {-2,2}}]
+Show[g2, AspectRatio -> Automatic, PlotRange -> {{-1,11}, {-2,2}}]
 showit
 
 Suppose an observer B passes another observer C traveling at 0.8c. At
@@ -39,6 +58,8 @@ relative velocity):
 
 
 TODO: summarize v*x, note rotation of earth/remote planet
+
+TODO: note how far he'd drive in time given v*x
 
 Let's suppose the events we are watching in the Andromeda Galaxy are
 at rest with respect to the stationary person on the sidewalk. This is
