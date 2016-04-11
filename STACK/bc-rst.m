e4640663c11d575d492977178bdc9501451e9d7f
Barry Carter <carter.barry@gmail.com>
2016-04-10 20:30:44 -0600

    Checkpoint

--- STACK/bc-rst.m
+++ STACK/bc-rst.m
@@ -41,9 +41,130 @@ Using these, we see:
   - The object's transit time is $t=\alpha$, which is expected, since
   that's essentially the definition of right ascension.
 
-  - At transit, the object's altitude is $\delta -\lambda$
+  - At transit, the object's altitude is:
 
-TODO: is this rightg?!!
+$\frac{\pi }{2}-\left| \delta -\lambda  \right|$
+
+(you can substitute $\frac{\pi }{2}\to 90 {}^{\circ}$ if you're using degrees)
+
+
+TODO: table
+
+tab = {
+ {"Event", "LST", "Azimuth", "Altitude"},
+
+ {"any", "t", raDecLatLonHA2az[alpha, delta, lambda,psi,t],
+              raDecLatLonHA2alt[alpha, delta, lambda,psi,t]},
+
+
+};
+
+Grid[tab, Frame -> All]
+showit
+
+(* join list of strings with character *)
+
+joinStrings[list_, char_] := 
+ StringJoin[Flatten[Table[{ToString[TeXForm[i]],char}, {i,list}]]];
+
+joinStrings[list_, char_] := 
+ Flatten[Table[{ToString[TeXForm[i]],char}, {i,list}]];
+
+
+
+
+
+
+(* convert 2D table to TeX *)
+
+tab2tex[t_] := 
+
+
+
+
+
+TODO: explain LST, azimuth case if 180, degree transform
+
+Pi/2 - Abs[delta-lambda]
+
+this is correct:
+
+ArcTan[Sqrt[(Cos[lambda]*Sin[delta] - Cos[delta]*Sin[lambda])^2], 
+ Cos[delta]*Cos[lambda] + Sin[delta]*Sin[lambda]]
+
+FullSimplify[ArcTan[Sqrt[(Cos[lambda]*Sin[delta] - Cos[delta]*Sin[lambda])^2], 
+ Cos[delta]*Cos[lambda] + Sin[delta]*Sin[lambda]], conds]
+
+ArcTan[Sqrt[Sin[delta - lambda]^2], Cos[delta - lambda]]
+
+above is also correct
+
+Plot[ArcTan[Sqrt[Sin[var]^2], Cos[var]] + var*Sign[var] - Pi/2, {var,-Pi,Pi}]
+
+above is truly 0 in all cases
+
+Pi/2 - (delta-lambda)*Sign[(delta-lambda)]
+
+Piecewise[{delta < lambda, delta - lambda + Pi/2},
+ {delta == lambda, Pi/2},
+ {delta > lambda, -delta + lambda + Pi/2
+
+Plot[ArcTan[Sqrt[Sin[var]^2], Cos[var]] + Abs[var] - Pi/2, {var,-Pi,Pi}]
+
+Plot[{ArcTan[Sqrt[Sin[var]^2], Cos[var]], Pi/2-Abs[var]}, {var,-Pi,Pi}]
+
+
+
+
+FullSimplify[%, lambda -> delta +var, conds]
+
+ArcTan[Cot[delta - lambda]] <- when delta>lambda ONLY
+
+
+$\frac{1}{2} \pi  \text{sgn}(\delta -\lambda )-\delta +\lambda$
+
+where $\text{sgn}(\text{x})$ is +1 when x is positive, -1 when x is
+negative, and 0 when x is 0. This can also be written:
+
+ArcTan[Cot[delta - lambda]]
+
+
+Simplify[raDecLatLonHA2alt[alpha,delta,lambda,psi,alpha],
+{delta>lambda , delta<Pi/2, lambda<Pi/2, delta>-Pi/2, lambda>-Pi/2}]
+
+TODO: non-conditional form
+
+Plot[ArcTan[Cot[var]]+var-Pi/2, {var,0,Pi}] <- this is 0
+
+Plot[ArcTan[Cot[var]]+var+Pi/2, {var,-Pi,0}] <- this is 0
+
+Plot[ArcTan[Cot[var]]+var-Sign[var]*Pi/2, {var,-Pi,Pi}] <- this is 0
+
+thus,
+
+Sign[delta-lambda]*Pi/2 - (delta-lambda)
+
+Piecewise[{
+ {delta < lambda, -Pi/2 - (delta-lambda)}, 
+ {delta == lambda,  Pi/2}, 
+ {delta > lambda, Pi/2 - (delta-lambda)}
+}];
+
+
+
+TODO: degrees vs Pi
+
+ArcTan[Cot[var]] == Sign[var]*Pi/2 - var
+
+Plot[{ArcTan[Cot[var]], Sign[var]*Pi/2 - var}, {var,-Pi,Pi}]
+
+Plot[{ArcTan[Cot[var]] - (Sign[var]*Pi/2 - var)}, {var,-Pi,Pi}]
+
+
+$\tan ^{-1}(\left| \sin (\delta -\lambda ) \right|,\cos (\delta -\lambda ))$
+
+Note that t
+
 
   - 
 
@@ -67,28 +188,9 @@ psi = longitude (just me, normally also phi)
 
 TODO: source http://idlastro.gsfc.nasa.gov/ftp/pro/astro/hadec2altaz.pro
 
-
-If you know the current local sidereal time, $t$, and are willing to
-ignore refraction, these calculations are fairly easy:
-
-  - The objects transit time is when $t=\alpha$, where $\alpha$ is the
-  right ascension.
-
-  - The object's altitude at transit is 
-
-delta = declination
-lambda = latitude
-
-  - The object remains above the horizon for 
-
 TODO: if over 1 or under -1, then..
 
-TODO: how to find sid time
-
-pos[theta_,dec_] = {-Cos[theta]*Cos[dec], -Sin[theta]*Cos[dec], Sin[dec]};
-
-pos2[theta_,dec_,lat_] = FullSimplify[
-rotationMatrix[y,lat-Pi/2].pos[theta,dec],Element[{theta,dec,lat},Reals]]
+TODO: how to find sidereak time
 
 http://idlastro.gsfc.nasa.gov/ftp/pro/astro/hadec2altaz.pro
 
@@ -103,35 +205,22 @@ x = - ch * cd * sl + sd * cl
 y = - sh * cd
 z = ch * cd * cl + sd * sl
 r = Sqrt[x^2 + y^2]
-; now get Alt, Az
 
-http://en.wikipedia.org/wiki/Atan2 notes Mathematica oddness (which I
-have to re-reverse, urk?)
-
-az = ArcTan[y,x]
-alt = ArcTan[z,r]
+az = ArcTan[x,y]
+alt = ArcTan[r,z]
 
 conds = {-Pi/2<dec<Pi/2, -Pi/2<lat<Pi/2, -Pi<ha<Pi, -Pi<lon<Pi}
 
-az2 = FullSimplify[az, conds]
-alt2 = FullSimplify[alt, conds]
+raDecLatLonHA2az[alpha_, delta_, lambda_, psi_, t_] = FullSimplify[ 
+ az /. {lat -> lambda, dec -> delta, ha -> t - alpha},
+ conds];
 
+raDecLatLonHA2alt[alpha_, delta_, lambda_, psi_, t_] = FullSimplify[ 
+ alt /. {lat -> lambda, dec -> delta, ha -> t - alpha},
+ conds];
 
+(* pure comps end here *)
 
-Simplifying conditions
-
-
-
-bad = alt[[1]]^2
-
-radeclatlontime2az[ra_,dec_,lat_,lon_,s_] = az /. ha -> lst-ra
-
-radeclatlontime2el[ra_,dec_,lat_,lon_,s_] = alt /. ha -> lst-ra
-
-(* t is local sidereal time *)
-
-faz = raDecLatLonHA2az
-fel = raDecLatLonHA2el
 
 raDecLatLonHA2az[alpha_, delta_, lambda_, psi_, t_] = 
  ArcTan[Cos[lambda]*Sin[delta] - Cos[delta]*Cos[alpha - t]*Sin[lambda], 
