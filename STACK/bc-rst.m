4afa2cddeaa7633c19cc321cec049afcdd811cc8
Barry Carter <carter.barry@gmail.com>
2016-04-11 20:33:03 -0600

    About to cleanup

--- STACK/bc-rst.m
+++ STACK/bc-rst.m
@@ -2,22 +2,22 @@
 
 (* http://idlastro.gsfc.nasa.gov/ftp/pro/astro/hadec2altaz.pro *)
 
-sh = Sin[ha]
-ch = Cos[ha]
-sd = Sin[dec]
-cd = Cos[dec]
-sl = Sin[lat]
-cl = Cos[lat]
+sh = Sin[ha];
+ch = Cos[ha];
+sd = Sin[dec];
+cd = Cos[dec];
+sl = Sin[lat];
+cl = Cos[lat];
 
-x = - ch * cd * sl + sd * cl
-y = - sh * cd
-z = ch * cd * cl + sd * sl
-r = Sqrt[x^2 + y^2]
+x = - ch * cd * sl + sd * cl;
+y = - sh * cd;
+z = ch * cd * cl + sd * sl;
+r = Sqrt[x^2 + y^2];
 
-az = ArcTan[x,y]
-alt = ArcTan[r,z]
+az = ArcTan[x,y];
+alt = ArcTan[r,z];
 
-conds = {-Pi/2<dec<Pi/2, -Pi/2<lat<Pi/2, -Pi<ha<Pi, -Pi<lon<Pi}
+conds = {-Pi/2<dec<Pi/2, -Pi/2<lat<Pi/2, -Pi<ha<Pi, -Pi<lon<Pi};
 
 raDecLatLonHA2az[alpha_, delta_, lambda_, psi_, t_] = FullSimplify[ 
  az /. {lat -> lambda, dec -> delta, ha -> t - alpha},
@@ -34,17 +34,32 @@ Clear[sh, ch, sd, cd, sl, cl, x, y, z, r, az, alt];
 conds = {alpha > -Pi, alpha < Pi, delta > -Pi/2, delta < Pi/2, lambda > -Pi/2,
  lambda < Pi/2, psi > -Pi, psi < Pi, t > -Pi, t < Pi};
 
-rise = FullSimplify[alpha - ArcCos[-(Tan[delta] Tan[lambda])],conds]
-set = FullSimplify[alpha + ArcCos[-(Tan[delta] Tan[lambda])],conds]
+rise = FullSimplify[alpha - ArcCos[-(Tan[delta] Tan[lambda])],conds];
+set = FullSimplify[alpha + ArcCos[-(Tan[delta] Tan[lambda])],conds];
 
 riseAz=FullSimplify[raDecLatLonHA2az[alpha, delta, lambda, psi, rise], conds];
 setAz=FullSimplify[raDecLatLonHA2az[alpha, delta, lambda, psi, set], conds];
 
+(* 
+
+formulas below are pre-simplification:
+
 transAz=FullSimplify[raDecLatLonHA2az[alpha, delta, lambda, psi, alpha],conds];
 nadirAz=FullSimplify[raDecLatLonHA2az[alpha,delta,lambda,psi,alpha+Pi],conds];
 
-maxEl=FullSimplify[raDecLatLonHA2alt[alpha, delta, lambda, psi, alpha],conds];
-minEl=FullSimplify[raDecLatLonHA2alt[alpha,delta,lambda,psi,alpha+Pi],conds];
+maxEl=Simplify[raDecLatLonHA2alt[alpha, delta, lambda, psi, alpha],conds];
+minEl=Simplify[raDecLatLonHA2alt[alpha,delta,lambda,psi,alpha+Pi],conds];
+
+*)
+
+transAz = 
+ Piecewise[{{delta>lambda, 0},{delta==lambda,"Zenith"},{delta<lambda,Pi}}];
+
+nadirAz = 
+Piecewise[{ {delta>-lambda, 0}, {delta==-lambda,"Nadir"}, {delta<-lambda,Pi}}];
+
+maxEl = Pi/2-Abs[delta - lambda];
+minEl = Abs[delta + lambda]-Pi/2;
 
 (* formulas end here *)
 
@@ -60,13 +75,35 @@ Plot[
  {t,0,24}]
 showit
 
-
 TODO: graphics = show not sine wave or easy
 
-(* most of this is from ../ASTRO/playground5.m *)
+(* functions that can be improved *)
 
-By http://idlastro.gsfc.nasa.gov/ftp/pro/astro/hadec2altaz.pro (plus
-some additional calculations/simplifications),
+(* nadirAz and transAz *)
+
+Piecewise[{{delta>lambda, 0},{delta==lambda,"Zenith"},{delta<lambda,Pi}}]
+
+Piecewise[{ {delta>-lambda, 0},
+ {delta==-lambda,"Nadir"},
+ {delta<-lambda,Pi}}]
+
+(* maxEl and minEl *)
+
+ArcTan[Abs[Sin[delta - lambda]], Cos[delta - lambda]]
+
+Pi/2-Abs[delta - lambda]
+Abs[delta + lambda]-Pi/2
+
+ArcTan[Abs[Sin[delta + lambda]], -Cos[delta + lambda]]
+
+Plot[{ArcTan[Abs[Sin[x]], Cos[x]], Pi/2-Abs[x]}, {x,-Pi,Pi}]
+Plot[{ArcTan[Abs[Sin[x]], Cos[x]] -( Pi/2-Abs[x])}, {x,-Pi,Pi}]
+
+Plot[{ArcTan[Abs[Sin[x]], -Cos[x]], Abs[x]-Pi/2}, {x,-Pi,Pi}]
+
+
+
+(* most of this is from ../ASTRO/playground5.m *)
 
 tab = {
  {"Event", "Time", phi, Z},
@@ -80,14 +117,53 @@ tab = {
 
  {"Set", set, setAz, 0},
 
- {"Nadir", FullSimplify[alpha+Pi], nadirAz, minEl}
+ {"Lowest Point", FullSimplify[alpha+Pi], nadirAz, minEl}
 
 };
 
 tab // TeXForm
-showit;
 
+Using http://idlastro.gsfc.nasa.gov/ftp/pro/astro/hadec2altaz.pro (and
+some additional calculations/simplifications):
 
+$
+   \begin{array}{|c|c|c|c|}
+\hline
+    \text{Event} & \text{Time} & \phi  & Z \\
+\hline
+    \text{Any} & \text{t} & \tan ^{-1}(\cos (\lambda ) \sin (\delta )-\cos
+      (\delta ) \cos (\alpha -t) \sin (\lambda ),\cos (\delta ) \sin (\alpha
+      -t)) & \tan ^{-1}\left(\sqrt{(\cos (\lambda ) \sin (\delta )-\cos (\delta
+      ) \cos (\alpha -t) \sin (\lambda ))^2+\cos ^2(\delta ) \sin ^2(\alpha
+      -t)},\cos (\delta ) \cos (\lambda ) \cos (\alpha -t)+\sin (\delta ) \sin
+      (\lambda )\right) \\
+\hline
+    \text{Rise} & \alpha -\cos ^{-1}(-\tan (\delta ) \tan (\lambda )) & \tan
+      ^{-1}\left(\sec (\lambda ) \sin (\delta ),\cos (\delta ) \sqrt{1-\tan
+      ^2(\delta ) \tan ^2(\lambda )}\right) & 0 \\
+\hline
+    \text{Transit} & \alpha  & 
+\begin{cases}
+                   \delta >\lambda  & 0 \\
+                   \delta =\lambda  & \text{Zenith} \\
+                   \delta <\lambda  & \pi 
+                  \end{cases}
+    & \frac{\pi }{2}-\left| \delta -\lambda  \right| \\
+\hline
+    \text{Set} & \alpha +\cos ^{-1}(-\tan (\delta ) \tan (\lambda )) & \tan
+      ^{-1}\left(\sec (\lambda ) \sin (\delta ),-\cos (\delta ) \sqrt{1-\tan
+      ^2(\delta ) \tan ^2(\lambda )}\right) & 0 \\
+\hline
+    \text{Lowest Point} & \alpha +\pi  & 
+\begin{cases}
+                   \delta >-\lambda  & 0 \\
+                   \delta =-\lambda  & \text{Nadir} \\
+                   \delta <-\lambda  & \pi 
+                  \end{cases}
+    & \left| \delta +\lambda  \right|-\frac{\pi }{2} \\
+\hline
+   \end{array}
+$
 
 where:
 
@@ -101,14 +177,24 @@ where:
 
   - $\lambda$ is the latitude of the observer
 
-  - $\psi$ is the longitude of the observer (we normally use $\phi$
-  for longitude, but it was already taken by azimuth above)
-
   - $t$ is the current local sidereal time
 
 Note the two-argument form of arctangent is required so that the
 results are in the correct quadrant:
 https://en.wikipedia.org/wiki/Inverse_trigonometric_functions#Two-argument_variant_of_arctangent
+
+Additional caveats:
+
+  - If $\left| \delta -\lambda \right|>\frac{\pi }{2}$, the object is
+  always below the horizon, and the equations for rising time and
+  setting time will not work.
+
+  - If $\left| \delta +\lambda \right|>\frac{\pi }{2}$, the object is
+  always above the horizon (circumpolar), and the equations for rising
+  and setting time will also not work.
+
+
+
 
 TODO: circumpolar/cishorizonal <- not a word
 
