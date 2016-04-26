bd8e1e9af0802b37d0e1d70f0ec7029e331239a3
Barry Carter <carter.barry@gmail.com>
2016-04-26 13:08:32 -0600

    Checkpoint

--- STACK/bc-minkowski.m
+++ STACK/bc-minkowski.m
@@ -4,12 +4,196 @@ Is relativitiy really a geometric theory?
 
 NOTE: time then distance
 
+ANSWER STARTS HERE:
+
+This may or may not be helpful.
+
+[[image20.gif]]
+
+The diagram above shows the following in the planet frame:
+
+  - The solid blue line is Earth, which remains 8 light years distant from
+  the planet at all times. I've drawn dots for t=-8 to t=10.
+
+  - The solid green line is the planet, which remains at x=0 at all
+  times. I've drawn dots for t=0 to t=18.
+
+  - The solid red line is the ship, which travels from the planet to
+  Earth in 10 years, covering 8 light years, at least in the planet's
+  frame. Note that the dots are drawn every year of planet time, not
+  every year of ship time.
+
+  - The dashed blue lines are light-speed signals traveling from Earth
+  to the planet, intersecting the ship as they pass.
+
+  - The dashed red lines are light-speed signals traveling from the
+  ship to Earth.
+
+Caveats:
+
+  - The x and t scale are not equal on this graph. Because we're
+  dealing with a distance of 8 light years and a timeframe of 26
+  years, I couldn't find a good way to create a graph with x and t
+  scaled equally.
+
+  - I chose x as horizontal and t as vertical. Some people do it the
+  other way.
+
+Things To Notice:
+
+  - When the ship leaves the planet, it sees Earth's signal from t =
+  -8. In other words, it sees the Earth as it looked at t = -8. This
+  isn't the time it assigns Earth, it's just what Earth light it's
+  currently receiving.
+
+  - By the time the ship arrives, it sees Earth at t = 10. Again, this
+  isn't the time it assigns Earth, just what it actually sees.
+
+  - Thus, considering only the light actually reaching the ship, 18
+  years passes on Earth during the ship's 10 year journey. Notice that
+  this has nothing to do with dilation or contraction, it's pretty
+  much just the Doppler Effect.
+
+  - In your example, Earth only starts sending signals at t=0. In this
+  case, the ship receives the first signal at about 4.44 years (still
+  in the planet's frame), a little before it's halfway through it's
+  journey.
+
+  - Since the ship still receives the last signal at t=10, we note the
+  ship receives 11 signals during its 10 year journey, but the first
+  signal doesn't arrive until t = 4.4 (Earth time, not ship time), so
+  it really receives the 11 signals in 5.56 years (one signal every
+  0.505 years or so).
+
+  - The first signal the ship sends doesn't arrive until Earth time t
+  = 8. This makes sense, since, in this frame, the Earth is 8 light
+  years away.
+
+  - The last signal the ship sends arrives at t = 10 (just as the ship
+  is landing). Thus, the Earth sees 10 signals arriving in the space
+  of two years, one signal every 0.2 years.
+
+TODO: animation and verbiage
+
+[[image22.gif]]
+
+While this diagram is important, let's zoom in a bit:
+
+[[image23.gif]]
+
+Notice what we see here:
+
+  - The ship now remains at x=0 throughout its journey. In other
+  words, we are now looking at things from the ship's point of view.
+
+  - When the ship leaves, it still sees light 
+
+TODO: note zooming in
+
+
+
+
+
+TODO: caveats x/t stretch
+
+
+
+
 *)
 
+earth[t_][v_] = relativityMatrix[v].{8,t} 
+star[t_][v_] = relativityMatrix[v].{0,t}
+ship[t_][v_] = relativityMatrix[v].{0.8*t,t} 
 
-pos[earth][t_][v_] = relativityMatrix[v].{8,t}
-pos[star][t_][v_] = relativityMatrix[v].{0,t}
-pos[ship][t_][v_] = relativityMatrix[v].{0.8*t,t}
+earthpts[v_] = Table[earth[t][v], {t,-8,10}]
+starpts[v_] = Table[star[t][v], {t,0,18}]
+shippts[v_] = Table[ship[t][v], {t,0,10}]
+
+earthline[v_] = Line[{earth[-8][v], earth[10][v]}];
+starline[v_] = Line[{star[0][v], star[18][v]}];
+shipline[v_] = Line[{ship[0][v], ship[10][v]}];
+
+starcolor = RGBColor[0, 0.5, 0];
+
+earthLightLines[v_] = Table[Line[{earth[t][v], star[t+8][v]}], {t,-8,10}];
+
+starLightLines[v_] = Table[Line[{star[t][v], earth[t+8][v]}], {t,-8,10}];
+
+shipLightLines[v_] = Table[Line[{ship[t][v], earth[8+0.2*t][v]}],
+ {t, 0, 10}];
+
+graphpaper = Table[{
+ Line[{{i,-40}, {i,+40}}], Line[{{-40, i}, {40, i}}]},
+ {i, -40, 40}
+];
+
+TODO: reinclude starlightlines later
+
+earthtext[v_] = Table[Text[Style["t="<>ToString[t], FontSize->10], 
+ {earth[t][v][[1]] + .3, earth[t][v][[2]]}],
+ {t,-8,10}]
+
+g2[v_] = Graphics[{
+ RGBColor[0,0,1],
+ PointSize[0.01],
+ Point/@earthpts[v],
+ earthline[v],
+ earthtext[v],
+ starcolor,
+ Point/@starpts[v],
+ starline[v],
+ RGBColor[1,0,0],
+ shipline[v],
+ Point/@shippts[v],
+ RGBColor[0.5,0.5,1],
+ Dashed, RGBColor[0,0,1],
+ earthLightLines[v],
+ Dashed, RGBColor[1,0,0],
+ shipLightLines[v],
+}];
+
+
+xtics = Table[i, {i,-30,30}]
+ytics = Table[i, {i,-30,30}]
+
+start = Show[g2[0], Axes -> True, FrameTicks -> {All, All, All, All},
+ AxesLabel -> {"x", "t"}, PlotRange -> {{0,8.5}, {-8,18}},
+ Ticks -> {xtics, ytics}, AspectRatio -> 1]
+showit
+
+end = Show[g2[-0.8], Axes -> True, FrameTicks -> {All, All, All, All},
+ AxesLabel -> {"x", "t"}, PlotRange -> {{-25,25}, {-25,30}},
+ Ticks -> {xtics, ytics}, AspectRatio -> 1]
+showit
+
+end2 = Show[g2[-0.8], Axes -> True, FrameTicks -> {All, All, All, All},
+ AxesLabel -> {"x", "t"}, PlotRange -> {{0,25}, {-25,6}},
+ Ticks -> {xtics, ytics}, AspectRatio -> 1]
+showit
+
+TODO: disclaim non square
+
+show[v_] = Show[g2[v], Axes -> True, AxesLabel -> {"x", "t"},
+ PlotRange -> {{-25,25}, {-25, 30}}]
+
+show[0]
+showit
+
+show[-.8]
+showit
+
+tab = Table[show[v], {v, 0, -0.8, -0.01}];
+Export["/tmp/test.gif", tab];
+
+
+
+Show[g2[0], Axes -> True, AspectRatio -> 1, AxesLabel -> {"x", "t"}]
+
+Show[g2[0], Axes -> True, AxesLabel -> {"x", "t"},
+ PlotRange -> {{-25,15}, {-10, 30}}]
+
+t3[v_] = {RGBColor[0,0,1], Dashed, 
+ Table[Line[{pos[earth][t][v], pos[star][t+8][v]}], {t,0,10}]};
 
 t[v_] = Table[Point[pos[i][t][v]], {i, {earth,star,ship}}, {t,0,10}];
 
