591420572986bad32abb580bbdef82fe8218da60
Barry Carter <carter.barry@gmail.com>
2016-04-29 11:17:06 -0600

    Checkpoint

---
+++ OSM/osm.py
@@ -0,0 +1,8 @@
+# this is from https://gist.githubusercontent.com/aflaxman/287370/raw/2fa6c2e1b3839e5bb367b806825da9b40f068695/gistfile1.py
+
+execfile('/home/barrycarter/Download/gistfile1.py');
+
+x = read_osm("/home/barrycarter/Download/map2.osm");
+
+print dir(x);
+
