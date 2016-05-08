7e4a14a0af3f3234a434b2a29c1218b874471401
Barry Carter <carter.barry@gmail.com>
2016-05-08 12:54:23 -0600

    Checkpoint (not working)

--- STACK/bc-high-noon.m
+++ STACK/bc-high-noon.m
@@ -5,13 +5,13 @@ astronomy.stackexchange.com/questions/1213/solar-noon-meridian-crossing-time-ver
 refer: http://astronomy.stackexchange.com/questions/14492/need-simple-equation-for-rise-transit-and-set-time
 
 As derived in http://astronomy.stackexchange.com/questions/14492, the
-altitude of an object is:
+altitude of a fixed declination object is:
 
 $
-   \tan ^{-1}\left(\sqrt{(\sin (\lambda ) \cos (t-\alpha (t)) \cos (\delta
-    (t))-\cos (\lambda ) \sin (\delta (t)))^2+\sin ^2(t-\alpha (t)) \cos
-    ^2(\delta (t))},\cos (\lambda ) \cos (t-\alpha (t)) \cos (\delta (t))+\sin
-    (\lambda ) \sin (\delta (t))\right)
+   \tan ^{-1}\left(\sqrt{(\sin (\delta ) \cos (\lambda )-\cos (\delta ) \sin
+    (\lambda ) \cos (\alpha -t))^2+\cos ^2(\delta ) \sin ^2(\alpha -t)},\cos
+    (\delta ) \cos (\lambda ) \cos (\alpha -t)+\sin (\delta ) \sin (\lambda
+    )\right)
 $
 
 where:
@@ -23,6 +23,86 @@ where:
   - $\lambda$ is the latitude of the observer (which we assume is fixed)
 
   - $t$ is the current local sidereal time
+
+Note the two-argument form of arctangent is required so that the
+results are in the correct quadrant:
+https://en.wikipedia.org/wiki/Inverse_trigonometric_functions#Two-argument_variant_of_arctangent
+
+Let's assume for a moment that the Sun's declination is fixed
+(although the entire impetus behind this question is that it's not)
+and that it's the vernal equinox. What's the sun's altitude at $40
+{}^{\circ} N$ latitude 60 minutes before and after the Sun culminates?
+
+Plot[
+ raDecLatLonHA2alt[0,0,40*Degree,0,t/1440*2*Pi]/Degree,
+ {t,-60,60}, Frame -> {False, False, False, False}, 
+ FrameLabel -> {"Time before/after culmination (minutes)", None},
+ AxesLabel -> {None, "Altitude (degrees)"}, AxesOrigin -> {0,48}
+]
+showit
+
+
+Of course, in this case, we're interested in what happens because the
+Sun's declination is *not* constant, which we will get to shortly.
+
+First, let's look at a fixed object's altitude when it's close to the
+meridian ($t=\alpha$).
+
+Plot[{ 
+ raDecLatLonHA2alt[0,0,0*Degree,0,t/1440*2*Pi]/Degree,
+ raDecLatLonHA2alt[0,0,15*Degree,0,t/1440*2*Pi]/Degree,
+ raDecLatLonHA2alt[0,0,30*Degree,0,t/1440*2*Pi]/Degree,
+ raDecLatLonHA2alt[0,0,45*Degree,0,t/1440*2*Pi]/Degree,
+ raDecLatLonHA2alt[0,0,60*Degree,0,t/1440*2*Pi]/Degree,
+ raDecLatLonHA2alt[0,0,75*Degree,0,t/1440*2*Pi]/Degree,
+ raDecLatLonHA2alt[0,0,90*Degree,0,t/1440*2*Pi]/Degree
+}, {t,-60,60}, AxesOrigin -> {0,0}, PlotLegends -> 
+ {HoldForm[0*Degree], 15*Degree, 30*Degree, 45*Degree, 60*Degree,
+ 75*Degree, 90*Degree}, 
+PlotLabel -> "Altitude of 0 declination object"
+]
+showit
+
+TODO: short answer
+
+
+ by looking at the first non-zero term of the
+power series expansion around $t=\alpha$:
+
+$
+   \sin ^{-1}(\cos (\delta -\lambda ))-\frac{(t-\alpha )^2 (\cos (\delta ) \cos
+    (\lambda ))}{2 \left| \sin (\delta -\lambda ) \right|}
+$
+
+
+
+
+
+sh = Sin[ha];
+ch = Cos[ha];
+sd = Sin[dec];
+cd = Cos[dec];
+sl = Sin[lat];
+cl = Cos[lat];
+
+x = - ch * cd * sl + sd * cl;
+y = - sh * cd;
+z = ch * cd * cl + sd * sl;
+r = Sqrt[x^2 + y^2];
+
+az = ArcTan[x,y];
+alt = ArcTan[r,z];
+
+conds = {alpha > -Pi, alpha < Pi, delta > -Pi/2, delta < Pi/2, lambda > -Pi/2,
+ lambda < Pi/2, psi > -Pi, psi < Pi, t > -Pi, t < Pi};
+
+raDecLatLonHA2az[alpha_, delta_, lambda_, psi_, t_] = FullSimplify[ 
+ az /. {lat -> lambda, dec -> delta, ha -> t - alpha},
+ conds];
+
+raDecLatLonHA2alt[alpha_, delta_, lambda_, psi_, t_] = FullSimplify[ 
+ alt /. {lat -> lambda, dec -> delta, ha -> t - alpha},
+ conds];
 
 To find when the elevation is maximal, we take the derivative and set
 equal to 0:
