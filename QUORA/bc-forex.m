8fb56fcfe2185f5d5f2cf63a8706cdf691c5f435
Barry Carter <carter.barry@gmail.com>
2016-05-17 14:46:44 -0600

    Checkpoint

---
+++ QUORA/bc-forex.m
@@ -0,0 +1,20 @@
+(*
+
+Blog entry about FOREX on quora (or wherever) w/ actual trades?
+
+TODO: major disclaimers
+
+TODO: mention NADEX limits
+
+TODO: why GBPUSD
+
+TODO: laziness of USD/* (not doing it)
+
+TODO: fake money + down on US = bad?
+
+TODO: general strategy with math after main section
+
+TODO: nadex vol not real?
+
+
+*)
