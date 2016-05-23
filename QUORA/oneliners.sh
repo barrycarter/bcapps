c7c9694e3d351ecec8a17adeb71a7e99f5b0c21f
Barry Carter <carter.barry@gmail.com>
2016-05-23 17:42:50 -0600

    Checkpoint: made lots of mistakes I should've git'd for reference, but deleted them sigh

--- QUORA/oneliners.sh
+++ QUORA/oneliners.sh
@@ -1,5 +1,32 @@
+# query that gives me all census blockgroups
+
+ogr2ogr -f CSV joined.csv -sql "SELECT u.GEOID, STATEFP, ALAND, AWATER, INTPTLAT, INTPTLON, B00001e1 FROM ACS_2014_5YR_BG u JOIN X00_COUNTS v ON u.GEOID_Data = v.GEOID" /home/barrycarter/CENSUS/ACS_2014_5YR_BG.gdb.zip
+
+exit;
+
+# in theory, a single join (as below) should let me get
+# size/population/location area for all census blockgroups at once,
+# but I keep getting empty columns for some reason, so I'm doing it in
+# two steps and using text join (note GEOID MUST be first column since
+# I will join against it)
+
+ogr2ogr -f CSV bg2.csv -sql "SELECT GEOID, B00001e1 FROM X00_COUNTS" /home/barrycarter/CENSUS/ACS_2014_5YR_BG.gdb.zip
+
+# ogr2ogr -f CSV bg1.csv -sql "SELECT GEOID, STATEFP, ALAND, AWATER, INTPTLAT, INTPTLON FROM ACS_2014_5YR_BG" /home/barrycarter/CENSUS/ACS_2014_5YR_BG.gdb.zip
+
+exit;
+
+ogr2ogr -f CSV output.csv -sql "SELECT GEOID, B00001e1 FROM X00_COUNTS" /home/barrycarter/CENSUS/ACS_2014_5YR_BG.gdb.zip
+
+exit;
+
+ogr2ogr -f CSV output.csv -sql "SELECT GEOID, B00001e1 FROM X00_COUNTS" /home/barrycarter/CENSUS/ACS_2014_5YR_BG.gdb.zip
+
+exit;
+
 # generates a file that, after cleanup, can be used to load tracts
 # into Mathematica for plotting
 
 echo "SELECT '{'||intptlong||','||intptlat||'},' FROM tracts;" | sqlite3 tracts.db > /tmp/tracts.m
 
+exit;
