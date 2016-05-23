9058e18745e37bd59ee32781c71a7a9ebd1902dd
Barry Carter <carter.barry@gmail.com>
2016-05-22 21:14:05 -0600

    About to cleanup

--- QUORA/bc-us-split.m
+++ QUORA/bc-us-split.m
@@ -2,11 +2,50 @@
 
 https://www.census.gov/geo/maps-data/data/gazetteer2010.html
 
-http://www2.census.gov/geo/docs/maps-data/data/gazetteer/Gaz_counties_national.zip
+TODO: direct URL to cousub
 
 TODO: better sources exist (more recent)
 
-TODO: use cousubs instead? (check sum total is correct)
+below from bc-state-names.m
 
 *)
+
+usa = Import["/home/barrycarter/BCGIT/STACK/us_states.kml", "Data"];
+state[n_] := usa[[1,2,2,n]]
+name[n_] := usa[[1,6,2,n]]
+centroid[n_] := Flatten[Apply[List,state[n][[1]]]]
+ewpoints[n_] := Transpose[Partition[Flatten[Apply[List,state[n],1]],2]]
+width[n_] := Max[ewpoints[n][[1]]]-Min[ewpoints[n][[1]]]
+
+states = Table[i, {i,Flatten[{1,Range[3,10],Range[12,50]}]}];
+cities = CityData[{Large, "United States"}];
+
+(* remove Honolulu and Anchorage *)
+
+cities = Select[cities, #[[1]] != "Anchorage" && #[[1]] != "Honolulu" &];
+
+t2108 = Table[Text[Style[i[[1]]], Reverse[CityData[i, "Coordinates"]]], 
+ {i, cities}];
+
+g3 = Graphics[t2108]
+
+g4= Table[Point[Reverse[CityData[i, "Coordinates"]]], {i, cities}]
+Graphics[g4]
+showit
+
+
+TODO: add major cities
+TODO: weird gray dots? (do I have capitals included by mistake?)
+
+g1 = Graphics[{Opacity[0.1], EdgeForm[Thin],Table[state[i],{i,states}]}];
+g2 = Plot[-0.093365*x + 29.8953056335449, {x,-125,-67}]
+
+Show[{g1,g2,g3}, AspectRatio -> 1]
+showit
+
+g = Table[{
+ EdgeForm[Thin],
+ Opacity[0.1],
+ state[i]
+}, {i,states}];
 
--- QUORA/bc-us-split.pl
+++ QUORA/bc-us-split.pl
@@ -1,8 +1,12 @@
 #!/bin/perl
 
-# Does things with US county data
+# TODO: not necess only such line
+
+# Does things with US county subdivision data
 
 # TODO: see .m version of this file
+
+# TODO: answer: -0.093365*intptlong + 29.8953056335449;
 
 # TODO: what does "Created for statistical purposes only." mean? is my
 # use invalid? (if so, get from Mathematica shape data or something)
@@ -10,23 +14,32 @@
 require "/usr/local/lib/bclib.pl";
 
 # the database
-my($db) = "$bclib{githome}/QUORA/counties.db";
+my($db) = "$bclib{githome}/QUORA/tracts.db";
+
+# the select
+my($select) = "SUM(pop10) AS popt, SUM(aland+awater) AS areat FROM tracts";
 
 # the limiting condition for all queries
 my($cond) = "usps NOT IN ('PR', 'AK', 'HI')";
 
-my($tots) = sqlite3hashlist("SELECT SUM(pop10) AS popt,
-SUM(aland+awater) AS areat FROM counties WHERE $cond", $db);
+my($tots) = sqlite3hashlist("SELECT $select WHERE $cond", $db);
 
+# these values are 306675006 and 8081867092450 (if you google the
+# first, you actually get results, but not for the second)
 my($popt, $areat) = ($tots->{popt}, $tots->{areat});
 
 # TODO: slopes greater than 1 are ok too
-for ($i=-1; $i<=1; $i+0.1) {
-  print "$i ",find_intercept($i),"\n";
+for ($i=-0.0934; $i<=-0.0933; $i+=0.00001) {
+  my($pop,$area) = find_intercept($i);
+  print "$i $pop $area\n";
+#  if ($i==0) {next;}
+#  my($j) = 1/$i;
+#  ($pop,$area) = find_intercept($j);
+#  print "$j $pop $area\n";
 }
 
 # given a slope, determines the line intercept that best divides
-# nation into equal POPULATIONs (TODO: for areas too)
+# nation into equal populations and areas
 
 sub find_intercept {
   my($m) = @_;
@@ -34,8 +47,15 @@ sub find_intercept {
   # create function for findroot()
   my($f) = sub {my(@a)=pop_area_below_line($m,$_[0]); return $a[0];};
 
-  # note that the line's origin can be above/below the continental USA)
-  return findroot2($f, 20, 60, 0, "delta=.001");
+  # note that the line's intercept is where the line would hit the
+  # Greenwich meridian so can be ridiculously high (or low)
+
+  # .001 in latitude is about 317ft
+  my($pop) = findroot2($f, -1000, 1000, 0, "delta=.001");
+  $f = sub {my(@a)=pop_area_below_line($m,$_[0]); return $a[1];};
+  my($area) = findroot2($f, -1000, 1000, 0, "delta=.001");
+
+  return ($pop,$area);
 
 }
 
@@ -47,11 +67,9 @@ sub find_intercept {
 
 sub pop_area_below_line {
   my($m,$b) = @_;
-  my($q) = "SELECT SUM(pop10)/$popt AS popt, SUM(aland+awater)/$areat
-  AS areat FROM counties WHERE $cond AND intptlat < $m*intptlong +
-  $b";
+  my($q) = "SELECT $select WHERE $cond AND intptlat < $m*intptlong + $b";
   my($res) = sqlite3hashlist($q,$db);
-  return ($res->{popt}-0.5, $res->{areat}-0.5);
+  return ($res->{popt}/$popt-0.5, $res->{areat}/$areat-0.5);
 }
 
 =item queries
@@ -80,60 +98,16 @@ TODO: mention gis.stack answer
 
 =item comment
 
-CREATE TABLE counties (
- usps TEXT, geoid TEXT, ansicode TEXT, name TEXT,
+CREATE TABLE tracts (
+ usps TEXT, geoid TEXT,
  pop10 INT, hu10 INT, aland DOUBLE, awater DOUBLE,
  aland_sqmi DOUBLE, awater_sqmi DOUBLE,
  intptlat DOUBLE, intptlong DOUBLE
 );
 
-; sqlite3 below
 .separator \t
-; fun fact: if you add ";" to below, it fails
-.import "/home/barrycarter/20160522/Gaz_counties_national.txt" counties
-
-LOAD DATA INFILE
-"/home/barrycarter/20160522/Gaz_counties_national.txt" INTO TABLE counties;
-
-; delete the header row
-DELETE FROM counties WHERE usps='USPS';
-
-; without PR evaluates to
-; https://en.wikipedia.org/wiki/2010_United_States_Census exactly
-
-; with PR searching for 312471327 yields some results
-
-checks (and balances I suppose):
-
-SELECT SUM(pop10) FROM counties;
-+------------+
-| SUM(pop10) |
-+------------+
-|  312471327 | 
-+------------+
-
-SELECT SUM(aland+awater) FROM counties;
-+-------------------+
-| SUM(aland+awater) |
-+-------------------+
-|     9847308090685 | 
-+-------------------+
-
-SELECT SUM(aland+awater) FROM counties WHERE usps NOT IN ('PR');
+.import "/home/barrycarter/20160522/Gaz_tracts_national.txt" tracts
 
 TODO: my numbers dont quite add up, maybe mention what I get vs official sources
-
-Column 1USPS United States Postal Service State Abbreviation
-  Column 2GEOID Geographic Identifier - fully concatenated geographic code (State FIPS and County FIPS)
-  Column 3 ANSICODE American National Standards Institute code
-  Column 4NAME Name
-  Column 5 POP10 2010 Census population count.
-  Column 6 HU10 2010 Census housing unit count.
-  Column 7ALAND Land Area (square meters) - Created for statistical purposes only.
-  Column 8AWATER Water Area (square meters) - Created for statistical purposes only.
-  Column 9ALAND_SQMI Land Area (square miles) - Created for statistical purposes only.
-  Column 10AWATER_SQMI Water Area (square miles) - Created for statistical purposes only.
-  Column 11INTPTLAT Latitude (decimal degrees) First character is blank or "-" denoting North or South latitude respectively.
-  Column 12 INTPTLONG Longitude (decimal degrees) First character is blank or "-" denoting East or West longitude respectively.
 
 =end
