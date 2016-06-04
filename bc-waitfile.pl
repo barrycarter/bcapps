4fe0d67a11dc99b835d82e032ebdccaa22884d07
Barry Carter <carter.barry@gmail.com>
2016-06-04 10:55:20 -0600

    Now glob style

--- bc-waitfile.pl
+++ bc-waitfile.pl
@@ -1,18 +1,18 @@
 #!/bin/perl
 
 # Clone of bc-waitpid.pl
-# Wait until a file exists + then report
+# Wait until a fileglob exists + then report
 # --nox: do not send xmessage, just end
 # --message: add this to standard message
 
 require "/usr/local/lib/bclib.pl";
 
 my($fname) = @ARGV;
-unless ($fname) {die "Usage: $0 pid|string";}
+unless ($fname) {die "Usage: $0 filename|glob";}
 defaults("message=No message");
 
 # TODO: worry this could hang forever on a file that will never exist (but bc-daemon-check should show it?)
 
-while (!(-f $fname)) {sleep 5;}
+while (!(glob $fname)) {sleep 5;}
 
 unless ($globopts{nox}) {system("xmessage 'file $fname now exists: $globopts{message}'&");}
