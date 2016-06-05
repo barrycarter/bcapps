1131786cedf61b34a3fc120a008622531fe55136
Barry Carter <carter.barry@gmail.com>
2016-06-05 11:50:47 -0600

    Checkpoint

--- QUORA/bc-extract-quora.m
+++ QUORA/bc-extract-quora.m
@@ -2,6 +2,29 @@
 
 Graphs quora log stuff; to use "math -initfile quora-times.m"
 
+Subject: Quora log stats
+
+[[image1.gif]]
+
+The graph above shows the number of quora log entries over time.
+
+A log entry occurs whenever anyone posts or edits something.
+
+You can see your own log entries at https://www.quora.com/Barry-Carter/log substituting your own profile id for "Barry-Carter". If you don't know your profile id, you can click on your name at the top right corner of most pages, choose "Profile" and to get to https://www.quora.com/profile/Barry-Carter and then click on "Edits" on the left-hand bar to get to your log page.
+
+
+
+
+
+If you visit your edits page by clicking on your name, then "Profile",
+then "Edits" (my edits page is https://www.quora.com/Barry-Carter/log for example), you'll notice that each entry has a quora log number like #149368232.
+
+You can see details on the log entry by visiting https://www.quora.com/log/revision/149368232 for example.
+
+A log entry can be a question, an answer, a comment, a blog entry, or pretty much anything you can do on quora, plus any edits you've made to any of those things.
+
+There are currently about 150 million log entries on quora. 
+
 *)
 
 (* TODO: maybe put this in bclib.pl *)
@@ -17,28 +40,36 @@ list5 = Table[{unixToDate[i[[1]]/10^6], i[[2]]/10^6}, {i,qt2}];
 style = PlotMarkers -> 
  Graphics[{RGBColor[1,0,0], PointSize -> 0.01,  Point[{0,0}]}]
 
+p1 = DateListPlot[list5, 
+ PlotLabel -> "Quora Log Entries (millions) vs time", style]
 
-p1 = DateListPlot[list5, PlotLabel -> "Quora Log Entries (millions)", style]
-
- PlotMarkers -> Graphics[{RGBColor[1,0,0], PointSize -> 0.01,  Point[{0,0}]}]]
 
 p2= DateListLogPlot[list5, 
- PlotLabel -> "Quora Log Entries (millions), log scale"]
+ PlotLabel -> "Quora Log Entries (millions) vs time, log scale", style]
 
 list2 = Table[{i[[1]]/10^6/86400, i[[2]]/10^6}, {i,qt2}];
 
 p3 = ListLogPlot[list2, PlotLabel -> 
- "Quora Log Entries (millions), log scale, Unix days"];
-showit
+ "Quora Log Entries (millions), log scale, vs time in Unix days", style];
+
+p6 = ListPlot[list2, PlotLabel -> 
+ "Quora Log Entries (millions) vs time in Unix days", style];
+
+
+f[x_] = Exp[-15.1483 + 0.00200361*x]/10^6
+
+p4 = LogPlot[f[x], {x,15257.6,16953}]
+
+Show[{p3,p4}]
+
+p5 = Plot[f[x], {x,15257.6,16953}]
+
+Show[{p6,p5}]
 
 list3 = N[Table[{i[[1]]/10^6/86400, Log[i[[2]]]}, {i,qt2}]];
 Fit[list3,{1,x},x]
 
 FindFit[qt2/10^9, a + b*Exp[c*x], {a,b,c}, x]
-
-f[x_] = Exp[-15.1483 + 0.00200361*x]/10^6
-
-p4 = LogPlot[f[x], {x,15257.6,16953}]
 
 Show[{p4,Graphics[{RGBColor[1,0,0],p3}]}]
 
