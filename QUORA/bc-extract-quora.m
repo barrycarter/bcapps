24c82b54f98d2c56562d589dfbd4b6c6e22776a4
Barry Carter <carter.barry@gmail.com>
2016-06-05 12:07:08 -0600

    Checkpoint

--- QUORA/bc-extract-quora.m
+++ QUORA/bc-extract-quora.m
@@ -12,6 +12,28 @@ A log entry occurs whenever anyone posts or edits something.
 
 You can see your own log entries at https://www.quora.com/Barry-Carter/log substituting your own profile id for "Barry-Carter". If you don't know your profile id, you can click on your name at the top right corner of most pages, choose "Profile" and to get to https://www.quora.com/profile/Barry-Carter and then click on "Edits" on the left-hand bar to get to your log page.
 
+You can see the details of a log entry by visiting https://www.quora.com/log/revision/149368232, substituting the log id number for 149368232. Note that you can view any log entry, not just your own, and the page requires no authentication: you don't even have to be logged into quora.
+
+As the graph above shows, log entry growth is exponential, so let's look at a log plot instead:
+
+[[image2.gif]]
+
+If we quantify, using a numerical x scale, where x = the number of days since 1 Jan 1970 (in other words, the Unix time divided by 86400), and use numerical techniques, we find that [math]e^{0.00200361 x-15.1483}[/math] is a fairly good approximation to the number of log entries:
+
+[[image3.gif]]
+
+On a log scale plot:
+
+[[image4.gif]]
+
+Taking the derivative of this approximation, [math]0.00200361 e^{0.00200361 x-15.1483}[/math], and substituting x = 16957 (start of 5 Jun 2016), we see the log entries grow at a rate of approximately 301,000 entries per day or about 3.5 new entries per second.
+
+In theory, it should be possible to download all new log entries as they come in (to create a live feed of questions/answers for example), but it would require a little work as 3.5 entries per second isn't trivial.
+
+Caveats:
+
+  - 
+
 
 
 
