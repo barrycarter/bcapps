3b70dc0233a4cbd54b812d70ff4b11c905fb30a7
Barry Carter <carter.barry@gmail.com>
2016-06-13 20:49:42 -0600

    Checkpoint

--- playground17.pl
+++ playground17.pl
@@ -4,28 +4,39 @@
 
 require "/usr/local/lib/bclib.pl";
 
-# work directory for this project
-chdir("/home/barrycarter/20160613");
+# work directory for this project (this site is not in git because the
+# images are not freely usable)
+
+chdir("/home/barrycarter//home/user/BCINFO3-PRIV/sites/test/20160613");
 
 my(@text) = split(/\r\n/, read_file("1.txt"));
 
+# @out holds the output text for both the "buttons" page and the main page
+my(@out);
+
 # make indices match
 unshift(@text, "");
-debug(@text);
 
-die "TESTING";
-
-my($n) = 0;
-
-print "<table border>\n";
+push(@out,"<table border>");
 
 for $i (1..3) {
-  print "<tr>\n";
+  push(@out, "<tr>");
   for $j (1..5) {
-    print "<td><img src='$n.jpg'>$text[$n]</td>\n";
     $n++;
+    push(@out,"<td><img src='$n.jpg'><br>$text[$n]</td>");
   }
-  print "</tr>\n";
+  push(@out,"</tr>");
 }
 
-print "</table>\n";
+push(@out,"</table>");
+
+my($out) = join("\n",@out);
+
+write_file(join("\n",@out), "guitars.html");
+
+open(A,">table.html");
+
+print A "<table border><tr><th><a href='guitars.html'>Guitars</a></th></tr>\n";
+print A "<tr><td>$out</td></tr></table>\n";
+
+close(A);
