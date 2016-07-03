ce8483e84236bfe2f377baac5f7b5d05a6a649dd
Barry Carter <carter.barry@gmail.com>
2016-07-02 21:48:36 -0600

    Checkpoint

--- QUORA/bc-moderate-temperature.m
+++ QUORA/bc-moderate-temperature.m
@@ -22,10 +22,42 @@ TODO: note SJSU
 
 (* this test is for 10 years, really do 30 years *)
 
+$TimeZone = 0;
 sdate = {2006,7,1};
 edate = {2016,6,30};
 
-kbos = WeatherData["Boston", "Temperature", {sdate,edate}];
+kbos = Select[
+ WeatherData["Boston", "Temperature", {sdate,edate}, TimeZone -> 0],
+ NumberQ[#[[2]]] &];
+
+kbos2 = N[Table[{(FromDate[i[[1]]]-FromDate[sdate])/3600, i[[2]]}, {i, kbos}]];
+
+kbos3 = Mean /@ GatherBy[kbos2, First];
+
+kbos4[x_] = Interpolation[kbos3, x]
+
+kbos5 = Table[kbos4[x], {x, 
+
+
+kbos = Select[
+ WeatherData["Boston", "Temperature", {sdate,edate}, TimeZone -> 0],
+ #[[2]] == Missing["NotAvailable"] &];
+
+
+
+
+
+kbos2 = N[Table[{(FromDate[i[[1]]]-FromDate[sdate])/3600, i[[2]]}, {i, kbos}]];
+kbos3 = Mean /@ GatherBy[kbos2, First];
+kbos4[x_] = Interpolation[kbos3, x]
+
+
+
+kbos = Table[WeatherData["Boston", "Temperature", ToDate[i]],
+ {i, FromDate[sdate], FromDate[edate], 3600}];
+
+
+
 
 kbos2 = N[Table[{(FromDate[i[[1]]]-FromDate[sdate])/3600, i[[2]]}, {i, kbos}]];
 
