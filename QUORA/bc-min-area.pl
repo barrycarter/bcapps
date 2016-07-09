f624ec67e0be36d675be320e5703e551ec5118a4
Barry Carter <carter.barry@gmail.com>
2016-07-09 10:48:04 -0600

    Checkpoint

--- QUORA/bc-min-area.pl
+++ QUORA/bc-min-area.pl
@@ -13,12 +13,28 @@ my($workdir) = "/home/barrycarter/20160709";
 
 unless (-f "$workdir/stor.txt") {load_data();}
 
-# data is in data.pl file, so load it
-# eval(read_file("$workdir/data.pl"));
-# debug($VAR1);
-
 my($data) = retrieve("$workdir/stor.txt");
+my($dense, $totals) = @$data;
 
+# TODO: probably better way to do this
+my(@dense) = @$dense;
+my(@totals) = @$totals;
+
+# TODO: shortcut abort when every state has 50%+
+
+for $i (@dense) {
+
+  debug(%$i);
+
+  # TODO: store list of blockgroups since I'll want to map them eventually
+
+  # compute total population and total area for this state
+  $population{$i->{'statefp'}}+= $i->{'population'};
+  $totalarea{$i->{'statefp'}}+= $i->{'area'};
+
+  # testing
+  debug("STATE: $i->{statefp} has $population{$i->{statefp}}, $totalarea{$i->{statefp}}");
+}
 
 # load data into data.pl file in workdir
 
@@ -26,7 +42,6 @@ sub load_data {
   my(@totals) = sqlite3hashlist("SELECT statefp, SUM(aland+awater) AS atotal, SUM(population) AS ptotal FROM blockgroups GROUP BY statefp", $db);
   # most densely populated areas (not per state, but not an issue)
   my(@dense) = sqlite3hashlist("SELECT geoid, statefp, aland+awater AS area, population FROM blockgroups ORDER BY population/area DESC", $db);
-#  write_file(Dumper([[@dense], [@totals]]), "$workdir/data.pl");
   store([[@dense], [@totals]], "$workdir/stor.txt");
 }
 
