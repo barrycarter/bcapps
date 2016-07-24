15b6c49b9008a6e4104ef31bce200f3bf0fbf919
Barry Carter <carter.barry@gmail.com>
2016-07-24 10:51:26 -0600

    Checkpoint

--- WEATHER/bc-get-hilo.pl
+++ WEATHER/bc-get-hilo.pl
@@ -11,13 +11,15 @@ require "/usr/local/lib/bclib.pl";
 # TODO: early versions might be not efficient (even more so than above)
 my($file) = @ARGV;
 
-my(%hash, %vals);
+my(%hash, %vals, %dates);
 
 open(A,"bzegrep 'TMAX|TMIN' $file|");
 
 while (<A>) {
 
   my($orig) = $_;
+
+  my($date) = get_chars($_,12,17);
 
   my($key) = get_chars($_,18,21);
 
@@ -32,18 +34,29 @@ while (<A>) {
     # missing data
     if ($val == -9999) {next;}
 
-    # all good, push
+    # all good, push data
     push(@{$hash{$key}}, $val);
     # count how many of each
     $vals{$key}++;
-  }
 
+    # and push dates to find min and max
+    # NOTE: this is highly redundant and occurs for every valid hi/lo entry
+    $dates{$date} = 1;
+  }
 }
+
+my(@dates) = sort {$a <=> $b} keys %dates;
 
 my($stat) = $file;
 $stat=~s/\.dly\.bz2$//;
 my(@lows) = percentile($hash{TMIN}, [0,.01,.02,.05]);
 my(@highs) = percentile($hash{TMAX}, [.95,.98,.99,1]);
+
+# ultimately decided NOT to print first/last dates in this way, though
+# I still compute them above
+
+# print join(",",($stat,@lows,@highs,$vals{TMIN},$vals{TMAX},$dates[0], $dates[-1])),"\n";
+
 print join(",",($stat,@lows,@highs,$vals{TMIN},$vals{TMAX})),"\n";
 
 =item percentile(\@list, \@percentiles)
@@ -60,6 +73,8 @@ sub percentile {
   my(@p) = @$pref;
   my(@ret);
 
+  debug("L",@l);
+
   for $i (@p) {
     my($elt) = $#l*$i;
     my($int) = floor($elt);
@@ -75,3 +90,29 @@ sub get_chars {
   my($str,$x,$y) = @_;
   return substr($str,$x-1,$y-$x+1);
 }
+
+=item comment
+
+Format of *.dly.bz2 files (per README):
+
+ID            1-11   Character
+YEAR         12-15   Integer
+MONTH        16-17   Integer
+ELEMENT      18-21   Character
+VALUE1       22-26   Integer
+MFLAG1       27-27   Character
+QFLAG1       28-28   Character
+SFLAG1       29-29   Character
+VALUE2       30-34   Integer
+MFLAG2       35-35   Character
+QFLAG2       36-36   Character
+SFLAG2       37-37   Character
+  .           .          .
+  .           .          .
+  .           .          .
+VALUE31    262-266   Integer
+MFLAG31    267-267   Character
+QFLAG31    268-268   Character
+SFLAG31    269-269   Character
+
+=cut
--- WEATHER/bc-parse-ghcnd.pl
+++ WEATHER/bc-parse-ghcnd.pl
@@ -4,6 +4,27 @@
 
 require "/usr/local/lib/bclib.pl";
 
+# use Unix join command to get latitude/longitude/data years/name all
+# at once, hopefully
+
+# TODO: this is ugly, though I think it will work
+
+# TODO: using TMAX data as canonical, which it should be except for
+# years included, where I'll just pretend it is
+
+open(A, "join ghcnd-inventory.txt ghcnd-stations.txt | fgrep ' TMAX '|");
+
+while (<A>) {
+
+  my($name, $lat, $lon, $tmax, $syear, $eyear, $lat2, $lon2, $el, @rest) = 
+    split(/\s+/, $_);
+
+  my($location) = join(" ",@rest);
+
+}
+
+die "TESTING";
+
 open(A,"ghcnd-stations.txt")||die("Can't open, $!");
 open(B,">ghcnd-stations.tsv");
 
