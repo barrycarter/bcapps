c7b1401b7bc5638f6825bef3bb6fe4a8040d436f
Barry Carter <carter.barry@gmail.com>
2016-08-26 11:39:11 -0600

    Checkpoint and possibly done, not as helpful as I'd hoped

--- QUORA/bc-subject2question.pl
+++ QUORA/bc-subject2question.pl
@@ -2,8 +2,41 @@
 
 # Trivial script that grabs question titles from email subjects like:
 # New answer to "[title of question]"
-# In Alpine: select all emails with participant quora, export to file;
-# do NOT pipe to grep "Subject:" or similar because that cuts off subjects
+
+# In Alpine: select all emails from comment-noreply@quora.com quora,
+# export to file; do NOT pipe to grep "Subject:" or similar because
+# that cuts off subjects
+
+
+=item comment
+
+Quora sends people email as the following
+
+From: "Rory (Quora)" <appeals+bnbr@quora.com>
+From: Janet Go <research@quora.com>
+From: Quora <answer-noreply@quora.com>
+From: Quora <comment-noreply@quora.com>
+From: Quora <follow-noreply@quora.com>
+From: Quora <noreply@quora.com>
+From: Quora <upvote-noreply@quora.com>
+From: Quora Messages <messages-noreply@quora.com>
+From: Quora People You Follow <network-noreply@quora.com>
+From: Quora Session Recap <sessions-noreply@quora.com>
+From: Quora Weekly Digest <digest-noreply@quora.com>
+From: Write on Quora <write-noreply@quora.com>
+
+In Alpine, select emails from "answer-noreply@quora.com",
+"comment-noreply@quora.com", "<noreply@quora.com" [angle bracket is
+important, otherwise includes addresses that have noreply as a
+substring]. Sample commands (you must have advanced features enabled
+and be in your mailbox with no messages selected):
+
+;tfanswer-noreply@quora.com
+;btfcomment-noreply@quora.com
+;btf<noreply@quora.com
+aetemp.txt
+
+=cut
 
 require "/usr/local/lib/bclib.pl";
 
@@ -11,12 +44,55 @@ my(%qs);
 
 # the following are uninteresting
 
-@bad = ("upvoted your answer to: ", "followed you on Quora",
-	"requested your answer to a question");
+# TODO: should comments on my post be considered interesting? (and
+# suggested an edit is also contentious)
 
-my($reg) = join("|",@bad);
+# NOTE: some near duplicates below (eg, the first two) are because
+# quora has changed the format of its emails over the years
 
-debug("REG: $reg");
+@bad = (
+	"upvoted your answer to: ",
+	"upvoted your answer to ",
+	"voted up your answer to ",
+	"followed you on Quora",
+	" is now following you on Quora\$",
+	"requested your answer to a question",
+	"asked you to answer ",
+	"Can you answer this question about ",
+	"asked you to answer a question",
+	"sent you a message on Quora",
+	"Can you answer this question?",
+	"commented on your post",
+	"Question added to the topic ",
+	"mentioned you in an answer",
+	"suggested an edit for your answer to ",
+	"more people followed you in the past",
+	"Open Question about ",
+	"wants an answer to this question about ",
+	"is following this question about ",
+	"Subject: Can you answer",
+	"Subject: Question redirected: ",
+	" asked you to review ",
+	" promoted "
+       );
+
+# the following are interesting
+
+@good = ("replied to your comment on", "new answer to", 
+	 "commented on your answer",
+	 "Quora Moderation collapsed your answer to:",
+	 "Quora Moderation has flagged your answer to",
+	 "recently answered", "commented on",
+	 "Subject: Your answer was flagged as needing improvement on ",
+	 "suggested a move for your answer to ",
+	 "suggested a move for your answer from ",
+	 "wrote an answer you requested to ",
+	 "Subject: New comment on "
+	);
+
+
+my($bad) = join("|",@bad);
+my($good) = join("|",@good);
 
 while (<>) {
 
@@ -25,15 +101,17 @@ while (<>) {
   # silently skip anything that's not a subject
   unless (/^Subject: /) {next;}
 
-  # all of these things not interesting either
-  if
-    (/(upvoted your answer to: |followed you on Quora|
-       requested your answer to a question$)/x) {next;}
-
-  if (/(replied to your comment on|new answer to|commented on your answer )\s*\"(.*)\"/i) {
-    $qs{$2} = 1;
+  # and uninteresting things
+  if (/$bad/) {
+    debug("IGNORING: $_");
     next;
   }
 
-  debug("THUNK: $_");
+  if (/($good)\s*\"(.*)\"/i) {$qs{$2}++;}
 }
+
+for $i (sort {$qs{$b} <=> $qs{$a}} keys %qs) {
+  print "$qs{$i} $i\n";
+}
+
+
