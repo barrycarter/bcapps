ddb539a86ad6b4950b6e8acd6b463ecc23cb873b
Barry Carter <carter.barry@gmail.com>
2016-09-05 10:46:59 -0600

    Checkpoint

---
+++ MATHEMATICA/bc-silly-constants.m
@@ -0,0 +1,25 @@
+(* currently unused list of 'silly' constants, in constrast to
+nearestPhysicalConstant.mx; format is {name, value, uncertainty,
+units, source [optional]} *)
+
+sillyConstants = {
+ {"purity of Ivory soap", .9944, 0, 1},
+ {"universal answer", 42, 0, 1},
+ {"random integer", 57, 0, 1},
+ {"random Trek integer", 47, 0, 1},
+
+ {"time it takes to sing 'this song'", 105, 0.5, s, "https://en.wikipedia.org/wiki/The_Big_Bang_Theory"},
+
+}
+
+(*
+
+TODO: 
+
+  - add time it takes elements to form
+
+
+*)
+
+
+
--- QUORA/bc-trig.m
+++ QUORA/bc-trig.m
@@ -8,4 +8,12 @@ Expand[Im[Expand[(ExpToTrig[Exp[I*2*Pi/7]])^7]] /. {Cos[3*Pi/14] -> x,
  Sin[3*Pi/14]^2 -> 1-x^2, Sin[3*Pi/14]^4 -> (1-x^2)^2, 
  Sin[3*Pi/14]^6 -> (1-x^2)^3}]
 
+subs = {Cos[3*Pi/14] -> x, Sin[3*Pi/14] -> Sqrt[1-x^2]}
+subs2 = {x -> Sqrt[y]}
+
+Expand[Im[(Cos[2*Pi/7]+I*Sin[2*Pi/7])^7]/x /. subs] /. subs2
+
+Expand[Re[(Cos[2*Pi/7]+I*Sin[2*Pi/7])^7]] /. subs
+
+
 
