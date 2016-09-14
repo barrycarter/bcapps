0d7086683d676d2f4dddb2b8f056ec6d14331025
Barry Carter <carter.barry@gmail.com>
2016-09-14 14:16:55 -0600

    Checkpoint: working

--- QUORA/bc-quora-random.pl
+++ QUORA/bc-quora-random.pl
@@ -4,6 +4,10 @@
 # in the last 24 hours based on latest observations and assuming
 # linear, not exponential growth
 
+# TODO: generate n URLs not 5 exactly
+# TODO: allow user to set how many days old (eg, 1-5 days)
+# TODO: if possible, self-adjust formula
+
 # The formula I in my blog entry, is way off, alas
 
 require "/usr/local/lib/bclib.pl";
@@ -27,4 +31,11 @@ my($time) = time();
 my($hwm) = ($time-$x1)*$slope + $y1;
 my($lwm) = $hwm-$slope*86400;
 
-debug("HWM: $lwm - $hwm");
+for $i (1..5) {
+
+  my($rand) = int($lwm+rand()*($hwm-$lwm));
+  print "https://www.quora.com/log/revision/$rand\n";
+
+}
+
+# NOTE: can pipe output to bc-open-urls.pl if desired
