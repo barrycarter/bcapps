6cec98ddf7cb5096cda47ad46a26b45f7e7c31d1
Barry Carter <carter.barry@gmail.com>
2016-10-04 09:27:46 -0600

    Checkpoint: about to make minor changes

--- MAPS/bc-equidistant.pl
+++ MAPS/bc-equidistant.pl
@@ -4,27 +4,55 @@
 
 require "/usr/local/lib/bclib.pl";
 
-debug(antipode(5,10));
+# debug(antipode(5,10));
 
+gcmorestats(34.0522226126327, -118.243667974691, 32.7153237292363,
+-117.157244512871);
 
+die "TESTING";
 
+=item gcmorestats($lat1,$lon1,$lat2,$lon2)
 
-=item polepos($lat1,$lon1,$lat2,$lon2)
+NOTE: extends gcstats (sort of) in bclib.pl
 
-Given two latitude/longitude pairs, treat the geodesic that is
-equidistant from them as an "equator" and find the "North Pole"
+Given two latitude/longitude pairs, return as a hash the following
+information:
+
+  - midpoint: the midpoint latitude/longitude
+
+  - pole: the latitude/longitude from which a 1/2-radius circle would
+  form the geodesic of equidistant points
+
+  - quad: the latitude/longitude from which a 1/2-radius circle would
+  from the great circle path
+
+  - perhaps more later
 
 =cut
 
-sub polepos {
+sub gcmorestats {
   my($lat1,$lon1,$lat2,$lon2) = @_;
+  my(%hash);
+  debug("GOT",@_);
 
-  # entirely from Mathematica, see bc-equidistant.m
-  
+  # convert to xyz vectors
+  my(@v1) = sph2xyz($lon1,$lat1,1,"degrees=1");
+  my(@v2) = sph2xyz($lon2,$lat2,1,"degrees=1");
 
+  # the midpoint xyz (sum and average)
+  my(@mid) = vecapply(\@v1, \@v2, "+");
+  @mid = map($_/=2, @mid);
+  # re-using @mid here is probably bad
+  @mid = xyz2sph(@mid, "degrees=1");
+  $mid[0] = fmodn($mid[0], 360);
+  $mid[1] = fmodn($mid[1], 180);
+  $hash{midpoint} = [@mid[0..1]];
 
+  # the pole of the splitting circle
 
-
+  debug(%hash);
+  debug("BETA",@mid);
+#  debug("ALPHA", @mid);
 }
 
 =item antipode($lat,$lon)
