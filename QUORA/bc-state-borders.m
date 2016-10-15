70b54ebe37f1489c3fc782aea131d8b621b1dffe
Barry Carter <carter.barry@gmail.com>
2016-10-15 14:50:01 -0600

    Checkpoint

--- QUORA/bc-state-borders.m
+++ QUORA/bc-state-borders.m
@@ -11,8 +11,30 @@ state_latlon.csv from https://dev.maxmind.com/geoip/legacy/codes/state_latlon/
 
 *)
 
-usa = Import["/home/barrycarter/BCGIT/QUORA/state_latlon.csv", "Data"];
+usa = Import["/home/barrycarter/BCGIT/STACK/us_states.kml", "Data"];
+
+(* helper functions *)
+state[n_] := usa[[1,2,2,n]]
+name[n_] := usa[[1,6,2,n]]
+centroid[n_] := Flatten[Apply[List,state[n][[1]]]]
+ewpoints[n_] := Transpose[Partition[Flatten[Apply[List,state[n],1]],2]]
+width[n_] := Max[ewpoints[n][[1]]]-Min[ewpoints[n][[1]]]
+
+states = Table[f[n] = name[n], {n,1,50}]
+
+g = Table[{
+ EdgeForm[Thin],
+ Text[Style[f[i], FontSize-> 60*width[i]/StringLength[f[i]]], centroid[i]],
+ Opacity[0.1],
+ state[i]
+}, {i,states}];
+
+
+
+
 temp1102 = Flatten[Table[{i[[1]] <-> i[[2]]}, {i, borders}],1]
+
+
 
 (* assign lat lon *)
 
