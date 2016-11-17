8f3aa3612520e9a09b1ea452010791b8d28f8e7b
Barry Carter <carter.barry@gmail.com>
2016-11-17 12:19:22 -0700

    Checkpoint

---
+++ FEDORA/startup-nox.sh
@@ -0,0 +1,24 @@
+# these are things I must run manually BEFORE starting X11
+
+# Since I killed NetworkManager (perhaps a bad decision), must run
+# dhclient manually
+
+sudo dhclient
+
+# for some reason, neither Network Manager or the system default
+# routing brings up the lo interface-- it's harmless, but I dislike
+# using the Ethernet interface for loopback
+
+sudo route add -net 127.0.0.0 netmask 255.0.0.0 lo
+
+# TODO: uncomment below
+
+# this daemon destructively checks all my mail; however, since it's
+# destructive (removes mail from the remote servers), I can't run it
+# until I completely switch over to brighton
+
+# /home/barrycarter/BCGIT/bc-getmail.pl &
+
+# this starts recollindex, but I'm not quite ready to do that on brighton
+sudo recollindex -m -x
+
