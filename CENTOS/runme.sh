9d95bec56ea21ab7d91983ad960406b7c1aa6d61
Barry Carter <carter.barry@gmail.com>
2016-11-28 12:10:45 -0700

    Checkpoint

--- CENTOS/runme.sh
+++ CENTOS/runme.sh
@@ -12,18 +12,22 @@ printf '1,$s/SELINUX=enforcing/SELINUX=disabled/\nwq\n'|ex /etc/selinux/config
 
 # link repos to yum.repos.d
 
-sudo ln -s /home/user/BCGIT/CENTOS/*.repo /etc/yum.repos.d/
+# TODO: consider restoring this line
+# sudo ln -s /home/user/BCGIT/CENTOS/*.repo /etc/yum.repos.d/
 
 # instal rpmfusion repo
 # below cut/paste from https://rpmfusion.org/Configuration
 
-su -c 'yum -y localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/updates/6/i386/rpmfusion-free-release-6-1.noarch.rpm https://download1.rpmfusion.org/nonfree/el/updates/6/i386/rpmfusion-nonfree-release-6-1.noarch.rpm'
+# TODO: consider restoring this (right now, epel only)
+# su -c 'yum -y localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/updates/6/i386/rpmfusion-free-release-6-1.noarch.rpm https://download1.rpmfusion.org/nonfree/el/updates/6/i386/rpmfusion-nonfree-release-6-1.noarch.rpm'
 
 # install repos in /root/build
 
-cd /root/build/
-: note that nux is included above
-rpm -Uvh ius-release.rpm gf-release-7-10.gf.el7.noarch.rpm remi-release-7.rpm
+# TODO: consider uncommenting these lines later
+
+# cd /root/build/
+# : note that nux is included above
+# rpm -Uvh ius-release.rpm gf-release-7-10.gf.el7.noarch.rpm remi-release-7.rpm
 
 # and other useful symlinks
 
@@ -54,11 +58,14 @@ sudo crontab -u user /home/user/BCGIT/FEDORA/bc-public-crontab
 # TODO: reach final decision over installing perl modules this way or
 # from cpan or a combination like I'm doing now
 
+# TODO: right now using ONLY epel-release as my "extra" repo
+
 sudo yum -y install epel-release wget efibootmgr firefox ImageMagick OpenThreads PySolFC SDL-devel alpine aspell audacity bind c++-gtk-utils-gtk2-devel community-mysql community-mysql-server dosbox dosemu elinks emacs enscript esniper expect feh ffmpeg ftp fuse-encfs fvwm gd-devel getmail glade glade-devel gnumeric gnuplot gpm graphviz gtk+extra-devel gtk2-devel gtk3-devel html2ps lynx mencoder parallel mplayer mrtg nagios-plugins nano ncftp rdate perl-CPAN php-mysqlnd pidgin postgresql postgresql-server qgis qhull rdesktop recoll rsyslog rxvt samba screen snownews stella stellarium tcsh tigervnc tk unrtf util-linux vice vlc xdotool xemacs xemacs-packages-extra-el xinetd xorg-x11-server-Xorg xorg-x11-apps xpdf xsane xteddy xterm xv yum yum-utils zlib-devel zlib-static mod_ldap tor tor-arm-gui tor-arm onionshare privoxy recordmydesktop tmpwatch esmtp-local-delivery sendmail sendmail-cf fuse-sshfs fuse-zip fuse-encfs curlftpfs bindfs xcalc libpuzzle libpuzzle-devel pyephem python2-astropy python3-astropy erfa libnova ast R sagemath-notebook nmap p7zip "perl-Digest-*" "perl-Date-*" "perl-DateTime-*" "perl-Text-*" "perl-MIME-*" "perl-Math-*" "perl-Data-*" "perl-JSON-*" "perl-Algorithm-*" "perl-DBI-*" "perl-DB_File" "perl-File-*" "perl-Net-*" "perl-Number-*" "perl-Getopt-*" "perl-GD" "perl-HTML-*" "perl-HTTP-*" "perl-IO-*" lucene "perl-LWP-*" "perl-Inline" "perl-Inline-*" perl-OpenGL perl-utf8-all "perl-B-*" "perl-IPC-*" perl-Imager "perl-Flickr-*" ntpdate
 
 # upgrade all installed packages including new ones
 
-sudo yum -y upgrade
+# TODO: consider uncomment this, but it updates the version of CentOS itself
+# sudo yum -y upgrade
 
 # this installs cpan stuff (env var minimizes prompts)
 
