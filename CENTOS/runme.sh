07bf8f14b6c2976559c0465eb04578e6fc7ca31b
Barry Carter <carter.barry@gmail.com>
2016-11-30 16:33:46 -0700

    Checkpoint

--- CENTOS/runme.sh
+++ CENTOS/runme.sh
@@ -7,17 +7,13 @@
 # packages are skipped -- as noted in README, look at /runme.err on
 # completion
 
-# this is the stuff CentOS will install w/o even epel
-
-sudo yum -y --skip-broken install ImageMagick aspell elinks emacs enscript expect firefox ftp gnuplot gpm graphviz html2ps lynx mrtg nano nmap ntpdate perl-CPAN postgresql postgresql-server rdate samba screen sendmail sendmail-cf tcsh telnet tigervnc tigervnc-server tk tmpwatch wget xinetd xorg-x11-apps xsane xterm yum-utils zlib-devel zlib-static "perl-Digest-*" "perl-Date-*" "perl-DateTime-*" "perl-File-*" perl-GD "perl-HTML-*" "perl-HTTP-*" "perl-IO-*" "perl-IPC-*" perl-Inline "perl-JSON-*" "perl-LWP-*" "perl-Net-*" "perl-Number-*" "perl-Text-*"
-
-# now install epel and try it on packages raw centos won't install
+# this is the stuff CentOS and EPEL will install by themselves
 
 sudo yum -y install epel-release
 
-sudo yum -y --skip-broken install R alpine ast audacity curlftpfs erfa fuse-encfs fuse-sshfs fvwm getmail libnova nagios-plugins ncftp onionshare p7zip parallel "perl-MIME-*" "perl-Math-*" perl-OpenGL perl-utf8-all pidgin privoxy pyephem python2-astropy qgis qhull recordmydesktop stellarium tor unrtf x11vnc xcalc xdotool xemacs xemacs-packages-extra-el xpdf mariadb-server rh-java-common-lucene
+sudo yum -y --skip-broken install ImageMagick aspell elinks emacs enscript expect firefox ftp gnuplot gpm graphviz html2ps lynx mrtg nano nmap ntpdate perl-CPAN postgresql postgresql-server rdate samba screen sendmail sendmail-cf tcsh telnet tigervnc tigervnc-server tk tmpwatch wget xinetd xorg-x11-apps xsane xterm yum-utils zlib-devel zlib-static "perl-Digest-*" "perl-Date-*" "perl-DateTime-*" "perl-File-*" perl-GD "perl-HTML-*" "perl-HTTP-*" "perl-IO-*" "perl-IPC-*" perl-Inline "perl-JSON-*" "perl-LWP-*" "perl-Net-*" "perl-Number-*" "perl-Text-*" R alpine ast audacity curlftpfs erfa fuse-encfs fuse-sshfs fvwm getmail libnova nagios-plugins ncftp onionshare p7zip parallel "perl-MIME-*" "perl-Math-*" perl-OpenGL perl-utf8-all pidgin privoxy pyephem python2-astropy qgis qhull recordmydesktop stellarium tor unrtf x11vnc xcalc xdotool xemacs xemacs-packages-extra-el xpdf mariadb-server rh-java-common-lucene
 
-# still to go: sudo yum -y install PySolFC bindfs dosbox dosemu esmtp-local-delivery esniper feh fuse-zip gnumeric libpuzzle libpuzzle-devel lucene mariadb-server perl-Flickr-* perl-Imager python3-astropy rdesktop recoll rxvt sagemath-notebook snownews stella vice xteddy xv vlc mplayer mencoder
+# still to go: sudo yum -y install PySolFC bindfs dosbox dosemu esmtp-local-delivery esniper feh fuse-zip gnumeric libpuzzle libpuzzle-devel perl-Flickr-* perl-Imager python3-astropy rdesktop recoll rxvt sagemath-notebook snownews stella vice xteddy xv vlc mplayer mencoder
 
 # TODO: figure out Perl stuff I need
 
