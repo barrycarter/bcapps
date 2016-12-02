6d8faeebeba2e6e2a04060af76ca4a36118e0393
Barry Carter <carter.barry@gmail.com>
2016-12-02 10:16:48 -0700

    Checkpoint

---
+++ STACK/bc-coin-flip.m
@@ -0,0 +1,25 @@
+(*
+
+http://math.stackexchange.com/questions/6195/why-is-this-coin-flipping-probability-problem-unsolved/659670#659670
+
+*)
+
+(*
+
+upto 2 flips:
+
+
+H - stop with gain (50% chance)
+
+TH - stop with 50% pull
+
+TT - stop with 0% pull
+
+if currently at k/n
+
+1/2*(k+1)/(n+1) + 1/4*(k+2...... nope
+
+
+
+
+*)
