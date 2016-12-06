26441920d242d23bccfb91a12a690c58134af8b7
Barry Carter <carter.barry@gmail.com>
2016-12-06 13:30:55 -0700

    Checkpoint: working

--- ASTRO/bc-zodiac.c
+++ ASTRO/bc-zodiac.c
@@ -16,30 +16,32 @@
 #include "SpiceZfc.h"
 #define MAXWIN 200000
 
-// compute mercury (NAIF 1) ecliptic coords from earth as test
-void test (SpiceDouble et, SpiceDouble *value) {
+// returns the ecliptic longitude of planet at et, as viewed from Earth
+
+// TODO: I could make this more "NASA-like" by having it change a
+// pass-by-reference variable and return both latitude and longitude
+// and even distance (full spherical coords)
+
+SpiceDouble ecliptic_longitude (SpiceInt planet, SpiceDouble et) {
 
   // array to hold the XYZ and lt results from spkezp_c
   SpiceDouble res[3];
   SpiceDouble lt;
 
-  spkezp_c(1, et, "ECLIPDATE", "LT+S", 399, res, &lt);
+  spkezp_c(planet, et, "ECLIPDATE", "LT+S", 399, res, &lt);
 
   printf("%f -> %f %f %f\n", et, res[0], res[1], res[2]);
 
-  *value = res[2];
+  return atan2(res[1],res[0]);
+
 }
 
 int main (int argc, char **argv) {
 
-  SpiceDouble v[3];
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/000157.html");
 
   // should be 30 eclip long
-  test(unix2et(1490980772),v);
+  printf("%f\n", ecliptic_longitude(1, unix2et(1490980772))*dpr_c());
 
-  //  for (int i=0; i<366*86400; i+=86400) {
-  //    test(unix2et(i), v);
-  //  }
 }
