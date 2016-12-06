5af4d84ee94461f2016feed0ce937b3e046bea22
Barry Carter <carter.barry@gmail.com>
2016-12-06 14:28:22 -0700

    checkpoint

--- ASTRO/bc-zodiac.c
+++ ASTRO/bc-zodiac.c
@@ -32,6 +32,24 @@ SpiceDouble ecliptic_longitude (SpiceInt planet, SpiceDouble et) {
   return atan2(res[1],res[0]);
 }
 
+// returns the distance to the nearest cusp (multiple of 30 degrees)
+// for a given planet/time
+
+SpiceDouble distance_to_cusp (SpiceInt planet, SpiceDouble et) {
+  printf("LONG: %f, MOD: %f, ABS: %f\n",
+	 ecliptic_longitude(planet, et)*dpr_c(),
+	 fmod(ecliptic_longitude(planet, et), halfpi_c()/3)*dpr_c(),
+	 fabs(fmod(ecliptic_longitude(planet, et), halfpi_c()/3))*dpr_c()
+	 );
+
+  return 15*rpd_c()-fabs(fabs(fmod(ecliptic_longitude(planet, et), 30*rpd_c()))-15*rpd_c());
+
+  // TODO: there MUST be a better way to write this!
+  //  return fabs(fabs(fmod(ecliptic_longitude(planet, et), 30*rpd_c()))-15*rpd_c());
+}
+
+
+
 int main (int argc, char **argv) {
 
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
@@ -39,6 +57,8 @@ int main (int argc, char **argv) {
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/000157.html");
 
   for (int i=1; i<=366; i++) {
-    printf("%d %f\n", i, ecliptic_longitude(1, unix2et(86400*(i-1)+1483228800))*dpr_c());
+    printf("%d %f %f\n", i, 
+	   ecliptic_longitude(1, unix2et(86400*(i-1)+1483228800))*dpr_c(),
+	   distance_to_cusp(1, unix2et(86400*(i-1)+1483228800))*dpr_c());
   }
 }
