dbee6787c57f5896508a690b9778507a04001b3a
Barry Carter <carter.barry@gmail.com>
2016-12-08 18:01:21 -0700

    As sent to NASA

--- ASTRO/bc-zodiac-fail.c
+++ ASTRO/bc-zodiac-fail.c
@@ -8,18 +8,17 @@
 
 int main() {
 
-  SpiceDouble et, lt, pos[3];
+  SpiceDouble et;
+  SpiceDouble lt;
+  SpiceDouble pos[3];
 
   furnsh_c("/home/barrycarter/SPICE/KERNELS/de431_part-1.bsp");
-  furnsh_c("/home/barrycarter/BCGIT/ASTRO/000157.html");
   furnsh_c("/home/barrycarter/SPICE/KERNELS/naif0011.tls");
 
   str2et_c("BC 9998-08-20 00:00:00", &et);
-  printf("ET: %f\n",et);
-  spkgeo_c(10, et, "ECLIPDATE", 399, pos, &lt);
-  printf("ET: %f, LT: %f\n",et);
-
-  printf("ET: %f POS: %f, %f, %f\n", et, pos[0], pos[1], pos[2]);
+  printf("ALPHA: %f\n",et);
+  spkgeo_c(10, et, "J2000", 399, pos, &lt);
+  printf("BETA: %f\n",et);
 
   return 0;
 }
