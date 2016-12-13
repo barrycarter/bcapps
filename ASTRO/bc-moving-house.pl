c5671519dc1c75d6168e9206f19bf2c40ef36975
Barry Carter <carter.barry@gmail.com>
2016-12-13 14:33:01 -0700

    Checkpoint: about to changeup bc-zodiac.c and more

--- ASTRO/bc-moving-house.pl
+++ ASTRO/bc-moving-house.pl
@@ -4,6 +4,7 @@
 
 require "/usr/local/lib/bclib.pl";
 
+# TODO: check initial state!
 %state = ("S" => 1, "M" => 3, 1 => 1, 2 => 2, 4 => 2, 5 => 1, 6 => 6);
 
 @planets = split(//, "SM12456");
@@ -16,7 +17,7 @@ while (<A>) {
 
   s/^\s*//;
   my(@f) = split(/\s+/, $_);
-  my($ptime, $time, $delta) = ($f[0], $f[6], $f[7]);
+  my($ptime, $time, $delta) = (join(" ",@f[0..2]), $f[7], $f[8]);
 
   # interpret delta
   my($planet, $house) = split(//, $delta);
