b569e880e3820b8b1974a343eefbcb85565c2cc7
Barry Carter <carter.barry@gmail.com>
2016-12-23 10:14:37 -0700

    Checkpoint: about to bg rc-local to prevent hang

--- BRIGHTON/bc-servicectl.sh
+++ BRIGHTON/bc-servicectl.sh
@@ -1,10 +1,5 @@
 # list of services that can be disabled or masked safely
 
-
-# disabling mariadb and postgresql here (though they may not have even
-# started yet) because they dont start properly under systemctl --
-# hack start in rc.local
-
-systemctl disable auditd NetworkManager tuned cups wpa_supplicant irqbalance avahi-daemon vboxadd-service mariadb postgresql
+systemctl disable auditd NetworkManager tuned cups wpa_supplicant irqbalance avahi-daemon vboxadd-service
 
 systemctl mask gssproxy alsa-state
