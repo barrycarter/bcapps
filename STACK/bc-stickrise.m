041291db66ec3d7c196fa7f0ea0c065e9cea1c30
Barry Carter <carter.barry@gmail.com>
2017-01-05 13:51:19 -0700

    Checkpoint

--- STACK/bc-stickrise.m
+++ STACK/bc-stickrise.m
@@ -37,13 +37,40 @@ TODO: actually list parametric plot so we can see distances and stuff
 
 helper function below makes az range 0-360 because we want center
 
-(* fix[pair_] := {pair[[1]]+2*Pi, pair[[2]]} *)
+fix[pair_] := {Mod[pair[[1]],2*Pi], pair[[2]]}
 
-t = Table[N[decHaLat2azEl[0,ha,35*Degree]]/Degree,{ha,-Pi,Pi,2*Pi/48}]
-ListPlot[t]
+t = Table[N[fix[decHaLat2azEl[0,ha,35*Degree]]]/Degree,{ha,-Pi,Pi,2*Pi/48}]
+ListPlot[t, PlotRange -> {{0,360}, {-55,55}}]
+showit
+
+t = Table[N[fix[decHaLat2azEl[0,ha,35*Degree]]]/Degree,{ha,-Pi,Pi,2*Pi/48}]
+ListPlot[t, PlotRange -> {{0,360}, {-55,55}}]
+showit
+
+tab[dec_] = Table[N[fix[decHaLat2azEl[dec,ha,35*Degree]]]/Degree,
+ {ha,-Pi,Pi,2*Pi/48}];
+
+(* hours and degrees *)
+
+fix[pair_] := {Mod[pair[[1]]/Pi*180,360], pair[[2]]/Degree}
+
+tab[dec_] = Table[fix[decHaLat2azEl[dec,ha,35*Degree]], {ha,-Pi,Pi,2*Pi/48}];
+
+pts[dec_] = Table[
+ Text[ToString[Mod[ha/Pi*12+12,24]],
+ fix[decHaLat2azEl[dec,ha,35*Degree]]], {ha,-Pi,Pi,2*Pi/24}];
+Graphics[pts[0]]
+showit
+
+g1 = ListPlot[{tab[-23.5*Degree], tab[0*Degree], tab[23.5*Degree]}];
+g2 = Graphics[{pts[-23.5*Degree], pts[0*Degree], pts[23.5*Degree]}];
+Show[{g1,g2}]
 showit
 
 
+
+
+TODO: label points with hours!
 
 
 *)
