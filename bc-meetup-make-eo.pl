99d176ccd5bb26f9750ba5cac0c65e7792d1fe3b
Barry Carter <carter.barry@gmail.com>
2017-01-11 11:51:24 -0700

    Checkpoint

---
+++ bc-meetup-make-eo.pl
@@ -0,0 +1,12 @@
+#!/bin/perl
+
+# Given a download of your membership list from meetup.com, generates
+# an iMarcos script that visits each users page and turns them into an
+# event organizer
+
+require "/usr/local/lib/bclib.pl";
+
+while (<>) {
+  debug("THUNK: $_");
+}
+
