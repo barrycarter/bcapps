b5f014b739a6fe69b79427d48bb6575ca4974ff2
Barry Carter <carter.barry@gmail.com>
2017-01-12 07:38:30 -0700

    Checkpoint

--- bc-meetup-make-eo.pl
+++ bc-meetup-make-eo.pl
@@ -1,12 +1,25 @@
 #!/bin/perl
 
-# Given a download of your membership list from meetup.com, generates
-# an iMarcos script that visits each users page and turns them into an
-# event organizer
+# uses meetup's API to make everyone in one of my groups an event
+# organizer (see
+# https://www.meetup.com/help/customer/portal/articles/868742-who-can-add-meetups-/
+# for why this is necessary)
 
 require "/usr/local/lib/bclib.pl";
+require "/home/barrycarter/bc-private.pl";
 
-while (<>) {
-  debug("THUNK: $_");
-}
+# this is the group URL, can probably get group number if I want it,
+# but not needed?
 
+my($gurl) = "Socially-Awkward-People-Want-to-Socialize-SAPWWS";
+
+my($cmd) ="curl 'https://api.meetup.com/2/members?group_urlname=$gurl&key=$private{meetup}{key}'";
+
+# TODO: 86400 for testing only
+my($out,$err,$res) = cache_command2($cmd, "age=86400");
+
+my(%hash) = JSON::from_json($out);
+
+debug(%hash);
+
+# debug("OUT: $out");
