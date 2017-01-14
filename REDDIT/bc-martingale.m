0ad0200190f2dc67bf7753e9c90a916d22f14637
Barry Carter <carter.barry@gmail.com>
2017-01-14 08:56:48 -0700

    Checkpoint

---
+++ REDDIT/bc-martingale.m
@@ -0,0 +1,23 @@
+(*
+
+https://www.reddit.com/r/HomeworkHelp/comments/5npsfx/algebra_gambling_word_problem/
+
+Problem provided by instructor.
+
+"You are a plumber who makes $20 an hour. Near where you live, there is a casino which offers a game in which your money bet is doubled with a rate of success at 50%. A game takes 30 seconds to play. Given that information, what is the most efficient way to generate $2,000?"
+
+I don't know how to approach this. Please help! Thank you!
+
+*)
+
+(* probability of martingale success using multiplier 3, start at 1c
+bet, init cash is 500 [and then expand], 2000 target *)
+
+(* earn 1c and then double to 2000 *)
+
+Log[2000/.01]/Log[2]
+
+so 9m to play and 1.8s to work, 2^-18 victory chance
+
+
+
