bb070e4378d036af41116d0dc7aae6852236f699
Barry Carter <carter.barry@gmail.com>
2017-01-17 06:06:59 -0700

    Checkpoint

--- bc-wikidata-from-dump.pl
+++ bc-wikidata-from-dump.pl
@@ -6,6 +6,20 @@ require "/usr/local/lib/bclib.pl";
 
 open(A,"/home/barrycarter/20150530/20150525.json");
 
+# this skips the "[" line
+<A>;
+
+while (<A>) {
+  chomp();
+  s/\,$//;
+#  debug("THUNK: $_");
+  my($json) = JSON::from_json($_);
+  debug(var_dump("JSON",$json));
+  die "TESTING";
+}
+
+die "TESTING";
+
 read(A,$buf,1000000);
 
 $buf=~s/\{\"id\":\"Q8\".*$//s;
@@ -14,4 +28,22 @@ $buf=~s/,$//;
 
 debug(var_dump("hash",JSON::from_json($buf)));
 
+=item comment
 
+Useful entity things (JSON is object)
+
+JSON->{'aliases'}->{'en'}->[0]->{'value'} = 'cosmos'; 
+JSON->{'aliases'}->{'en'}->[1]->{'value'} = 'The Universe'; 
+
+# P1036 is Dewey Decimal Classification
+JSON->{'claims'}->{'P1036'}->[0]->{'mainsnak'}->{'datavalue'}->{'value'} = '113\';
+
+JSON->{'descriptions'}->{'en'}->{'value'} = 'totality of planets, stars, galaxies, intergalactic space, or all matter or all energy';
+
+JSON->{'labels'}->{'en'}->{'value'} = 'universe';
+
+JSON->{'sitelinks'}->{'enwiki'}->{'title'} = 'Universe';
+
+JSON->{'type'} = 'item';
+
+=cut
