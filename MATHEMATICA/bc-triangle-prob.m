5e3d19ea8314bdd43abc2f8edf02521ed7f0aa4e
Barry Carter <carter.barry@gmail.com>
2017-01-21 12:25:52 -0700

    Checkpoint

--- MATHEMATICA/bc-triangle-prob.m
+++ MATHEMATICA/bc-triangle-prob.m
@@ -14,27 +14,39 @@ cosb[z_] = (1-Re[z])/lena[z]
 
 (* NOTE: could probably also get this from (s-a)*(s-b)... formula *)
 
+(* TODO: maybe use C = Pi-A-B or something? *)
+
 (* see where the altitude dropped from A intersects BC and measure
 distance from C... then divide by b, the hypotenuse of the triangle *)
 
 (* TODO: for symmetry, should I drop the altitude from B? *)
 
-cosc[z_] = len[alta[z][1]-z]/lenb[z]
+cosc[z_] = Abs[alta[z][1]-z]/lenb[z]
 
+(* note this can be off a sign; if x is the cosine of an angle, this
+is the cosine of half that angle *)
 
+coshalf[x_] = Sqrt[(1+x)/2]
+
+medalen[z]*coshalf[cosa[z]] +
+medblen[z]*coshalf[cosb[z]] +
+medclen[z]*coshalf[cosc[z]]
 
 f[z_] = FullSimplify[
-medalen[z]*Re[z]/lenb[z] + 
- medblen[z]*(1-Re[z])/lena[z] + 
- medclen[z]*Abs[z-alta[z][1]]/lenb[z] -
+ medalen[z]*coshalf[cosa[z]] +
+ medblen[z]*coshalf[cosb[z]] +
+ medclen[z]*coshalf[cosc[z]] -
  3/4*(lena[z]+lenb[z]+lenc[z]),
  Element{z,Complexes}
 ]
 
-
-
-
-
+g[z_] = FullSimplify[
+ (medalen[z]*coshalf[cosa[z]] +
+ medblen[z]*coshalf[cosb[z]] +
+ medclen[z]*coshalf[cosc[z]]) / 
+ (lena[z]+lenb[z]+lenc[z]),
+ Element{z,Complexes}
+]
 
 Re[z]/b + 
 
