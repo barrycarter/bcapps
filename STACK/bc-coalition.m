7f08784fdb463b395283d759f799974b9e698364
Barry Carter <carter.barry@gmail.com>
2017-02-04 10:43:59 -0700

    Slow but workable

--- STACK/bc-coalition.m
+++ STACK/bc-coalition.m
@@ -4,9 +4,45 @@
 
 *)
 
+Clear[num];
 num[{}, 0] := 1
-num[s_, 0 ] := 0
-num[s_, n_] := Sum[num[Delete[s,i], n-s[[i]]],{i,1,Length[s]}]
+num[{}, n_] := 0
+num[s_, n_] := num[s,n] = If[n<0, 0, Sum[
+ num[Delete[s,i],n] + 
+ If[n<s[[i]],0,num[Delete[s,i], n-s[[i]]]],{i,1,Length[s]}]/Length[s]]
+
+num[{1,2,3},6]
+
+
+(* above works, below tests *)
+
+t1024 = Table[i,{i,1,20}]
+
+num[t1024, 2]
+
+
+
+
+num[{1,2,3},6]
+
+num[{1,2},3]
+
+num[{2}, 3] + num[{2}, 2] + num[{1}, 3] + num[{1}, 1]
+
+num[{1,2,4,7}, 7]
+?num
+
+
+
+(* wrong approach above? *)
+
+num[{1,2,4,7}, 7] = ...
+
+{2,4,7} to 6 + {1,4,7} to 5 + {1,2,7} to 7 + {1,2,4} to 7
+
+
+
+
 
 
 
