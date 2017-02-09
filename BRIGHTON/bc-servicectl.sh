f8be8990a73d3595681cd983c04c541ee2403214
Barry Carter <carter.barry@gmail.com>
2017-02-09 13:59:19 -0700

    Checkpoint

--- BRIGHTON/bc-servicectl.sh
+++ BRIGHTON/bc-servicectl.sh
@@ -5,18 +5,30 @@
 # you get "Failed to execute operation: No such file or directory" and
 # the entire command appears to fail
 
-# TODO: consider turning these off one at a time which solves above
-# problem and seems cleaner + can redirect output if needed
+# TODO: perl loop for below (so I can stop and disable and log)?
 
-systemctl disable auditd NetworkManager tuned cups wpa_supplicant irqbalance avahi-daemon vboxadd-service rpc-statd.service rpcbind.service abrt
+systemctl disable auditd
+systemctl disable NetworkManager
+systemctl disable tuned
+systemctl disable cups
+systemctl disable wpa_supplicant
+systemctl disable irqbalance
+systemctl disable avahi-daemon
+systemctl disable vboxadd-service
+systemctl disable rpc-statd.service
+systemctl disable rpcbind.service
+systemctl disable abrt-oops
+systemctl disable abrtd
+systemctl disable abrt-xorg
+
 
 # TODO: this is temporary until I set up email correctly
 systemctl disable sendmail
 
-systemctl mask gssproxy alsa-state
+systemctl mask gssproxy 
+systemctl mask alsa-state
 
 # and ones I want that arent default
 
-systemctl enable dnsmasq httpd
-
-
+systemctl enable dnsmasq 
+systemctl enable httpd
