b96d1eac7ea9012b448da67e77a513a5ab0b9d2b
Barry Carter <carter.barry@gmail.com>
2017-04-28 07:09:37 -0600

    Updates, since I'm running dullon/brighton in "hybrid" mode (ugly!)

---
+++ BACKUP/dumpall-brighton.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# This trivial script just runs bc-unix-dump.pl on all my drives
+# TODO: automate this if I add new drives etc
+
+# TODO: if I have a drive list somewhere for bc-rev-search, use it
+# here as well?
+
+# NOTE: running these all at the same time kills the system CPU-wise
+# if I happen to be on at that time, so changing these to run
+# sequentially, and not in parallel
+
+# note: some of these drives don't exist, but running bc-unix-dump.pl
+# on them is harmless, since it simply creates near empty files in the
+# mount directories
+
+# doing root drive first, since others may fail
+/home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl / bcunix
+
+# removing below since its now a symlink
+# /home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl /mnt/extdrive extdrive
+
+# some of these drives no longer exist, but thats ok because their
+# mount points are essentially empty
+
+# extdrive2 and 5 are sshfs mounted from dullon; dullon handles dumps
+# /home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl /mnt/extdrive2 extdrive2
+# /home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl /mnt/extdrive5 extdrive5
+
+/home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl /mnt/extdrive3 extdrive3
+/home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl /mnt/extdrive4 extdrive4
+/home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl /mnt/extdrive6 extdrive6
+
+# added for kemptown, which is hereby dubbed extdrive7
+
+/home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl /mnt/extdrive7 extdrive7
+
+# /mnt/extdrive4 later decomissioned
+# TODO: find more general way of deciding which drives to "dump"
+# /home/barrycarter/BCGIT/BACKUP/bc-unix-dump.pl /mnt/extdrive4 extdrive4
+
+# bc-unix-dump.pl won't work on /mnt/sshfs, which is HFS+, but I hope
+# to convert it over at some point
---
+++ bc-weekly-backup-brighton.pl
@@ -0,0 +1,89 @@
+#!/bin/perl
+
+# I make weekly backups of "important" files to a 4G flash drive. Most
+# of these are symlinks in /usr/local/etc/weekly-backups/files, but
+# some are generated. This program generates the necessary files, and
+# then tar/gpg's them for burning to flash drive
+
+# removing bzip of ls dumps because:
+# 1. interferes with overwriting [I could fix this]
+# 2. i have enough room to store them uncompressed
+# 3. i use them often enough that it's efficient to have them uncompressed
+# 4. saves time and CPU, making this proggie more efficient
+
+# as of 10 Jan 2015, putting bcunix file dumps in root dir
+
+require "/usr/local/lib/bclib.pl";
+require "/home/barrycarter/bc-private.pl";
+my($dir) = "/usr/local/etc/weekly-backups/files";
+
+# root only, and run at nice level 19
+if ($>) {die("Must run as root");}
+system("/usr/bin/renice 19 -p $$");
+
+# TODO: call subroutines!
+# TODO: all subroutines to run in parallel
+
+debug("CALLING dump_ls()");
+dump_ls();
+debug("CALLING dump_remote()");
+dump_remote();
+debug("CALLING dump_other()");
+dump_other();
+
+# TODO: tar the whole thing up, and encrypt it
+
+# <h>thanks shoeshine boy, you're humble and loveable</h>
+debug("ALL FINISHED, SIR");
+
+# dump filelists from all my machines (useful to see what files I'm
+# missing in case of disaster recovery)
+# subroutinizing this, since it takes forever (and so its nice to test
+# without running this each time)
+sub dump_ls {
+
+  # for remote machine (which no longer exists, but...)
+  # dropped 23 Dec 2015
+#  system('ssh root\@bcmac "/mnt/sshfs/bcmac-dump.sh 1> /tmp/bcdump.out 2> /tmp/bcdump.err &"');
+  # for extdrives and main drive
+  system('sh /home/barrycarter/BCGIT/BACKUP/dumpall-brighton.sh &');
+}
+
+# various other things I dumps
+sub dump_other {
+  local(*A);
+  # TODO: test that these commands work, they sometimes appear to dump out small files, perhaps due to errors
+  my($str) = << "MARK";
+mysqldump --skip-extended-insert=yes test > $dir/bcunix-mysql-test.txt.new; mv -f $dir/bcunix-mysql-test.txt $dir/bcunix-mysql-test.txt.old; mv -f $dir/bcunix-mysql-test.txt.new $dir/bcunix-mysql-test.txt
+rpm -qai > $dir/bcunix-rpmqai.txt.new; mv -f $dir/bcunix-rpmqai.txt $dir/bcunix-rpmqai.txt.old;  mv -f $dir/bcunix-rpmqai.txt.new $dir/bcunix-rpmqai.txt
+yum list > $dir/yumlist.txt.new; mv -f yumlist.txt yumlist.txt.old; mv -f yumlist.txt.new yumlist.txt
+pg_dumpall --host=/var/tmp > $dir/bcunix-pg-backup.txt.new; mv -f $dir/bcunix-pg-backup.txt $dir/bcunix-pg-backup.txt.old; mv -f $dir/bcunix-pg-backup.txt.new $dir/bcunix-pg-backup.txt
+MARK
+;
+  debug("STR: $str");
+  # TODO: this is broken, I can't mv while dumping, at least not safely
+  open(A,"|parallel -j 10");
+  print A $str;
+  close(A);
+}
+
+# connetions outside my LAN
+sub dump_remote {
+  local(*A);
+  # TODO: keep backup copies(?)
+  my($str) = << "MARK";
+ssh -i /home/barrycarter/.ssh/id_rsa.bc root\@bcinfo3 'mysqldump --skip-extended-insert=yes --databases wordpress requests' > $dir/bcinfo3-wordpress-requests.txt.new; mv -f $dir/bcinfo3-wordpress-requests.txt $dir/bcinfo3-wordpress-requests.txt.old;  mv -f $dir/bcinfo3-wordpress-requests.txt.new $dir/bcinfo3-wordpress-requests.txt
+rsync -trlzo -e 'ssh -i /home/barrycarter/.ssh/id_rsa.bc' root\@bcinfo3:/sites/DB/requests.db $dir/bcinfo3-requests.db
+rsync -trlzo -e 'ssh -i /home/barrycarter/.ssh/id_rsa.bc' root\@bcinfo3:/sites/DB/gocomics-dump.txt.bz2 $dir/bcinfo3-gocomics-dump.txt.bz2
+# rsync "root\@bcmac:/Users/*/*" "root\@bcmac:/Users/*/.*" bcmac/
+MARK
+;
+  debug("STR: $str");
+  open(A,"|parallel -j 10");
+  print A $str;
+  close(A);
+}
+
+# TODO: run program at least twice to make sure file overwrites work
+
+# TODO: rename files after checking (new -> cur -> old)
