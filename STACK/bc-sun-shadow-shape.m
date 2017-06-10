ee0d56c0f4a2243d4b5ad544a14499942a18aa30
Barry Carter <carter.barry@gmail.com>
2017-06-10 09:42:14 -0600

    Checkpoint: working at last!

--- STACK/bc-sun-shadow-shape.m
+++ STACK/bc-sun-shadow-shape.m
@@ -1,3 +1,28 @@
+(*** CANON FORMULAS START HERE ***)
+
+showit := Module[{file}, file = StringJoin["/tmp/math", 
+       ToString[RunThrough["date +%Y%m%d%H%M%S", ""]], ".gif"]; 
+     Export[file, %]; 
+     Run[StringJoin["display -update 1 ", file, "&"]]; Return[file]; ]
+
+lineBetweenPoints[p_, x0_, y0_, z0_, x1_, y1_, z1_] =
+ {x0 + p*(x1-x0),  y0 + p*(y1-y0),  z0 + p*(z1-z0)}
+
+lineHitsXY[x0_, y0_, z0_, x1_, y1_, z1_] = 
+Simplify[Take[lineBetweenPoints[z0/(z0-z1), x0, y0, z0, x1, y1, z1],2]]
+
+(* front half is theta from -Pi/2 to Pi/2, Phi from -Pi to Pi *)
+
+point[x_, z_, r_, theta_,phi_] = 
+ {x + r*Cos[theta]*Cos[phi], r*Sin[theta]*Cos[phi], z + Sin[phi]}
+
+sunHitsXY[x_, z_, r_, phi_, theta_] = 
+ Apply[lineHitsXY, Flatten[{point[x,z,r,theta,phi],0,0,1}]]
+
+(*** CANON FORMULAS END HERE ***)
+
+
+
 (*
 
 What is the shape of the shadow cast by the sun on a single point; not necessarily a circle of even an ellipse
@@ -40,37 +65,47 @@ Table[Take[Out[91] /. {x -> -2, z -> 2, r -> 0.1},2], {theta,-Pi/2,Pi/
 
 TODO: above is wrong, need to simplify and build this up
 
-lineBetweenPoints[p_, x0_, y0_, z0_, x1_, y1_, z1_] =
- {x0 + p*(x1-x0),  y0 + p*(y1-y0),  z0 + p*(z1-z0)}
-
 (* value of p where line hits XY plane and then the xy coords of it *)
 
 p0906 = Solve[z0 + p*(z1-z0) == 0, p]
 
 (* it's z0/(z0-z1) which I prob could've figured out... *)
 
-lineHitsXY[x0_, y0_, z0_, x1_, y1_, z1_] = 
-Simplify[Take[lineBetweenPoints[z0/(z0-z1), x0, y0, z0, x1, y1, z1],2]]
-
-front half is theta from -Pi/2 to Pi/2, Phi from -Pi to Pi
-
-point[x_, z_, theta_,phi_] = 
- {-x + r*Cos[theta]*Cos[phi], r*Sin[theta]*Cos[phi], z + Sin[phi]}
-
 Apply[lineHitsXY, Flatten[{point[x,z,theta,phi],0,0,1}]]
-
-sunHitsXY[x_, z_, r_, phi_, theta_] = 
- Apply[lineHitsXY, Flatten[{point[x,z,theta,phi],0,0,1}]]
 
 sunHitsXY[-2, 2, 1, 0, 0]
 
 NOT WORKING!
 
+lineBetweenPoints[p, -2+1, 0, 2, 0, 0, 1]
 
+lineHitsXY[-2+1, 0, 2, 0, 0, 1]
 
-
+point[-2, 2, 0, 0]
 
 ZWhereLineHitsXYPlane[x0_,y0_,z0_,x1_,y1_,z1_] = 
+
+(* tests *)
+
+t0931 = Flatten[Table[sunHitsXY[-2,2,1,phi,theta], 
+ {phi,-Pi/2, Pi/2, 0.1}, {theta, -Pi/2, Pi/2, 0.1}],1]
+
+ListPlot[t0931, PlotRange -> All]
+showit
+
+t0934 = Flatten[Table[sunHitsXY[-2,100,1,phi,theta], 
+ {phi,-Pi/2, Pi/2, 0.1}, {theta, -Pi/2, Pi/2, 0.1}],1]
+
+t0934 = Flatten[Table[sunHitsXY[-2,100,1,phi,theta], 
+ {phi,-Pi/2, Pi/2, 0.01}, {theta, -Pi/2, Pi/2, 0.01}],1];
+
+ListPlot[t0934, PlotRange -> All]
+
+ParametricPlot[sunHitsXY[-2,100,1,0,theta], {theta, -Pi/2, Pi/2},
+ PlotRange -> All, AxesOrigin -> {0,0}]
+
+
+
 
 
 
