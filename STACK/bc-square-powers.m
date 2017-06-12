c06f087ee40d006b5fd3e8ac79347fb21e8c3229
Barry Carter <carter.barry@gmail.com>
2017-06-11 23:48:21 -0600

    Checkpoint

--- STACK/bc-square-powers.m
+++ STACK/bc-square-powers.m
@@ -13,9 +13,15 @@ TODO: from a>1, b>1 only
 ContourPlot[f[a,b],{a,1,100},{b,1,100}, PlotLegends -> True, Contours -> 25,
  ColorFunction -> Hue]
 
+ContourPlot[f[a,n*a],{a,1,100},{n,0,1}, PlotLegends -> True, Contours -> 25,
+ ColorFunction -> Hue]
+
 
 ContourPlot[f[a,b],{a,0,1},{b,0,1}, PlotLegends -> True, Contours -> 25,
  ColorFunction -> Hue]
+
+ContourPlot[Log[f[a,b]]/f[a,b],{a,1,100},{b,1,100}, 
+ PlotLegends -> True, Contours -> 64, ColorFunction -> Hue]
 
 ContourPlot[f[a,b],{a,1,100},{b,1,100}, PlotLegends -> True, Contours -> 64,
  ColorFunction -> Hue]
@@ -33,6 +39,67 @@ t2132 = Table[{x,f[x,2*x]},{x,2,500}];
 
 Fit[t2132, {1,1/x}, x]
 
+https://math.stackexchange.com/questions/2319162/a-mathmatical-way-to-solve-for-x-in-a-b2-ax-bx
 
+Solve[(a+a)^2 == a^x + a^x, x][[1,1,2]]
+
+Plot[2^(1/(f[x,x]-2))/x,{x,1,100}]
+
+above is constantly 1
+
+Plot[2^(1/(f[x,5*x]-2))/x,{x,1,100}]
+
+Plot[2^(1/(f[x,5*x]-2))/x,{x,1,100}]
+
+Plot[f[x,2*x]/f[x,x], {x,1,100}, PlotRange -> All]
+
+Plot[f[x,1.01*x]/f[x,x], {x,1,100}, PlotRange -> All]
+
+Plot[(1/(f[x,x]-2))/Log[x],{x,1,100}]                                 
+
+above is also constant
+
+In[230]:= Table[(f[2+h,2]-f[2,2])/h, {h,.001,.01,.001}]                         
+shows derv in first is -1/E
+
+same in second
+
+f[2,2] == 3
+
+ContourPlot[f[a,b]-(3-a/E-b/E),
+ {a,1,100},{b,1,100}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
+
+ContourPlot[f[a,b]-(3-a/E-b/E),
+ {a,1,1000},{b,1,1000}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
+
+ContourPlot[(f[a,b]-(3-a/E-b/E))/(a+b),
+ {a,1,100},{b,1,100}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
+
+ContourPlot[(f[a,b]-(3-a/E-b/E)),
+ {a,1,10},{b,1,10}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
+
+ContourPlot[(f[a,b]-(3-a/E-b/E))/Sqrt[(a^2+b^2)],
+ {a,1,10},{b,1,10}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
+
+ContourPlot[3-f[a,b],
+ {a,1,10},{b,1,10}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
+
+ContourPlot[(3-f[a,b])/Sqrt[a^2+b^2],
+ {a,1,10},{b,1,10}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
+
+ContourPlot[f[a,b]-(2+Log[2]/Log[a]),
+ {a,1,10},{b,1,10}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
+
+ContourPlot[f[a,b]-(2+Log[2]/Log[a]),
+ {a,1,100},{b,1,100}, PlotLegends -> True, Contours -> 64,
+ ColorFunction -> Hue]
 
 
