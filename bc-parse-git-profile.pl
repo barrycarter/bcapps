59e2a5b0ef06c0403274e27f37b9920a151f9eb7
Barry Carter <carter.barry@gmail.com>
2017-07-22 09:25:39 -0600

    Checkpoint

--- bc-parse-git-profile.pl
+++ bc-parse-git-profile.pl
@@ -13,5 +13,26 @@ my($names) = $1;
 $names=~s/^\s*(.*?)\s*\((.*?)\)//;
 my($user, $name) = ($1, $2);
 
-debug("$user/$name");
+# TODO: this could be done more efficiently, since these are similar
+
+$all=~s%<span class="p-org"><div>(.*?)</div></span>%%;
+# TODO: will this inherit $1 above if I don't reset?
+my($org) = $1;
+
+$all=~s%<span class="p-label"><div>(.*?)</div></span>%%;
+my($loc) = $1;
+
+$all=~s%<a href="(.*?)" class="u-url"%%;
+my($url) = $1;
+
+# TODO: github doesnt close this quote properly!
+$all=~s%\"mailto:(.*?)</a>%%;
+my($email) = $1;
+
+# convert to usable form
+$email=~s/\&\#x(.*?)\;/chr(hex($1))/eg;
+
+debug("EM: $email");
+
+
 
