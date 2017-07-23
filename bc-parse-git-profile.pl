1087d289243592ac23ccd85e1f2e008f26828968
Barry Carter <carter.barry@gmail.com>
2017-07-22 19:40:31 -0600

    Cleaning up FLL stuff + more

--- bc-parse-git-profile.pl
+++ bc-parse-git-profile.pl
@@ -10,29 +10,26 @@ my($all) = cmdfile();
 # username and name
 $all=~s%<title>(.*?)</title>%%;
 my($names) = $1;
-$names=~s/^\s*(.*?)\s*\((.*?)\)//;
-my($user, $name) = ($1, $2);
+
+my($user, $name);
+if ($names=~s/^\s*(.*?)\s*\((.*?)\)//) {
+  ($user, $name) = ($1, $2);
+} else {
+  ($user, $name) = ($1, "");
+}
 
 # TODO: this could be done more efficiently, since these are similar
 
-$all=~s%<span class="p-org"><div>(.*?)</div></span>%%;
-# TODO: will this inherit $1 above if I don't reset?
-my($org) = $1;
+# <h>use of ternary operator here is hideous but cute</h>
+my($org) = $all=~s%<span class="p-org"><div>(.*?)</div></span>%%?$1:"";
+my($loc) = $all=~s%<span class="p-label">(.*?)</span>%%?$1:"";
+my($url) = $all=~s%<a href="(.*?)" class="u-url"%%?$1:"";
+my($email) = $all=~s%"mailto:(.*?)"%%?$1:"";
 
-$all=~s%<span class="p-label">(.*?)</span>%%;
-my($loc) = $1;
-
-$all=~s%<a href="(.*?)" class="u-url"%%;
-my($url) = $1;
-
-$all=~s%"mailto:(.*?)"%%;
-my($email) = $1;
+# remove html from org, if any
+$org=~s/<.*?>//g;
 
 # convert to usable form
 $email=~s/\&\#x(.*?)\;/chr(hex($1))/eg;
 
 print qq%"$user","$name","$org","$loc","$url","$email"\n%;
-
-
-
-
