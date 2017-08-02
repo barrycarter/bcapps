47adcbe11d385876fcb9ebfad202b39061e5f1cb
Barry Carter <carter.barry@gmail.com>
2017-08-01 21:43:42 -0600

    Checkpoint

--- ASTRO/bc-eclipse.c
+++ ASTRO/bc-eclipse.c
@@ -15,7 +15,11 @@
 
 int main (int argc, char **argv) {
 
-  SpiceDouble pos[3];
+  furnsh_c("/home/user/BCGIT/ASTRO/standard.tm");
+
+  SpiceDouble pos[3], normal[3], erad[3], north[3], proj[3];
+  SpicePlane plane;
+  SpiceInt dim = 0;
 
   // if insufficient argc, complain, don't just seg fault
   if (argc != 5) {
@@ -23,15 +27,36 @@ int main (int argc, char **argv) {
     exit(-1);
   }
 
+  // process input
   double lat = atof(argv[1]);
   double lon = atof(argv[2]);
   double stime = atof(argv[3]);
   double etime = atof(argv[4]);
 
-  // fixed ITRF93 position of lat/lon
-  // TODO: pull 6378.140 and 6356.755 from SPICE dont hardcode
-  georec_c(lon*rpd_c(), lat*rpd_c(), 0, 6378.140, (6378.140-6356.755)/6378.137, pos);
+  // define north pole (TODO: has to be a better way to do this)
+  north[0] = 0;
+  north[1] = 0;
+  north[2] = 1;
 
+  // radii of Earth (0 and 1 are equatorial, 2 is polar)
+  bodvrd_c("EARTH", "RADII", 3, &dim, erad);
+
+  printf("ERAD (%d): %f %f %f\n", dim, erad[0], erad[1], erad[2]);
+
+  // fixed ITRF93 position of lat/lon
+  georec_c(lon*rpd_c(),lat*rpd_c(),0,erad[0],(erad[0]-erad[2])/erad[0], pos);
+
+  // and the surface normal to this location
+  surfnm_c(erad[0],erad[1],erad[2],pos,normal);
+
+  // and the plane associated with this surface normal
+  nvc2pl_c(normal, 1, &plane);
+
+  // project the "north pole" vector to the plane
+  vprjp_c(north, &plane, &proj);
+
+  printf("PROJ (%f %f): %f %f %f\n", lat, lon, proj[0], proj[1], proj[2]);
   printf("POS (%f %f): %f %f %f\n", lat, lon, pos[0], pos[1], pos[2]);
+  printf("SRFNM (%f %f): %f %f %f\n", lat, lon, normal[0], normal[1], normal[2]);
 
 }
