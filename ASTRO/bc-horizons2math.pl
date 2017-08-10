427baa5c676f71fb354d4feab5f592d9203a194c
Barry Carter <carter.barry@gmail.com>
2017-08-10 17:21:21 -0600

    Checkpoint -- almost got it

--- ASTRO/bc-horizons2math.pl
+++ ASTRO/bc-horizons2math.pl
@@ -1,59 +1,30 @@
 #!/bin/perl
 
-# Converts output of HORIZONS (eg, earth-on-eclipse-day.html.bz2) to
-# Mathematica usable format
+# Converts output of HORIZONS CSV (eg, earth-on-eclipse-day.csv.bz2) to
+# Mathematica usable format (fairly trivial)
 
-# see bc-osculating.pl for comments
+# --fields: which fields desired (ie: 1,2,3,4)
 
 require "/usr/local/lib/bclib.pl";
 
-# TODO: this is really ugly and not useful in general
-# TODO: allow templating and stuff, hard coding for eclipse only
-@fields = ("JD", "X", "Y", "Z");
+my(@fields) = split(/\,/, $globopts{fields});
 
 # skip to start of data
 while (<> !~/\$\$SOE/) {next;}
 
-# to store below
-my($data, $eof);
+while (<>) {
 
-# forever loop (but we do break out of it eventually)
-for (;;) {
+  chomp;
+  my(@f) = split(/\,\s*/, $_);
 
-  my(%hash) = ();
-
-  # TODO: the number of lines varies, need to do this much better
-  # read 4 lines at a time
-  for $i (1..4) {
-    $data = <>;
-    chomp($data);
-    debug("DATA: $data");
-
-    # end of data
-    if ($data=~/\$\$EOE/) {$eof=1; last;}
-
-    # TODO: ugly, first line will be JD
-    if ($i==1) {
-      $data=~s/^\s*(.*?)\s*\=//;
-      $hash{JD} = $1;
-      next;
-    }
-
-    while ($data=~s/^\s*([A-Z]+)\s*\=\s*([0-9E\+\-\.]+)//) {$hash{$1}=$2;}
-  }
-
-  # TODO: this is ugly, can I break out nested loops w/o GOTO?
-  if ($eof) {last;}
-
-  debug("HASH",%hash);
-  my(@data) = ();
+  my(@data);
 
   for $i (@fields) {
-    $hash{$i}=~s/E/*10^/;
-    push(@data, "Rationalize[$hash{$i},0]");
+    $f[$i]=~s/E/*10^/;
+    push(@data, "Rationalize\[$f[$i]\]");
   }
 
-  debug("DATA","<start>",@data,"</start>");
+  print "{",join(", ",@data),"},\n";
 
-  print "{",join(", ",@data),"}, \n";
 }
+
