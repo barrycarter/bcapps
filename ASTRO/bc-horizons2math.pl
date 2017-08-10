ab002bf6ac3a1b186bbdca6fd372c1bc5aceb233
Barry Carter <carter.barry@gmail.com>
2017-08-10 16:50:59 -0600

    Checkpoint (ugly)

---
+++ ASTRO/bc-horizons2math.pl
@@ -0,0 +1,56 @@
+#!/bin/perl
+
+# Converts output of HORIZONS (eg, earth-on-eclipse-day.html.bz2) to
+# Mathematica usable format
+
+# see bc-osculating.pl for comments
+
+require "/usr/local/lib/bclib.pl";
+
+# TODO: this is really ugly and not useful in general
+# TODO: allow templating and stuff, hard coding for eclipse only
+@fields = ("JD", "X", "Y", "Z");
+
+# skip to start of data
+while (<> !~/\$\$SOE/) {next;}
+
+# to store below
+my($data);
+
+# forever loop (but we do break out of it eventually)
+for (;;) {
+
+  my(%hash) = ();
+
+  # TODO: the number of lines varies, need to do this much better
+  # read 4 lines at a time
+  for $i (1..4) {
+    $data = <>;
+    chomp($data);
+    debug("DATA: $data");
+
+    # end of data
+    if ($data=~/^\$\$EOE/) {last;}
+
+    # TODO: ugly, first line will be JD
+    if ($i==1) {
+      $data=~s/^\s*(.*?)\s*\=//;
+      $hash{JD} = $1;
+      next;
+    }
+
+    while ($data=~s/^\s*([A-Z]+)\s*\=\s*([0-9E\+\-\.]+)//) {$hash{$1}=$2;}
+  }
+
+  debug("HASH",%hash);
+  my(@data) = ();
+
+  for $i (@fields) {
+    $hash{$i}=~s/E/*10^/;
+    push(@data, $hash{$i});
+  }
+
+  debug("DATA","<start>",@data,"</start>");
+
+#  print "{",join(", ",@data),"}, \n";
+}
