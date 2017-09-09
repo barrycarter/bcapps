930e905d59e0c8eaa5d798cac99e08e9452e14ba
Barry Carter <carter.barry@gmail.com>
2017-09-09 07:13:15 -0600

    Checkpoint

---
+++ STACK/bc-star-size.m
@@ -0,0 +1,33 @@
+(*
+
+
+https://nasa.tumblr.com/post/150688852794/zodiac
+
+[user@saopaulo ~/20170907]$ perl -nle '/^(\d+),(\d+):\s*\(\s*(\d+)/; print $_; print "GOT: $1 $2 $3"' orion2.txt | less
+
+betel = {52, 76, 84, 129, 196, 230, 254, 239, 254, 255, 248, 251, 236,
+207, 150, 114, 86, 67, 52};
+
+beltll = {74, 78, 157, 250, 251, 242, 246, 184, 100, 74}
+
+
+
+hard to find raw astro!!!
+
+TODO: (lots) Aladin Lite, Bessel function, pixel plotting, second ring
+too faint to notice
+
+rigel = {39, 60, 90, 122, 167, 199, 233, 244, 248, 252, 253, 253, 252,
+228, 208, 200, 164, 107, 67, 38};
+
+Plot[(BesselJ[1,x]/x)^2, {x,-3,3}]
+
+slightly different row below
+
+betel = {47, 76, 84, 121, 207, 231, 251, 252, 254, 253, 254, 250, 246,
+202, 154, 111, 85, 69};
+
+
+
+
+*)
---
+++ bc-tumblr-inline.pl
@@ -0,0 +1,40 @@
+#!/bin/perl
+
+# tumblr_backup.py works great, but doesnt download inline images;
+# this program is a trivial hack that assumes you've run
+# tumblr_backup.py and then attemts to grab inline images, assuming
+# you're storing blogs at /home/user/TUMBLR
+
+require "/usr/local/lib/bclib.pl";
+
+for $i (@ARGV) {
+
+  debug("DOING: $i");
+
+  # TODO: generalize location
+  # TODO: handle errors better?
+  dodie('chdir("/home/user/TUMBLR/$i/posts")');
+
+  # TODO: for large tumblrs, grepping through all posts is tedious,
+  # try to set up an mtime check vs last dld image or something
+
+  open(A,"grep tumblr_inline *|");
+
+  while (<A>) {
+    while (s%"(http://.*?)"%%) {
+      my($url) = $1;
+      unless ($url=~m%/(tumblr_inline.*)$%) {next;}
+      my($fname) = $1;
+      if (-f $fname) {
+	debug("SKIPPING: $url (already dld)");
+	next;}
+      debug("FETCHING: $url");
+    }
+  }
+
+  close(A);
+
+}
+
+
+
