8008c0937d34d0236bc7d95b77df077a2dbee6d5
Barry Carter <carter.barry@gmail.com>
2017-10-10 19:40:00 -0600

    Checkpoint

---
+++ STACK/bc-tetration.m
@@ -0,0 +1,24 @@
+(*
+
+ https://math.stackexchange.com/questions/2466696/how-to-solve-2-tetrated-0-5-times
+
+2^^0.5
+
+A^A = A^^2
+
+A^A^A^A = A^^4
+
+log(A^^n) = A*log(A^^(n-1)) <- not true
+
+log(A^^n) = (A^^(n-1))*log(A)
+
+A^^n = A^(A^^(n-1))
+
+log(A^^n) = (A^^(n-1))*log(A)
+
+
+
+
+
+
+
