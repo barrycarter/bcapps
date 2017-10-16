f22fe084a2ecf7c242b8428957a9976c825e0a6a
Barry Carter <carter.barry@gmail.com>
2017-10-16 17:51:32 -0600

    Checkpoint

--- WATSON/bc-watson-playground.pl
+++ WATSON/bc-watson-playground.pl
@@ -15,15 +15,53 @@ my($url, $user, $pass) = ($auth->{url}, $auth->{username}, $auth->{password});
 
 my($vstring) = "version=2017-09-01";
 
-# this assumes you've created an environment, a configuration inside
-# that environment, and a collection inside the configuration (you can
-# do all these things online using the GUI)
+# this assumes you've created an environment, a configuration, and a
+# collection inside the environment (you can do all these things
+# online using the GUI)
 
 # TODO: caching only during testing
+
+# obtain the environment
 ($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments?$vstring'", "age=300");
-
 my($env) = JSON::from_json($out);
-
-# the 0th environment is the system environment = "Shared system data sources"
 my($envid) = $env->{'environments'}->[1]->{'environment_id'};
 
+debug("ENV: $out","ENVID: $envid");
+
+# and now the collection
+($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections?$vstring'", "age=300");
+my($coll) = JSON::from_json($out);
+my($collid) = $coll->{'collections'}->[0]->{'collection_id'};
+
+debug("COLL: $out","COLLID: $collid");
+
+# and the documents (nope)
+($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid?$vstring'", "age=300");
+debug("OUT: $out");
+
+# the fields
+($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid/fields?$vstring'", "age=300");
+debug("OUT: $out");
+
+# the docs filenames
+($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid/query?$vstring&return=enriched_text.entities.text'", "age=300");
+my($ents) = JSON::from_json($out);
+
+# everything?
+($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid/query?$vstring'", "age=300");
+my($all) = JSON::from_json($out);
+
+debug(var_dump("ALL", $all));
+
+
+# debug("OUT: $out");
+
+
+
+# TODO: configuration might be largely eirrelevant
+
+# and now the configuration inside the environment
+
+# ($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/configurations?$vstring'", "age=300");
+
+# debug("OUT: $out");
