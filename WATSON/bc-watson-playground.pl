57c8f2c25008dc4b48066ecdae34988cc51c1a5e
Barry Carter <carter.barry@gmail.com>
2017-10-16 18:38:24 -0600

    Checkpoint

--- WATSON/bc-watson-playground.pl
+++ WATSON/bc-watson-playground.pl
@@ -25,34 +25,37 @@ my($vstring) = "version=2017-09-01";
 ($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments?$vstring'", "age=300");
 my($env) = JSON::from_json($out);
 my($envid) = $env->{'environments'}->[1]->{'environment_id'};
-
-debug("ENV: $out","ENVID: $envid");
+# debug("ENV: $out","ENVID: $envid");
 
 # and now the collection
 ($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections?$vstring'", "age=300");
 my($coll) = JSON::from_json($out);
 my($collid) = $coll->{'collections'}->[0]->{'collection_id'};
-
-debug("COLL: $out","COLLID: $collid");
+# debug("COLL: $out","COLLID: $collid");
 
 # and the documents (nope)
 ($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid?$vstring'", "age=300");
-debug("OUT: $out");
 
 # the fields
 ($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid/fields?$vstring'", "age=300");
-debug("OUT: $out");
 
 # the docs filenames
-($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid/query?$vstring&return=enriched_text.entities.text'", "age=300");
-my($ents) = JSON::from_json($out);
+# ($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid/query?$vstring&return=enriched_text.entities.text'", "age=300");
+# my($ents) = JSON::from_json($out);
 
 # everything?
-($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid/query?$vstring'", "age=300");
+($out, $err, $res) = cache_command2("curl -u $user:$pass '$url/v1/environments/$envid/collections/$collid/query?$vstring&count=9999'", "age=300");
 my($all) = JSON::from_json($out);
 
-debug(var_dump("ALL", $all));
+# show entities (from 0th book, Colour of Magic in this case)
 
+my($ents) = $all->{'results'}->[0]->{'enriched_text'}->{'entities'};
+
+for $i (@$ents) {
+  debug("$i->{text} ($i->{type}) $i->{count}");
+}
+
+# debug(var_dump("ALL", $all));
 
 # debug("OUT: $out");
 
