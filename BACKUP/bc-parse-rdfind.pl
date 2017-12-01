f37fae0a86a78e883a0380d884c8cce5dbd46369
Barry Carter <carter.barry@gmail.com>
2017-12-01 08:29:33 -0700

    Things are about to get ugly

--- BACKUP/bc-parse-rdfind.pl
+++ BACKUP/bc-parse-rdfind.pl
@@ -8,22 +8,29 @@
 
 require "/usr/local/lib/bclib.pl";
 
+# for permissions purposes (ugly!)
+if ($>) {die("Must be root");}
+
+my($bytes);
+
 while (<>) {
 
   # TODO: ? I could've done this w/ arrays, but ... ?
   # make sure its a symlink recommendation
-  unless (/symlink\s*(.*?)\s+to\s+(.*)$/) {next;}
+  # " to " HAS to be one space because some of my filenames end in spaces
+  # <h>How bad is your life going if you filenames ending in spaces?</h>
+  unless (/symlink\s*(.*?) to (.*)$/) {next;}
 
   my($f1, $f2) = ($1, $2);
 
-  # space omitted below; no apostrophes, quotes, or other types of spaces
+  # no quotes, but spaces/apostrophes ok + other printables (since I quote)
 
-  unless ($f1=~/[\!-~]/) {warn "BAD FILENAME: $f1"; next;}
-  unless ($f2=~/[\!-~]/) {warn "BAD FILENAME: $f2"; next;}
+  unless ($f1=~/[ -~]/) {warn "BAD FILENAME: $f1"; next;}
+  unless ($f2=~/[ -~]/) {warn "BAD FILENAME: $f2"; next;}
 
   # no apostrophes, quotes, or other types of spaces
-  if ($f1=~/[\"\'\s]/) {warn "BAD FILENAME: $f1"; next;}
-  if ($f2=~/[\"\'\s]/) {warn "BAD FILENAME: $f2"; next;}
+  if ($f1=~/\"/) {warn "BAD FILENAME: $f1"; next;}
+  if ($f2=~/\"/) {warn "BAD FILENAME: $f2"; next;}
 
   # my restriction: the filename itself must match (because otherwise
   # you get all sorts of weird random links)
@@ -34,6 +41,7 @@ while (<>) {
 
   # if either n1 or n2 is empty, warn and move on
   if ($n1=~/^\s*$/ || $n2=~/^\s*$/) {
+    # this actually ignores a file whose name is " ", but I'm ok w/ that
     warn "BAD LINE: $_";
     next;
   }
@@ -56,12 +64,22 @@ while (<>) {
   # but am separating it if others want to use this
   unless ($m1 eq "extdrive5" && $m2 eq "kemptown") {next;}
 
+  # painful (slow) test saved for last: target must exist and not be
+  # symlink itself
+
+  unless (-f $f1) {warn("NO TARGET: $f1"); next;}
+  if (-l $f1) {warn("TARGET IS SYMLINK: $f1"); next;}
+
+  # ignore cases where source is already a symlink
+  if (-l $f2) {warn("SOURCE IS SYMLINK: $f2"); next;}
+
+  $bytes+= (-s $f2);
+  if (rand()<.001) {debug("BYTES SAVED: $bytes");}
+
   # I am deleting the second file and linking to first, which is the
   # opposite of what rdfind normally does (I ran it backwards by
   # mistake + it would take forever to run again)
+  print qq%rm "$f2"; ln -s "$f1" "$f2"\n%;
 
-
-  
-
-  debug("GOT: $f1 -> $f2, $n1 -> $n2, $m1 -> $m2");
+  #  debug("GOT: $f1 -> $f2, $n1 -> $n2, $m1 -> $m2");
 }
