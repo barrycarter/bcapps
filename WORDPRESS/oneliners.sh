27e38552e241a6db36fdea39dffc3c12e7130810
Barry Carter <carter.barry@gmail.com>
2017-12-07 15:30:05 -0700

    Checkpoint

--- WORDPRESS/oneliners.sh
+++ WORDPRESS/oneliners.sh
@@ -1,3 +1,3 @@
 # download all post info on my site
 
-wp --ssh=barrycar@bc4 --path=public_html/wordpress --fields=ID,post_title,post_name,post_date,post_status,post_author,post_parent,post_type,filter,guid,menu_order,post_content --posts_per_page=50 --format=yaml post list
+wp --ssh=barrycar@bc4 --path=public_html/wordpress --fields=ID,post_title,post_name,post_date,post_status,post_author,post_parent,post_type,filter,guid,menu_order,post_content --format=yaml post list
