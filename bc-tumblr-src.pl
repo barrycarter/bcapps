e892709a46d8f0beb60a34a62ad3c22aed5b85c7
Barry Carter <carter.barry@gmail.com>
2017-12-08 08:53:13 -0700

    Checkpoint

--- bc-tumblr-src.pl
+++ bc-tumblr-src.pl
@@ -18,27 +18,71 @@ my(%seen);
 
 while (<>) {
 
+  chomp;
+
+  debug("\nLINE: $_");
+
   # figure out what blog this came from using filename
   s/(^.*?)://;
   my($fname) = $1;
-#  debug("FILENAME: $fname");
+  debug("FILENAME: $fname");
 
   # first thing in path that looks like a name
-  unless ($fname=~s%/(\w+?)/%%) {warn("BAD BLOG: $fname"); next;}
+  unless ($fname=~s%/([\w-]+?)/%%) {warn("BAD BLOG: $fname"); next;}
   my($blog) = $1;
-#  debug("BLOG: $blog");
+  debug("BLOG: $blog");
+
+  # this is probably bad in a loop
+  # can maybe memoize this per blog
+  unless (-d "$blog/media") {
+    debug("$blog has no media subdir, creating one");
+    mkdir("$blog/media");
+  }
 
   while (s/src="(.*?)"//) {
 
     my($src) = $1;
 
     # seem to be quite a few repeats, kill them here first
-    if ($seen{$src}) {next;}
+    if ($seen{$src}) {debug("REP1"); next;}
     $seen{$src} = 1;
 
-    debug("SRC: $src");
-  }
+    # TODO: assuming last part of path is unique, if wrong, this fails
+    my($base) = $src;
+    $base=~s%^.*/%%;
 
-#  debug("GOT: $_");
+    # also check if shortened form seen
+    if ($seen{$base}) {debug("REP2"); next;}
+    $seen{$base} = 1;
+
+    # key test: do we have it already?
+
+    # <h>glob is an anagram of blog</h>
+
+    # we need the * after $base because tumblr-backup.py will add a .mp4, eg
+
+    # this also assumes that tumblr files length means globs are unique too
+    my(@glob) = glob("$blog/*/$base*");
+
+    # <h>variable name re-use... only in Perl!</h>
+    my($seen) = 0;
+
+    # for now, just print what we dont have
+    # TODO: give URL for stuff we dont have
+    unless (@glob) {print "$base\n";}
+
+    # TODO: is this too slow?
+    if (@glob) {
+      debug("WE HAVE: $base");
+    } else {
+      debug("NO $base");
+    }
+
+#    for $i ("images", "media") {
+#      if (-f "$blog/$i/$src") {$seen=1; last;}
+#    }
+
+  }
 }
+
 
