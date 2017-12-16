849c92d19c10b12db16399f82baddd5f7bd8522c
Barry Carter <carter.barry@gmail.com>
2017-12-16 10:44:42 -0700

    Checkpoint

--- WORDPRESS/oneliners.sh
+++ WORDPRESS/oneliners.sh
@@ -1,3 +1,42 @@
+# for testing revisions
+
+wp post get 28625 --ssh=barrycar@bc4 --path=public_html/test20171209.barrycarter.org > out6.txt
+
+exit;
+
+fortune | wp post create --ssh=barrycar@bc4 --path=public_html/test20171209.barrycarter.org --post_title="Output of fortune command at `date`" --post_status=publish --post_category=2 --ID=12345 -
+
+exit;
+
+fortune | wp post update 4 - --ssh=barrycar@bc4 --path=public_html/test20171209.barrycarter.org --post_title="Output of fortune command at `date`" --post_status=publish --post_category=2
+
+exit;
+
+# even using an existing id doesn't hurt
+
+fortune | wp post create --ssh=barrycar@bc4 --path=public_html/test20171209.barrycarter.org --post_title="Output of fortune command at `date`" --post_status=publish --post_category=2 --post_id=4 -
+
+exit;
+
+# can't set post_id, but harmless to try
+
+fortune | wp post create --ssh=barrycar@bc4 --path=public_html/test20171209.barrycarter.org --post_title="Output of fortune command at `date`" --post_status=publish --post_category=2 --post_id=1234 -
+
+
+exit;
+
+# below published live post
+
+fortune | wp post create --ssh=barrycar@bc4 --path=public_html/test20171209.barrycarter.org --post_title="Output of fortune command at `date`" --post_status=publish --post_category=2 -
+
+exit;
+
+# below works to create a post with a category (but still draft?)
+
+fortune | wp post create --ssh=barrycar@bc4 --path=public_html/test20171209.barrycarter.org --post_title="Output of fortune command at `date`" --post-status=publish --post_category=2 -
+
+exit;
+
 # test blog
 
 EDITOR=emacs; export EDITOR; wp --ssh=barrycar@bc4 --path=public_html/test20171209.barrycarter.org post edit 2
