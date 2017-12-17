95de6cb7bc644170014e07f60a7bbaee7c775b43
Barry Carter <carter.barry@gmail.com>
2017-12-17 10:14:11 -0700

    Checkpoint

--- STACK/bc-moonar-eclipse.m
+++ STACK/bc-moonar-eclipse.m
@@ -12,15 +12,34 @@ TODO: 1.1 billion stars but also n bright stars
 
 *)
 
+(* minimal fail example *)
+
+Table[{p,s}, {p, AstronomicalData["Planet"]}, 
+ {s, AstronomicalData[p, "Satellites"]}]
+
+
+
 (* define distance from Sun = avg distance from Earth, in meters *)
 
 planets = AstronomicalData["Planet"]
 
-Table[dist[p] = AstronomicalData[p, "SemimajorAxis"][[1]], 
-    {p, AstronomicalData["Planet"]}];
+Table[dist[p] = AstronomicalData[p, "SemimajorAxis"][[1]], {p, planets}]
 
 Table[dist[s] = dist[p], {s, AstronomicalData[p, "Satellites"]},
- {p, AstronomicalData["Planet"]}]
+ {p, Take[planets,{3,8}]}]
+
+Table[{s,p}, {p, AstronomicalData["Planet"]},
+ {s, AstronomicalData[p, "Satellites"]}]
+
+
+
+ 
+
+
+
+
+
+
 
 
 
@@ -42,3 +61,33 @@ ang[x_] := 2*ArcTan[AstronomicalData[x,"Diameter"]/2]
 
 
 
+(*
+
+posted as https://mathematica.stackexchange.com/questions/162140/iterator-order-breaks-table-when-using-astronomicaldata
+
+Subject: Iterator order breaks Table when using AstronomicalData
+
+<pre><code>
+
+(* this breaks *)
+
+Table[{s,p},
+ {s,AstronomicalData[p,"Satellites"]},
+ {p,AstronomicalData["Planet"]}
+];
+
+Table::iterb: Iterator {s, $Failed} does not have appropriate bounds.
+
+(* but if you switch the iteration order, it works fine *)
+
+Table[{s,p},
+ {p,AstronomicalData["Planet"]},
+ {s,AstronomicalData[p,"Satellites"]}
+];
+
+</code></pre>
+
+Why?
+
+
+*)
