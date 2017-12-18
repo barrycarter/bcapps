0dec59080a7a89469eefa0cdcc790202b0e569d2
Barry Carter <carter.barry@gmail.com>
2017-12-18 07:17:25 -0700

    Checkpoint

--- WORDPRESS/bc-parse-wp-output.pl
+++ WORDPRESS/bc-parse-wp-output.pl
@@ -8,6 +8,10 @@
 
 require "/usr/local/lib/bclib.pl";
 
+# TODO: change target dir
+# target dir is where each post is written to a file
+my($targetdir) = "/home/user/20171218/wp";
+
 # ignore the first separators
 # TODO: this doesnt work unless you actually redirect STDIN, hmmm
 # my($tridash) = <>;
@@ -18,9 +22,9 @@ my(%hash, $key);
 
 while (<>) {
 
-  chomp;
+  if (++$count > 10000) {die "TESTING";}
 
-  debug("GOT: *$_*");
+  chomp;
 
   if ($_ eq "- " || eof()) {
     # parse yaml and clear hash
@@ -31,7 +35,10 @@ while (<>) {
   }
 
   # TODO: assuming two spaces = YAML key
-  if (s/^  (.*?):\s*//) {$key = $1;}
+  if (s/^  (\S+?):\s*//) {$key = $1;}
+
+  debug("GOT: $_");
+  debug("KEY: $key");
 
   # always append to curkey
   $hash{$key} .= $_;
@@ -43,6 +50,42 @@ sub parse_yaml {
   my($hashref) = @_;
   my(%hash) = %{$hashref};
 
+  # ignore unpublished posts
+  unless ($hash{post_status} eq "publish") {return;}
+
+  # figure out filename
+  # TODO: this might be redundant
+  my($fname) = $hash{post_name};
+
+  if ($fname=~m/^\s*$/) {
+    warn("BAD POST NAME FOR LIVE POST: ...");
+    debug(%hash);
+    next;
+  }
+
+  local(*A);
+  open(A, ">$targetdir/$fname.wp");
+
+  # all fields except content
+  for $i (sort keys %hash) {
+    if ($i eq "post_content") {next;}
+    print A "$i: $hash{$i}\n";
+  }
+
+  # TODO: remove weird pipe things surrounding content
+
+  # real newlines cleanup YAML spaces
+  $hash{post_content}=~s/\r/\n/sg;
+  # TODO: this might kill intentional indentation (intended indentation?)
+  $hash{post_content}=~s/^\t//mg;
+
+
+  # a separator, and then content
+  print A "\n","="x60,"\n$hash{post_content}\n";
+
+  close(A);
+
+  # TODO: parse author/category properly
   debug("GOT HASH",%hash);
 }
 
