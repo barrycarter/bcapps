47d3a80863f162ace7cbfc0d547b0e76c3a4af4a
Barry Carter <carter.barry@gmail.com>
2018-01-10 20:36:51 -0700

    Checkpoint, about to re-break into two progs

--- VIDEO/montagify.pl
+++ VIDEO/montagify.pl
@@ -1,12 +1,73 @@
 #!/bin/perl
 
+# Given a list of videos on the STDIN, ultimately creates a video that
+# shows 25 subvideos per screen at 24 times the regular speed
+
+require "/usr/local/lib/bclib.pl";
+
+# get the name and create a subdir to store files
+my($name) = @ARGV;
+unless ($name) {die "Usage: $0 name";}
+if ($#ARGV > 0) {die "ERROR: exactly one argument required";}
+my($targetdir) = "$bclib{home}/VIDEOFRAMES/$name";
+unless (-d $targetdir) {dodie("mkdir('$targetdir')");}
+# TODO: this is an awful way to make <> now be STDIN
+@ARGV = ();
+
+# TODO: make this flexible?
+my($size) = "256:144";
+
+open(A, ">$targetdir/rip.sh")||die("Can't open rip.sh, $!");
+
+# the commands I will run (or at least print)
+my(@cmds);
+
+# TODO: require an existing subdir for writing
+
+# montage = one for every 25 videos (M)
+# video = one for every video (V)
+# frame = one for every second of every video (F)
+
+my($montage);
+my($video);
+
+my($out, $err, $res);
+
+while (<>) {
+
+  chomp;
+
+  unless (-f $_) {die "STDIN: $_ is not a file";}
+
+  # if 25 limit (or first run), reached, next montage
+  if ($video%25 == 0) {
+    $montage = sprintf("%08d",++$montage);
+    $video = 0;
+  }
+
+  # increment video, but keep in %04d form
+  $video = sprintf("%04d",++$video);
+
+
+  # break this video in "one second" chunks (assuming 24 frames = one second)
+  # TODO: better way to do this w/o assuming it's 24 fps?
+  push(@cmds, qq{ffmpeg -i "$_" -vf "select=not(mod(n\\,24)), scale=$size" -vsync vfr $targetdir/M${montage}V${video}F%08d.jpg});
+
+}
+
+print A join("\n", @cmds),"\n";
+close(A);
+
+
+
+die "TESTING";
+
+
 # thin wrapper around montage
 
 # Given a list of directories on the stdin that presumably have frame
 # JPGs in them, create 5x5 montages, but using a blank frame so that
 # each "episode" in the directory maintains its place
-
-require "/usr/local/lib/bclib.pl";
 
 # TODO: allow this to vary?
 
