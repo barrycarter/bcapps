6a2417a6c0df9068faff49d73a28cbed820125a1
Barry Carter <carter.barry@gmail.com>
2018-03-11 07:44:57 -0600

    Checkpoint: need to fix some things up

--- TWITTER/bc-linkedin-zip.pl
+++ TWITTER/bc-linkedin-zip.pl
@@ -16,54 +16,23 @@ my($file) = @ARGV;
 my($zip) = Archive::Zip->new();
 $zip->read($file);
 
-# find the primary email address
+# find the primary email address by splitting the Email Addresses.csv file
+# and mtime of file
 
 my($data) = $zip->memberNamed("Email Addresses.csv");
-
+my($mtime) = strftime("%Y%m%d.%H%M%S", gmtime($data->lastModTime()));
 my(@lines);
-
 for $i (split(/\n/, $data->contents())) {push(@lines, [csv($i)]);}
-
-debug($lines[2]);
-
-die "TESTING";
-
-my(@contents) = split(/\n/, $data->contents());
-my($listref) = arraywheaders2hashlist(\@contents);
+my($listref) = arraywheaders2hashlist(\@lines);
 my(@hashes) = @$listref;
 
+my($primary);
+
 for $i (@hashes) {
-  debug($i->{Primary});
+  if ($i->{Primary} eq "Yes") {$primary="$i->{'Email Address'}"; last;}
 }
 
-debug("HASHES", @hashes);
+unless ($primary && $mtime) {die "Something went wrong";}
 
-die "ESTING";
-
-debug("CONTENTS: $contents");
-
-die "TESTING";
-
-unless ($contents=~s%archive for (.*?)<%%i) {die "NO USERNAME";}
-
-
-unless ($file=~/takeout\-(\d{8}T\d{6}Z)\-\d+\.zip/) {
-  die("INVALID FILENAME: $file");
-}
-
-my($date) = $1;
-
-my($zip) = Archive::Zip->new();
-$zip->read($file);
-
-# find data/js/user_details.js, mtime, and contents (for username)
-
-my($data) = $zip->memberNamed("Takeout/index.html");
-my($contents) = $data->contents();
-unless ($contents=~s%archive for (.*?)<%%i) {die "NO USERNAME";}
-
-my($sn) = $1;
-
-# print the recommended action
-
-print "ln -s '$file' $sn-$date.zip\n";
+# recommended symlink
+print "ln -s '$file' $primary-$mtime.zip\n";
