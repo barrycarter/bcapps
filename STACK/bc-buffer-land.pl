4a7ef90d6ab51fc0d4d811652b9606c8a805a89a
Barry Carter <carter.barry@gmail.com>
2018-08-08 09:35:32 -0600

    Checkpoint

--- STACK/bc-buffer-land.pl
+++ STACK/bc-buffer-land.pl
@@ -9,9 +9,38 @@
 
 require "/usr/local/lib/bclib.pl";
 
+my(%dist);
+
 open(A, "bzcat /home/barrycarter/20180807/dist2coast.signed.txt.bz2|");
 
+# warn "Random entries for testing";
+
 while (<A>) {
-  debug("GOT: $_");
+
+#  if (rand() < .99) {next;}
+
+  chomp;
+  my($lon, $lat, $dist) = split(/\s+/, $_);
+
+  # each entry represents .04 x .04 lat/lon square, so size is
+  # dependent on cosine of latitude (in degrees)
+  my($area) = cos($lat*$DEGRAD);
+  # hideous double use of dist as hash and scalar
+  # also hideous using real value as key
+  $dist{$dist} += $area;
+
+  # just a counter
+  if (++$count%100000==0) {debug("COUNT: $count");}
+
 }
+
+my($accum) = 0;
+
+for $i (sort {$a <=> $b} keys %dist) {
+  $accum += $dist{$i};
+  print "$i $dist{$i} $accum\n";
+}
+
+
+
 
