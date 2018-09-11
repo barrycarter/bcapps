184211a7a55557e80884043b6b9309bdb66835c7
Barry Carter <carter.barry@gmail.com>
2018-09-10 23:04:40 -0600

    Checkpoint

---
+++ STACK/bc-nearest-star.m
@@ -0,0 +1,11 @@
+(*
+
+https://astronomy.stackexchange.com/questions/27605/of-the-stars-in-the-gaia-hipparchos-catalogs-which-one-is-the-most-isolated
+
+Header is in data[[1]]
+
+*)
+
+data = Import["!zcat /home/user/BCGIT/ASTRO/hygdata_v3.csv.gz", "CSV"];
+
+data[[1]]
