59fd88a2f8e129c6e525268b4a2969875f13a34d
Barry Carter <carter.barry@gmail.com>
2018-09-23 22:44:24 -0600

    Checkpoint: about to split Earth in bc-buffer-land-4.m

--- MATHEMATICA/bc-derv-wave.m
+++ MATHEMATICA/bc-derv-wave.m
@@ -8,6 +8,10 @@ Can I do "wavelets" using finite differences
 f[t_] = t^2;
 
 g[a_, b_, c_, d_, t_] = a + b*Cos[c*t - d]
+g1[a_, b_, c_, d_, t_] = D[a + b*Cos[c*t - d], t]
+g2[a_, b_, c_, d_, t_] = D[a + b*Cos[c*t - d], t, t]
+g3[a_, b_, c_, d_, t_] = D[a + b*Cos[c*t - d], t, t, t]
+
 
 
 Solve[{
@@ -28,7 +32,45 @@ important "in theory" *)
 
 f[x_,y_,z_] := (z-x)/2;
 
-f[{a[i-1], a[i
+d0[i_] = a[i]
+
+d1[i_] = f[a[i-1], a[i], a[i+1]]
+
+d2[i_] = f[d1[i-1], d0[i], d0[i+1]]
+
+d3[i_] = f[d2[i-1], d1[i], d1[i+1]]
+
+pts = Table[t^2, {t,1,100}]
+
+(* Given a list and an element index, return the 0-3rd 'derivative' *)
+
+dervs[a_, i_] := {
+ a[[i]], (-a[[-1 + i]] + a[[1 + i]])/2, 
+ (a[[-2 + i]] - a[[i]] + 2*a[[1 + i]])/4,
+ (-a[[-3 + i]] + a[[-1 + i]] - 4*a[[i]] + 2*a[[2 + i]])/8
+};
+
+dervs[pts, 50]
+
+(* Find best fit cos wave given dervs at given index *)
+
+eqs[list_, i_] := {
+ g[a,b,c,d,i] == dervs[list, i][[1]],
+ g1[a,b,c,d,i] == dervs[list, i][[2]],
+ g2[a,b,c,d,i] == dervs[list, i][[3]],
+ g3[a,b,c,d,i] == dervs[list, i][[4]]
+};
+
+bestfit[list_, i_] := Solve[{
+ g[a,b,c,d,i] == dervs[list, i][[1]],
+ g1[a,b,c,d,i] == dervs[list, i][[2]],
+ g2[a,b,c,d,i] == dervs[list, i][[3]],
+ g3[a,b,c,d,i] == dervs[list, i][[4]]
+}, {a,b,c,d}]
+
+
+
+
 
 
 
