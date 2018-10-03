6b67b6ca727d22f42ec5364b3d94974b05ad4d19
Barry Carter <carter.barry@gmail.com>
2018-10-03 11:09:18 -0600

    In production, not 100% happy w/ it, but ok

--- WEATHER/bc-get-weather-2.pl
+++ WEATHER/bc-get-weather-2.pl
@@ -9,8 +9,6 @@
 
 require "/usr/local/lib/bclib.pl";
 
-warn "testing"; $globopts{test} = 1;
-
 # cache for a long time if testing
 if ($globopts{test}) {$age=3600;} else {$age=-1;}
 
@@ -108,58 +106,9 @@ for $i (sort {$order{$a} <=> $order{$b}} keys %data) {
 
 my($forecasts) = join("\n", @forecasts);
 
-debug("$str\n$forecasts");
-
-# TODO: use .new format
+write_file_new("$str$forecasts", "$bclib{home}/ERR/forecast.inf");
 
 # TODO: consider stopping if not enough good/recent data
-
-
-# debug("$output{weather}/$output{temperature}/$output{windChill}/$output{humidity} ($output{dewpoint})");
-
-# debug("TS: $timestamp");
-
-# debug(var_dump("JSON", $json));
-
-=item
-
-JSON->{'properties'}->{'icon'} = 'https://api.weather.gov/icons/land/day/bkn?size=medium';
-
-JSON->{'properties'}->{'timestamp'} = '2018-10-03T14:52:00+00:00';
-
-JSON->{'properties'}->{'textDescription'} = 'Mostly Cloudy';
-
-JSON->{'properties'}->{'temperature'}->{'value'} = '17.2';
-JSON->{'properties'}->{'windChill'}->{'value'} = undef;
-JSON->{'properties'}->{'relativeHumidity'}->{'value'} = '83.608459924942';
-JSON->{'properties'}->{'dewpoint'}->{'value'} = '14.4';
-JSON->{'properties'}->{'windDirection'}->{'value'} = 0;
-JSON->{'properties'}->{'windSpeed'}->{'value'} = 0;
-JSON->{'properties'}->{'windGust'}->{'value'} = undef;
-JSON->{'properties'}->{'barometricPressure'}->{'value'} = 102070; [in Pa]
-
-sample:
-
-                "textDescription": "Clear",
-                "textDescription": "Cloudy",
-                "textDescription": "Light Rain",
-                "textDescription": "Mostly Clear",
-                "textDescription": "Mostly Cloudy",
-                "textDescription": "Partly Cloudy",
-                "textDescription": "Rain and Fog/Mist",
-                "textDescription": "Rain",
-                "textDescription": "Thunderstorms and Rain",
-                "textDescription": "Thunderstorms",
-
-
-NAF below is windchill
-
-@8:52 AM, September 27
-Mostly Cloudy/60F/NAF/78% (53F)
-ESE 6G (30.05in) 
-
-=cut
-
 # TODO: add heat index?
 
 # this subroutine is specific to this program, do not confuse w/
@@ -181,141 +130,10 @@ sub windinfo {
   return $ret;
 }
 
-
-die "END HERE";
-
-my(%data);
-
-#  debug("WEATHER: $day/$tod: $weather");
-#  debug("$i->{startTime} => $day/$tod ($count)");
-#  debug($i->{startTime},str2time($i->{startTime}));
-#  debug("KI:", keys %{$i});
-# }
-
-debug(%order);
-
-# debug($forecasts);
-
-# now, the current conditions
-
-$json = JSON::from_json($out);
-my(%metar) = parse_metar($json->{properties}->{rawMessage});
-
-debug("METAR", %metar);
-
-# debug("OUT: $out");
-
-# debug(var_dump("JSON", $json));
-
-die "TESTING";
-
-for $i ("maxTemperature", "minTemperature", "probabilityOfPrecipitation",
-	"skyCover", "weather") {
-  debug("I: $i");
-  my($data) = $json->{properties}->{$i}->{values};
-
-
-
-  debug(var_dump("DATA", $data));
-}
-
-
-# forecast data
-
-# JSON->{'properties'}->{'maxTemperature'}->{'values'}->[0]
-# JSON->{'properties'}->{'minTemperature'}->{'values'}->[0]
-# JSON->{'properties'}->{'probabilityOfPrecipitation'}->{'values'}->[0]
-# JSON->{'properties'}->{'skyCover'}->{'values'}->[0]
-# JSON->{'properties'}->{'weather'}->{'values'}->[0]
-# won't use below but it is available
-# JSON->{'properties'}->{'temperature'}->{'values'}->[0]
-
-
-die "TESTING";
-
-# <h>intermediate variables are for sissies!</h>
-for $i (@{$json->{forecast}->{simpleforecast}->{forecastday}}) {
-
-  # to save screen "real estate", shorten some conditions
-  # TODO: do this better
-  # TODO: do this for current_observation -> weather as well
-  $i->{conditions}=~s/clear/CLR/isg;
-  $i->{conditions}=~s/partly cloudy/PCL/isg;
-  $i->{conditions}=~s/mostly cloudy/MCL/isg;
-  $i->{conditions}=~s/chance of a thunderstorm/TSRM/isg;
-  $i->{conditions}=~s/chance of rain/RAIN?/isg;
-  $i->{conditions}=~s/chance rain/RAIN?/isg;
-  $i->{conditions}=~s/thunderstorm/TSTRM!/isg;
-  $i->{conditions}=~s/ice pellets/SLEET/isg;
-
-  # want colons to line up!
-  $i->{date}->{day}=~s/^(\d)$/0$1/;
-
-  # I want date:conditions/hi/lo/%pop (probability of precipitation)
-  push(@forecast, join("", $i->{date}->{weekday_short},$i->{date}->{day},":",$i->{conditions},"/",
-	      $i->{high}->{fahrenheit}, "/", $i->{low}{fahrenheit}, "/",
-	      $i->{pop},"%"));
-}
-
-# now, current: conditions/temp/wc/humid (dewpt)
-
-  $current = join("",
-		$json->{current_observation}->{weather}, "/",
-		$json->{current_observation}->{temp_f}, "F/",
-		$json->{current_observation}->{windchill_f}, "F/",
-		$json->{current_observation}->{relative_humidity}, " (",
-		$json->{current_observation}->{dewpoint_f}, "F)"
-	       );
-
-# time of obs (compacted w/ time first)
-$time = $json->{current_observation}->{observation_time};
-# get rid of useless string and tz
-$time=~s/^last updated on //isg;
-# this IS case sensitive
-$time=~s/\s*[A-Z]+$//;
-$time=~s/^(.*?),\s*(.*?)$/$2, $1/;
-
-# wind and pressure (dir spdGgust (press))
-# treat no gust special
-unless ($json->{current_observation}->{wind_gust_mph}) {
-  $json->{current_observation}->{wind_gust_mph} = "";
-}
-
-$wind = join("", 
-	     $json->{current_observation}->{wind_dir}, " ",
-	     $json->{current_observation}->{wind_mph}, "G",
-	     $json->{current_observation}->{wind_gust_mph}, " (",
-	     $json->{current_observation}->{pressure_in}, "in)"
-);
-
-# moon info now printed by bc-get-astro, removed below
-$forecast = join("\n",@forecast[0..6]);
-
-# print in order I want (even if I change mind later)
-# some astro functions absorbed by bc-get-astro.pl
-$str = << "MARK";
-\@$time
-$current
-$wind
-$forecast
-MARK
-;
-
-# there really aren't 6 days of forecasts but adding blank lines doesn't hurt
-
-if ($globopts{show}) {print $str;}
-
-# and write to info file
-write_file_new($str, "/home/barrycarter/ERR/forecast.inf");
-
 # parses shortForecast
 
 sub parse_forecast {
   my($forecast) = @_;
-#  debug("$forecast");
-
-  # TODO: unhappy w/ my abbrevs below, so just returning as is for now
-  return ($forecast);
 
   # handle special case "x then y"
   # TODO: can there be 3 or more here?
@@ -326,45 +144,25 @@ sub parse_forecast {
   # TODO: work in progress as I see more forecasts
   # NOTE: order is important (eg, "Slight Chance" before "Chance")
 
-  $forecast=~s/Mostly\s+/~/;
+  $forecast=~s/Mostly\s+/M/;
+
+  # these mean the same thing (wow!)
+  $forecast=~s/Partly (Cloudy|Sunny)/PCLD/;
 
   $forecast=~s/(Sunny|Clear)/CLR/;
 
-  $forecast=~s/Slight Chance\s+(.*?)$/$1??/;
+  $forecast=~s/Slight Chance\s+/??$1/;
 
   $forecast=~s/Rain Showers/RA/;
 
   $forecast=~s/Showers And Thunderstorms/TS/;
 
-  $forecast=~s/Chance\s+(.*?)$/$1?/;
+  $forecast=~s/Chance\s+/?$1/;
 
-  $forecast=~s/Partly Cloudy/PCL/;
+  $forecast=~s/Cloudy/CLD/;
 
   # TODO: not really happy about this one (so not generalizing)
   $forecast=~s/Scattered TS/TS/;
 
   return $forecast;
 }
-
-
-
-
-=item format
-
-Format of data from https://api.weather.gov/gridpoints/ABQ/102,118
-(adding /forecast to the end of this URL gives text format, which is
-less useful to me)... fields that are possibly interesting only.
-
-
-
-Examples:
-
-apparentTemperature, SIZE: 168 (hourly)
-
-dewpoint, SIZE: 82 (sporadic, but end of validTime gives how many
-hours good for
-
-Cool things I don't need: hainesIndex davisStabilityIndex (other less
-interesting indexes exist too), mixingHeight
-
-=cut
