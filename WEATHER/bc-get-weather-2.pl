ef0989be647f984ada61699f5629b6bbd6fb97dd
Barry Carter <carter.barry@gmail.com>
2018-10-06 13:16:27 -0600

    Checkpoint: sort of working

--- WEATHER/bc-get-weather-2.pl
+++ WEATHER/bc-get-weather-2.pl
@@ -14,54 +14,17 @@ require "/usr/local/lib/bclib.pl";
 require "$bclib{home}/bc-private.pl";
 
 # cache for a long time if testing
+# warn "TESTING"; $globopts{test} = 1;
 if ($globopts{test}) {$age=3600;} else {$age=-1;}
 
 my($out, $err, $res, $json, %output);
 
 # get current data for KABQ
 
-($out, $err, $res) = cache_command2("curl https://api.weather.gov/stations/KABQ/observations/latest", "age=$age");
+my($str1) = get_weather_data("KABQ");
+my($str2) = get_weather_data("PWS_LORAXABQ");
 
-$json = JSON::from_json($out);
-$data = $json->{properties};
-
-$output{timestamp} = strftime("@ %Y%m%d.%H%M%S", localtime(str2time($data->{timestamp})));
-
-# convert to proper temperature units
-
-for $i ("temperature", "windChill", "dewpoint") {
-
-  # special case if undefined
-  if (defined($data->{$i}->{value})) { 
-    $output{$i} = sprintf("%0.1fF", $data->{$i}->{value}*1.8+32);
-  } else {
-    $output{$i} = "NAF";
-  }
-}
-
-# $output{humidity} = sprintf("%0.1f%%", $data->{relativeHumidity}->{value});
-$output{humidity} = sprintf("%0.0f%%", $data->{relativeHumidity}->{value});
-$output{pressure} = sprintf("%0.2fin", $data->{barometricPressure}->{value}*0.00029530);
-
-# windspeed and gust are in m/s, direction is in degrees
-
-$output{wind} = windinfo($data->{'windDirection'}->{'value'},
-			 $data->{'windSpeed'}->{'value'},
-			 $data->{'windGust'}->{'value'}, "m/s"
-			 );
-
-# TODO: parse this better
-
-$output{weather} = parse_forecast($data->{textDescription});
-
-$str = << "MARK";
-$output{timestamp}
-$output{weather}/$output{temperature}/$output{windChill}/$output{humidity} ($output{dewpoint})
-$output{wind} ($output{pressure})
-MARK
-;
-
-# now to get forecast data
+# now to get forecast data (really should be a subroutine but..)
 
 # 102,118 is the grid where I live (in Albuquerque)
 
@@ -111,15 +74,11 @@ for $i (sort {$order{$a} <=> $order{$b}} keys %data) {
 
 my($forecasts) = join("\n", @forecasts);
 
-my($pws) = get_pws_data();
-
-# TODO: temporary + probably a bad idea
-
-($out, $err, $res) = cache_command2("$bclib{githome}/bc-quikbak.pl $bclib{home}/ERR/forecast.inf");
+# ($out, $err, $res) = cache_command2("$bclib{githome}/bc-quikbak.pl $bclib{home}/ERR/forecast.inf");
 
 debug("OUT: $out, ERR: $err, RES: $res");
 
-write_file_new("$str$forecasts\n$pws", "$bclib{home}/ERR/forecast.inf");
+write_file_new("$str1$forecasts\n$str2", "$bclib{home}/ERR/forecast.inf");
 
 # TODO: consider stopping if not enough good/recent data
 # TODO: add heat index?
@@ -182,6 +141,8 @@ sub parse_forecast {
 
   $forecast=~s/Cloudy/CLD/;
 
+  $forecast=~s/Isolated/ISO/;
+
   # TODO: not really happy about this one (so not generalizing)
   $forecast=~s/Scattered TS/TS/;
 
@@ -191,21 +152,32 @@ sub parse_forecast {
 # NOTE: this is a subroutine purely for code cleanliness, and is
 # specific to this program
 
-sub get_pws_data {
+# because weather.gov's API sucks (several hours behind on hourly
+# data, using this subroutine for KABQ data too
+
+# returns SLIGHTLY different data for PWS than for airports/METAR
+
+sub get_weather_data {
+
+  my($station) = @_;
 
   my(%output);
 
   # $age is global
 
-  my($out, $err, $res) = cache_command2("curl 'https://api.aerisapi.com/observations/PWS_LORAXABQ?&client_id=$private{aeris}{accessid}&client_secret=$private{aeris}{secretkey}'", "age=$age");
+  my($out, $err, $res) = cache_command2("curl 'https://api.aerisapi.com/observations/$station?&client_id=$private{aeris}{accessid}&client_secret=$private{aeris}{secretkey}'", "age=$age");
 
   my($json) = JSON::from_json($out);
 
   my($data) = $json->{response}->{ob};
 
-  $output{timestamp} = strftime("%Y%m%d.%H%M%S", localtime($data->{timestamp}));
+  $output{timestamp}=strftime("%Y%m%d.%H%M%S", localtime($data->{timestamp}));
+
+  # only used for METAR
+  $output{weather} = parse_forecast($data->{weatherShort});
 
   # convert to proper temperature units
+  # truly horrible converting in place!
   for $i ("tempC", "windchillC", "dewpointC") {
 
   # special case if undefined
@@ -226,17 +198,28 @@ sub get_pws_data {
 
   # windspeed and gust are in m/s, direction is in degrees
 
-  $output{wind} = windinfo($data->{'windDirDEG'}, $data{'windSpeedMPH'},
+  $output{wind} = windinfo($data->{'windDirDEG'}, $data->{'windSpeedMPH'},
 			 $data->{'windGustSpeedMPH'}, "mph");
 
-  # TODO: last line is just for fun and temporary
-  my($str) = << "MARK";
+  # slightly different string for PWS
+
+  my($str);
+  if ($station=~/^PWS/) {
+    $str = << "MARK";
 Local/$output{tempC}/$output{windchillC}/$output{humidity} ($output{dewpointC}) [$output{timestamp}]
 $output{wind} ($output{pressure})
-Light: $data->{light}, Sky: $data->{sky}, Rad: $data->{solradWM2}
 MARK
 ;
+  } else {
+    $str = << "MARK";
+\@ $output{timestamp}
+$output{weather}/$output{tempC}/$output{windchillC}/$output{humidity} ($output{dewpointC})
+$output{wind} ($output{pressure})
+MARK
+;
+  }
 
+  debug("STR: $str");
   return $str;
 
 }
