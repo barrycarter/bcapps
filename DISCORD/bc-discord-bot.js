68a22b31c4d146d0fda17a60e23b1c65ac796b19
Barry Carter <carter.barry@gmail.com>
2018-11-14 20:55:44 -0700

    Checkpoint: trivial but working

--- DISCORD/bc-discord-bot.js
+++ DISCORD/bc-discord-bot.js
@@ -1,41 +1,28 @@
 // Usage: $0 <client secret>
 
-// Do I need both hook and login?
-var hookid = process.argv[3];
-var hooktoken = process.argv[4];
-
 const Discord = require("discord.js");
 const client = new Discord.Client();
-// const hook = new Discord.WebhookClient(hookid, hooktoken);
 
 // TODO: read from file instead of providing on command line
 client.login(process.argv[2]);
 
-var channel, channels;
+var channel, timer;
 
 client.on("ready", () => {
-    console.log("I am ready!");
-    channels = client.channels;
-    // this is a one-channel bot for now
-    channel = channels.get('509413318851559436');
-    channel.send("I have been summoned!");
-    console.log(channel);
-    //    console.log(channels('509413318851559435'));
-    //    console.log("start");
-    //    for (var [key, val] of channels) {console.log(""+key+", "+var_dump(val));}
-    //    console.log("end");
+    console.log("Starting...");
+    // this is my single channel for right now
+    channel = client.channels.get('509413318851559436');
+    channel.send("I have been called into life at "+new Date());
+    timer = setInterval(tellTime, 10000);
 });
-
-// hook.send("Shiver me timers, matey");
-
-// client.send_message("I am alive!");
 
 client.on("message", (message) => {
     if (message.content.startsWith("ping")) {
-      message.channel.send("pong!");
-      message.channel.send(channels);
+      message.channel.send("pong at "+ new Date());
     }
 });
+
+function tellTime () {channel.send("At the tone, "+new Date());}
 
 function var_dump(obj) {
   var out = '';
