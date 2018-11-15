6cb3e127e59de68e98b4fd9189745d2de81f8d0a
Barry Carter <carter.barry@gmail.com>
2018-11-14 19:45:33 -0700

    Checkpoint

--- DISCORD/bc-discord-bot.js
+++ DISCORD/bc-discord-bot.js
@@ -1,15 +1,29 @@
 // Usage: $0 <client secret>
 
+// Do I need both hook and login?
+var hookid = process.argv[3];
+var hooktoken = process.argv[4];
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
+// const hook = new Discord.WebhookClient(hookid, hooktoken);
 
 // TODO: read from file instead of providing on command line
 client.login(process.argv[2]);
+
+client.on("ready", () => {
+    console.log("I am ready!");
+    console.log(client.guilds);
+    console.log(client.channels);
+});
+
+// hook.send("Shiver me timers, matey");
+
+// client.send_message("I am alive!");
 
 client.on("message", (message) => {
     if (message.content.startsWith("ping")) {
       message.channel.send("pong!");
     }
   });
-
 
