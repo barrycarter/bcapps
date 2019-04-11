2d8b040dddea56aa5aaa9fdd1cbe896929c3b346
Barry Carter <carter.barry@gmail.com>
2019-04-11 17:02:18 -0600

    Still working!

--- ASTRO/bc-sun-moon-stuff.c
+++ ASTRO/bc-sun-moon-stuff.c
@@ -22,6 +22,8 @@ void isDecreasing(void(* udfuns)(SpiceDouble et,SpiceDouble *value),
   *isdecr = (res2 < res1);
 }
 
+// TODO: can probably combine next and prev routines
+
 // returns the next time (after et) target reaches elevation elev at lat/lon
 
 SpiceDouble nextTime(SpiceInt target, SpiceDouble et, SpiceDouble elev, SpiceDouble lat, SpiceDouble lon) {
@@ -55,18 +57,56 @@ SpiceDouble nextTime(SpiceInt target, SpiceDouble et, SpiceDouble elev, SpiceDou
 
 }
 
+SpiceDouble prevTime(SpiceInt target, SpiceDouble et, SpiceDouble elev, SpiceDouble lat, SpiceDouble lon) {
+
+  // just 1 result cell and cnfine has to be 2 big for beg and end
+  SPICEDOUBLE_CELL(result, 10);
+  SPICEDOUBLE_CELL(cnfine,2);
+  SpiceDouble beg, end;
+
+  // elevation function
+  void elevationFunction(SpiceDouble et, SpiceDouble *value) {
+    *value = altitude(target, et, lat, lon);
+  }
+
+  // loop for 400 days but early abort
+  for (int i=-1; i>-400; i--) {
+
+    // the window for a day
+    wninsd_c(et+i*86400, et+(i+1)*86400, &cnfine);
+
+    // search within that window
+    gfuds_c(elevationFunction, isDecreasing, "=", elev, 0., 60., 10, &cnfine, &result);
+
+    // if at least 1 result, break out of for loop
+    if (wncard_c(&result) >= 1) {break;}
+  }
+
+  // return the one result
+  wnfetd_c(&result, wncard_c(&result)-1, &beg, &end);
+  return beg;
+}
+
 int main(int argc, char **argv) {
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
 
   double et = unix2et(1555022362);
   double lat =  35.05*rpd_c();
   double lon = -106.5*rpd_c();
-
   double elvs[4] = {-0.83333333333, -6, -12, -18};
 
+  printf("SUN az: %f, el: %f\n", azimuth(10, et, lat, lon)/rpd_c(), 
+	 altitude(10, et, lat, lon)/rpd_c());
+
+  printf("MOON az: %f, el: %f\n", azimuth(301, et, lat, lon)/rpd_c(), 
+	 altitude(301, et, lat, lon)/rpd_c());
+
   for (int i=0; i <= 3; i++) {
+    printf("SUN PREV @%f: %f\n", elvs[i], et2unix(prevTime(10, et, elvs[i]*rpd_c(), lat, lon)));
     printf("SUN NEXT @%f: %f\n", elvs[i], et2unix(nextTime(10, et, elvs[i]*rpd_c(), lat, lon)));
   }
+
+  printf("MOON PREV AT HORIZON: %f\n", et2unix(prevTime(301, et, elvs[0]*rpd_c(), lat, lon)));
 
   printf("MOON NEXT AT HORIZON: %f\n", et2unix(nextTime(301, et, elvs[0]*rpd_c(), lat, lon)));
 
