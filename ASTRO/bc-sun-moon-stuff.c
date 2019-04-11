c140cd8c133d1d75907866e52f6fd2442b4b5f38
Barry Carter <carter.barry@gmail.com>
2019-04-11 12:02:40 -0600

    Checkpoint: got rid of double radian conversion error

--- ASTRO/bc-sun-moon-stuff.c
+++ ASTRO/bc-sun-moon-stuff.c
@@ -17,13 +17,43 @@
 
 void azalt(ConstSpiceChar *targ, SpiceDouble et, SpiceDouble lat, SpiceDouble lon) {
 
-  SpiceDouble pos[3], normal[3], north[3], state[3], surf[3], lt;
-  SpicePlane plane;
+  SpiceDouble targetPosition[3], targetPositionTopographic[3];
+  SpiceDouble observerPosition[3], surfaceNormal[3];
+  SpiceDouble lt;
+
+    //targpos[3], normal[3], north[3], state[3], surf[3], lt;
+    //  SpicePlane plane;
 
   // HACK: cheating a bit here hardcoding Earth's radii
 
+  // find position of object in ITRF93 frame
+  // TODO: this CANNOT ALWAYS BE SUN!
+  spkezp_c(10, et, "ITRF93", "CN+S", 399, targetPosition, &lt);
+
+  // find observer position in ITRF93
+  georec_c(lon, lat, 0, 6378.137, 0.0033528128, observerPosition);
+
+  // subtract to get topographic position
+  vsub_c(targetPosition, observerPosition, targetPositionTopographic);
+
+  // the surface normal vector from the observer (z axis)
+  surfnm_c(6378.137, 6378.137, 6356.7523, observerPosition, surfaceNormal);
+
+  printf("observerPosition: %f %f %f\n", observerPosition[0], observerPosition[1], observerPosition[2]);
+  printf("surfaceNormal: %f %f %f\n", surfaceNormal[0], surfaceNormal[1], surfaceNormal[2]);
+
+  // angle between surfaceNormal and object
+  double normalAngle = vsep_c(surfaceNormal, targetPositionTopographic);
+
+  double unixtime = et2unix(et);
+  printf("%f %f\n", unixtime, normalAngle/rpd_c());
+
+  return;
+
+  /*
+
   // pos = fixed ITRF93 position of lat/lon
-  georec_c(lon*rpd_c(),lat*rpd_c(), 0, 6378.137, 0.0033528128, pos);
+  georec_c(lon, lat, 0, 6378.137, 0.0033528128, pos);
 
   // surface normal there
   surfnm_c(6378.137, 6378.137, 6356.7523, pos, normal);
@@ -38,24 +68,35 @@ void azalt(ConstSpiceChar *targ, SpiceDouble et, SpiceDouble lat, SpiceDouble lo
   // the matrix rotating ITRF93 to topographic frame (z = up, y = north)
   SpiceDouble matrix[3][3];
 
-  twovec_c(north, 2, normal, 1, matrix);
+  //  twovec_c(north, 2, normal, 3, matrix);
+  twovec_c(normal, 3, north, 2, matrix);
 
   // vector to target at time
-  spkcpo_c(targ, et, "ITRF93", "OBSERVER", "CN+S", pos, "Earth", "ITRF93", state, &lt);
+
+  printf("STATE: %f %f %f\n", state[0], state[1], state[2]);
 
   // rotate into topographic frame
   SpiceDouble loc[3];
   mtxv_c(matrix, state, loc);
+  printf("ROTATED: %f %f %f\n", loc[0], loc[1], loc[2]);
+
+  // spherical coordinates
+  SpiceDouble r, colat, lont;
+  recsph_c(loc, &r, &colat, &lont);
+
+  printf("%f %f %f\n", r, colat/rpd_c(), lont/rpd_c());
 
   // project this vector to plane
   vprjp_c(state, &plane, surf);
 
   // the angle between the surface and north vectors
-  SpiceDouble az = acos(vdot_c(surf, north)/vnorm_c(surf)/vnorm_c(north));
+  //  SpiceDouble az = acos(vdot_c(surf, north)/vnorm_c(surf)/vnorm_c(north));
 
   // the angle between the normal and object
 
-  printf("ANGLE: %f\n", az/rpd_c());
+  // printf("ANGLE: %f\n", az/rpd_c());
+
+  */
 
   /*
   printf("STATE (%f %f %f): %f %f %f\n", lat, lon, stime, state[0], state[1], state[2]);
@@ -71,12 +112,13 @@ int main(int argc, char **argv) {
   furnsh_c("/home/barrycarter/BCGIT/ASTRO/standard.tm");
 
   // currently does nothing
-  azalt("Sun", unix2et(0), 35*rpd_c(), -106*rpd_c());
+  //  azalt("Sun", unix2et(0), 35*rpd_c(), -106*rpd_c());
 
 
-  //  for (int i=1554962400; i<1555048800; i+=600) {
-  //    azalt("Sun", unix2et(i), 35*rpd_c(), -106*rpd_c());
-  //  }
+  for (int i=1554962400; i<1555048800; i+=600) {
+    //    printf("I: %d ",i);
+    azalt("Sun", unix2et(i), 35*rpd_c(), -106*rpd_c());
+  }
 
   return 0;
 
