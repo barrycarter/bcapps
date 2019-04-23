b092972b108a20df2d881a9469b40a8c45b66c8e
Barry Carter <carter.barry@gmail.com>
2019-04-23 12:47:01 -0600

    Checkpoint: working/cool

--- MATHEMATICA/bc-mercator.m
+++ MATHEMATICA/bc-mercator.m
@@ -10,6 +10,63 @@ conds = {theta > 0, theta < Pi/2, lat > lat1, lat < lat2}
 
 y[theta_] = Log[Tan[theta] + Sec[theta]]
 
+tab1030 = Table[{theta, y[theta]}, 
+ {theta, -85*Degree, 85*Degree, 170*Degree/32}]
+
+t1029[theta_] = Interpolation[tab1030, InterpolationOrder -> 1][theta]
+
+Plot[{y[theta]-t1029[theta]}, {theta, -85*Degree, 85*Degree}, PlotRange -> All]
+
+
+theta[y_] = Gudermannian[y]
+
+limit = Gudermannian[Pi]
+
+85.0511 is the square limit in degs
+
+Plot[y[theta], {theta, limit-1*Degree, limit}]
+
+(* the limit of the northernmost tile when zoom is z *)
+
+slimit[z_] = Gudermannian[Pi-Pi/(2^(z-1))]
+
+t1100 = N[Table[y[theta], {theta, slimit[4], limit, (limit-slimit[4])/256}]]
+
+t1105 = N[Table[
+ 255*(y[theta]-y[slimit[4]])/(y[limit] - y[slimit[4]]),  
+ {theta, slimit[4], limit, (limit-slimit[4])/255}]]
+
+t1105 - Table[i, {i, 0, 255}]
+
+tt110[z_] := N[Table[
+ 255*(y[theta]-y[slimit[z]])/(y[limit] - y[slimit[z]]),  
+ {theta, slimit[z], limit, (limit-slimit[z])/255}]]
+
+t1112 = Table[i, {i, 0, 255}]
+
+ListPlot[tt110[4] - t1112]
+
+ListPlot[tt110[5] - t1112]
+
+ListPlot[tt110[7] - t1112]
+
+ListPlot[tt110[9] - t1112]
+
+11 pixels at 4, halving each after
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Plot[y[theta*Degree]/Degree, {theta, 0, 85}]
 
 FullSimplify[y'[theta], conds]
@@ -67,11 +124,6 @@ plot1[lat1_] := Plot[distort[lat, lat1, lat1+1*Degree], {lat, lat1,
 lat1+1*Degree}]
 
 t1424 = Table[plot1[lat], {lat, 0, 85*Degree, 5*Degree}];
-
-
-
-
-
 
 Plot[f2[lat, 60*Degree, 70*Degree] - f1[lat, 60*Degree, 70*Degree],
 {lat, 60*Degree, 70*Degree}]
