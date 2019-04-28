87c306da201962cd0554dd6004f4de1cd9d1b463
Barry Carter <carter.barry@gmail.com>
2019-04-28 16:49:47 -0600

    Checkpoint

--- bclib.js
+++ bclib.js
@@ -4,13 +4,37 @@ Given a string that looks like an HTTP QUERY_STRING, convert it to a
 hash. This is one of the few functions here that does NOT take a hash
 as its argument
 
+NOTE: there are probably more clever ways to do this
+
 */
 
 function str2hash(str) {
 
   let returnHash = {};
 
-  console.log(str.split("&"));
+  str.split("&").forEach(function(kv) {
+      let arr = kv.split("=");
+      returnHash[arr[0]] = arr[1];
+    });
+
+  return returnHash;
+
+}
+
+/*
+
+Converts a slippy tile or an equirectangular tile to a latitude and longitude:
+
+z: tile zoom value
+x: tile x value (may be fractional)
+y: tile y value (may be fractional)
+
+projection: if 1, Mercator project, otherwise equirectangular project
+
+*/
+
+function tile2LngLat(obj) {
+
 
 }
   
@@ -31,9 +55,18 @@ maxZoom: never get tiles higher than this zoom level
 projection: if 1, assume tiles are Mercator projected (slippy tiles);
 otherwise, assume they are equirectangular
 
+opacity; tile opacity
+
 */
 
 function placeTilesOnMap(obj) {
 
+}
+
+/** transparent debugger */
+
+function td(x, str) {
+  console.log(`TD(${str}): ${x}`);
+  return x;
 }
 
