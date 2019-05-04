b7351f25b90f57c96a527dcc3be31e682e63a1eb
Barry Carter <carter.barry@gmail.com>
2019-05-04 13:31:22 -0600

    Still wrong, but cleaner

--- MAPS/bc-gradients.pl
+++ MAPS/bc-gradients.pl
@@ -28,8 +28,15 @@ sub longest_gradient {
   my(@c2) = csv($hashref->{c2});
 
   my($dist) = 0;
+  my(@dir);
 
-  for $i (0..2) {$dist += abs($c1[$i]-$c2[$i])+1;}
+  # this calculates the distance between the colors and the per-color direction
+
+  for $i (0..2) {
+    my($temp) = $c2[$i] - $c1[$i];
+    $dist += abs($temp)+1;
+    $dir[$i] = signum($temp);
+  }
 
   # this is the start of the gradient
   my(@rgb) = @c1;
@@ -37,7 +44,7 @@ sub longest_gradient {
   # this is the "perfect" gradient value (used below)
   my(@pfc) = @c1;
 
-  my(@diff, $frac);
+  my(@diff, $frac, $max);
 
   # ramp from 0 to 1 in steps of 1/$dist
 
@@ -45,40 +52,21 @@ sub longest_gradient {
 
     $frac = $i/$dist;
 
+    # keep track of which diff is maximal
+    $max = 0;
+
     # find the perfect rgb value and diff from current rgb value
     for $j (0..2) {
       $pfc[$j] = $c1[$j] + $frac*($c2[$j]-$c1[$j]);
       $diff[$j] = abs($pfc[$j] - $rgb[$j]);
+      if ($diff[$j] > $diff[$max]) {$max = $j;}
+      debug("PERFECT($j): $pfc[$j], RGB($j): $rgb[$j], DIFF($j): $diff[$j]");
     }
 
-    debug("DIFF", @diff);
-
-
-    # this is a very dumb way to find the max, but, since there are
-    # only 3 elts, it might be more efficient; it also allows
-    # consistent breaks between ties, assuming floating point math is
-    # consistent
-
-    if ($diff[0] >= $diff[1] && $diff[0] >= $diff[2]) {
-      $rgb[0]++;
-    } elsif ($diff[1] >= $diff[0] && $diff[1] >= $diff[2]) {
-      $rgb[1]++;
-    } elsif ($diff[2] >= $diff[0] && $diff[2] >= $diff[1]) {
-      $rgb[2]++;
-    } else {
-      die "BAD THING HAS HAPPENED";
-    }
-  
-
-
-#    debug("PFC", @pfc);
+    # increment the one with the biggest diff
+    $rgb[$max] += $dir[$max];
 
   }
-
-
-  debug("DIST: $dist");
-
-  debug(@c1, @c2);
 }
 
 # TODO: doc or something, find linear between 2 numbers at p
