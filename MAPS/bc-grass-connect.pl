2babf1b617b29be9ac4a4b6433bf07b78b974a64
Barry Carter <carter.barry@gmail.com>
2019-05-06 17:21:41 -0600

    Checkpoint

--- MAPS/bc-grass-connect.pl
+++ MAPS/bc-grass-connect.pl
@@ -41,6 +41,17 @@ while (true) {
 
 }
 
+=item flyfile($text)
+
+TODO: seriously improve this
+
+Returns the contents of a PNG file with $text written in it
+
+=cut
+
+
+
+
 debug("ALL DONE");
 
 # for $i (keys %ENV) {debug("$i -> $ENV{$i}");}
@@ -70,27 +81,3 @@ debug("OUT: $out, ERR: $err, RES: $res");
 
 debug("BETA");
 
-=item todo
-
-The following MAY let me connect to grass without the grass shell, but
-might be harder, so abandoned for now.
-
-# trying to connect to the grass shell without actually invoking it
-# via ENV vars or full paths
-
-my($bin) = "/usr/local/grass-7.4.1/bin/";
-
-GISBASE=/usr/local/grass-7.4.1
-GISRC=/tmp/grass7-user-26669/gisrc
-GIS_LOCK=26669
-GRASS_ADDON_BASE=/mnt/villa/user/.grass7/addons
-GRASS_GNUPLOT=gnuplot -persist
-GRASS_HTML_BROWSER=xdg-open
-GRASS_PAGER=more
-GRASS_PROJSHARE=/usr/share/proj
-GRASS_PYTHON=python
-GRASS_VERSION=7.4.1
-LD_LIBRARY_PATH=/usr/local/grass-7.4.1/lib:/lib64:/usr/local/lib:/usr/lib:/lib
-PYTHONPATH=/usr/local/grass-7.4.1/etc/python
-
-=cut
