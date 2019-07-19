ad89a9c1786315d1b62793cf461bcc4337b4fa3f
Barry Carter <carter.barry@gmail.com>
2019-07-19 14:32:55 -0600

    Working with large font

--- bc-alarm-snooze.pl
+++ bc-alarm-snooze.pl
@@ -5,6 +5,7 @@
 # Options:
 
 # --width: width of popup
+# --fontsize: displayed font size
 
 # TODO: this is bad because Perl script keeps running until snooze or
 # cancel is hit
@@ -19,18 +20,25 @@ require "/usr/local/lib/bclib.pl";
 
 # default option values
 
-defaults("width=1600");
+defaults("width=1600&fontsize=30");
 
 # TODO: this might be bad-- maybe required quoted arg[0]
 
 my($msg) = join(" ", @ARGV);
 
-my($cmd) = "yad --text='$msg' --text-align='center' --width=$globopts{width} 
+# --fontname doesn't work w/ text entry, this is the hack
+
+$msg = qq%<span font="$globopts{fontsize}">$msg</span>%;
+
+my($cmd) = qq%yad --text='$msg' --text-align='center' --width=$globopts{width} 
             --button 1m:1 --button 2m:2 --button 3m:3 
             --buttons-layout='center' --sticky --undecorated 
-            --fontname='Serif bold italic 20' --always-print-result";
+            --fontname='Bitstream Vera CSans Mono Bold Oblique 12' 
+            --always-print-result%;
 
 $cmd=~s/\n/ /sg;
+
+debug($cmd);
 
 my($out, $err, $res) = cache_command2($cmd);
 
