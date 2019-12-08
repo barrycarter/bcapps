b4257bf9c6549ef6ac41f1d61baad69eb6962d59
Barry Carter <carter.barry@gmail.com>
2019-12-07 17:50:33 -0700

    Checkpoint

--- COW/bc-pop-center-parse.pl
+++ COW/bc-pop-center-parse.pl
@@ -2,13 +2,26 @@
 
 require "/usr/local/lib/bclib.pl";
 
-my($data, $file) = cmdfile();
+my($dir) = "$bclib{githome}/COW/";
+
+my($data) = read_file("$dir/bc-pop-center-output.txt");
+
+# load CC2 to CC3 conversions and back
+
+my(%cc223, %cc322);
+
+for $i (split(/\n/, read_file("$dir/bc-cc2cc3.csv"))) {
+
+    my($name, $cc2, $cc3, $ccnum) = csv($i);
+    $cc223{$cc2} = $cc3;
+    $cc322{$cc3} = $cc2;
+}
 
 # load dependent data from dependentcountries_territories.csv
 
 my(%conversions);
 
-for $i (split(/\n/, read_file("$bclib{githome}/COW/dependentcountries_territories.csv"))) {
+for $i (split(/\n/, read_file("$dir/dependentcountries_territories.csv"))) {
 
   my($iso, $name, $admin0) = csv($i);
 
@@ -16,29 +29,31 @@ for $i (split(/\n/, read_file("$bclib{githome}/COW/dependentcountries_territorie
 
 }
 
-# grab natgrid data
+# the file bc-gpw-natl-grid.txt is the output of:
 
-open(A, "ogrinfo -al /home/user/20191204/gpw_v4_national_identifier_grid_rev11_30_sec.shp | fgrep -v POLYGON|");
+# ogrinfo -al /home/user/20191204/gpw_v4_national_identifier_grid_rev11_30_sec.shp | fgrep -v POLYGON|");
+
+# which gives meta information about countries without polygon information
 
 my(%chash);
 my($country) = 0;
 
-while (<A>) {
+for $i (split(/\n/, read_file("$dir/bc-gpw-natl-grid.txt"))) {
 
-    if (/^\s*$/) {next;}
+    if ($i=~/^\s*$/) {next;}
 
-    if (s/Value\s*\(Integer\)\s*\= (\d+)//) {
+    if ($i=~s/Value\s*\(Integer\)\s*\= (\d+)//) {
 	$country = $1;
 	next;
     }
 
-    s/^\s*(\S+?)\s+\(\S+\)\s+\=\s+(.*?)$//;
+    $i=~s/^\s*(\S+?)\s+\(\S+\)\s+\=\s+(.*?)$//;
 
     $chash{$country}{$1} = $2;
 
 }
 
-# debug("KEYS", keys %chash);
+print "CC2,CC3,Name,clng,clat,r_value,MEANUNITKM\n";
 
 for $i (split(/\n/, $data)) {
 
@@ -59,10 +74,8 @@ for $i (split(/\n/, $data)) {
 
     %cinfo = %{$chash{$hash{cc}}};
 
-    print "$cinfo{ISOCODE},$cinfo{NAME0},$lng,$lat,$r,$cinfo{MEANUNITKM}\n"
+    unless ($cc322{$cinfo{ISOCODE}}) {$cc322{$cinfo{ISOCODE}} = "N/A";}
+
+    print "$cc322{$cinfo{ISOCODE}},$cinfo{ISOCODE},$cinfo{NAME0},$lng,$lat,$r,$cinfo{MEANUNITKM}\n"
 
 }
-
-
-
-
