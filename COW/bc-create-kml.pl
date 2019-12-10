050d59fd2d6cd993b6b6698ff970e664aa60faf6
Barry Carter <carter.barry@gmail.com>
2019-12-10 13:01:19 -0700

    Checkpoint

---
+++ COW/bc-create-kml.pl
@@ -0,0 +1,42 @@
+#!/bin/perl
+
+require "/usr/local/lib/bclib.pl";
+
+print read_file("$bclib{githome}/kmlhead.txt");
+
+print << "MARK";
+<Style id="popcenter"><IconStyle><Icon>
+<href>
+https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png
+</href>
+</Icon></IconStyle></Style>
+MARK
+    ;
+
+for $i (split(/\n/, read_file("$bclib{githome}/COW/bc-pop-centers-no-deps.csv"))) {
+
+    my($cc2, $cc3, $name, $lng, $lat, $r, $pop, $npts) = split(/\,\s*/, $i);
+
+    $pop = round($pop);
+
+    if ($cc2 eq "CC2") {next;}
+
+print << "MARK";
+<Placemark>
+<name>$name</name>
+<description>Pop: $pop</description>
+<styleUrl>#popcenter</styleUrl> 
+<Point><coordinates>$lng,$lat,0</coordinates></Point>
+</Placemark>   
+
+MARK
+;
+
+}
+
+print read_file("$bclib{githome}/kmlfoot.txt");
+
+# TODO: update answer, also look for cool things like center of pop in
+# other country or in water
+
+# TODO: another verison for nondeps
