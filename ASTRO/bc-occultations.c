f9968a7f14847c00b6d1869eb38b872f50236cb4
Barry Carter <carter.barry@gmail.com>
2020-01-14 16:26:54 -0700

    Checkpoint

--- ASTRO/bc-occultations.c
+++ ASTRO/bc-occultations.c
@@ -15,10 +15,12 @@ int main(int argc, char **argv) {
 
   // variables we will use
   SpiceInt moonID, sunID, planetID;
-  SpiceDouble beg, end;
+  SpiceDouble beg, end, beger, ender;
   SpiceBoolean found;
-  SPICEDOUBLE_CELL(cnfine,2);
-  SPICEDOUBLE_CELL(result,2*MAXWIN);
+  SPICEDOUBLE_CELL(cnfine, 2);
+  SPICEDOUBLE_CELL(cnfiner, 2);
+  SPICEDOUBLE_CELL(result, 2*MAXWIN);
+  SPICEDOUBLE_CELL(resulter, 2*MAXWIN);
 
   // check for correct number or arguments and assign to strings
   if (argc != 6) {
@@ -33,6 +35,8 @@ int main(int argc, char **argv) {
   SpiceDouble syear = atof(argv[4]);
   SpiceDouble eyear = atof(argv[5]);
 
+  furnsh_c("/home/user/BCGIT/ASTRO/standard.tm");
+
   // convert sun/moon/planet names into NAIF ids w/ error checking
 
   bods2c_c(moon, &moonID, &found);
@@ -44,26 +48,40 @@ int main(int argc, char **argv) {
   bods2c_c(planet, &planetID, &found);
   if (!found) {printf("PLANET %s NOT FOUND\n", planet); exit(-1);}
 
-  furnsh_c("/home/user/BCGIT/ASTRO/standard.tm");
-
   wninsd_c(year2et(syear), year2et(eyear), &cnfine);
 
   // defining geometry finder function via given arguments
+  // this is for at least a partial lunar eclipse
 
   void gfq (SpiceDouble et, SpiceDouble *value) {
-    //    *value = penUmbralData(et, sunID, planetID, moonID, 0);
     *value = penUmbralData(et, sunID, planetID, moonID, 1);
   }
 
   // TODO: 3600 too big?
 
-  gfuds_c(gfq, isDecreasing, ">", 1, 0, 3600, MAXWIN, &cnfine, &result);
+  gfuds_c(gfq, isDecreasing, ">", 0, 0, 3600, MAXWIN, &cnfine, &result);
 
   SpiceInt nres = wncard_c(&result);
 
   for (int i=0; i<nres; i++) {
 
     wnfetd_c(&result,i,&beg,&end);
+
     printf("%d %d %d %f %f %f %f\n", moonID, sunID, planetID, et2unix(beg), et2unix(end), beg, end);
+
+    // create a window for the partial eclipse
+    wninsd_c(beg, end, &cnfiner);
+
+    gfuds_c(gfq, isDecreasing, ">", 1, 0, 1, MAXWIN, &cnfiner, &resulter);
+
+    for (int j=0; j < wncard_c(&resulter); j++) {
+
+      wnfetd_c(&resulter, j, &beger, &ender);
+      printf("TOTAL: %f %f %f %f\n", et2unix(beger), et2unix(ender), beger, ender);
+    }
+
+    // empty out cell we inserted into earlier
+    removd_c(beg,&cnfiner);
+    removd_c(end,&cnfiner);
   }
 }
