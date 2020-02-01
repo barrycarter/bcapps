079bae70cc171eaa384aac3554d2da821258c0fe
Barry Carter <carter.barry@gmail.com>
2020-02-01 12:36:29 -0700

    Checkpoint

--- MATHEMATICA/bc-functions-of-relations.m
+++ MATHEMATICA/bc-functions-of-relations.m
@@ -8,6 +8,8 @@ TODO: can compute from subset special case
 
 TODO: single answer inside set or no?
 
+TODO: solve can't solve it?
+
 
 convert[{f,x,y}, a][f_, x_, y_] = (Sqrt[Abs[-f + x]^2 + Abs[y]^2] +
 Sqrt[Abs[f + x]^2 + Abs[y]^2])/2;
@@ -41,4 +43,33 @@ v1212 = variableSolutions[eq1];
 
 v1212[[3, 1, 1, 1]] /. v1212[[3]]
 
+{a} -> a_
+
+
+Clear[convert];
+extractVariables[exp_] := DeleteDuplicates@Cases[exp, _Symbol, Infinity];
+
+variableSolutions[exp_] := Table[Solve[exp, i], {i, extractVariables[exp]}];
+
+eq1 = (Norm[{x,y}-{f,0}] + Norm[{x,y}-{-f,0}] == 2*a);
+
+v1212 = variableSolutions[eq1];
+
+v1213 = v1212[[3, 1, 1, 1]] /. v1212[[3]];
+
+v1214 = extractVariables[v1212[[3, 1, 1, 1]] /. v1212[[3]]];
+
+v1215 = v1212[[3, 1, 1, 1]];
+
+convert[v1214, v1215][v1214] = v1212[[3]];
+
+t1232 = ToExpression[ToString[v1214[[1]]]<>"_"];
+
+t1235 = Table[ToExpression[ToString[i]<>"_"], {i, v1214}]
+
+h[t1235] = a + f + x;
+
+h[{1, 2, 3}]
+
+convert[{a,f,x}, y][1, 2, 3];
 
