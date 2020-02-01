dbeac74d213d5a0727f67036299ad359c22dedda
Barry Carter <carter.barry@gmail.com>
2020-02-01 12:15:54 -0700

    Checkpoint

--- MATHEMATICA/bc-functions-of-relations.m
+++ MATHEMATICA/bc-functions-of-relations.m
@@ -6,6 +6,8 @@ TODO: chain joining
 
 TODO: can compute from subset special case
 
+TODO: single answer inside set or no?
+
 
 convert[{f,x,y}, a][f_, x_, y_] = (Sqrt[Abs[-f + x]^2 + Abs[y]^2] +
 Sqrt[Abs[f + x]^2 + Abs[y]^2])/2;
@@ -26,7 +28,17 @@ variableSolutions[exp_] := Table[Solve[exp, i], {i, extractVariables[exp]}];
 
 eq1 = (Norm[{x,y}-{f,0}] + Norm[{x,y}-{-f,0}] == 2*a);
 
-extractVariables[eq1]
+variableSolutions[eq1]
 
+
+extractVariables[exp_] := DeleteDuplicates@Cases[exp, _Symbol, Infinity];
+
+variableSolutions[exp_] := Table[Solve[exp, i], {i, extractVariables[exp]}];
+
+eq1 = (Norm[{x,y}-{f,0}] + Norm[{x,y}-{-f,0}] == 2*a);
+
+v1212 = variableSolutions[eq1];
+
+v1212[[3, 1, 1, 1]] /. v1212[[3]]
 
 
