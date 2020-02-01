168344c022ed17dcb2987d8d16409bc3b93c0335
Barry Carter <carter.barry@gmail.com>
2020-01-31 20:17:12 -0700

    Checkpoint

--- MATHEMATICA/bc-dependencies-graph.m
+++ MATHEMATICA/bc-dependencies-graph.m
@@ -29,3 +29,27 @@ implies[s_] := Flatten[Table[t -> i, {t, Subsets[s]}, {i, Subsets[t]}]]
 Length[implies[{a,b, c, d, e}]];
 Graph[implies[{a,b,c}], VertexLabels -> Automatic]
 
+Subsets[{a, b,c, d}];
+implies[s_] := Flatten[Table[t -> i, {t, Subsets[s]}, {i, Subsets[t, {Max[0,Length[t]-1]}]}]]
+
+Length[implies[{a,b, c, d, e}]]
+Graph[implies[{a,b,c, d, e}], VertexLabels -> Automatic]
+
+s = {a, b, c, d};
+
+restDetermineOne[s_] :=
+ Table[i -> Complement[s, i], 
+ {i, Subsets[s, {Length[s]-1}]}];
+ 
+ Graph[restDetermineOne[{a, b, c, d, e}], VertexLabels -> Automatic]
+
+<formulas>
+
+direct[s_] := Flatten[Table[t -> i, {t, Subsets[s]}, {i, Subsets[t,
+{Max[0,Length[t]-1]}]}]]
+
+restDetermineOne[s_] :=
+ Table[i -> Complement[s, i], 
+ {i, Subsets[s, {Length[s]-1}]}];
+ 
+</formulas>
