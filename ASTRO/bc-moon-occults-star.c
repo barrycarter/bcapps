42bb81534d30293c99b3d9757a0fea5de1a80300
Barry Carter <carter.barry@gmail.com>
2020-02-11 09:36:11 -0700

    Checkpoint

--- ASTRO/bc-moon-occults-star.c
+++ ASTRO/bc-moon-occults-star.c
@@ -14,15 +14,15 @@ int main(int argc, char **argv) {
 
   wninsd_c(year2et(2020), year2et(2021), &cnfine);
 
-  int chosen;
+  double moonpos[3], starpos[3], lt, sep;
 
-  double moonpos[3], starpos[3], lt;
+  // NOTE: we use global starpos here for star position
 
   void gfq (SpiceDouble et, SpiceDouble *value) {
-
     spkezp_c(301, et, "J2000", "CN+S", 399, moonpos, &lt);
     double angsep = asin((EARTH_RADIUS + MOON_RADIUS)/vnorm_c(moonpos));
-    double minsep = 2;
+    *value = vsep_c(moonpos, starpos)/angsep;
+  }
 
     for (int i=0; i<2865; i++) {
 
@@ -31,24 +31,18 @@ int main(int argc, char **argv) {
       starpos[1] = hygdata[i][4];
       starpos[2] = hygdata[i][5];
 
-      double sep = vsep_c(moonpos, starpos)/angsep;
-      if (sep < minsep) {
-	minsep = sep;
-	chosen = i;
-      }
+      
+      // TODO: reset result after each result
+      gfuds_c(gfq, isDecreasing, "LOCMIN", 1., 0, 86400*10, MAXWIN, &cnfine, &result);
+      int count = wncard_c(&result);
+      double beg, end;
+
+  for (int j=0; j<count; j++) {
+    wnfetd_c (&result, j, &beg, &end);
+    gfq(beg, &sep);
+    printf("%f %f %d %f %f\n", et2unix(beg), et2unix(end), i, sep, hygdata[j][1]);
+  }
     }
-    *value = minsep;
-  }
-
-  gfuds_c(gfq, isDecreasing, "<", 1., 0, 3600, MAXWIN, &cnfine, &result);
-  int count = wncard_c(&result);
-  double beg, end;
-    
-  for (int i=0; i<count; i++) {
-
-    // find the time of event (beg == end in this case)
-    wnfetd_c (&result, i, &beg, &end);
-    printf("%f %f %d\n", et2unix(beg), et2unix(end), chosen);
-  }
   return 0;
 }
+
