f5c5d7e69b49cd8abb87532c9b9223760163bbe3
Barry Carter <carter.barry@gmail.com>
2020-02-11 10:50:32 -0700

    Checkpoint

--- ASTRO/bc-moon-occults-star.c
+++ ASTRO/bc-moon-occults-star.c
@@ -14,22 +14,36 @@ int main(int argc, char **argv) {
 
   wninsd_c(year2et(2020), year2et(2021), &cnfine);
 
-  double moonpos[3], starpos[3], lt, sep;
+  double moonpos[3], starpos[3], r, colat, lon, lt, sep;
+  int count;
+
 
   // NOTE: we use global starpos here for star position
 
   void gfq (SpiceDouble et, SpiceDouble *value) {
     spkezp_c(301, et, "J2000", "CN+S", 399, moonpos, &lt);
+    recsph_c(moonpos, &r, &colat, &lon);
+
     double angsep = asin((EARTH_RADIUS + MOON_RADIUS)/vnorm_c(moonpos));
     *value = vsep_c(moonpos, starpos)/angsep;
+
+    // if (count != 355) {return;}
+
+    //    printf("MOON: %d %f %f %f %f %f %f %f %f\n", count, et2unix(et), lon/pi_c()*12, (halfpi_c()-colat)/pi_c()*180, r, starpos[0], starpos[1], starpos[2], vsep_c(moonpos, starpos)/pi_c()*180);
+
   }
 
     for (int i=0; i<2865; i++) {
+
+      // TODO: TESTING!!!
 
       // TODO: add proper motion
       starpos[0] = hygdata[i][3];
       starpos[1] = hygdata[i][4];
       starpos[2] = hygdata[i][5];
+
+      count = hygdata[i][0];
+      //      if (count != 355) {continue;}
 
       
       // TODO: reset result after each result
@@ -40,7 +54,7 @@ int main(int argc, char **argv) {
   for (int j=0; j<count; j++) {
     wnfetd_c (&result, j, &beg, &end);
     gfq(beg, &sep);
-    printf("%f %f %d %f %f\n", et2unix(beg), et2unix(end), i, sep, hygdata[j][1]);
+    printf("ALPHA: %f %f %f %f %f\n", et2unix(beg), et2unix(end), sep, hygdata[j][1], hygdata[i][0]);
   }
     }
   return 0;
