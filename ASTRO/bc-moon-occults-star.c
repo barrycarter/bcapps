763b8c1c6a504466b6459aac36f620cd4d045d86
Barry Carter <carter.barry@gmail.com>
2020-03-09 16:09:53 -0600

    Checkpoint

--- ASTRO/bc-moon-occults-star.c
+++ ASTRO/bc-moon-occults-star.c
@@ -1,10 +1,13 @@
 #include "bclib.h"
 #include "bc-hygdata.h"
-#define EARTH_RADIUS 6371009/1000.
-#define MOON_RADIUS 1738.1
 #define MAXWIN 20000
 
 // $0 viewer occulter syear eyear
+
+// TODO: retrograde motion
+
+// TODO: choose good skip value based on input object (eg, Moon = 10
+// days, Mars = 100 days?)
 
 int main(int argc, char **argv) {
 
@@ -15,15 +18,38 @@ int main(int argc, char **argv) {
     exit(-1);
   }
 
-  int viewer = atoi(argv[1]);
-  int occulter = atoi(argv[2]);
+  SpiceChar *viewer = argv[1];
+  SpiceChar *occulter = argv[2];
   double syear = atof(argv[3]);
   double eyear = atof(argv[4]);
 
+  furnsh_c("/home/user/BCGIT/ASTRO/bc-maxkernel.tm");
+
+  // convert viewer/occulter names into NAIF ids w/ error checking
+
+  SpiceInt viewerID, occulterID;
+  SpiceBoolean found;
+
+  bods2c_c(viewer, &viewerID, &found);
+  if (!found) {printf("VIEWER %s NOT FOUND\n", viewer); exit(-1);}
+
+  bods2c_c(occulter, &occulterID, &found);
+  if (!found) {printf("OCCULTER %s NOT FOUND\n", occulter); exit(-1);}
+
+  // find radius of viewer and occulter
+  SpiceInt dim;
+  SpiceDouble occulterRs[3], viewerRs[3];
+
+  printf("ALPHA: %s %s\n", viewer, occulter);
+
+  bodvrd_c(viewer, "RADII", 3, &dim, viewerRs);
+  bodvrd_c(occulter, "RADII", 3, &dim, occulterRs);
+
+  SpiceDouble occulterR = occulterRs[0];
+  SpiceDouble viewerR = viewerRs[0];
+
   SPICEDOUBLE_CELL(cnfine, MAXWIN);
   SPICEDOUBLE_CELL(result, MAXWIN);
-
-  furnsh_c("/home/user/BCGIT/ASTRO/standard.tm");
 
   wninsd_c(year2et(syear), year2et(eyear), &cnfine);
 
@@ -33,10 +59,10 @@ int main(int argc, char **argv) {
   // declare new one each time
 
   void gfq (SpiceDouble et, SpiceDouble *value) {
-    spkezp_c(301, et, "J2000", "CN+S", 399, moonpos, &lt);
+    spkezp_c(occulterID, et, "J2000", "CN+S", viewerID, moonpos, &lt);
     recsph_c(moonpos, &r, &colat, &lon);
 
-    double angsep = asin((EARTH_RADIUS + MOON_RADIUS)/vnorm_c(moonpos));
+    double angsep = asin((occulterR + viewerR)/vnorm_c(moonpos));
     *value = vsep_c(moonpos, starpos)/angsep;
 
     //    printf("MOON: %d %f %f %f %f %f %f %f %f\n", count, et2unix(et), lon/pi_c()*12, (halfpi_c()-colat)/pi_c()*180, r, starpos[0], starpos[1], starpos[2], vsep_c(moonpos, starpos)/pi_c()*180);
