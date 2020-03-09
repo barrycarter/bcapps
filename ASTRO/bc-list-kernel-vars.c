5a06fb9b7091d500955daa46f12a3d9d76134b63
Barry Carter <carter.barry@gmail.com>
2020-03-09 11:16:56 -0600

    Checkpoint

--- ASTRO/bc-list-kernel-vars.c
+++ ASTRO/bc-list-kernel-vars.c
@@ -3,6 +3,8 @@
 // output of this can be piped to:
 // sort -u | fgrep BODY | perl -nle '/body(\d+)/i; print $1' | sort -u
 
+// look at spkobj_c (list objects in kernel)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,9 +16,19 @@ int main (int argc, char **argv) {
 
   furnsh_c("/home/user/BCGIT/ASTRO/bc-maxkernel.tm");
 
+  // how many SPK files loaded
+  SpiceInt count;
+  ktotal_c ("spk", &count);
+
+  printf("%d SPK files loaded\n", count);
+
+  exit(-1);
+
   SpiceInt n1, lenout=100, room=10000;
   SpiceBoolean found;
   SpiceChar kvars[room][lenout];
+
+
 
   gnpool_c("*", 0, room, lenout, &n1, kvars, &found);
 
