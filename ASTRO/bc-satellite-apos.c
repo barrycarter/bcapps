19bab6059087a7f4272f5ecca92ce1af706bb288
Barry Carter <carter.barry@gmail.com>
2020-03-09 21:49:30 -0600

    Checkpoint

--- ASTRO/bc-satellite-apos.c
+++ ASTRO/bc-satellite-apos.c
@@ -9,16 +9,32 @@
 #include "SpiceZfc.h"
 #include "bclib.h"
 
+char *strs[8] = {"rp", "ecc", "inc", "lnode", "argp", "m0", "t0", "mu"};
+
 int main (int argc, char **argv) {
 
   furnsh_c("/home/user/BCGIT/ASTRO/bc-maxkernel.tm");
 
+  // Jupiter mass parameter
+
+  SpiceInt dim;
+  SpiceDouble mu;
+  bodvrd_c("599", "GM", 1, &dim, &mu);
+
+
   // state of Io at time = 0
 
-  SpiceDouble state[6], lt;
-
+  SpiceDouble state[6], lt, elts[8];
   spkezr_c("501", 0, "IAU_JUPITER", "CN+S", "599", state, &lt);
 
-  printf("STATE: %f %f %f %f %f %f\n", state[0], state[1], state[2], state[3], state[4], state[5]);
+  // osculating elements wrt Jupiter
+
+  oscelt_c(state, 0, mu, elts);
+
+  //  printf("STATE: %f %f %f %f %f %f\n", state[0], state[1], state[2], state[3], state[4], state[5]);
+
+  for (int i=0; i < 8; i++) {
+    printf("%s: %f\n", strs[i], elts[i]);
+  }
 
 }
