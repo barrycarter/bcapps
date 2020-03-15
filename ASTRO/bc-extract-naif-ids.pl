7886d829bc8ff19db132f953e6246d559ddea28a
Barry Carter <carter.barry@gmail.com>
2020-03-14 20:18:20 -0600

    Checkpoint

--- ASTRO/bc-extract-naif-ids.pl
+++ ASTRO/bc-extract-naif-ids.pl
@@ -15,6 +15,17 @@ my($spath) = "/home/user/SPICE/SPICE64/cspice/exe/";
 
 chdir("$bclib{githome}/ASTRO/");
 
+my($naifids) = read_file("naif_ids.html");
+
+while ($naifids=~s%<pre>\s*NAIF ID\s*NAME\s*(.*?)</pre>%%s) {
+
+
+
+}
+
+
+die "TESTING";
+
 for $i (glob "/home/user/SPICE/KERNELS/*.bsp") {
 
     # filename without path and without bsp extension
@@ -52,11 +63,34 @@ for $i (glob "/home/user/SPICE/KERNELS/*.bsp") {
 
 	# does this line have a NAIF ID?
 	if ($j=~/^\s*(.*?)\s+(\d+)\s/) {
-	    print "$2,$1\n";
+	    print uc("$2,$1\n");
 	} elsif ($j=~/^\s*$/) {
 	    # do nothing, ignore blank line
 	} else {
-	    debug("IGNORING: $j");
+	    debug("IGNORING (CMT): $j");
+	}
+    }
+
+    # read brief file
+    my($brf) = read_file("SPICEMETA/$fname.brf");
+
+    # find section of interest
+    if ($brf=~/\s*Bodie.*?Start of Interval \(ET\)\s*End of Interval \(ET\)\s*(.*?)$/is) {
+	$brf = $1;
+    } else {
+	warn("BAD BRF FILE: $fname");
+	$brf = "";
+    }
+
+    # loop through lines
+    for $j (split(/\n/, $brf)) {
+	
+	if ($j=~/^(\d+)\*?\s+(.*?)\*?\s{2,}/) {
+	    print uc("$1,$2\n");
+	} elsif ($j=~/^[\s\-]*$/) {
+	    # ignore blank line silently
+	} else {
+	    debug("IGNORING (BRF): $j");
 	}
     }
 }
