4455c5ba80375038a2bfe829de28ba1ae1831870
Barry Carter <carter.barry@gmail.com>
2020-03-27 20:11:03 -0600

    Checkpoint

--- ASTRO/bc-terminator.c
+++ ASTRO/bc-terminator.c
@@ -7,7 +7,7 @@
 // this the wrong way to do things
 #include "/home/user/BCGIT/ASTRO/bclib.h"
 
-// Usage: $0 -i naif_id -t time_in_unix_seconds -n num_pts
+// Usage: $0 -i naif_id -t time_in_unix_seconds -n num_pts [-u 0]
 
 // TODO: use longopts later?
 
@@ -15,24 +15,31 @@
 
 // TODO: refraction? (can I do it?)
 
+// TODO: indicate which side is sunlight and dark
+
 int main(int argc, char **argv) {
 
   char optid;
   SpiceInt npts=100, frameID, id=399;
   SpiceDouble time=0;
-  SpiceChar frame[100], name[100];
+  SpiceChar frame[100], name[100], type[100];
   SpiceBoolean found;
 
-  furnsh_c("bc-maxkernel.tm");
+  furnsh_c("/home/user/BCGIT/ASTRO/bc-maxkernel.tm");
+
+  // default value for type
+  strcpy(type, "UMBRAL");
+  
 
   // assign from opts
-  while ((optid = getopt(argc, argv, "i:t:n:")) != -1) {
+  while ((optid = getopt(argc, argv, "i:t:n:u:")) != -1) {
     if (optid == 'i') {id = atoi(optarg);}
     if (optid == 't') {time = unix2et(atof(optarg));}
     if (optid == 'n') {npts = atoi(optarg);}
+    if (optid == 'u') {if (atoi(optarg) == 0) {strcpy(type,"PENUMBRAL");}}
   }
 
-  printf("I: %d, T: %f, N: %d\n", id, time, npts);
+  printf("I: %d, T: %f, N: %d, U: %s\n", id, time, npts, type);
 
   // convert planet to string, complain if not found
 
@@ -45,11 +52,10 @@ int main(int argc, char **argv) {
 
   printf("NAME: %s, FRAME: %s\n", name, frame);
 
-  // TODO: maybe penumbral for where sunset is occurring
-
   SpiceDouble trgepc, obspos[3], trmpts[npts][3], r, lng, lat;
 
-  edterm_c("UMBRAL", "10", name, time, frame, "CN+S", name, npts,
+  // TODO: does not include refraction
+  edterm_c(type, "10", name, time, frame, "CN+S", name, npts,
   	   &trgepc, obspos, trmpts);
 
   for (int i=0; i<npts; i++) {
