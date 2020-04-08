8cdd010b7e9cfe929b4a34d9b0d476274a760545
Barry Carter <carter.barry@gmail.com>
2020-04-08 14:48:55 -0600

    Checkpoint

--- COVID19/bc-parse-covid.pl
+++ COVID19/bc-parse-covid.pl
@@ -4,6 +4,10 @@ require "/usr/local/lib/bclib.pl";
 
 # fields: Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered
 
+# fields for 04-07-2020: FIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key
+
+# fields for 01-22-2020: <EF><BB><BF>Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered
+
 # should be run with list of files such as:
 # /home/user/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/*.csv
 
@@ -15,7 +19,9 @@ while (<>) {
 
     if ($cases eq "Confirmed") {next;}
 
+    debug
+
     if ($adm eq "New Mexico" || $adm eq "NM") {
-	debug($_);
+#	debug($_);
     }
 }
