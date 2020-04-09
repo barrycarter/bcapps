1e272983ca2c05089ba671c90c43387f78b96f02
Barry Carter <carter.barry@gmail.com>
2020-04-09 15:39:47 -0600

    Checkpoint

--- COVID19/bc-parse-covid.pl
+++ COVID19/bc-parse-covid.pl
@@ -2,13 +2,40 @@
 
 require "/usr/local/lib/bclib.pl";
 
-my(@conf) = split(/\n/, read_file("/home/user/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"));
+my(@data) = split(/\r\n/,read_file("/home/user/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"));
 
-my($hashlist) = arraywheaders2hashlist([map(s/\r\n//g; $_=[csv($_)], @conf)]);
+@headers = csv(shift(@data));
 
-for $i (@$hashlist) {
-    for $j (sort keys %$i) {
-	debug("$j $i->{$j}");
+# $data{place}{date} = # of incidents on that day in place
+my(%data);
+my($name);
+
+for $i (@data) {
+
+    my(@cols) = csv($i);
+
+    my(%colhash);
+
+    for $j (0..$#headers) {
+
+	if ($headers[$j] eq "Combined_Key") {$name = $cols[$j]; next;}
+
+	if ($headers[$j] =~m%(\d+/\d+/\d+)%) {
+	    $data{$name}{$headers[$j]} = $cols[$j];
+	    next;
+	}
     }
 }
+
+for $i (sort keys %data) {
+    debug("I: $i");
+}
+
+# my($hashlist) = arraywheaders2hashlist([map(s/\r\n//g; $_=[csv($_)], @conf)]);
+
+#for $i (@$hashlist) {
+#    for $j (sort keys %$i) {
+#	debug("$j $i->{$j}");
+#    }
+# }
 
