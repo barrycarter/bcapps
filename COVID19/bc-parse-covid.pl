8b980d63c53691b7fabc3a46f774967c035cfa6c
Barry Carter <carter.barry@gmail.com>
2020-04-09 15:16:02 -0600

    Checkpoint

--- COVID19/bc-parse-covid.pl
+++ COVID19/bc-parse-covid.pl
@@ -4,67 +4,11 @@ require "/usr/local/lib/bclib.pl";
 
 my(@conf) = split(/\n/, read_file("/home/user/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"));
 
-my($hashlist) = arraywheaders2hashlist([map($_=[csv($_)], @conf)]);
+my($hashlist) = arraywheaders2hashlist([map(s/\r\n//g; $_=[csv($_)], @conf)]);
 
 for $i (@$hashlist) {
-    debug(%$i);
+    for $j (sort keys %$i) {
+	debug("$j $i->{$j}");
+    }
 }
-
-# my($hashlist) = arraywheaders2hashlist(\@conf);
-
-# debug(@hashlist);
-
-die "TESTING";
-
-
-my(@reclist);
-
-for $i (@conf) {
-    my(@cols) = csv($i);
-    push(@reclist, \@cols);
-}
-
-
-
-debug(@{$hashlist});
-
-die "TESTNG";
-
-
-
-# my($hashlist) = arraywheaders2hashlist(\@conf);
-
-for $i (@{$hashlist}) {
-
-#    debug(keys %{$i});
-
-#    for $j (keys %{$i}) {
-#	debug("I: $i, J: $j, VAL: $i{$j}");
-#    }
-
-}
-
-# debug(@{$hashlist});
-
-# debug(csv($conf[5]));
-
-# $conf[5] = \@{csv($conf[5])};
-
-# debug("<START>", $conf[5], "<END>");
-
-# debug(@conf);
-
-# my($confs) = arraywheaders2hashlist(\@conf);
-
-# my(@confs) = @{$confs};
-
-# for $i (@confs) {
-
-#    debug(%$i);
-
-#    for $j (keys (%{$i})) {
-#	debug("I: $i, J: $j");
-#    }
-# }
-
 
