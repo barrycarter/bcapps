14dbbbb745a94aac6230a2627f0f0e79135b652e
Barry Carter <carter.barry@gmail.com>
2020-04-19 14:53:45 -0600

    Checkpoint

--- MAPS/bc-read-bin-file.pl
+++ MAPS/bc-read-bin-file.pl
@@ -9,32 +9,46 @@ my(%hash);
 for $i (1..200) {
     $hash{lng} = rand()*360 - 180;
     $hash{lat} = rand()*180 - 90;
-    debug($hash{lng}, $hash{lat});
-    lngLat2byte(\%hash);
+#    debug($hash{lng}, $hash{lat});
+    debug(lngLat2byte(\%hash));
 }
 
 sub lngLat2byte {
 
+    # if file not already open, open it
+
+    unless (-r POPCOUNT) {
+	open(POPCOUNT, "/mnt/popcount/gpw_v4_population_count_rev11_2020_30_sec_1.all.bin")||die("ERROR: $!");
+    }
+
     my(%hash) = %{$_[0]};
-
-    # TODO: watch out for corner cases, including 0 lat and +-180 lng
-    # (also maybe +- 90 lat)
-
-    # $hash{lng}/90 between -2, -1, 0, 1
-
-    my($chunk) = 3+floor($hash{lng}/90);
-    if ($hash{lat}<0) {$chunk += 4;}
 
     # TODO: don't hardcode these values
 
-    # where in data
+    my($ilng) = round($hash{lng}*120);
+    my($ilat) = round($hash{lat}*120);
 
-    my($byte) = $hash{lat}
+    # chunk numbers are 0 through 7
+    my($chunk) = 2+floor($ilng/10800);
+    if ($ilat < 0) {$chunk += 4;}
 
-    debug($chunk);
+    # where in chunk is data?
+
+    my($row) = $ilat%10800;
+    my($col) = $ilng%10800;
+
+    my($byte) = ($chunk*116640000 + $row*10800 + $col)*8;
+
+    seek(POPCOUNT, $byte, SEEK_SET);
+    debug("ERROR: $!");
+
+    my($data);
+    sysread(POPCOUNT, $data, 8);
+
+    debug("BYTE: $byte, DATA: $data");
+
+    return(unpack("d", $data));
 }
-
-
 
 =item format
 
