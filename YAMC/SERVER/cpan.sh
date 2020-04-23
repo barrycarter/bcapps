8feb84dbffa67759c9401e53a554e93f9b51661b
Barry Carter <carter.barry@gmail.com>
2020-04-23 11:54:36 -0600

    Moved server stuff (for digitalocean server) to BCGIT

---
+++ YAMC/SERVER/cpan.sh
@@ -0,0 +1,8 @@
+# CPAN installs
+
+cpan Protocol::WebSocket Net::WebSocket::Server Protocol::WebSocket::Handshake::Server Digest::MD5 Digest::SHA1 Date::Parse Text::Unidecode Statistics::Distributions Math::Round Astro::Nova Astro::MoonPhase JSON IO::Socket::SSL Image::PNG
+
+
+
+
+
---
+++ YAMC/SERVER/yum.sh
@@ -0,0 +1,4 @@
+# yum installs
+
+yum -y install git bzip2 cpan gcc expect nodejs-ws golang yum-utils epel-release openssl openssl-devel certbot nmap-ncat nmap lsof ImageMagick nginx wget bind-utils transmission-cli docker perl-FCGI fcgi fcgiwrap
+
