60b267df55fe42581d47a6c6599aa87bd1417944
Barry Carter <carter.barry@gmail.com>
2020-05-27 17:05:57 -0600

    Checkpoint

---
+++ ASTRO/bc-osculating.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "SpiceUsr.h"
+#include "SpiceZfc.h"
+#include "bclib.h"
+
+/*
+                    rp      Perifocal distance.
+                    ecc     Eccentricity.
+                    inc     Inclination.
+                    lnode   Longitude of the ascending node.
+                    argp    Argument of periapsis.
+                    m0      Mean anomaly at epoch.
+                    t0      Epoch.
+                    mu      Gravitational parameter.
+*/
+
+/* 
+
+Thin wrapper around oscelt_c that computes osculating elements given:
+
+  - center: the object being orbited
+  - body: the object that is orbiting
+  - et: the ephemeris time
+  - frame: the frame for which to compute the parameters
+
+*/
+
+
+void osculatingElements(SpiceInt center, SpiceInt body, SpiceDouble et, char *frame, SpiceDouble elts[8]) {
+
+  SpiceDouble starg[6], lt;
+
+  // get position and velocity
+  spkez_c(body, et, frame, "CN+S", center, starg, &lt);
+
+  // get GM of center body
+  SpiceInt dim;
+  SpiceDouble mu[1];
+  bodvcd_c(center, "GM", 1, &dim, mu);
+
+  oscelt_c (starg, et, mu[0], elts);
+}
+
+
+// determine the osculating elements for a given object wrt to another
+// object ever a given time in a given reference frame
+
+int main (int argc, char **argv) {
+
+  furnsh_c("/home/user/BCGIT/ASTRO/standard.tm");
+
+  char *strs[8] = {"rp", "ecc", "inc", "lnode", "argp", "m0", "t0", "mu"};
+
+  SpiceDouble answer[8];
+
+  osculatingElements(10, 399, 0, "J2000", answer);
+
+  double mult = 1;
+
+  for (int i=0; i < 8; i++) {
+
+    if (i >=2 && i <= 5) {mult = 180/pi_c();}
+
+    printf("%s: %f\n", strs[i], answer[i]*mult);
+  }
+}
+
