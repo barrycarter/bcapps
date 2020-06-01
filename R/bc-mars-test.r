f83c1ea597abb231ed4b4c81cfbcd72d3fbf8b1b
Barry Carter <carter.barry@gmail.com>
2020-06-01 13:53:18 -0600

    Checkpoint

--- R/bc-mars-test.r
+++ R/bc-mars-test.r
@@ -2,7 +2,18 @@ data = read.csv("/tmp/mars.txt", stringsAsFactors=FALSE, header=FALSE);
 
 f = function(e, n) {return(2*(e-1)/(n-1) - 1);}
 
-t1303 = f(c(1:length(data[,1])), length(data[,1]));
+index = f(c(1:length(data[,1])), length(data[,1]));
+
+fit = lm(data[,1] ~ poly(index, 3, raw=TRUE));
+
+fit2 = lm(data[,1] ~ poly(index, 4, raw=TRUE));
+
+plot(index, data[,1], col="red");
+
+lines(index, fitted(fit), col="green");
+
+lines(index, fitted(fit2), col="purple");
+
 
 vals = data[,1];
 
@@ -57,7 +68,21 @@ fit = lm(c(4,5,6) ~ c(1,2,3))
 # WRONG: fit = lm(c(4,5,6) ~ poly(c(1,2,3),1), raw=TRUE)
 
 
-fit = lm(c(4,5,6) ~ poly(c(1,2,3),1, raw=FALSE))
+fit = lm(c(4,5,6) ~ poly(c(1,2,3),1, raw=TRUE))
 
+# given set of coefficients, return polynomial
 
+f = function(arr) {
+  g = function(x) {
+    tot = arr[1];
+    tot = tot+arr[2]*x;
+    tot = tot+arr[3]*x^2
+    return(tot);
+ }
+ return(g);
+}
+
+g = function(arr, x) {
+ tot = 0;
+ tot 
 
