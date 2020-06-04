87053a5d8335799d742ce0f4106534512f3180d8
Barry Carter <carter.barry@gmail.com>
2020-06-04 15:52:09 -0600

    Checkpoint

--- JULIA/bc-interpolation.jl
+++ JULIA/bc-interpolation.jl
@@ -4,7 +4,9 @@ include("/home/user/BCGIT/bclib.jl")
 
 # TODO: don't load from tmp file
 
-mars = CSV.read("/tmp/mars2.txt", header = false);
+# mars = CSV.read("/tmp/mars2.txt", header = false);
+
+mars = CSV.read(`bzcat /home/user/20200604/mars-dec-per-hour.txt.bz2`, header = false);
 
 decs = mars[:,1];
 
