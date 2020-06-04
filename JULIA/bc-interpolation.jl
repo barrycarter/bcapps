f7eacc01463eca670eed50c55301ddb1c002667c
Barry Carter <carter.barry@gmail.com>
2020-06-04 15:07:53 -0600

    Checkpoint

---
+++ JULIA/bc-interpolation.jl
@@ -0,0 +1,6 @@
+<formulas>
+
+
+
+
+</formulas>
--- bclib.jl
+++ bclib.jl
@@ -1,2 +1,30 @@
 import Pkg, Plots, CSV, GLM, DataFrames, Polynomials, StatsBase;
 
+
+#== splitArray(arr, n)
+
+Splits arr into n equal sized pieces plus 1 extra piece that is smaller
+
+Returns an array of arrays
+
+=#
+
+function splitArray(arr, n)
+
+   ret = [];
+   len = length(arr);
+   clen = floor(len/n);
+   st = 1;
+
+   while st < len
+      en = convert(Int, st + clen - 1)
+      if en > len en = len end
+      push!(ret, arr[st:en])
+      st = convert(Int, st + clen)
+   end
+
+   ret
+end
+
+
+
