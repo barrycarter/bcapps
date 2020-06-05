f27ba22a5f853490f99c9695ad6d2d99037e81f4
Barry Carter <carter.barry@gmail.com>
2020-06-05 13:18:11 -0600

    Checkpoint

--- JULIA/bc-interpolation.jl
+++ JULIA/bc-interpolation.jl
@@ -12,3 +12,28 @@ decs = mars[:,1];
 
 </formulas>
 
+t1552 = splitArray(Dict("arr" => decs, "n" => 10))
+
+t1554 = list2NormalPoly(Dict("deg" => 4, "arr" => t1552["arrarr"][4]))
+
+# SEE BELOW: t1558 = map(x -> list2NormalPoly(Dict("deg" => 4, "arr" => x)), t1552["arrarr"])
+
+t1558 = map(x -> list2NormalPoly(Dict("deg" => 4, "arr" => x))["maxres"], 
+  t1552["arrarr"])
+
+t1602 = splitArray(Dict("arr" => decs, "n" => 1000))
+
+t1603 = map(x -> list2NormalPoly(Dict("deg" => 4, "arr" => x))["maxres"], 
+  t1602["arrarr"])
+
+TODO: functionalize, minimize (degree+1)*(number of pieces)
+
+
+
+
+
+
+
+
+
+
