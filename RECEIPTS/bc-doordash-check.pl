0e9c80a403baf2fbde70ec06f79e237b988544d4
Barry Carter <carter.barry@gmail.com>
2020-06-22 08:03:56 -0600

    Checkpoint

---
+++ RECEIPTS/bc-doordash-check.pl
@@ -0,0 +1,13 @@
+#!/bin/perl
+
+# Another program that's really just for me, this checks the following
+# for the doordash receipts in my database:
+#
+# - all receipts have a reference number
+# - the reference numbers are in date order
+# - I have a PDF receipt for each reference number
+# - the reference number appears somewhere in the comments
+
+
+
+# TODO: everything
