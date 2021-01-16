b6eaf82df29c8cfce6d3445f3aa4d77434cda448
Barry Carter <github@barrycarter.info>
2021-01-15 17:35:21 -0700

    Checkpoint

--- FINANCES/bc-parse-predictit.pl
+++ FINANCES/bc-parse-predictit.pl
@@ -9,7 +9,8 @@ require "/usr/local/lib/bclib.pl";
 
 while (<>) {
 
-  chomp();
+  chomp($_);
+  s/\r//g;
 
   # use this hash to unique identify the line
 
@@ -19,10 +20,76 @@ while (<>) {
 
   # fix date to MySQL format
 
-  $date = strftime("%Y-%m-%d %H:%M:%S", localtime(str2time($date))));
+  $date = strftime("%Y-%m-%d %H:%M:%S", localtime(str2time($date)));
 
-  debug($date, $hash);
+  # convert $type (like "Sell Yes") to two fields
+
+  my($bs, $yn) = split(/\s+/, $type);
+
+  # for market and contract, just remove apostrophes
+
+  $market=~s/\'//g;
+
+  $contract=~s/\'//g;
+
+  # for price, just remove $ (TODO: check between 0.01 and 0.99?)
+
+  $price=~s/\$//g;
+
+  # profitloss only applies to closing transactions and I could theoretically calculate it... format is $10.68 or ($123.44)
+
+  $profitloss = fixval_local($profitloss); 
+
+  $fees = fixval_local($fees);
+
+  $creditdebit = fixval_local($creditdebit);
+
+  $risk = fixval_local($risk);
+
+  print "INSERT INTO predictit (date, buysell, yesno, market, contract, shares, price, profitloss, fees, risk, creditdebit, url, hash)\n";
+
+  print "VALUES ('$date', '$bs', '$yn', '$market', '$contract', '$shares', '$price', '$profitloss', '$fees', '$risk', '$creditdebit', '$url', '$hash');\n";
 
 }
 
+# one off to change things like $10.68 to 10.68 or ($123.44) to -123.44
 
+sub fixval_local {
+
+  my($num) = @_;
+
+  $num=~s/\$//g;
+
+  if ($num=~s/\((.*?)\)/$1/) {$num *= -1;}
+
+  return $num;
+
+}
+
+=item sql
+
+Table:
+
+DROP TABLE IF EXISTS predictit;
+
+CREATE TABLE predictit (
+ oid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+ INDEX(oid),
+ date DATETIME,
+ buysell TEXT,
+ yesno TEXT,
+ market TEXT,
+ contract TEXT,
+ shares DOUBLE,
+ price DOUBLE,
+ profitloss DOUBLE,
+ fees DOUBLE,
+ risk DOUBLE,
+ creditdebit DOUBLE,
+ url TEXT,
+ hash TEXT
+);
+
+CREATE UNIQUE INDEX i1 ON predictit(hash(50));
+
+=cut
