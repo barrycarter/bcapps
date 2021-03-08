5e937feafd982d01334a3d52bdd666c1fa1a7be0
Barry Carter <github@barrycarter.info>
2021-03-07 18:03:33 -0700

    more work

--- WEATHER/bc-get-weather-openweathermap.pl
+++ WEATHER/bc-get-weather-openweathermap.pl
@@ -17,18 +17,84 @@ my($json) = JSON::from_json($out);
 
 my($timestr) = strftime("@ %Y%m%d.%H%M%S", localtime($json->{current}->{dt}));
 
-my($weather) = sprintf("%s/%0.1fF/%0.1fF/%d% (%0.1fF)\n", 
+my($weather) = sprintf("%s (%d%)/%0.1fF/%0.1fF/%d% (%0.1fF)", 
  $json->{current}->{weather}[0]->{description},
+  $json->{current}->{clouds},
  $json->{current}->{temp},
  $json->{current}->{feels_like},
  $json->{current}->{humidity},
  $json->{current}->{dew_point}
 );
 
-print("$timestr\n$weather");
+my($baro)=sprintf("%0.2fin", $json->{current}->{pressure}/33.863886666667);
 
+my($wind) = windinfo();
 
+# print("$timestr\n$weather\n$wind ($baro)\n");
 
+debug(get_forecasts());
 
+sub windinfo {
+  my($dir, $speed, $gust, $unit) = 
+    ($json->{current}->{wind_deg}, $json->{current}->{wind_speed},
+     $json->{current}->{wind_gust}, "mph");
+
+  if ($speed == 0 && $gust == 0) {return "CALM";}
+
+  my(@winddirs) = ("N","NNE","NE","ENE", "E","ESE","SE","SSE", "S","SSW","SW","WSW", "W","WNW","NW","NNW","N");
+
+  # tweak for unit
+  my($mult);
+  if ($unit eq "m/s") {
+    $mult = 3600/1609.344;
+  } elsif ($unit eq "mph") {
+    $mult = 1;
+  }
+
+  # convert from m/s to mph
+  $speed = sprintf("%0.0f", $speed*$mult);
+  $gust = sprintf("%0.0f", $gust*$mult);
+  $dir = $winddirs[round($dir/22.5)];
+  my($ret) = "$dir $speed";
+  if ($gust > $speed) {$ret .= "G$gust";}
+  return $ret;
+}
+
+sub get_forecasts {
+
+  my(@ret)
+;
+  for $i (0..5) {
+    my($dt) = strftime("%a%d", localtime($json->{daily}[$i]->{dt}));
+    my($hi) = sprintf("%01.f", $json->{daily}[$i]->{temp}->{max});
+    my($lo) = sprintf("%01.f", $json->{daily}[$i]->{temp}->{min});
+    my($we) = fix_weather($json->{daily}[$i]->{weather}[0]->{description});
+    my($pop) = $json->{daily}[$i]->{pop}*100;
+    my($str) = "$dt:$we/$hi/$lo/$pop%";
+    push(@ret, $str);
+  }
+
+  return join("\n", @ret);
+}
+
+sub fix_weather {
+
+  my($weather) = @_;
+
+  my(%hash) = (
+	       "clear sky" => "CLR", "overcast clouds" => "OVC",
+	       "light" => "LT", "snow" => "SN"
+	       );
+
+  for $i (keys %hash) {
+    $weather=~s/\s*$i\s*/$hash{$i}/g;
+  }
+
+  return $weather;
+}
+
+# TODO: parse hourly data
 
 # TODO: personal weather station(s) [direct from HTML?] [existing prog?]
+
+# TODO: add some comments
