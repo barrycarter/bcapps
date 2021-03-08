f0acf218f565839a07839c6b752c7011f361f99b
Barry Carter <github@barrycarter.info>
2021-03-07 17:02:59 -0700

    Replacement to AERIS API

---
+++ WEATHER/bc-get-weather-openweathermap.pl
@@ -0,0 +1,20 @@
+#!/bin/perl
+
+# As AERIS stops giving free API data, moving to openweathermap.org
+
+require "/usr/local/lib/bclib.pl";
+require "$bclib{home}/bc-private.pl";
+
+# cache during testing only
+
+if ($globopts{test}) {$age=3600;} else {$age=-1;}
+
+my($out, $err, $res) = cache_command2("curl 'https://api.openweathermap.org/data/2.5/onecall?lat=$bclib{latitude}&lon=$bclib{longitude}&appid=$bclib{openweathermap}{appid}&units=imperial'", "age=$age");
+
+my($json) = JSON::from_json($out);
+
+debug($json);
+
+
+
+# TODO: personal weather station(s) [direct from HTML?] [existing prog?]
