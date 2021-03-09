560bdacbaf6b4707dfe2b4abfbf09ef022fe2538
Barry Carter <github@barrycarter.info>
2021-03-09 07:11:26 -0700

    more abbreviations

--- WEATHER/bc-get-weather-openweathermap.pl
+++ WEATHER/bc-get-weather-openweathermap.pl
@@ -86,7 +86,8 @@ sub fix_weather {
   my(%hash) = (
 	       "clear sky" => "CLR", "overcast clouds" => "OVC",
 	       "light" => "LT", "snow" => "SN", "broken clouds" => "BKN",
-	       "rain" => "RN", "scattered clouds" => "SCT"
+	       "rain" => "RN", "scattered clouds" => "SCT",
+	       "few clouds" => "FEW"
 	       );
 
   for $i (keys %hash) {
