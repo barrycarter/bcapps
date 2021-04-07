8df8d532789735f3fe10f25d1f6ae1facada0e2f
Barry Carter <github@barrycarter.info>
2021-04-07 10:24:41 -0600

    Adds the last few lines of a file (only useful to me)

---
+++ RECEIPTS/bc-add-tail.pl
@@ -0,0 +1,35 @@
+#!/bin/perl
+
+# this trivial script looks at the n last lines of a file and adds the
+# rightmost columns as numbers, a weird thing only I have a purpose
+# for
+
+# --n: number of lines (default 20)
+
+# I originally tried to set this up as an alias, but escaping the "$" in an alias when inside a Perl script is difficult
+
+require "/usr/local/lib/bclib.pl";
+
+defaults("n=20");
+
+my($file) = @ARGV;
+
+print "\n";
+
+open(A, "tail -n $globopts{n} $file|");
+
+while (<A>) {
+
+  # if the line ends in a number, add it and print the line
+  # things like 5*100 count as numbers
+
+  unless (/([\-\d\*]+)$/) {next;}
+
+  $tot += eval($1);
+
+  print "$tot: $_\n";
+
+}
+
+print "Total: $tot\n";
+
