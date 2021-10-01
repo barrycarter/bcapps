7ffb65a7b6891dea1e2533c95a0879daf2679be8
Barry Carter <github@barrycarter.info>
2021-10-01 07:08:09 -0600

    minor changes for new repo (which is really old repo)

--- bc-paragraphify.pl
+++ bc-paragraphify.pl
@@ -3,6 +3,8 @@
 # in the STDIN, convert single new lines to space, double new lines to
 # true newline, so that paragraphs are "wiki style"
 
+# TODO: maybe convert '= ' to nothing if requested
+
 while (<>) {
   chomp();
   if (/^$/) {print "\n\n"; next;}
