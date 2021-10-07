8f4b11bb7df90ee863bb535c44e11a41d2a530ee
Barry Carter <github@barrycarter.info>
2021-10-07 17:07:00 -0600

    almost there

--- BACKUP/bc-prevdone2moredone.pl
+++ BACKUP/bc-prevdone2moredone.pl
@@ -11,26 +11,36 @@ require "/usr/local/lib/bclib.pl";
 # hashes here); X is the current location of the file and Y is the old
 # location of the file (where it was when it was backed up)
 
+# have to treat the private and public lists separately
 
-my(@converts) = `egrep -hv '^ *\$|^#' $bclib{githome}/BACKUP/bc-conversions.txt $bclib{home}/BCPRIV/bc-conversions-private.txt`;
+my(@pubconvert) = `egrep -v '^ *\$|^#' $bclib{githome}/BACKUP/bc-conversions.txt`;
+my(@privconvert) = `egrep -v '^ *\$|^#' $bclib{home}/BCPRIV/bc-conversions-private.txt`;
 
-my(@regex);
+my(@pubregex, @privregex);
 
-for $i (@converts) {
-    unless ($i=~/^\"(.*?)\" \"(.*?)\"$/) {die "BAD LINE: $_";}
+for $i (@pubconvert) {
+  unless ($i=~/^\"(.*?)\" \"(.*?)\"$/) {die "BAD LINE: $_";}
+  
+  # push the from to (in that order to list of @regex
+  
+  push(@pubregex, [$2, $1]);
+}
 
-    # push the from to (in that order to list of @regex
-
-    push(@regex, [$2, $1]);
-  }
-
-debug(@regex);
+for $i (@privconvert) {
+  unless ($i=~/^\"(.*?)\" \"(.*?)\"$/) {die "BAD LINE: $_";}
+  
+  # push the from to (in that order to list of @regex
+  
+  push(@privregex, [$2, $1]);
+}
 
 # now read previouslydone.txt.srt and create version with fake backup locations (in other words, backup locations for where the files are now)
 
-open(A,"previouslydone.txt.srt")||die("Can't open previouslydone.txt.srt, $!");
+# open(A,"previouslydone.txt.srt")||die("Can't open previouslydone.txt.srt, $!");
 
-while (<A>) {
+# rewritten to take stdin and print stdout, so much easier for testing
+
+while (<>) {
 
   chomp();
   my($file, $time) = split(/\0/, $_);
@@ -39,14 +49,27 @@ while (<A>) {
 
   print "$file\0$time\n";
 
+  # store the original filename since we have to run it through two lists
+  my($origfile) = $file;
+
   # loop through regex
 
-  for $j (@regex) {
-    
+  for $j (@pubregex) {
     # we now run the files through EACH regex, one at a time (order matters)
-
     if ($file=~s/$j->[0]/$j->[1]/) {
-      debug("NEW: $file");
+      debug("$j->[0] -> $j->[1]");
+      print "$file\0$time\n";
+    }
+  }
+
+  $file = $origfile;
+
+  debug("NOW PRIV");
+
+  for $j (@privregex) {
+    # we now run the files through EACH regex, one at a time (order matters)
+    if ($file=~s/$j->[0]/$j->[1]/) {
+      debug("$j->[0] -> $j->[1]");
       print "$file\0$time\n";
     }
   }
