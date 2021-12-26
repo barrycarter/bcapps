2cd0a8f767cb2c3312f7dcf88b82e552b0921c54
Barry Carter <github@barrycarter.info>
2021-12-26 05:06:15 -0700

    added a list feature

--- bc-section-checklist.pl
+++ bc-section-checklist.pl
@@ -1,11 +1,14 @@
 #!/bin/perl
 
-# Extract a section of a file (delimited by <tagname> and </tagname>),
-# copy that section to a timestamped new file and bring up that
-# timestamped file in emacs
+# Extract a section of a file (delimited by <tagname> and </tagname>
+# where both tags are left-aligned), copy that section to a
+# timestamped new file and bring up that timestamped file in emacs
 
 # --section: the section to extract
+
 # --stdout: print the section to STDOUT, don't write it to file
+
+# --list: list sections in given file
 
 require "/usr/local/lib/bclib.pl";
 
@@ -13,9 +16,44 @@ my($dir) = "/usr/local/etc/section-checklist";
 
 unless (-d $dir) {die "$dir does not exist";}
 
-unless ($globopts{section}) {die "--section required";}
+unless ($globopts{section} || $globopts{list}) {die "--section or --list required";}
 
 my($data, $fname) = cmdfile();
+
+# if list requested
+
+if ($globopts{list}) {
+
+  # this is a terrible way to find "XML tags"
+
+  my(%tags, %tage);
+
+  for $i (split(/\n/, $data)) {
+
+    # tags = start of tag, tage = end of tag
+
+    # because we are looking for only special tags, disallow spaces
+
+    # no forward slashes in start tab
+
+    if ($i=~/^<([^<>\s\/]+)>\s*$/) {$tags{$1} = 1;}
+    if ($i=~/^<\/([^<>\s]+)>\s*$/) {$tage{$1} = 1;}
+
+  }
+
+  # anything that has both, print it out
+
+  for $j (sort keys %tags) {
+    if ($tage{$j}) {print "$j\n";}
+  }
+
+#  while ($data=~s%<([^<>]*?)>(.*?)<\/\1>%%s) {
+#    debug(length($data));
+#    print "$1 ",length($2), "\n";
+#  }
+
+exit;
+}
 
 my($section) = $globopts{section};
 
