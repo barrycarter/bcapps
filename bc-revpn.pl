f09012101d14072f42721145525ae2c49355974f
Barry Carter <github@barrycarter.info>
2022-05-06 16:59:53 -0600

    added logging

--- bc-revpn.pl
+++ bc-revpn.pl
@@ -9,4 +9,4 @@ chomp($server);
 
 print STDERR $server;
 
-system("sudo cp -f /etc/resolv.conf.nordvpn /etc/resolv.conf; sudo pkill openvpn; sudo openvpn --config $server --config /home/user/BCGIT/BRIGHTON/bc-openvpn.cfg --auth-nocache --auth-user-pass /home/user/BCPRIV/nordvpn.txt&");
+system("sudo cp -f /etc/resolv.conf.nordvpn /etc/resolv.conf; sudo pkill openvpn; sudo openvpn --log-append /home/user/log/nordvpn.log --config $server --config /home/user/BCGIT/BRIGHTON/bc-openvpn.cfg --auth-nocache --auth-user-pass /home/user/BCPRIV/nordvpn.txt&");
