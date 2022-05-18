59183e2fc1f6bd7602b0ee10bf8dc9bf8022f128
Barry Carter <github@barrycarter.info>
2022-05-18 10:31:13 -0600

    changes for surfshark

--- bc-revpn.pl
+++ bc-revpn.pl
@@ -4,9 +4,9 @@
 
 # random server (UDP US)
 
-my($server) = `ls /etc/openvpn/server/ovpn_udp/us* | sort -R | head -1`;
+my($server) = `ls /etc/openvpn/surfshark/us*.prod.surfshark.com_udp.ovpn | sort -R | head -1`;
 chomp($server);
 
 print STDERR $server;
 
-system("sudo cp -f /etc/resolv.conf.nordvpn /etc/resolv.conf; sudo pkill openvpn; sudo openvpn --log-append /home/user/log/nordvpn.log --config $server --config /home/user/BCGIT/BRIGHTON/bc-openvpn.cfg --auth-nocache --auth-user-pass /home/user/BCPRIV/nordvpn.txt&");
+system("sudo cp -f /home/user/BCGIT/BRIGHTON/resolv.conf.surfshark /etc/resolv.conf; sudo pkill openvpn; sudo openvpn --log-append /home/user/log/surfshark.log --config $server --config /home/user/BCGIT/BRIGHTON/bc-openvpn.cfg --auth-nocache --auth-user-pass /home/user/BCPRIV/surfshark.txt&");
