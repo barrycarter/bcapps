c74157f665d9df80035fac1c7f46ec9bf6b05e91
Barry Carter <github@barrycarter.info>
2023-01-06 03:18:54 -0700

    about to clean this up to do some real work

--- MAPS/playground2.py
+++ MAPS/playground2.py
@@ -4,7 +4,7 @@
 
 from bclib import *
 import shapefile
-
+import matplotlib.pyplot as plt
 
 sf = shapefile.Reader("ne_10m_time_zones.shp")
 
@@ -19,9 +19,22 @@ sr = sf.shapeRecords()
 
 sh = sr[0].record.time_zone
 
-print(sh)
+# print(sh)
 
 # debug0(object=sh)
+
+tz = (filter(lambda x: x.record.tz_name1st == 'America/Denver', sf.shapeRecords()))[0]
+
+debug0(object=tz, exclude="__")
+
+print(tz.shape.points)
+
+x = [point[0] for point in tz.shape.points]
+y = [point[1] for point in tz.shape.points]
+
+plt.plot(x,y)
+plt.show()
+
 
 
 
