27a557a42d05c8f57b1803cf23b10cb7f6e6252f
Barry Carter <github@barrycarter.info>
2024-01-05 13:16:44 -0700

    Reddit stuff

--- ONEOFF/makegraph.m
+++ ONEOFF/makegraph.m
@@ -12,6 +12,54 @@ FindClique[g1]
 
 Normal[AdjacencyMatrix[g1]] >> /tmp/graph.txt
 
+g2 = Normal[AdjacencyMatrix[g1]]
+
+g3 = AdjacencyGraph[1-g2]
+
+
+(* below loads an existing graph *)
+
+x = << graph1.txt
+
+x2 = AdjacencyGraph[x]
+
+
+FindClique[x2, {17}, 1]
+
+FindClique[{x2, 1}, 10]
+
+(* below 1 works *)
+
+FindClique[x2, {80}, 1]
+
+FindClique[x2, {86}, 1] (* works *)
+
+x3 = FindClique[x2, {86}, 1][[1]]
+
+x4 = Table[x[[i]][[j]], {i, x3}, {j, x3}]
+
+(* new start below just to clean it up a bit *)
+
+g = Graph[Flatten[Table[If[Random[] < 0.98, {i <-> j}, {}], 
+ {i, 2, 160}, {j, 1, i-1}]]];
+
+g1 = Normal[AdjacencyMatrix[g]];
+
+Export["/tmp/out.csv", g1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
